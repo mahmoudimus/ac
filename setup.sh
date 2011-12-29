1b9f16ad42e8b845261aa7b3ac5dab38ff78ccae
Don Brown <mrdon@twdata.org>
2011-12-29 16:13:15 +1100

    Add setup.sh script for beac gatekeeper support
    Change-Id: Ic380f1ac7ec4b721898ee24a8bdad3160a209242

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# grab the post-commit hooks
+curl -f https://bamboo.extranet.atlassian.com/download/resources/com.atlassian.bamboo.plugins.gatekeeper:gatekeeperMisc/commit-msg -o .git/hooks/commit-msg
+chmod a+x .git/hooks/commit-msg
+
+# add remotes beac and branch
+git remote add beac https://bamboo.extranet.atlassian.com/plugins/servlet/p/remoteapps
+
+git fetch
+
+echo "You're all set!"
+echo ""
+echo " * branch master tracks remote origin git@bitbucket.org:mrdon/remoteapps-plugin.git"
+echo " * remote beac (https://bamboo.extranet.atlassian.com/plugins/servlet/p/remoteapps) has been added"
+echo ""
+echo "To commit, run ./commit.sh"
