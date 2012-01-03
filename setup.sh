51844067efb180658ed5aafe5d567fa312976f58
Don Brown <mrdon@twdata.org>
2012-01-03 17:45:48 +1100

    New gatekeeper url
    Change-Id: I765fb1e6fc024b0b5f2cc3dd4f3af251f585f1ad

--- setup.sh
+++ setup.sh
@@ -5,13 +5,13 @@ curl -f http://linode.twdata.org/commit-msg -o .git/hooks/commit-msg
 chmod a+x .git/hooks/commit-msg
 
 # add remotes beac and branch
-git remote add beac https://bamboo.extranet.atlassian.com/plugins/servlet/p/remoteapps
+git remote add beac https://bamboo.extranet.atlassian.com/plugins/servlet/p/remoteapps-plugin-gatekeeper
 
 git fetch
 
 echo "You're all set!"
 echo ""
 echo " * branch master tracks remote origin git@bitbucket.org:mrdon/remoteapps-plugin.git"
-echo " * remote beac (https://bamboo.extranet.atlassian.com/plugins/servlet/p/remoteapps) has been added"
+echo " * remote beac (https://bamboo.extranet.atlassian.com/plugins/servlet/p/remoteapps-plugin-gatekeeper) has been added"
 echo ""
 echo "To commit, run ./commit.sh"
