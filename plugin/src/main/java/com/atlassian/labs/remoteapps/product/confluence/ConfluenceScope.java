e0061e2e5f560614edda84bbcd8607840aca1a17
Joseph Clark <jclark@atlassian.com>
2012-01-03 18:09:46 +1100

    Extended the base Confluence scope to allow v1 JSON-RPC and XML-RPC calls, as this makes it possible for remote apps to store content using wiki markup; still a popular method for remote content creation.

--- plugin/src/main/java/com/atlassian/labs/remoteapps/product/confluence/ConfluenceScope.java
+++ plugin/src/main/java/com/atlassian/labs/remoteapps/product/confluence/ConfluenceScope.java
@@ -3,7 +3,6 @@ package com.atlassian.labs.remoteapps.product.confluence;
 import com.atlassian.labs.remoteapps.modules.permissions.scope.ApiResourceInfo;
 import com.atlassian.labs.remoteapps.modules.permissions.scope.ApiScope;
 import com.atlassian.labs.remoteapps.modules.permissions.scope.JsonRpcApiScope;
-import com.atlassian.labs.remoteapps.modules.permissions.scope.RpcEncodedSoapApiScope;
 import com.atlassian.labs.remoteapps.modules.permissions.scope.XmlRpcApiScope;
 import com.google.common.base.Function;
 import com.google.common.collect.Collections2;
@@ -13,39 +12,49 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.Collection;
 
 import static com.google.common.collect.Iterables.concat;
-import static com.google.common.collect.Iterables.transform;
 
 /**
  *
  */
 class ConfluenceScope implements ApiScope
 {
-    private final XmlRpcApiScope xmlRpcApiScope;
-    private final JsonRpcApiScope jsonrpcScope;
+    private final XmlRpcApiScope v2XmlRpcApiScope;
+    private final XmlRpcApiScope v1XmlRpcApiScope;
+    private final JsonRpcApiScope v2JsonRpcScope;
+    private final JsonRpcApiScope v1JsonRpcScope;
     private final Iterable<ApiResourceInfo> apiResourceInfo;
 
     protected ConfluenceScope(Collection<String> methods)
     {
-        jsonrpcScope = new JsonRpcApiScope("/rpc/json-rpc/confluenceservice-v2", methods);
-        xmlRpcApiScope = new XmlRpcApiScope("/rpc/xmlrpc", Collections2.transform(methods, new Function<String, String>()
-        {
-            @Override
-            public String apply(String from)
-            {
-                return "confluence2." + from;
-            }
-        }));
-        this.apiResourceInfo = concat(jsonrpcScope.getApiResourceInfos(), xmlRpcApiScope.getApiResourceInfos());
+        v1JsonRpcScope = new JsonRpcApiScope("/rpc/json-rpc/confluenceservice-v1", methods);
+        v2JsonRpcScope = new JsonRpcApiScope("/rpc/json-rpc/confluenceservice-v2", methods);
+        v1XmlRpcApiScope = new XmlRpcApiScope("/rpc/xmlrpc", Collections2.transform(methods, xmlRpcTransform("confluence1")));
+        v2XmlRpcApiScope = new XmlRpcApiScope("/rpc/xmlrpc", Collections2.transform(methods, xmlRpcTransform("confluence2")));
+        this.apiResourceInfo = concat(v1JsonRpcScope.getApiResourceInfos(), v2JsonRpcScope.getApiResourceInfos(), v1XmlRpcApiScope.getApiResourceInfos(), v2XmlRpcApiScope.getApiResourceInfos());
     }
+
     @Override
     public boolean allow(HttpServletRequest request, String user)
     {
-        return xmlRpcApiScope.allow(request, user) || jsonrpcScope.allow(request, user);
+        return v1XmlRpcApiScope.allow(request, user) || v2XmlRpcApiScope.allow(request, user) || v1JsonRpcScope.allow(request, user) || v2JsonRpcScope.allow(request, user);
     }
 
     @Override
     public Iterable<ApiResourceInfo> getApiResourceInfos()
     {
         return apiResourceInfo;
+    }
+
+    private Function<String, String> xmlRpcTransform(final String serviceName)
+    {
+        return new Function<String, String>()
+        {
+
+            @Override
+            public String apply(String from)
+            {
+                return serviceName + "." + from;
+            }
+        };
     }
 }
