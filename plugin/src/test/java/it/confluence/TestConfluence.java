cffa3d0f8f55f050cc76367decdbbba02ec0f6ed
Don Brown <mrdon@twdata.org>
2012-01-04 16:21:57 +1100

    Commenting out streams for JIRA as it breaks on v5
    * Comment out flaky web hook test for now
    
    Change-Id: I15ea95c03678603ad972d2d5b333a17edf411e9f

--- plugin/src/test/java/it/confluence/TestConfluence.java
+++ plugin/src/test/java/it/confluence/TestConfluence.java
@@ -14,6 +14,7 @@ import com.atlassian.webdriver.pageobjects.WebDriverTester;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.After;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.MethodRule;
@@ -148,6 +149,7 @@ public class TestConfluence
 	}
 
     @Test
+    @Ignore
 	public void testPageCreatedWebHookFired() throws IOException, JSONException, InterruptedException, XmlRpcFault
     {
         String content = "<h1>Love me</h1>";
