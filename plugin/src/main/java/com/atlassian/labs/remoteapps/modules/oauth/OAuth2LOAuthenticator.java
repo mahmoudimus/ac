76d9e7e3cdd03c1a3e966bfc126d7259098baae0
Don Brown <mrdon@twdata.org>
2012-02-14 02:26:56 +0800

    Add docco comments to document authentication
    Change-Id: Ia09d1fdc9cd4cf3910e9583daaae6858ac2f8327

--- plugin/src/main/java/com/atlassian/labs/remoteapps/modules/oauth/OAuth2LOAuthenticator.java
+++ plugin/src/main/java/com/atlassian/labs/remoteapps/modules/oauth/OAuth2LOAuthenticator.java
@@ -31,11 +31,11 @@ import java.security.Principal;
 public class OAuth2LOAuthenticator implements Authenticator
 {
     /**
-     * The request attribute key that the request dispatcher uses to store the
-     * original URL for a forwarded request.
+     * The request attribute key that the request dispatcher uses to store the original URL for a
+     * forwarded request.
      */
     private static final String FORWARD_REQUEST_URI = "javax.servlet.forward.request_uri";
-    
+
     private final Logger log = LoggerFactory.getLogger(getClass());
 
     private final OAuthLinkManager oAuthLinkManager;
@@ -46,70 +46,118 @@ public class OAuth2LOAuthenticator implements Authenticator
 
     @Autowired
     public OAuth2LOAuthenticator(AuthenticationController authenticationController,
-                                 ApplicationProperties applicationProperties,
-                                 OAuthLinkManager oAuthLinkManager, UserManager userManager, PermissionManager permissionManager)
+            ApplicationProperties applicationProperties,
+            OAuthLinkManager oAuthLinkManager, UserManager userManager,
+            PermissionManager permissionManager)
     {
         this.oAuthLinkManager = oAuthLinkManager;
         this.userManager = userManager;
         this.permissionManager = permissionManager;
-        this.authenticationController = Check.notNull(authenticationController, "authenticationController");
+        this.authenticationController = Check.notNull(authenticationController,
+                "authenticationController");
         this.applicationProperties = Check.notNull(applicationProperties, "applicationProperties");
     }
-    
+
     public Result authenticate(HttpServletRequest request, HttpServletResponse response)
     {
+        /*!
+        Remote Apps supports a special OAuth-based authentication process that allows a Remote App
+        to bypass any explicit end-user involvement to provide a more seamless user experience. The
+        process is known as 2-legged OAuth combined with a way to specify the user via the request
+         parameter 'user_id'.
+         <p>
+         To begin this process, the request is processed to extract OAuth signature information from
+         the 'Authorization' request header.
+         */
         OAuthMessage message = OAuthServlet.getMessage(request, getLogicalUri(request));
 
+        /*!
+        The OAuth message is validated according to against standard OAuth criteria:
+        <ul>
+         <li>The required fields are present</li>
+         <li>The timestamp is valid</li>
+         <li>The nonce matches</li>
+         <li>The signature is valid</li>
+        </ul>
+        <p>
+        Additionally, the consumer key, also known as the client key, matches an installed and enabled
+          Remote App.
+         */
         String consumerKey;
         try
         {
             consumerKey = message.getConsumerKey();
             oAuthLinkManager.validateOAuth2LORequest(message);
         }
+        /*!
+        If any of these criteria fail, the authorization process is treated as a failure
+          and the request will be rejected with a 403.
+         */
         catch (IOException e)
         {
             log.warn("Exception authenticating request", e);
             return new Result.Failure(new DefaultMessage("OAuth exception:" + e.getMessage()));
-        }
-        catch (URISyntaxException e)
+        } catch (URISyntaxException e)
         {
             log.warn("Exception authenticating request", e);
             return new Result.Failure(new DefaultMessage("OAuth exception:" + e.getMessage()));
-        }
-        catch (OAuthException e)
+        } catch (OAuthException e)
         {
             log.warn("Exception authenticating request", e);
             return new Result.Failure(new DefaultMessage("OAuth exception:" + e.getMessage()));
         }
 
+        /*!
+        The user that the request is made as is specified in the request parameter 'user_id'.
+         */
         final String userId = request.getParameter(OAuth2LOFilter.USER_ID);
 
-        Principal user = null;
+        Principal user;
         if (userId != null)
         {
+            /*!
+            The user must be a valid user in the system and must be able to both log in and have access
+            to the specified remote app.  If either of these cases fail, a 401 is returned.
+             */
             user = userManager.resolve(userId);
             if (!authenticationController.canLogin(user, request))
             {
-                // user exists but is not allowed to login
                 log.warn("Access denied to user '{}' because that user cannot login", userId);
                 sendError(response, HttpServletResponse.SC_UNAUTHORIZED, message);
                 return new Result.Failure(new DefaultMessage("Permission denied"));
             }
+
             else if (!permissionManager.canAccessApi(userId, consumerKey))
             {
-                // user exists but is not allowed to access this remote app
-                log.warn("Access denied to user '{}' because that user is not allowed to make api calls from the remote app '{}'", userId, consumerKey);
+                log.warn("Access denied to user '{}' because that user is not allowed " +
+                                "to make api calls from the remote app '{}'", userId, consumerKey);
                 sendError(response, HttpServletResponse.SC_UNAUTHORIZED, message);
                 return new Result.Failure(new DefaultMessage("Permission denied"));
             }
         }
+        /*!
+        There are a few APIs provided by the application which can be accessed without specifying
+        a valid user.  These are generally provided by the Remote Apps plugin itself.  Examples include:
+        <ul>
+         <li>Deleting an instance of a cached macro's content</li>
+         <li>Deleting all cached macro content for an app</li>
+        </ul>
+         */
         else
         {
             user = NonUserAdminPrincipal.INSTANCE;
         }
+
+        /*!
+        If the request passed all the above checks, the app and user are marked to have successfully authenticated.
+        <p>
+        This process only authenticates the user to the host application.  From here, the request needs
+        to be authorized to ensure it has access to the appropriate API scope.
+         */
         request.setAttribute(OAuth.OAUTH_CONSUMER_KEY, consumerKey);
         log.info("Authenticated app '{}' as user '{}' successfully", consumerKey, user.getName());
         return new Result.Success(user);
+        /*!-helper methods*/
     }
 
     private String getLogicalUri(HttpServletRequest request)
@@ -126,8 +174,7 @@ public class OAuth2LOAuthenticator implements Authenticator
                     uriPathBeforeForwarding,
                     newUri.getQuery(),
                     newUri.getFragment()).toString();
-        }
-        catch (URISyntaxException e)
+        } catch (URISyntaxException e)
         {
             log.warn("forwarded request had invalid original URI path: " + uriPathBeforeForwarding);
             return null;
@@ -139,9 +186,9 @@ public class OAuth2LOAuthenticator implements Authenticator
         response.setStatus(status);
         try
         {
-            response.addHeader("WWW-Authenticate", message.getAuthorizationHeader(applicationProperties.getBaseUrl()));
-        }
-        catch (IOException e)
+            response.addHeader("WWW-Authenticate",
+                    message.getAuthorizationHeader(applicationProperties.getBaseUrl()));
+        } catch (IOException e)
         {
             log.error("Failure reporting OAuth error to client", e);
         }
