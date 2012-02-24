76e1a8469e4fc19e0fd39b2e30c7f4f476a10c7b
Don Brown <mrdon@twdata.org>
2012-02-25 01:22:42 +0700

    Add docco for macro retrieval process
    Change-Id: Ib4c6ae90f5dd3916af5a38f5a3d0e3aa6117753f

--- plugin/src/main/java/com/atlassian/labs/remoteapps/modules/confluence/MacroInstance.java
+++ plugin/src/main/java/com/atlassian/labs/remoteapps/modules/confluence/MacroInstance.java
@@ -9,9 +9,12 @@ import java.util.Map;
 
 import static com.google.common.collect.Maps.newHashMap;
 
-/**
- *
+/*!
+  Each instance of a Remote App macro, no matter if the macro is rendered from retrieved storage-format
+  XML or from an IFrame, shares certain properties.
  */
+
+/*!-constructor and getters */
 public class MacroInstance
 {
     final ConversionContext conversionContext;
@@ -53,10 +56,42 @@ public class MacroInstance
     {
         return parameters;
     }
-    
+
+    public ApplicationLinkOperationsFactory.LinkOperations getLinkOperations()
+    {
+        return linkOperations;
+    }
+
+    /*!#url-parameters
+    ## URL Parameters
+
+    Every macro instance will have its content retrieved from the Remote App on either the server
+    or client side.  As part of this URL construction, key query parameters are added to the URL
+    to pass along macro instance information as well as give the context in which the macro was
+    rendered.
+    */
     public Map<String, String> getUrlParameters()
     {
         Map<String,String> params = newHashMap();
+
+        /*!
+        ### Context Parmeters
+
+        Query parameters that provide information about the context in which the macro is being
+        rendered are prefixed with `ctx_`.
+
+        * `ctx_output_type` - The output type for the rendering that is executing the macro.  Possible
+            values include:<ul>
+            <li>`preview`</li>
+            <li>`display`</li>
+            <li>`word` (Microsoft Word export)</li>
+            <li>`pdf` (PDF export)</li>
+            <li>`html_export`</li>
+            </ul>
+        * `ctx_page_id` - The page or blog post id of the containing entity
+        * `ctx_page_title` - The page or blog post title as a convenience.  Any further information
+            on the entity must be looked up via a separate RPC call/REST resource retrieval.
+        */
         params.put("ctx_output_type", conversionContext.getOutputType());
         if (conversionContext.getEntity() != null)
         {
@@ -65,25 +100,60 @@ public class MacroInstance
             params.put("ctx_page_id", pageId);
             params.put("ctx_page_title", pageTitle);
 
-            // deprecated
+            /*!
+            #### Deprecated
+
+            The following parameters are included, but are depecated and will be removed before 1.0:
+
+            * `page_id`
+            * `pageId`
+            * `pageTitle`
+             */
             params.put("page_id", pageId);
             params.put("pageId", pageId);
             params.put("pageTitle", pageTitle);
         }
 
+        /*!
+        ### Macro Instance Parameters
+
+        * `body` - The body of the macro, if allowed. Empty string if not present.
+        * `key` - The <a href="#hash">unique hash key</a> of the macro instance.
+         */
         params.put("body", body);
         params.put("key", getHashKey());
 
+        /*!
+        ### Macro Parameters
+
+        Finally, any configured macro parameters and their values are sent as query parameters who's
+        name exactly matches the configured parameter name.
+         */
         params.putAll(getParameters());
         params.remove(Macro.RAW_PARAMS_KEY);
         return params;
     }
 
-    public ApplicationLinkOperationsFactory.LinkOperations getLinkOperations()
-    {
-        return linkOperations;
-    }
+    /*!#hash
 
+    ## Unique Hash Key
+
+    Every macro instance has a unique hash key that represents the state of key values in the macro
+    instance.  The primary usage of this key value is to be the cache key for retrieved storage format
+    macro types, though it may be generally useful for other types.
+
+    The hash is composed of the following properties:
+
+    * The app base URL
+    * The macro parameters' values
+    * The macro body
+    * The relative path to the app macro retrieval/IFrame URL
+    * The entity (usually page) id
+
+    Any values outside these listed, including things like the page title, are not included in the
+     key.  If an app needs to know when they change, the app must register a web hook and update
+     that information itself.
+     */
     public String getHashKey()
     {
         StringBuilder sb = new StringBuilder();
