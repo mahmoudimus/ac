6893c4dc733f52f65c996c103e04e1c75433f4ed
Don Brown <mrdon@twdata.org>
2012-04-16 05:48:38 -0600

    Update source before checking out release sha
    Change-Id: I7cdcbaf4cdce0b7d40dbaaf97b65cfe4b8a6f3cc

--- bin/release-prepare.sh
+++ bin/release-prepare.sh
@@ -3,5 +3,8 @@
 echo "Reading release.properties from `pwd`"
 source ./release.properties
 
+echo "Updating source"
+git pull origin master
+
 echo "Checking out $GIT_SHA1..."
 git checkout $GIT_SHA1
