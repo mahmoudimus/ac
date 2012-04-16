5f48b76131d7e8c209673bade020447f82146ba6
Don Brown <mrdon@twdata.org>
2012-04-16 05:01:31 -0600

    Fix wrong release info encoding
    Change-Id: Id0b043cd66083c4a7081e5c83d84219dbd3afdc1

--- bin/store-release-info.sh
+++ bin/store-release-info.sh
@@ -8,7 +8,8 @@ API_VERSION=`cat pom.xml | sed -n 's/.*API_VERSION>\([.0-9]*\)<.*/\1/p'`
 VERSION=$API_VERSION.$BUILD_NUMBER
 
 echo "Recording the version '$VERSION' in release.properties"
-echo "GIT_SHA1=$GIT_SHA1\nVERSION=$API_VERSION.$BUILD_NUMBER" > release.properties
+echo "GIT_SHA1=$GIT_SHA1
+VERSION=$API_VERSION.$BUILD_NUMBER" > release.properties
 
 echo "Writing pom.release.xml with new build number"
 cat pom.xml | sed "s/BUILD_NUMBER>\(.*\)</BUILD_NUMBER>$BUILD_NUMBER</" > pom.release.xml
