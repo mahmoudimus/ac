49b55beacb2ae319e5bbce719ec265a41ccf24e3
Don Brown <mrdon@twdata.org>
2012-04-26 01:28:35 -0600

    Flush out the rest of the JIRA issue webhooks
    Change-Id: I2bdad8c5c21846194ea286e958ee7c015a441e26

--- plugin/src/main/java/com/atlassian/labs/remoteapps/product/jira/JiraWebHookProvider.java
+++ plugin/src/main/java/com/atlassian/labs/remoteapps/product/jira/JiraWebHookProvider.java
@@ -1,6 +1,7 @@
 package com.atlassian.labs.remoteapps.product.jira;
 
 import com.atlassian.applinks.api.ApplicationLink;
+import com.atlassian.jira.event.ProjectCreatedEvent;
 import com.atlassian.jira.event.issue.IssueEvent;
 import com.atlassian.jira.event.type.EventType;
 import com.atlassian.labs.remoteapps.product.jira.webhook.JiraEventSerializerFactory;
@@ -29,6 +30,18 @@ public class JiraWebHookProvider implements WebHookProvider
         builder.put(EventType.ISSUE_CREATED_ID, "issue_created");
         builder.put(EventType.ISSUE_REOPENED_ID, "issue_reopened");
         builder.put(EventType.ISSUE_RESOLVED_ID, "issue_resolved");
+        builder.put(EventType.ISSUE_COMMENT_EDITED_ID, "issue_comment_edited");
+        builder.put(EventType.ISSUE_COMMENTED_ID, "issue_commented");
+        builder.put(EventType.ISSUE_DELETED_ID, "issue_deleted");
+        builder.put(EventType.ISSUE_MOVED_ID, "issue_moved");
+        builder.put(EventType.ISSUE_UPDATED_ID, "issue_updated");
+        builder.put(EventType.ISSUE_WORKLOG_DELETED_ID, "issue_worklog_deleted");
+        builder.put(EventType.ISSUE_WORKLOG_UPDATED_ID, "issue_worklog_updated");
+        builder.put(EventType.ISSUE_WORKLOGGED_ID, "issue_work_logged");
+        builder.put(EventType.ISSUE_WORKSTARTED_ID, "issue_work_started");
+        builder.put(EventType.ISSUE_WORKSTOPPED_ID, "issue_work_stopped");
+        builder.put(EventType.ISSUE_GENERICEVENT_ID, "issue_generic_event");
+
         return builder.build();
     }
 
