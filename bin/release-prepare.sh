72f767b525d26707f9ce7794494debd5e106a6bf
Don Brown <mrdon@twdata.org>
2012-05-21 07:59:35 -0600

    Add output to help debug a failed release
    Change-Id: Idb247b07f1b17f38ed2f77378a548c02336f6709

--- bin/release-prepare.sh
+++ bin/release-prepare.sh
@@ -6,5 +6,5 @@ source ./release.properties
 echo "Updating source"
 git pull origin master
 
-echo "Checking out $GIT_SHA1..."
+echo "Checking out $GIT_SHA1.  If this fails, it is due to this build being a merge."
 git checkout $GIT_SHA1
