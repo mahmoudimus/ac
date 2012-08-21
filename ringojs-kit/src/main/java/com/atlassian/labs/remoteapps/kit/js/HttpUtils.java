422c333cbf2c10d53d6680e4dd407df34544883f
Robert Bergman <rbergman@atlassian.com>
2012-08-20 18:06:45 -0600

    Add router module (and support) to ringo kit
    Change-Id: I93ef7e6cf8748c585b44e7dec981bb80e0dd9906

--- ringojs-kit/src/main/java/com/atlassian/labs/remoteapps/kit/js/HttpUtils.java
+++ ringojs-kit/src/main/java/com/atlassian/labs/remoteapps/kit/js/HttpUtils.java
@@ -8,13 +8,19 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.StringWriter;
+import java.net.URL;
 import java.util.Map;
 
 /**
- * Used by atlassian/mustache
+ * Used by atlassian/renderer
  */
 public class HttpUtils
 {
+    private enum TemplateExtension
+    {
+        mustache, mu
+    }
+
     private final Plugin plugin;
 
     public HttpUtils(PluginRetrievalService pluginRetrievalService)
@@ -22,28 +28,87 @@ public class HttpUtils
         plugin = pluginRetrievalService.getPlugin();
     }
 
-    public String render(String template, Map<String,Object> context)
+    // @todo this is all pretty much bogus -- replace with Atlassian Template Renderer service
+    public String render(String path, Map<String,Object> context)
     {
-        StringWriter writer = new StringWriter();
-        InputStream resourceAsStream = null;
-        try
+        String extension = null;
+        URL resource = null;
+
+        int exti = path.lastIndexOf('.') + 1;
+        if (exti > 1 && exti < path.length())
         {
-            resourceAsStream = plugin.getResourceAsStream(template);
-            Mustache.compiler().compile(
-                new InputStreamReader(resourceAsStream)).execute(context,
-                writer);
+            extension = path.substring(exti);
+            resource = plugin.getResource(path);
         }
-        finally
+        else
         {
+            for (TemplateExtension ext : TemplateExtension.values())
+            {
+                extension = ext.name();
+                String tryPath = path + "." + extension;
+                resource = plugin.getResource(tryPath);
+                if (resource != null)
+                {
+                    path = tryPath;
+                    break;
+                }
+            }
+        }
+
+        if (resource != null)
+        {
+            return getRenderer(path, extension).render(path, context);
+        }
+        else
+        {
+            throw new IllegalArgumentException("No acceptable template file found for path '" + path + "'");
+        }
+    }
+
+    private Renderer getRenderer(String path, String extension)
+    {
+        Renderer renderer;
+        if (TemplateExtension.mustache.name().equalsIgnoreCase(extension)
+            || TemplateExtension.mu.name().equalsIgnoreCase(extension))
+        {
+            renderer = new MustacheRenderer();
+        }
+        else
+        {
+            throw new IllegalArgumentException("Unrecognized template file extension for path '" + path + "'");
+        }
+        return renderer;
+    }
+
+    private static interface Renderer
+    {
+        String render(String path, Map<String, Object> context);
+    }
+
+    private class MustacheRenderer implements Renderer
+    {
+        @Override
+        public String render(String path, Map<String, Object> context)
+        {
+            InputStream stream = plugin.getResourceAsStream(path);
+            InputStreamReader reader = new InputStreamReader(stream);
+            StringWriter writer = new StringWriter();
             try
             {
-                resourceAsStream.close();
+                Mustache.compiler().compile(reader).execute(context, writer);
             }
-            catch (IOException e)
+            finally
             {
-                throw new RuntimeException(e);
+                try
+                {
+                    stream.close();
+                }
+                catch (IOException ioe)
+                {
+                    throw new RuntimeException(ioe);
+                }
             }
+            return writer.toString();
         }
-        return writer.toString();
     }
 }
---
+++ ringojs-kit/src/main/resources/modules/atlassian/response.js
@@ -0,0 +1,94 @@
+var {Deferred} = require("atlassian/deferred");
+var renderer = require("atlassian/renderer");
+var fs = require("fs");
+
+module.exports = function (defaultView) {
+
+  var deferred = Deferred();
+
+  var data = {
+    status: 200,
+    headers: {"Content-Type": "text/html"},
+    body: []
+  };
+
+  return {
+
+    // response.writeHead(statusCode, headers)
+    // response.writeHead(headers)
+    writeHead: function (statusCode, headers) {
+      if (typeof statusCode === "object") {
+        headers = statusCode;
+      }
+      else {
+        data.status = statusCode;
+      }
+      if (headers) {
+        Object.keys(headers).forEach(function (k) {
+          data.headers[k] = headers[k];
+        });
+      }
+    },
+
+    // response.write(chunk)
+    write: function (chunk) {
+      data.body.push(chunk);
+    },
+
+    // response.end()
+    // response.end(chunk)
+    end: function (chunk) {
+      if (chunk != null) this.write(chunk);
+      deferred.resolve();
+    },
+
+    // response.sendNotFound()
+    // response.sendNotFound(reason)
+    sendNotFound: function (reason) {
+      reason = reason || "Not Found";
+      this.writeHead(404);
+      this.end(reason);
+    },
+
+    // response.send(body)
+    // response.send(body, headers)
+    // response.send(body, headers, statusCode)
+    send: function (body, headers, statusCode) {
+      this.writeHead(statusCode || 200, headers);
+      this.end(body);
+    },
+
+    // response.render(locals)
+    // response.render(view, locals)
+    // response.render(locals, headers, statusCode)
+    // response.render(view, locals, headers, statusCode)
+    render: function (view, locals, headers, statusCode) {
+      if (typeof view !== "string") {
+        statusCode = headers;
+        headers = locals;
+        locals = view;
+        view = defaultView;
+      }
+      try {
+        var body = renderer.render("app/views/" + view, locals);
+        this.send(body, headers, statusCode);
+      }
+      catch (ex) {
+        var je = ex.javaException;
+        if (je && je.cause instanceof java.io.FileNotFoundException) {
+          this.sendNotFound(je.cause.getMessage());
+        }
+        else {
+          throw ex;
+        }
+      }
+    },
+
+    _toJsgiResponse: function () {
+      deferred.wait();
+      return data;
+    }
+
+  };
+
+};
