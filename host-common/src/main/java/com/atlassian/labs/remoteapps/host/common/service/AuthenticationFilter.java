027d59afa6963f52a4caacd37a1e66d448b206bd
Don Brown <mrdon@twdata.org>
2012-09-07 16:54:42 -0600

    Store auth in session for rpc and other local calls
    Change-Id: I6d4802598e37b28e1eba2d45f188cfc6c3263eed

--- host-common/src/main/java/com/atlassian/labs/remoteapps/host/common/service/AuthenticationFilter.java
+++ host-common/src/main/java/com/atlassian/labs/remoteapps/host/common/service/AuthenticationFilter.java
@@ -6,6 +6,7 @@ import net.oauth.OAuth;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.net.URI;
 import java.util.Map;
@@ -39,19 +40,26 @@ public class AuthenticationFilter implements Filter
             HttpServletRequest req = (HttpServletRequest) request;
             URI uri = URI.create(req.getRequestURI());
 
+            AuthenticationInfo info;
+
             // @todo not as robust a check as it maybe could be
             if (!uri.getPath().contains("/public/") && (req.getHeader("Authorization") != null || req.getParameter(
                     OAuth.OAUTH_SIGNATURE) != null))
             {
-                // validate the request and obtain the client key
-                String clientKey = signedRequestHandler.validateRequest(req);
-                requestContext.setClientKey(clientKey);
+                info = authenticateOauth(req);
 
-                // extract the user id, if any
-                Map params = req.getParameterMap();
-                String[] userIds = (String[]) params.get("user_id");
-                String userId = userIds != null && userIds.length == 1 ? userIds[0] : null;
-                requestContext.setUserId(userId);
+                // persist to the session
+                storeAuthenticationInSession(req.getSession(true), info);
+            }
+            else
+            {
+                info = retrieveAuthenticationFromSession(req.getSession(true));
+            }
+
+            if (info != null)
+            {
+                requestContext.setClientKey(info.clientKey);
+                requestContext.setUserId(info.userId);
             }
 
             // complete the request handling
@@ -64,9 +72,46 @@ public class AuthenticationFilter implements Filter
         }
     }
 
+    private AuthenticationInfo authenticateOauth(HttpServletRequest req) throws ServletException
+    {
+        AuthenticationInfo info = new AuthenticationInfo();
+        // validate the request and obtain the client key
+        info.clientKey = signedRequestHandler.validateRequest(req);
+
+        // extract the user id, if any
+        Map params = req.getParameterMap();
+        String[] userIds = (String[]) params.get("user_id");
+        info.userId = userIds != null && userIds.length == 1 ? userIds[0] : null;
+        return info;
+    }
+
+    private AuthenticationInfo retrieveAuthenticationFromSession(HttpSession session)
+    {
+        AuthenticationInfo info = null;
+        if (session.getAttribute("clientKey") != null)
+        {
+            info = new AuthenticationInfo();
+            info.clientKey = (String) session.getAttribute("clientKey");
+            info.userId = (String) session.getAttribute("userId");
+        }
+        return info;
+    }
+
+    private void storeAuthenticationInSession(HttpSession session, AuthenticationInfo info)
+    {
+        session.setAttribute("clientKey", info.clientKey);
+        session.setAttribute("userId", info.userId);
+    }
+
     @Override
     public void destroy()
     {
         requestContext.clear();
+    }
+
+    private static class AuthenticationInfo
+    {
+        public String clientKey;
+        public String userId;
     }
 }
