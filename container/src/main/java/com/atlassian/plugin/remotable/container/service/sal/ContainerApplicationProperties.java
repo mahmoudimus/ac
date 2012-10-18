4be7b41679d258a320ec139bc26888d90c408ae6
Samuel Le Berrigaud <sam@leberrigaud.org>
2012-10-18 18:10:18 +0200

    Add properties to ContainerApplicationProperties. - display name - version (from maven) - build number (always 0 for now)
    Change-Id: Id4a8432906ed4d6700c3d83478f2d71369e35773

--- container/src/main/java/com/atlassian/plugin/remotable/container/service/sal/ContainerApplicationProperties.java
+++ container/src/main/java/com/atlassian/plugin/remotable/container/service/sal/ContainerApplicationProperties.java
@@ -1,14 +1,19 @@
 package com.atlassian.plugin.remotable.container.service.sal;
 
 import com.atlassian.sal.api.ApplicationProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Date;
+import java.util.Properties;
 
 /**
  * Simple implementation of application properties
  */
-public class ContainerApplicationProperties implements ApplicationProperties
+public final class ContainerApplicationProperties implements ApplicationProperties
 {
     private final String baseUrl;
 
@@ -26,13 +31,13 @@ public class ContainerApplicationProperties implements ApplicationProperties
     @Override
     public String getDisplayName()
     {
-        throw new UnsupportedOperationException();
+        return "Atlassian Remotable Plugins - Standalone Container";
     }
 
     @Override
     public String getVersion()
     {
-        throw new UnsupportedOperationException();
+        return MavenUtils.getVersion("com.atlassian.plugins", "remotable-plugins-container");
     }
 
     @Override
@@ -44,7 +49,7 @@ public class ContainerApplicationProperties implements ApplicationProperties
     @Override
     public String getBuildNumber()
     {
-        throw new UnsupportedOperationException();
+        return String.valueOf(0);
     }
 
     @Override
@@ -57,5 +62,44 @@ public class ContainerApplicationProperties implements ApplicationProperties
     public String getPropertyValue(String key)
     {
         throw new UnsupportedOperationException();
+    }
+
+    private static final class MavenUtils
+    {
+        private static final Logger logger = LoggerFactory.getLogger(MavenUtils.class);
+
+        private static final String UNKNOWN_VERSION = "unknown";
+
+        static String getVersion(String groupId, String artifactId)
+        {
+            final Properties props = new Properties();
+            InputStream resourceAsStream = null;
+            try
+            {
+                resourceAsStream = MavenUtils.class.getResourceAsStream(String.format("/META-INF/maven/%s/%s/pom.properties", groupId, artifactId));
+                props.load(resourceAsStream);
+                return props.getProperty("version", UNKNOWN_VERSION);
+            }
+            catch (Exception e)
+            {
+                logger.debug("Could not find version for maven artifact {}:{}", groupId, artifactId);
+                logger.debug("Got the following exception", e);
+                return UNKNOWN_VERSION;
+            }
+            finally
+            {
+                if (resourceAsStream != null)
+                {
+                    try
+                    {
+                        resourceAsStream.close();
+                    }
+                    catch (IOException ioe)
+                    {
+                        // ignore
+                    }
+                }
+            }
+        }
     }
 }
