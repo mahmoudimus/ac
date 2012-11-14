9c12e8e6bddacaa35e5628607cdc9e4997e65e5b
Samuel Le Berrigaud <sam@leberrigaud.org>
2012-11-14 11:06:01 +0100

    JRADEV-16426: No longer freeze on disable/uninstall/shutdown - No longer uninstall i18n plugin - Disable i18n plugin by way of import on package from Remotable plugin - Ensure i18n plugin is re-enabled when Remotable plugin is.
    Change-Id: Ib7b4408850e27b2bc2e126137880be143a8a6ed6

--- host-common/src/main/java/com/atlassian/plugin/remotable/host/common/util/BundleUtil.java
+++ host-common/src/main/java/com/atlassian/plugin/remotable/host/common/util/BundleUtil.java
@@ -1,24 +1,69 @@
 package com.atlassian.plugin.remotable.host.common.util;
 
 import com.atlassian.plugin.osgi.factory.OsgiPlugin;
+import com.google.common.base.Function;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 
-/**
- *
- */
-public class BundleUtil
+import static com.google.common.collect.Lists.*;
+
+public final class BundleUtil
 {
+    private static final Bundle NOT_FOUND_BUNDLE = null;
+
+    private BundleUtil()
+    {
+    }
+
     public static Bundle findBundleForPlugin(BundleContext bundleContext, String pluginKey)
     {
-        for (Bundle bundle : bundleContext.getBundles())
+        return findBundleForPlugin(newArrayList(bundleContext.getBundles()), pluginKey);
+    }
+
+    private static Bundle findBundleForPlugin(Iterable<Bundle> bundles, final String pluginKey)
+    {
+        return Iterables.find(bundles,
+                new Predicate<Bundle>()
+                {
+                    @Override
+                    public boolean apply(Bundle b)
+                    {
+                        return pluginKey.equals(b.getHeaders().get(OsgiPlugin.ATLASSIAN_PLUGIN_KEY));
+                    }
+                },
+                NOT_FOUND_BUNDLE);
+    }
+
+    public static Bundle findBundleWithName(final BundleContext bundleContext, final String symbolicName)
+    {
+        return findBundleWithName(newArrayList(bundleContext.getBundles()), symbolicName);
+    }
+
+    private static Bundle findBundleWithName(Iterable<Bundle> bundles, final String symbolicName)
+    {
+        return Iterables.find(bundles,
+                new Predicate<Bundle>()
+                {
+                    @Override
+                    public boolean apply(Bundle b)
+                    {
+                        return b.getSymbolicName().equals(symbolicName);
+                    }
+                },
+                NOT_FOUND_BUNDLE);
+    }
+
+    public static Iterable<String> toBundleNames(Bundle[] bundles)
+    {
+        return Iterables.transform(newArrayList(bundles), new Function<Bundle, String>()
         {
-            String maybePluginKey = (String) bundle.getHeaders().get(OsgiPlugin.ATLASSIAN_PLUGIN_KEY);
-            if (pluginKey.equals(maybePluginKey))
+            @Override
+            public String apply(Bundle b)
             {
-                return bundle;
+                return b.getSymbolicName();
             }
-        }
-        return null;
+        });
     }
 }
--- plugin/src/main/java/com/atlassian/plugin/remotable/plugin/integration/plugins/I18nPropertiesPluginManager.java
+++ plugin/src/main/java/com/atlassian/plugin/remotable/plugin/integration/plugins/I18nPropertiesPluginManager.java
@@ -1,9 +1,13 @@
 package com.atlassian.plugin.remotable.plugin.integration.plugins;
 
-import com.atlassian.plugin.remotable.plugin.loader.StartableForPlugins;
-import com.atlassian.plugin.*;
+import com.atlassian.plugin.JarPluginArtifact;
+import com.atlassian.plugin.ModuleDescriptor;
+import com.atlassian.plugin.Plugin;
+import com.atlassian.plugin.PluginAccessor;
+import com.atlassian.plugin.PluginController;
 import com.atlassian.plugin.osgi.bridge.external.PluginRetrievalService;
 import com.atlassian.plugin.osgi.factory.OsgiPlugin;
+import com.atlassian.plugin.remotable.plugin.loader.StartableForPlugins;
 import com.atlassian.util.concurrent.CopyOnWriteMap;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
@@ -13,24 +17,28 @@ import org.osgi.framework.BundleContext;
 import org.osgi.framework.Constants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.io.*;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.net.URL;
-import java.util.*;
+import java.util.Enumeration;
+import java.util.Map;
+import java.util.Properties;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.jar.Attributes;
 import java.util.jar.Manifest;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
-import static com.atlassian.plugin.remotable.host.common.util.BundleUtil.findBundleForPlugin;
-import static com.google.common.collect.Maps.newHashMap;
-import static com.google.common.collect.Maps.newTreeMap;
-import static com.google.common.collect.Sets.newTreeSet;
-import static java.lang.Math.abs;
+import static com.atlassian.plugin.remotable.host.common.util.BundleUtil.*;
+import static com.google.common.base.Preconditions.*;
+import static java.lang.Math.*;
 
 /**
  * This class loads a plugin called "remotable.plugins.i18n", which solely exists to store and expose generated i18n files
@@ -39,18 +47,19 @@ import static java.lang.Math.abs;
  * around that.
  */
 @Component
-public class I18nPropertiesPluginManager implements DisposableBean
+public final class I18nPropertiesPluginManager
 {
+    private static final Logger log = LoggerFactory.getLogger(I18nPropertiesPluginManager.class);
     private static final String I18N_SYMBOLIC_NAME = "remotable.plugins.i18n";
+
     private final PluginAccessor pluginAccessor;
     private final PluginController pluginController;
     private final BundleContext bundleContext;
 
-    private volatile Bundle i18nBundle;
-    private static final Logger log = LoggerFactory.getLogger(I18nPropertiesPluginManager.class);
-
     private final AtomicBoolean started = new AtomicBoolean(false);
-    private final Map<String,String> i18nToRegister = CopyOnWriteMap.newHashMap();
+    private final Map<String, String> i18nToRegister = CopyOnWriteMap.newHashMap();
+
+    private volatile Bundle i18nBundle;
 
     @Autowired
     public I18nPropertiesPluginManager(PluginAccessor accessor,
@@ -59,9 +68,9 @@ public class I18nPropertiesPluginManager implements DisposableBean
                                        PluginRetrievalService pluginRetrievalService
     )
     {
-        this.bundleContext = bundleContext;
-        this.pluginAccessor = accessor;
-        this.pluginController = pluginController;
+        this.bundleContext = checkNotNull(bundleContext);
+        this.pluginAccessor = checkNotNull(accessor);
+        this.pluginController = checkNotNull(pluginController);
         startableForPlugins.register(pluginRetrievalService.getPlugin().getKey(), new Runnable()
         {
             @Override
@@ -84,7 +93,7 @@ public class I18nPropertiesPluginManager implements DisposableBean
             Enumeration<URL> files = bundle.findEntries("/", "*.properties", false);
             if (files != null)
             {
-                while(files.hasMoreElements())
+                while (files.hasMoreElements())
                 {
                     URL url = files.nextElement();
                     String name = url.getFile();
@@ -110,7 +119,7 @@ public class I18nPropertiesPluginManager implements DisposableBean
 
             pluginController.installPlugins(new JarPluginArtifact(tmpFile));
             tmpFile.delete();
-            i18nBundle = findBundleForPlugin(bundleContext, I18N_SYMBOLIC_NAME);
+            setI18nBundle();
         }
         catch (IOException e)
         {
@@ -159,7 +168,7 @@ public class I18nPropertiesPluginManager implements DisposableBean
         return name;
     }
 
-    private synchronized void registerI18n(final Map<String,String> i18n)
+    private synchronized void registerI18n(final Map<String, String> i18n)
     {
         forBundle(i18nBundle, new BundleManipulator()
         {
@@ -181,74 +190,106 @@ public class I18nPropertiesPluginManager implements DisposableBean
         for (String name : i18n.keySet())
         {
             I18nModuleDescriptor descriptor = new I18nModuleDescriptor();
-            descriptor.init(pluginAccessor.getPlugin(I18N_SYMBOLIC_NAME), DocumentHelper.createElement("i18n-something")
-                                                                                        .addAttribute("key", name)
-                                                                                        .addElement("resource")
-                                                                                        .addAttribute("type", "i18n")
-                                                                                        .addAttribute("name", "i18n")
-                                                                                        .addAttribute("location", name)
-                                                                                        .getParent());
+            descriptor.init(findI18nPlugin(), DocumentHelper.createElement("i18n-something")
+                    .addAttribute("key", name)
+                    .addElement("resource")
+                    .addAttribute("type", "i18n")
+                    .addAttribute("name", "i18n")
+                    .addAttribute("location", name)
+                    .getParent());
             i18nBundle.getBundleContext().registerService(ModuleDescriptor.class.getName(), descriptor, null);
         }
     }
 
     private void loadBundle()
     {
-        Plugin plugin = pluginAccessor.getPlugin(I18N_SYMBOLIC_NAME);
-        if (plugin == null)
+        if (findI18nPlugin() == null)
         {
-            try
-            {
-                File tmpFile = File.createTempFile(I18N_SYMBOLIC_NAME, ".jar");
-                ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(tmpFile));
-                zout.putNextEntry(new ZipEntry("META-INF/MANIFEST.MF"));
-                Manifest mf = new Manifest();
-                mf.getMainAttributes().put(Attributes.Name.MANIFEST_VERSION, "1");
-                mf.getMainAttributes().putValue(Constants.BUNDLE_SYMBOLICNAME, I18N_SYMBOLIC_NAME);
-                mf.getMainAttributes().putValue(OsgiPlugin.ATLASSIAN_PLUGIN_KEY, I18N_SYMBOLIC_NAME);
-                mf.getMainAttributes().putValue(Constants.BUNDLE_VERSION, "1");
-                mf.getMainAttributes().putValue(Constants.BUNDLE_DESCRIPTION, "I18n properties files for remote plugins");
-                mf.getMainAttributes().putValue(Constants.BUNDLE_NAME, "Remotable Plugins I18n plugin");
-                mf.getMainAttributes().putValue(Constants.BUNDLE_MANIFESTVERSION, "2");
-                mf.getMainAttributes().putValue("Spring-Context", "*");
-                mf.write(zout);
-                zout.close();
-                pluginController.installPlugins(new JarPluginArtifact(tmpFile));
-                tmpFile.delete();
-            }
-            catch (IOException e)
-            {
-                throw new RuntimeException("Unable to retrieve bundle", e);
-            }
+            createAndInstallI18nPlugin();
         }
 
-        Set<String> bundleNames = newTreeSet();
-        Bundle[] bundles = bundleContext.getBundles();
-        for (Bundle bundle : bundles)
-        {
-            bundleNames.add(bundle.getSymbolicName());
-            if (bundle.getSymbolicName().equals(I18N_SYMBOLIC_NAME))
-            {
-                i18nBundle = bundle;
-                return;
-            }
-        }
-
-        throw new IllegalStateException("The i18n bundle is not found in all possible: " + bundleNames);
+        ensureI18nPluginIsEnabled();
+        setI18nBundle();
     }
 
-    @Override
-    public void destroy() throws Exception
+    private Plugin findI18nPlugin()
     {
-        if (i18nBundle != null)
+        return pluginAccessor.getPlugin(I18N_SYMBOLIC_NAME);
+    }
+
+    private Plugin createAndInstallI18nPlugin()
+    {
+        final File i18nPluginTempFile = createI18nPluginTempFile();
+        pluginController.installPlugins(new JarPluginArtifact(i18nPluginTempFile));
+
+        if (!i18nPluginTempFile.delete())
         {
-            pluginController.uninstall(pluginAccessor.getPlugin(I18N_SYMBOLIC_NAME));
+            log.warn("Could not delete i18n temp file at '{}'", i18nPluginTempFile);
         }
+
+        return findI18nPlugin();
+    }
+
+    private File createI18nPluginTempFile()
+    {
+        try
+        {
+            final File tmpFile = File.createTempFile(I18N_SYMBOLIC_NAME, ".jar");
+
+            ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(tmpFile));
+            addManifest(zout);
+            zout.close();
+            return tmpFile;
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException("Could not create " + I18N_SYMBOLIC_NAME + " plugin temp jar file.", e);
+        }
+    }
+
+    private void addManifest(ZipOutputStream zout) throws IOException
+    {
+        zout.putNextEntry(new ZipEntry("META-INF/MANIFEST.MF"));
+        final Manifest mf = createI18nPluginManifest();
+        mf.write(zout);
+    }
+
+    private Manifest createI18nPluginManifest()
+    {
+        final Manifest mf = new Manifest();
+        mf.getMainAttributes().put(Attributes.Name.MANIFEST_VERSION, "1");
+        mf.getMainAttributes().putValue(Constants.BUNDLE_SYMBOLICNAME, I18N_SYMBOLIC_NAME);
+        mf.getMainAttributes().putValue(OsgiPlugin.ATLASSIAN_PLUGIN_KEY, I18N_SYMBOLIC_NAME);
+        mf.getMainAttributes().putValue(Constants.BUNDLE_VERSION, "1");
+        mf.getMainAttributes().putValue(Constants.BUNDLE_DESCRIPTION, "I18n properties files for remote plugins");
+        mf.getMainAttributes().putValue(Constants.BUNDLE_NAME, "Remotable Plugins I18n plugin");
+        mf.getMainAttributes().putValue(Constants.BUNDLE_MANIFESTVERSION, "2");
+        mf.getMainAttributes().putValue("Spring-Context", "*");
+
+        // forces a dependency between the i18n plugin and the remotable plugin, so that disabling the latter disables the former
+        mf.getMainAttributes().putValue(Constants.IMPORT_PACKAGE, "com.atlassian.plugin.remotable.api");
+
+        return mf;
+    }
+
+    private void setI18nBundle()
+    {
+        this.i18nBundle = findBundleWithName(bundleContext, I18N_SYMBOLIC_NAME);
+        if (i18nBundle == null)
+        {
+            throw new IllegalStateException("The i18n bundle (" + I18N_SYMBOLIC_NAME + ") was not found amongst bundles: " + toBundleNames(bundleContext.getBundles()));
+        }
+    }
+
+    private void ensureI18nPluginIsEnabled()
+    {
+        pluginController.enablePlugins(I18N_SYMBOLIC_NAME);
     }
 
     private static interface BundleManipulator
     {
         boolean includeEntry(String name);
+
         void finish(Bundle bundle, ZipOutputStream zout) throws IOException;
     }
 }
