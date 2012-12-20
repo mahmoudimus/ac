337ebc6cfc9d022ead16f22424048f70774a66f7
Don Brown <mrdon@twdata.org>
2012-12-19 21:04:58 -0700

    Fix release to checkout into branch directly
    Change-Id: I3e1ccfa86dccbba279fcd01239a3ef59f096778e

--- bin/release-prepare.sh
+++ bin/release-prepare.sh
@@ -6,11 +6,9 @@ source ./release.properties
 echo "Updating source"
 git fetch origin
 
-echo "Checking out $GIT_SHA1.  If this fails, it is due to this build being a merge."
-git checkout $GIT_SHA1
+echo "Checking out $GIT_SHA1 into branch.  If this fails, it is due to this build being a merge."
 
-echo "Branching $VERSION"
-git checkout -b remotable-plugins-$VERSION.x
+git checkout -b remotable-plugins-$VERSION.x $GIT_SHA1
 
 echo "Replacing versions in the poms"
 find . -type f -name "pom.xml" | xargs perl -pi -e "s/<version>$API_VERSION-SNAPSHOT/<version>$VERSION/g"
