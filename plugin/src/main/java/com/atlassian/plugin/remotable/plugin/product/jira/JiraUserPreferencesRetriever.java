0738ed673113242213c8a8552d3df35e1810b4b3
Alek Mierzwicki <amierzwicki@atlassian.com>
2013-02-06 15:17:11 +0100

    Using TimeZoneService instead of TimeZoneResolver (which is marked as internal).
    ARA-301
    
    Change-Id: I07dc9eaab1c91f0132e9373695f0e695e1b2811e

--- plugin/src/main/java/com/atlassian/plugin/remotable/plugin/product/jira/JiraUserPreferencesRetriever.java
+++ plugin/src/main/java/com/atlassian/plugin/remotable/plugin/product/jira/JiraUserPreferencesRetriever.java
@@ -2,23 +2,24 @@ package com.atlassian.plugin.remotable.plugin.product.jira;
 
 import com.atlassian.crowd.embedded.api.User;
 import com.atlassian.jira.bc.JiraServiceContextImpl;
-import com.atlassian.jira.timezone.TimeZoneResolver;
+import com.atlassian.jira.timezone.TimeZoneInfo;
+import com.atlassian.jira.timezone.TimeZoneService;
 import com.atlassian.jira.user.util.UserManager;
 import com.atlassian.plugin.remotable.plugin.UserPreferencesRetriever;
 
-import javax.annotation.Nullable;
 import java.util.TimeZone;
+import javax.annotation.Nullable;
 
 public class JiraUserPreferencesRetriever implements UserPreferencesRetriever
 {
 
     private final UserManager userManager;
-    private final TimeZoneResolver timeZoneResolver;
+    private final TimeZoneService timeZoneService;
 
-    public JiraUserPreferencesRetriever(final UserManager userManager, final TimeZoneResolver timeZoneResolver)
+    public JiraUserPreferencesRetriever(final UserManager userManager, final TimeZoneService timeZoneService)
     {
         this.userManager = userManager;
-        this.timeZoneResolver = timeZoneResolver;
+        this.timeZoneService = timeZoneService;
     }
 
     @Override
@@ -26,6 +27,7 @@ public class JiraUserPreferencesRetriever implements UserPreferencesRetriever
     {
         final User user = userManager.getUser(userName);
         final JiraServiceContextImpl jiraServiceContext = new JiraServiceContextImpl(user);
-        return (user == null) ? timeZoneResolver.getDefaultTimeZone(jiraServiceContext) : timeZoneResolver.getUserTimeZone(jiraServiceContext);
+        final TimeZoneInfo timeZoneInfo = (user == null) ? timeZoneService.getUserTimeZoneInfo(jiraServiceContext) : timeZoneService.getDefaultTimeZoneInfo(jiraServiceContext);
+        return timeZoneInfo.toTimeZone();
     }
 }
