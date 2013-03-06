7d797b8ddc89ff3bace1067e24a44df0546b5607
Samuel Le Berrigaud <sam@leberrigaud.org>
2013-03-06 12:04:35 +0100

    Up release scripts to use correct versions.
    Change-Id: Ieab5da7acb6c2fc607db5cdef326b317b0e7ab6a

--- bin/store-release-info.sh
+++ bin/store-release-info.sh
@@ -3,11 +3,17 @@
 BUILD_NUMBER=$2
 GIT_SHA1=$1
 
-echo "Extracting the api version"
 API_VERSION=`cat pom.xml | sed -n 's/.*<api.version>\([.0-9]*\)<.*/\1/p'`
-VERSION=$API_VERSION.$BUILD_NUMBER
+echo "Extracted API version as $API_VERSION"
 
-echo "Recording the version '$VERSION' in release.properties"
+CURRENT_VERSION=`cat pom.xml | sed -n 's/.*<version>\([.0-9]*-SNAPSHOT\)<.*/\1/p'`
+echo "Extracted current version as $CURRENT_VERSION"
+
+VERSION=$API_VERSION.$BUILD_NUMBER
+echo "Setting new version as $VERSION"
+
+echo "Recording version information `pwd`/release.properties"
 echo "GIT_SHA1=$GIT_SHA1
 API_VERSION=$API_VERSION
+CURRENT_VERSION=$CURRENT_VERSION
 VERSION=$API_VERSION.$BUILD_NUMBER" > release.properties
