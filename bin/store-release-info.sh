3e82ef4a06b2c928dfad1623e05c41d133ea5d41
Jonathan Doklovic <doklovic@atlassian.com>
2013-06-25 14:29:59 -0700

    fixing release info script
    Change-Id: I0c59ea20d8e2392cf4b48a9cbd7f1f87869acc5f

--- bin/store-release-info.sh
+++ bin/store-release-info.sh
@@ -9,7 +9,7 @@ echo "Extracted API version as $API_VERSION"
 CUR_Q=`cat pom.xml | sed -n 's/.*<version>\([0-9]\.[0-9]\)\([.0-9]*\)\(.*\)-SNAPSHOT.*<.*/\3/p'`
 echo "Extracted qualifier as $CUR_Q"
 
-CURRENT_VERSION=`cat pom.xml | sed -n 's/.*<version>\([.0-9]*-SNAPSHOT\)<.*/\1/p'`
+CURRENT_VERSION=`cat pom.xml | sed -n 's/.*<version>\([.0-9]*.*-SNAPSHOT\)<.*/\1/p'`
 echo "Extracted current version as $CURRENT_VERSION"
 
 VERSION=${API_VERSION}.${BUILD_NUMBER}${CUR_Q}
