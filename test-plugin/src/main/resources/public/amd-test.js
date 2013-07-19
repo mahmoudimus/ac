2bc81bcb2aca81e7d2af5b68784ecf8ba49d5abb
Samuel Le Berrigaud <sam@leberrigaud.org>
2013-07-19 09:05:16 +0200

    ACDEV-315L Removing no longer used test plugin.
    Change-Id: Ie8d78aa444bad9a5b1e84a63d4ef759e974cedf6

--- test-plugin/src/main/resources/public/amd-test.js
+++
@@ -1,8 +0,0 @@
-AP.require(["env", "request", "dialog"], function (env, request, dialog) {
-
-  // check that modules were returned and have some of the expected values set on them
-  $("#amd-env").text((!!env && !!env.getUser).toString());
-  $("#amd-request").text((!!request && !!request.__target__).toString());
-  $("#amd-dialog").text((!!dialog && !!dialog.getButton).toString());
-
-});
