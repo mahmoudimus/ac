4b2dbe512951016eeb89bc2b91b846477d39eb29
Jonathon Creenaune <jcreenaune@atlassian.com>
2013-07-25 11:31:58 +1000

    ACDEV-322 Fix broken test

--- plugin/src/test/java/com/atlassian/plugin/remotable/plugin/module/webpanel/extractor/TestWebPanelAllParametersExtractor.java
+++ plugin/src/test/java/com/atlassian/plugin/remotable/plugin/module/webpanel/extractor/TestWebPanelAllParametersExtractor.java
@@ -1,8 +1,8 @@
 package com.atlassian.plugin.remotable.plugin.module.webpanel.extractor;
 
 import com.atlassian.jira.issue.Issue;
-import com.atlassian.plugin.remotable.plugin.module.webpanel.extractor.jira.IssueIdWebPanelParameterExtractor;
-import com.atlassian.plugin.remotable.plugin.module.webpanel.extractor.jira.ProjectIdWebPanelParameterExtractor;
+import com.atlassian.plugin.remotable.plugin.module.webpanel.extractor.jira.IssueWebPanelParameterExtractor;
+import com.atlassian.plugin.remotable.plugin.module.webpanel.extractor.jira.ProjectWebPanelParameterExtractor;
 import com.google.common.collect.ImmutableMap;
 import org.junit.Test;
 
@@ -19,17 +19,19 @@ import static org.mockito.Mockito.when;
 public class TestWebPanelAllParametersExtractor
 {
     private static final long ISSUE_ID = 100l;
+    private static final String ISSUE_KEY = "ABC-123";
 
     @Test
     public void testGetExtractedWebPanelParameters()
     {
         final List<WebPanelParameterExtractor> parameterExtractors = newArrayList(
-                new IssueIdWebPanelParameterExtractor(),
-                new ProjectIdWebPanelParameterExtractor()
+                new IssueWebPanelParameterExtractor(),
+                new ProjectWebPanelParameterExtractor()
         );
 
         Issue issue = mock(Issue.class);
         when(issue.getId()).thenReturn(ISSUE_ID);
+        when(issue.getKey()).thenReturn(ISSUE_KEY);
 
         final WebPanelURLParametersSerializer webPanelURLParametersSerializer = new WebPanelURLParametersSerializer(parameterExtractors);
         final Map<String, Object> context = ImmutableMap.<String, Object>builder()
@@ -41,6 +43,7 @@ public class TestWebPanelAllParametersExtractor
         Map<String, Object> issueMap = (Map<String, Object>) extractedWebPanelParameters.get("issue");
         assertThat(issueMap.keySet(), hasItem("id"));
         assertThat((Long) issueMap.get("id"), is(ISSUE_ID));
+        assertThat((String) issueMap.get("key"), is(ISSUE_KEY));
     }
 
 }
