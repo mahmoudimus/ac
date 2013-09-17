e6dea11196460ac9b00bfea48724d5cc752abc36
Anders Holmgren <aholmgren@atlassian.com>
2013-09-17 17:33:42 +1000

    ACDEV-397: added space deserialiser

--- plugin/src/main/java/com/atlassian/plugin/connect/plugin/module/confluence/context/serializer/SpaceSerializer.java
+++ plugin/src/main/java/com/atlassian/plugin/connect/plugin/module/confluence/context/serializer/SpaceSerializer.java
@@ -1,7 +1,10 @@
 package com.atlassian.plugin.connect.plugin.module.confluence.context.serializer;
 
+import com.atlassian.confluence.content.service.SpaceService;
+import com.atlassian.confluence.content.service.space.SpaceLocator;
 import com.atlassian.confluence.spaces.Space;
-import com.atlassian.plugin.connect.plugin.module.context.ParameterSerializer;
+import com.atlassian.user.User;
+import com.atlassian.user.UserManager;
 import com.google.common.collect.ImmutableMap;
 
 import java.util.Map;
@@ -9,14 +12,54 @@ import java.util.Map;
 /**
  * Serializes space objects.
  */
-public class SpaceSerializer implements ParameterSerializer<Space>
+public class SpaceSerializer extends AbstractConfluenceParameterSerializer<Space, SpaceLocator>
 {
-    @Override
-    public Map<String, Object> serialize(Space space)
+   public static final String SPACE_FIELD_NAME = "space";
+
+    public SpaceSerializer(final SpaceService spaceService, UserManager userManager)
     {
-        return ImmutableMap.<String, Object>of("space", ImmutableMap.of(
-                "id", space.getId(),
-                "key", space.getKey()
-        ));
+        super(userManager, SPACE_FIELD_NAME,
+                new ParameterUnwrapper<SpaceLocator, Space>()
+                {
+                    @Override
+                    public Space unwrap(SpaceLocator wrapped)
+                    {
+                        return wrapped.getSpace();
+                    }
+                },
+                // There is no lookup by id
+//                new AbstractConfluenceIdParameterLookup<SpaceLocator>()
+//                {
+//                    @Override
+//                    public SpaceLocator lookup(User user, Long id)
+//                    {
+//                        // TODO: The confluence space service does not check permissions. Need to do that somewhere
+//                        return spaceService.getIdSpaceLocator(id);
+//                    }
+//                },
+                new AbstractConfluenceKeyParameterLookup<SpaceLocator>()
+                {
+                    @Override
+                    public SpaceLocator lookup(User user, String key)
+                    {
+                        // TODO: The confluence space service does not check permissions. Need to do that somewhere
+                        return spaceService.getKeySpaceLocator(key);
+                    }
+                }
+        );
+    }
+
+    @Override
+    public Map<String, Object> serialize(final Space space)
+    {
+        return ImmutableMap.<String, Object>of(SPACE_FIELD_NAME,
+                ImmutableMap.of(ID_FIELD_NAME, space.getId(),
+                        KEY_FIELD_NAME, space.getKey()));
+    }
+
+    @Override
+    protected boolean isResultValid(SpaceLocator serviceResult)
+    {
+        return serviceResult.getSpace() != null;
     }
 }
---
+++ plugin/src/test/java/com/atlassian/plugin/connect/plugin/module/confluence/context/serializer/SpaceSerializerTest.java
@@ -0,0 +1,111 @@
+package com.atlassian.plugin.connect.plugin.module.confluence.context.serializer;
+
+import com.atlassian.confluence.content.service.SpaceService;
+import com.atlassian.confluence.content.service.space.KeySpaceLocator;
+import com.atlassian.confluence.spaces.Space;
+import com.atlassian.confluence.spaces.SpaceManager;
+import com.atlassian.plugin.connect.plugin.module.context.ParameterDeserializer;
+import com.atlassian.user.EntityException;
+import com.atlassian.user.User;
+import com.atlassian.user.UserManager;
+import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableMap;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SpaceSerializerTest
+{
+    @Mock
+    private SpaceService spaceService;
+
+    @Mock
+    private UserManager userManager;
+
+    @Mock
+    private User user;
+
+//    @Mock
+//    private ErrorCollection errorCollection;
+
+    @Mock
+    private Space space1;
+
+    @Mock
+    private SpaceManager spaceManager;
+
+    @Test
+    public void shouldReturnAbsentIfNoSpaceInParams()
+    {
+        final ParameterDeserializer<Space> serializer = new SpaceSerializer(spaceService, userManager);
+        final Optional<Space> space = serializer.deserialize(ImmutableMap.of("blah", new Object()), "fred");
+        assertThat(space.isPresent(), is(false));
+    }
+
+    @Test
+    public void shouldReturnAbsentIfSpaceIsNotMap()
+    {
+        final ParameterDeserializer<Space> serializer = new SpaceSerializer(spaceService, userManager);
+        final Optional<Space> space = serializer.deserialize(ImmutableMap.of("space", new Object()), "fred");
+        assertThat(space.isPresent(), is(false));
+    }
+
+    @Test
+    public void shouldReturnAbsentIfNoIdOrKeyInSpace()
+    {
+        final ParameterDeserializer<Space> serializer = new SpaceSerializer(spaceService, userManager);
+        final Optional<Space> space = serializer.deserialize(
+                ImmutableMap.<String, Object>of("space", ImmutableMap.of("foo", new Object())),
+                "fred");
+        assertThat(space.isPresent(), is(false));
+    }
+
+    @Test
+    public void shouldReturnAbsentIfNoUserForUsername() throws EntityException
+    {
+        final ParameterDeserializer<Space> serializer = new SpaceSerializer(spaceService, userManager);
+        final Optional<Space> space = serializer.deserialize(
+                ImmutableMap.<String, Object>of("space", ImmutableMap.of(
+                        "id", 10,
+                        "key", "mykey")),
+                "fred");
+
+        assertThat(space.isPresent(), is(false));
+        verify(userManager, times(1)).getUser("fred");
+    }
+
+    @Test
+    public void shouldReturnAbsentIfNoSpaceForKey() throws EntityException
+    {
+        when(userManager.getUser("fred")).thenReturn(user);
+        when(spaceService.getKeySpaceLocator("mykey")).thenReturn(new KeySpaceLocator(spaceManager, "mykey"));
+        when(spaceManager.getSpace("mykey")).thenReturn(null);
+
+        final ParameterDeserializer<Space> serializer = new SpaceSerializer(spaceService, userManager);
+        final Optional<Space> space = serializer.deserialize(
+                ImmutableMap.<String, Object>of("space", ImmutableMap.of("key", "mykey")), "fred");
+
+        assertThat(space.isPresent(), is(false));
+        verify(spaceService, times(1)).getKeySpaceLocator("mykey");
+    }
+
+    @Test
+    public void shouldReturnSpaceWhenTheStarsAlign() throws EntityException
+    {
+        when(userManager.getUser("fred")).thenReturn(user);
+        when(spaceService.getKeySpaceLocator("mykey")).thenReturn(new KeySpaceLocator(spaceManager, "mykey"));
+        when(spaceManager.getSpace("mykey")).thenReturn(space1);
+
+        final ParameterDeserializer<Space> serializer = new SpaceSerializer(spaceService, userManager);
+        final Optional<Space> space = serializer.deserialize(
+                ImmutableMap.<String, Object>of("space", ImmutableMap.of("key", "mykey")), "fred");
+
+        assertThat(space.isPresent(), is(true));
+    }
+}
