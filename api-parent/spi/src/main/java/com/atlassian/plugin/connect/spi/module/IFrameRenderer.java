08be56cc740424e0b06f57f7d24ff71e22598004
Anders Holmgren <aholmgren@atlassian.com>
2013-09-27 17:02:04 +1000

    ACDEV-397: Javadocs and tidy

--- api-parent/spi/src/main/java/com/atlassian/plugin/connect/spi/module/IFrameRenderer.java
+++ api-parent/spi/src/main/java/com/atlassian/plugin/connect/spi/module/IFrameRenderer.java
@@ -8,11 +8,46 @@ import java.util.Map;
 
 public interface IFrameRenderer
 {
+    /**
+     * Renders the block container for an iFrame into the containing page
+     * @param iframeContext
+     * @param remoteUser
+     * @return
+     * @throws IOException
+     */
     String render(IFrameContext iframeContext, String remoteUser) throws IOException;
 
+    /**
+     * Renders the block container for an iFrame into the containing page
+     * @param iframeContext
+     * @param extraPath
+     * @param queryParams
+     * @param remoteUser
+     * @return
+     * @throws IOException
+     */
     String render(IFrameContext iframeContext, String extraPath, Map<String, String[]> queryParams, String remoteUser) throws IOException;
 
+    /**
+     * Renders the inline container for an iFrame into the containing page
+     * @param iframeContext
+     * @param extraPath
+     * @param queryParams
+     * @param remoteUser
+     * @return
+     * @throws IOException
+     */
     String renderInline(IFrameContext iframeContext, String extraPath, Map<String,  String[]> queryParams, String remoteUser) throws IOException;
 
+    /**
+     * Renders the content of an iFrame (i.e. what goes inside the iframe)
+     * @param iframeContext
+     * @param pageInfo
+     * @param extraPath
+     * @param queryParams
+     * @param remoteUser
+     * @param writer
+     * @throws IOException
+     */
     void renderPage(IFrameContext iframeContext, PageInfo pageInfo, String extraPath, Map<String, String[]> queryParams, String remoteUser, Writer writer) throws IOException;
 }
