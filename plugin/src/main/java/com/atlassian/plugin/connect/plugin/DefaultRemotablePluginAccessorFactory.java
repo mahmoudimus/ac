94032a3b238242efeb257a1b2ff741f14849f91f
Peter Brownlow <pbrownlow@atlassian.com>
2013-09-30 11:20:24 +1000

    extracted OAuthRemotablePluginAccessor from DefaultRemotablePluginAccessorFactory and OAuthRemotablePluginAccessorTest from DefaultRemotablePluginAccessorFactoryTest

--- plugin/src/main/java/com/atlassian/plugin/connect/plugin/DefaultRemotablePluginAccessorFactory.java
+++ plugin/src/main/java/com/atlassian/plugin/connect/plugin/DefaultRemotablePluginAccessorFactory.java
@@ -5,45 +5,32 @@ import com.atlassian.applinks.api.event.ApplicationLinkAddedEvent;
 import com.atlassian.applinks.api.event.ApplicationLinkDeletedEvent;
 import com.atlassian.event.api.EventListener;
 import com.atlassian.event.api.EventPublisher;
-import com.atlassian.fugue.Option;
 import com.atlassian.oauth.ServiceProvider;
 import com.atlassian.plugin.Plugin;
 import com.atlassian.plugin.PluginAccessor;
 import com.atlassian.plugin.connect.plugin.module.applinks.RemotePluginContainerModuleDescriptor;
 import com.atlassian.plugin.connect.plugin.util.http.CachingHttpContentRetriever;
 import com.atlassian.plugin.connect.spi.ConnectAddOnIdentifierService;
-import com.atlassian.plugin.connect.spi.PermissionDeniedException;
 import com.atlassian.plugin.connect.spi.RemotablePluginAccessor;
 import com.atlassian.plugin.connect.spi.RemotablePluginAccessorFactory;
 import com.atlassian.plugin.connect.spi.applinks.RemotePluginContainerApplicationType;
-import com.atlassian.plugin.connect.spi.http.AuthorizationGenerator;
-import com.atlassian.plugin.connect.spi.http.HttpMethod;
 import com.atlassian.plugin.event.PluginEventListener;
 import com.atlassian.plugin.event.events.PluginDisabledEvent;
 import com.atlassian.plugin.event.events.PluginEnabledEvent;
 import com.atlassian.plugin.event.events.PluginModuleEnabledEvent;
 import com.atlassian.sal.api.ApplicationProperties;
-import com.atlassian.uri.Uri;
-import com.atlassian.uri.UriBuilder;
 import com.atlassian.util.concurrent.CopyOnWriteMap;
 import com.google.common.base.Function;
 import com.google.common.base.Supplier;
 import com.google.common.base.Suppliers;
-import net.oauth.OAuth;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.net.URI;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 
-import static com.atlassian.fugue.Option.option;
 import static com.google.common.base.Preconditions.checkNotNull;
-import static com.google.common.collect.Maps.newHashMap;
-import static com.google.common.collect.Maps.transformValues;
-import static java.util.Collections.singletonList;
 
 @Component
 public final class DefaultRemotablePluginAccessorFactory implements RemotablePluginAccessorFactory, DisposableBean
@@ -224,7 +211,7 @@ public final class DefaultRemotablePluginAccessorFactory implements RemotablePlu
         checkNotNull(plugin, "Plugin not found: '%s'", pluginKey);
 
         // don't need to get the actual provider as it doesn't really matter
-        return new OAuthSigningRemotablePluginAccessor(pluginKey, plugin.getName(), displayUrl, getDummyServiceProvider());
+        return new OAuthSigningRemotablePluginAccessor(pluginKey, plugin.getName(), displayUrl, getDummyServiceProvider(), httpContentRetriever, oAuthLinkManager);
     }
 
     private ServiceProvider getDummyServiceProvider()
@@ -237,88 +224,6 @@ public final class DefaultRemotablePluginAccessorFactory implements RemotablePlu
     public void destroy() throws Exception
     {
         eventPublisher.unregister(this);
-    }
-
-    private class OAuthSigningRemotablePluginAccessor extends DefaultRemotablePluginAccessorBase
-    {
-        private final ServiceProvider serviceProvider;
-
-        private OAuthSigningRemotablePluginAccessor(String key,
-                                                    String name,
-                                                    Supplier<URI> displayUrl,
-                                                    ServiceProvider serviceProvider)
-        {
-            super(key, name, displayUrl, httpContentRetriever);
-            this.serviceProvider = serviceProvider;
-        }
-
-        @Override
-        public String signGetUrl(URI targetPath, Map<String, String[]> params)
-        {
-            return signGetUrlForType(serviceProvider, getTargetUrl(targetPath), params);
-        }
-
-        @Override
-        public AuthorizationGenerator getAuthorizationGenerator()
-        {
-            return new OAuthAuthorizationGenerator(serviceProvider);
-        }
-    }
-
-    private String signGetUrlForType(ServiceProvider serviceProvider, URI targetUrl, Map<String, String[]> params) throws PermissionDeniedException
-    {
-        final UriBuilder uriBuilder = new UriBuilder(Uri.fromJavaUri(targetUrl));
-
-        // adding all the parameters of the signed request
-        for (Map.Entry<String, String> param : signRequest(serviceProvider, targetUrl, params, HttpMethod.GET))
-        {
-            final String value = param.getValue() == null ? "" : param.getValue();
-            uriBuilder.addQueryParameter(param.getKey(), value);
-        }
-        return uriBuilder.toString();
-    }
-
-    private List<Map.Entry<String, String>> signRequest(ServiceProvider serviceProvider,
-                                                        URI url,
-                                                        Map<String, String[]> queryParams,
-                                                        HttpMethod method)
-    {
-        String timestamp = System.currentTimeMillis() / 1000 + "";
-        String nonce = System.nanoTime() + "";
-        String signatureMethod = OAuth.RSA_SHA1;
-        String oauthVersion = "1.0";
-
-        Map<String, List<String>> params = newHashMap(transformValues(queryParams, new Function<String[], List<String>>()
-        {
-            @Override
-            public List<String> apply(String[] from)
-            {
-                return Arrays.asList(from);
-            }
-        }));
-
-        params.put(OAuth.OAUTH_SIGNATURE_METHOD, singletonList(signatureMethod));
-        params.put(OAuth.OAUTH_NONCE, singletonList(nonce));
-        params.put(OAuth.OAUTH_VERSION, singletonList(oauthVersion));
-        params.put(OAuth.OAUTH_TIMESTAMP, singletonList(timestamp));
-
-        return oAuthLinkManager.signAsParameters(serviceProvider, method, url, params);
-    }
-
-    private class OAuthAuthorizationGenerator extends DefaultAuthorizationGeneratorBase
-    {
-        private final ServiceProvider serviceProvider;
-
-        private OAuthAuthorizationGenerator(ServiceProvider serviceProvider)
-        {
-            this.serviceProvider = serviceProvider;
-        }
-
-        @Override
-        public Option<String> generate(HttpMethod method, URI url, Map<String, List<String>> parameters)
-        {
-            return option(oAuthLinkManager.generateAuthorizationHeader(method, serviceProvider, url, parameters));
-        }
     }
 
     private static enum ToUriFunction implements Function<String, URI>
--- plugin/src/test/java/com/atlassian/plugin/connect/plugin/DefaultRemotablePluginAccessorFactoryTest.java
+++ plugin/src/test/java/com/atlassian/plugin/connect/plugin/DefaultRemotablePluginAccessorFactoryTest.java
@@ -4,37 +4,22 @@ import com.atlassian.applinks.api.ApplicationLink;
 import com.atlassian.applinks.api.ApplicationType;
 import com.atlassian.applinks.api.event.ApplicationLinkAddedEvent;
 import com.atlassian.applinks.api.event.ApplicationLinkDeletedEvent;
-import com.atlassian.applinks.spi.auth.AuthenticationConfigurationManager;
 import com.atlassian.event.api.EventPublisher;
-import com.atlassian.httpclient.api.HttpClient;
-import com.atlassian.httpclient.api.Request;
-import com.atlassian.httpclient.api.ResponsePromise;
-import com.atlassian.httpclient.api.ResponseTransformation;
 import com.atlassian.httpclient.api.factory.HttpClientFactory;
-import com.atlassian.httpclient.api.factory.HttpClientOptions;
-import com.atlassian.oauth.ServiceProvider;
-import com.atlassian.oauth.consumer.ConsumerService;
-import com.atlassian.oauth.serviceprovider.ServiceProviderConsumerStore;
 import com.atlassian.plugin.Plugin;
 import com.atlassian.plugin.PluginAccessor;
 import com.atlassian.plugin.connect.plugin.license.LicenseRetriever;
-import com.atlassian.plugin.connect.plugin.license.LicenseStatus;
 import com.atlassian.plugin.connect.plugin.module.applinks.RemotePluginContainerModuleDescriptor;
 import com.atlassian.plugin.connect.plugin.util.LocaleHelper;
 import com.atlassian.plugin.connect.plugin.util.http.CachingHttpContentRetriever;
 import com.atlassian.plugin.connect.spi.ConnectAddOnIdentifierService;
 import com.atlassian.plugin.connect.spi.RemotablePluginAccessor;
 import com.atlassian.plugin.connect.spi.applinks.RemotePluginContainerApplicationType;
-import com.atlassian.plugin.connect.spi.http.HttpMethod;
 import com.atlassian.plugin.event.events.PluginDisabledEvent;
 import com.atlassian.plugin.event.events.PluginEnabledEvent;
 import com.atlassian.plugin.osgi.bridge.external.PluginRetrievalService;
 import com.atlassian.sal.api.ApplicationProperties;
-import com.atlassian.util.concurrent.Promise;
-import com.google.common.base.Function;
 import com.google.common.base.Supplier;
-import com.google.common.collect.Maps;
-import net.oauth.OAuth;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -42,25 +27,17 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.net.URI;
-import java.util.*;
 import java.util.concurrent.ExecutionException;
 
-import static com.google.common.collect.Lists.newArrayList;
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.*;
-import static org.mockito.Mockito.any;
 
 @RunWith(MockitoJUnitRunner.class)
 public class DefaultRemotablePluginAccessorFactoryTest
 {
     private static final String PLUGIN_KEY = "plugin key";
     private static final String PLUGIN_NAME = "plugin name";
-    private static final String GET_FULL_URL = "http://server:1234/contextPath/path?param=param+value&lic=active&loc=whatever";
-    private static final Map<String,String> GET_HEADERS = Collections.singletonMap("header", "header value");
-    private static final Map<String,String> GET_PARAMS = Collections.singletonMap("param", "param value");
-    private static final URI GET_PATH = URI.create("/path");
-    public static final String EXPECTED_GET_RESPONSE = "expected";
     public static final String BASE_URL = "http://server:1234/contextPath";
 
     @Mock private ApplicationLinkAccessor applicationLinkAccessor;
@@ -68,11 +45,10 @@ public class DefaultRemotablePluginAccessorFactoryTest
     @Mock private ApplicationProperties applicationProperties;
     @Mock private EventPublisher eventPublisher;
     @Mock private ConnectAddOnIdentifierService connectIdentifier;
-
-    @Mock LicenseRetriever licenseRetriever;
-    @Mock LocaleHelper localeHelper;
-    @Mock HttpClientFactory httpClientFactory;
-    @Mock PluginRetrievalService pluginRetrievalService;
+    @Mock private OAuthLinkManager oAuthLinkManager;
+    @Mock private LicenseRetriever licenseRetriever;
+    @Mock private LocaleHelper localeHelper;
+    @Mock private PluginRetrievalService pluginRetrievalService;
 
     private DefaultRemotablePluginAccessorFactory factory;
 
@@ -82,69 +58,7 @@ public class DefaultRemotablePluginAccessorFactoryTest
         Plugin plugin = mockPlugin();
         when(pluginAccessor.getPlugin(PLUGIN_KEY)).thenReturn(plugin);
 
-        HttpClient httpClient = mockHttpClient(mockRequest(EXPECTED_GET_RESPONSE));
-        when(httpClientFactory.create(any(HttpClientOptions.class))).thenReturn(httpClient);
-
-        when(licenseRetriever.getLicenseStatus(PLUGIN_KEY)).thenReturn(LicenseStatus.ACTIVE);
-        when(localeHelper.getLocaleTag()).thenReturn("whatever");
-        factory = new DefaultRemotablePluginAccessorFactory(applicationLinkAccessor, mockOAuthLinkManager(), mockCachingHttpContentRetriever(), pluginAccessor, applicationProperties, eventPublisher, connectIdentifier);
-    }
-
-    private OAuthLinkManager mockOAuthLinkManager()
-    {
-        return new MockOAuthLinkManager(mock(ServiceProviderConsumerStore.class), mock(AuthenticationConfigurationManager.class), mock(ConsumerService.class, RETURNS_DEEP_STUBS));
-    }
-
-    private static class MockOAuthLinkManager extends OAuthLinkManager
-    {
-        public MockOAuthLinkManager(ServiceProviderConsumerStore serviceProviderConsumerStore, AuthenticationConfigurationManager authenticationConfigurationManager, ConsumerService consumerService)
-        {
-            super(serviceProviderConsumerStore, authenticationConfigurationManager, consumerService);
-        }
-
-        @Override
-        public List<Map.Entry<String, String>> signAsParameters(ServiceProvider serviceProvider,
-                                                                HttpMethod method,
-                                                                URI url,
-                                                                Map<String, List<String>> originalParams)
-        {
-            Map<String, List<String>> paramsWithPredicatableOAuthValues = new HashMap<String, List<String>>(originalParams.size());
-
-            for (Map.Entry<String, List<String>> param : originalParams.entrySet())
-            {
-                if (OAuth.OAUTH_NONCE.equals(param.getKey()))
-                {
-                    paramsWithPredicatableOAuthValues.put(param.getKey(), Arrays.asList("fake_nonce"));
-                }
-                else if (OAuth.OAUTH_TIMESTAMP.equals(param.getKey()))
-                {
-                    paramsWithPredicatableOAuthValues.put(param.getKey(), Arrays.asList("fake_timestamp"));
-                }
-                else
-                {
-                    paramsWithPredicatableOAuthValues.put(param.getKey(), param.getValue());
-                }
-            }
-
-            return newArrayList(Maps.transformValues(paramsWithPredicatableOAuthValues, new Function<List<String>, String>()
-            {
-                @Override
-                public String apply(List<String> strings)
-                {
-                    // TODO: Doesn't handle multiple values with the same param name
-                    return strings.get(0);
-                }
-            }).entrySet());
-        }
-
-        @Override
-        public String generateAuthorizationHeader(HttpMethod method,
-                                                  ServiceProvider serviceProvider,
-                                                  URI url,
-                                                  Map<String, List<String>> originalParams)
-        {
-            return null; // results in no Authorization header being added, allowing us to assert that headers-in == headers-out
-        }
+        factory = new DefaultRemotablePluginAccessorFactory(applicationLinkAccessor, oAuthLinkManager, mockCachingHttpContentRetriever(), pluginAccessor, applicationProperties, eventPublisher, connectIdentifier);
     }
 
     @Test
@@ -154,41 +68,9 @@ public class DefaultRemotablePluginAccessorFactoryTest
     }
 
     @Test
-    public void createdRemotePluginAccessorHasCorrectPluginKey()
+    public void createdRemotePluginAccessorIsOfTheExpectedType()
     {
-        assertThat(createRemotePluginAccessor().getKey(), is(PLUGIN_KEY));
-    }
-
-    @Test
-    public void createdRemotePluginAccessorHasCorrectPluginName()
-    {
-        assertThat(createRemotePluginAccessor().getName(), is(PLUGIN_NAME));
-    }
-
-    @Test
-    public void createdRemotePluginAccessorCorrectlyCallsTheHttpContentRetriever() throws ExecutionException, InterruptedException
-    {
-        assertThat(createRemotePluginAccessor().executeAsync(HttpMethod.GET, GET_PATH, GET_PARAMS, GET_HEADERS).get(), is(EXPECTED_GET_RESPONSE));
-    }
-
-    @Test
-    public void createdRemotePluginAccessorCorrectlySignsTheRequestUrl()
-    {
-        assertThat(createRemotePluginAccessor().signGetUrl(GET_PATH, Collections.singletonMap("param", new String[]{"value"})),
-                is("http://server:1234/contextPath/path?param=value&oauth_nonce=fake_nonce&oauth_version=1.0&oauth_signature_method=RSA-SHA1&oauth_timestamp=fake_timestamp"));
-    }
-
-    @Test
-    public void createdRemotePluginAccessorHasCorrectBaseUrl()
-    {
-        assertThat(createRemotePluginAccessor().getBaseUrl().toString(), is(BASE_URL));
-    }
-
-    @Test
-    public void createdRemotePluginAccessorCreatesCorrectGetUrl()
-    {
-        // FIXME: using multiple parameter values causes only the first value to be used
-        assertThat(createRemotePluginAccessor().createGetUrl(GET_PATH, Collections.singletonMap("param", new String[]{"value1"})), is("http://server:1234/contextPath/path?param=value1"));
+        assertThat(createRemotePluginAccessor(), is(instanceOf(OAuthSigningRemotablePluginAccessor.class)));
     }
 
     @Test
@@ -291,45 +173,7 @@ public class DefaultRemotablePluginAccessorFactoryTest
 
     private CachingHttpContentRetriever mockCachingHttpContentRetriever()
     {
-        return new CachingHttpContentRetriever(licenseRetriever, localeHelper, httpClientFactory, mock(PluginRetrievalService.class, RETURNS_DEEP_STUBS));
-    }
-
-    private HttpClient mockHttpClient(Request request)
-    {
-        HttpClient httpClient = mock(HttpClient.class, RETURNS_DEEP_STUBS);
-        when(httpClient.newRequest(GET_FULL_URL)).thenReturn(request);
-        return httpClient;
-    }
-
-    private Request mockRequest(String promisedHttpResponse) throws InterruptedException, ExecutionException
-    {
-        Request request = mock(Request.class);
-        {
-            when(request.setHeaders(GET_HEADERS)).thenReturn(request);
-            when(request.setAttributes(any(Map.class))).thenReturn(request);
-            {
-                ResponseTransformation responseTransformation = mockResponseTransformation(promisedHttpResponse);
-                ResponsePromise responsePromise = mock(ResponsePromise.class);
-                when(responsePromise.transform()).thenReturn(responseTransformation);
-                when(request.execute(any(Request.Method.class))).thenReturn(responsePromise);
-            }
-        }
-        return request;
-    }
-
-    private ResponseTransformation mockResponseTransformation(String promisedHttpResponse) throws InterruptedException, ExecutionException
-    {
-        ResponseTransformation responseTransformation = mock(ResponseTransformation.class);
-        when(responseTransformation.ok(any(Function.class))).thenReturn(responseTransformation);
-        when(responseTransformation.forbidden(any(Function.class))).thenReturn(responseTransformation);
-        when(responseTransformation.others(any(Function.class))).thenReturn(responseTransformation);
-        when(responseTransformation.fail(any(Function.class))).thenReturn(responseTransformation);
-        {
-            Promise<String> promise = mock(Promise.class);
-            when(promise.get()).thenReturn(promisedHttpResponse);
-            when(responseTransformation.toPromise()).thenReturn(promise);
-        }
-        return responseTransformation;
+        return new CachingHttpContentRetriever(licenseRetriever, localeHelper, mock(HttpClientFactory.class, RETURNS_DEEP_STUBS), mock(PluginRetrievalService.class, RETURNS_DEEP_STUBS));
     }
 
     private void removePluginAppLink(String pluginKey)
