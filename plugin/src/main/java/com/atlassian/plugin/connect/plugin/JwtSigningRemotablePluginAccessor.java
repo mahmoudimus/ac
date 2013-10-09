1dcf142097c1537d179ad5266328705796e31887
Peter Brownlow <pbrownlow@atlassian.com>
2013-10-09 12:55:51 +1100

    reading jwt expiry window from system property

--- plugin/src/main/java/com/atlassian/plugin/connect/plugin/JwtSigningRemotablePluginAccessor.java
+++ plugin/src/main/java/com/atlassian/plugin/connect/plugin/JwtSigningRemotablePluginAccessor.java
@@ -8,6 +8,7 @@ import com.atlassian.jwt.core.writer.JsonSmartJwtJsonBuilder;
 import com.atlassian.jwt.core.writer.JwtClaimsBuilder;
 import com.atlassian.jwt.httpclient.CanonicalHttpUriRequest;
 import com.atlassian.jwt.writer.JwtJsonBuilder;
+import com.atlassian.plugin.connect.plugin.util.ConfigurationUtils;
 import com.atlassian.plugin.connect.plugin.util.http.HttpContentRetriever;
 import com.atlassian.plugin.connect.spi.http.AuthorizationGenerator;
 import com.atlassian.uri.Uri;
@@ -20,12 +21,22 @@ import java.net.URI;
 import java.security.NoSuchAlgorithmException;
 import java.util.Map;
 
+/**
+ * Constructs and signs outgoing URLs using the JWT protocol.<p>
+ * See {@link JwtService} for more details.<p>
+ * Set the system property {@link #JWT_EXPIRY_SECONDS_PROPERTY} with an integer value to control the size of the expiry window (default is {@link #JWT_EXPIRY_WINDOW_SECONDS_DEFAULT}).
+ */
 public class JwtSigningRemotablePluginAccessor extends DefaultRemotablePluginAccessorBase
 {
     private final JwtService jwtService;
     private final ApplicationLinkAccessor applicationLinkAccessor;
 
-    private static final int JWT_EXPIRY_WINDOW_SECONDS = 60 * 3; // TODO: make a configuration option?
+    private static final String JWT_EXPIRY_SECONDS_PROPERTY = "com.atlassian.connect.jwt.expiry_seconds";
+    /**
+     * Default of 3 minutes.
+     */
+    private static final int JWT_EXPIRY_WINDOW_SECONDS_DEFAULT = 60 * 3;
+    private static final int JWT_EXPIRY_WINDOW_SECONDS = ConfigurationUtils.getIntSystemProperty(JWT_EXPIRY_SECONDS_PROPERTY, JWT_EXPIRY_WINDOW_SECONDS_DEFAULT);
     private static final AuthorizationGenerator AUTH_GENERATOR = new JwtAuthorizationGenerator(); // it's tiny and does very little, so share this instance
 
     public JwtSigningRemotablePluginAccessor(String pluginKey,
