778745c0a8d86e9c45e9c9f4cd941f61e73d8111
Peter Brownlow <pbrownlow@atlassian.com>
2013-10-16 13:10:21 +1100

    ACDEV-447: corrected bean vs builder int vs Integer mismatch ACDEV-447: using JD's new container management to get WebPanelConnectModuleDescriptorFactory dependencies ACDEV-447: added lots more unit tests

--- plugin/src/main/java/com/atlassian/plugin/connect/plugin/capabilities/descriptor/WebPanelConnectModuleDescriptorFactory.java
+++ plugin/src/main/java/com/atlassian/plugin/connect/plugin/capabilities/descriptor/WebPanelConnectModuleDescriptorFactory.java
@@ -1,21 +1,12 @@
 package com.atlassian.plugin.connect.plugin.capabilities.descriptor;
 
-import com.atlassian.plugin.AutowireCapablePlugin;
-import com.atlassian.plugin.ModuleDescriptor;
 import com.atlassian.plugin.Plugin;
-import com.atlassian.plugin.PluginParseException;
 import com.atlassian.plugin.connect.plugin.capabilities.beans.WebPanelCapabilityBean;
-import com.atlassian.plugin.connect.plugin.module.webpanel.IFrameRemoteWebPanel;
-import com.atlassian.plugin.hostcontainer.DefaultHostContainer;
-import com.atlassian.plugin.module.ModuleFactory;
-import com.atlassian.plugin.web.WebInterfaceManager;
-import com.atlassian.plugin.web.descriptors.DefaultWebPanelModuleDescriptor;
+import com.atlassian.plugin.module.ContainerManagedPlugin;
 import com.atlassian.plugin.web.descriptors.WebPanelModuleDescriptor;
 import org.dom4j.Element;
 import org.dom4j.dom.DOMElement;
 import org.osgi.framework.BundleContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import static org.apache.commons.lang.StringEscapeUtils.escapeHtml;
@@ -24,39 +15,11 @@ import static org.apache.commons.lang.StringEscapeUtils.escapeHtml;
 @Component
 public class WebPanelConnectModuleDescriptorFactory implements ConnectModuleDescriptorFactory<WebPanelCapabilityBean,WebPanelModuleDescriptor>
 {
-    private static final Logger log = LoggerFactory.getLogger(WebItemModuleDescriptorFactory.class);
-
     @Override
     public WebPanelModuleDescriptor createModuleDescriptor(Plugin plugin, BundleContext addonBundleContext, WebPanelCapabilityBean bean)
     {
-        Element domElement = createDomElement(bean, bean.getKey());
-        //AlwaysDisplayCondition condition = new AlwaysDisplayCondition(); // bean.getCondition() != null ? bean.getCondition() : new AlwaysDisplayCondition();
-        //IFrameContext iFrameContext = new IFrameContextImpl(plugin.getKey(), bean.getUrl(), bean.getKey(), new IFrameParamsImpl(domElement));
-        //IFrameRenderer iFrameRenderer = new IFrameRendererImpl(templateRenderer, iFrameHost, remotablePluginAccessorFactory, userPreferencesRetriever, licenseRetriever, localeHelper, userManager);
-
-        //final IFrameRemoteWebPanel remoteWebPanel = new IFrameRemoteWebPanel(iFrameRenderer, iFrameContext, condition, contextMapURLSerializer, userManager, new UrlVariableSubstitutor());
-        final IFrameRemoteWebPanel remoteWebPanel = ((AutowireCapablePlugin) plugin).autowire(IFrameRemoteWebPanel.class);
-
-        ModuleFactory moduleFactory = new ModuleFactory()
-        {
-            @Override
-            @SuppressWarnings("unchecked")
-            public <T> T createModule(String name, ModuleDescriptor<T> moduleDescriptor) throws PluginParseException
-            {
-                if (!moduleDescriptor.getModule().getClass().isInstance(remoteWebPanel))
-                {
-                    String message = String.format("Cannot to convert %s to %s", remoteWebPanel, moduleDescriptor.getModule().getClass().getCanonicalName());
-                    log.error(message);
-                    throw new IllegalArgumentException(message);
-                }
-
-                return (T) remoteWebPanel;
-            }
-        };
-
-        WebInterfaceManager webInterfaceManager = ((AutowireCapablePlugin)plugin).autowire(WebInterfaceManager.class); //getService(addonBundleContext, WebInterfaceManager.class);
-        final WebPanelModuleDescriptor descriptor = new DefaultWebPanelModuleDescriptor(new DefaultHostContainer(), moduleFactory, webInterfaceManager);
-        descriptor.init(plugin, domElement);
+        final WebPanelModuleDescriptor descriptor = ((ContainerManagedPlugin)plugin).getContainerAccessor().createBean(WebPanelModuleDescriptor.class);
+        descriptor.init(plugin, createDomElement(bean, bean.getKey()));
         return descriptor;
     }
 
@@ -76,8 +39,6 @@ public class WebPanelConnectModuleDescriptorFactory implements ConnectModuleDesc
                 .setText(escapeHtml(bean.getName().getValue()));
 
         webPanelElement.addAttribute("system", "true");
-
-        webPanelElement.element("resource");
 
         return webPanelElement;
     }
--- plugin/src/test/java/com/atlassian/plugin/connect/plugin/capabilities/descriptor/WebPanelConnectModuleDescriptorFactoryTest.java
+++ plugin/src/test/java/com/atlassian/plugin/connect/plugin/capabilities/descriptor/WebPanelConnectModuleDescriptorFactoryTest.java
@@ -1,33 +1,41 @@
 package com.atlassian.plugin.connect.plugin.capabilities.descriptor;
 
-import com.atlassian.plugin.AutowireCapablePlugin;
 import com.atlassian.plugin.Plugin;
 import com.atlassian.plugin.connect.plugin.capabilities.beans.WebPanelCapabilityBean;
 import com.atlassian.plugin.connect.plugin.capabilities.beans.WebPanelLayout;
 import com.atlassian.plugin.connect.plugin.capabilities.beans.nested.I18nProperty;
-import com.atlassian.plugin.web.WebInterfaceManager;
+import com.atlassian.plugin.module.ContainerAccessor;
+import com.atlassian.plugin.module.ContainerManagedPlugin;
 import com.atlassian.plugin.web.descriptors.WebPanelModuleDescriptor;
+import com.google.common.base.Objects;
+import org.dom4j.Attribute;
+import org.dom4j.Element;
+import org.hamcrest.Description;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatcher;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.osgi.framework.BundleContext;
 
 import static com.atlassian.plugin.connect.plugin.capabilities.beans.WebPanelCapabilityBean.newWebPanelBean;
+import static com.google.common.base.Preconditions.checkNotNull;
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Matchers.argThat;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class WebPanelConnectModuleDescriptorFactoryTest
 {
-    private interface PluginForTests extends Plugin, AutowireCapablePlugin{};
+    private interface PluginForTests extends Plugin, ContainerManagedPlugin {}
 
-    private WebPanelModuleDescriptor descriptor;
     @Mock private PluginForTests plugin;
-    @Mock private WebInterfaceManager webInterfaceManager;
+    @Mock private ContainerAccessor containerAccessor;
+    @Mock private WebPanelModuleDescriptor descriptor;
 
     @Before
     public void beforeEachTest()
@@ -35,8 +43,8 @@ public class WebPanelConnectModuleDescriptorFactoryTest
         WebPanelConnectModuleDescriptorFactory webPanelFactory = new WebPanelConnectModuleDescriptorFactory();
         when(plugin.getKey()).thenReturn("my-key");
         when(plugin.getName()).thenReturn("My Plugin");
-        when(plugin.autowire(WebInterfaceManager.class)).thenReturn(webInterfaceManager);
-        //when(webFragmentHelper.loadCondition(anyString(), any(Plugin.class))).thenReturn(new DynamicMarkerCondition());
+        when(plugin.getContainerAccessor()).thenReturn(containerAccessor);
+        when(containerAccessor.createBean(WebPanelModuleDescriptor.class)).thenReturn(descriptor);
 
         WebPanelCapabilityBean bean = newWebPanelBean()
                 .withName(new I18nProperty("My Web Panel", "my.webpanel"))
@@ -46,37 +54,199 @@ public class WebPanelConnectModuleDescriptorFactoryTest
                 .withWeight(50)
                 .build();
 
-        descriptor = webPanelFactory.createModuleDescriptor(plugin, mock(BundleContext.class), bean);
-        descriptor.enabled();
+        webPanelFactory.createModuleDescriptor(plugin, mock(BundleContext.class), bean);
     }
 
-    // TODO: test these:
-    // url
-    // width
-    // height
-    // condition
+    // TODO: test condition
 
     @Test
-    public void completeKeyIsCorrect() throws Exception
+    public void keyIsCorrect() throws Exception
     {
-        assertThat(descriptor.getCompleteKey(), is("my-key:my-web-panel"));
+        verify(descriptor).init(eq(plugin), argThat(hasAttribute("key", withValue("my-web-panel"))));
     }
 
     @Test
     public void locationIsCorrect()
     {
-        assertThat(descriptor.getLocation(), is("atl.admin/menu"));
+        verify(descriptor).init(eq(plugin), argThat(hasAttribute("location", withValue("atl.admin/menu"))));
     }
 
     @Test
-    public void i18nNameKeyIsCorrect()
+    public void widthIsCorrect()
     {
-        assertThat(descriptor.getI18nNameKey(), is("my.webpanel"));
+        verify(descriptor).init(eq(plugin), argThat(hasAttribute("width", withValue("10px"))));
+    }
+
+    @Test
+    public void heightIsCorrect()
+    {
+        verify(descriptor).init(eq(plugin), argThat(hasAttribute("height", withValue("100%"))));
     }
 
     @Test
     public void weightIsCorrect()
     {
-        assertThat(descriptor.getWeight(), is(50));
+        verify(descriptor).init(eq(plugin), argThat(hasAttribute("weight", withValue("50"))));
+    }
+
+    @Test
+    public void urlIsCorrect()
+    {
+        verify(descriptor).init(eq(plugin), argThat(hasAttribute("url", withValue("http://www.google.com"))));
+    }
+
+    @Test
+    public void isEnabled()
+    {
+        verify(descriptor).init(eq(plugin), argThat(hasAttribute("state", withValue("enabled"))));
+    }
+
+    @Test
+    public void isSystem()
+    {
+        verify(descriptor).init(eq(plugin), argThat(hasAttribute("system", withValue("true"))));
+    }
+
+    @Test
+    public void i18nKeyIsCorrect()
+    {
+        verify(descriptor).init(eq(plugin), argThat(hasElement("label", withAttribute("key", withValue("my.webpanel")))));
+    }
+
+    @Test
+    public void i18TextIsCorrect()
+    {
+        verify(descriptor).init(eq(plugin), argThat(hasElement("label", withText("My Web Panel"))));
+    }
+
+    private static ArgumentMatcher<Element> hasElement(String name, ArgumentMatcher<Element> expectedValueMatcher)
+    {
+        return new SubElementMatcher(name, expectedValueMatcher);
+    }
+
+    private static ArgumentMatcher<Element> hasAttribute(String name, ArgumentMatcher<Attribute> expectedValueMatcher)
+    {
+        return withAttribute(name, expectedValueMatcher); // just a synonym to make test assertions read as natural English
+    }
+
+    private static ArgumentMatcher<Element> withAttribute(String name, ArgumentMatcher<Attribute> expectedValueMatcher)
+    {
+        return new ElementAttributeMatcher(name, expectedValueMatcher);
+    }
+
+    private static ArgumentMatcher<Attribute> withValue(String expectedValue)
+    {
+        return new AttributeValueMatcher(expectedValue);
+    }
+
+    private static ArgumentMatcher<Element> withText(String expectedValue)
+    {
+        return new ElementTextMatcher(expectedValue);
+    }
+
+    private static class AttributeValueMatcher extends ArgumentMatcher<Attribute>
+    {
+        private final String expectedValue;
+
+        private AttributeValueMatcher(String expectedValue)
+        {
+            this.expectedValue = checkNotNull(expectedValue);
+        }
+
+        @Override
+        public boolean matches(Object argument)
+        {
+            assertThat(argument, is(instanceOf(Attribute.class)));
+            return Objects.equal(expectedValue, ((Attribute)argument).getValue());
+        }
+
+        @Override
+        public void describeTo(Description description)
+        {
+            description.appendText("Attribute with value ");
+            description.appendValue(expectedValue);
+        }
+    }
+
+    private static class ElementTextMatcher extends ArgumentMatcher<Element>
+    {
+        private final String expectedValue;
+
+        private ElementTextMatcher(String expectedValue)
+        {
+            this.expectedValue = checkNotNull(expectedValue);
+        }
+
+        @Override
+        public boolean matches(Object argument)
+        {
+            assertThat(argument, is(instanceOf(Element.class)));
+            return Objects.equal(expectedValue, ((Element)argument).getText());
+        }
+
+        @Override
+        public void describeTo(Description description)
+        {
+            description.appendText("Attribute with text ");
+            description.appendValue(expectedValue);
+        }
+    }
+
+    private static class SubElementMatcher extends ArgumentMatcher<Element>
+    {
+        private final String name;
+        private final ArgumentMatcher<Element> expectedValueMatcher;
+
+        private SubElementMatcher(String name, ArgumentMatcher<Element> expectedValueMatcher)
+        {
+            this.name = checkNotNull(name);
+            this.expectedValueMatcher = checkNotNull(expectedValueMatcher);
+        }
+
+        @Override
+        public boolean matches(Object argument)
+        {
+            assertThat(argument, is(instanceOf(Element.class)));
+            return expectedValueMatcher.matches(((Element)argument).element(name));
+        }
+
+        @Override
+        public void describeTo(Description description)
+        {
+            description.appendText("Element with child Element ");
+            description.appendValue(name);
+            description.appendText(" { ");
+            expectedValueMatcher.describeTo(description);
+            description.appendText(" } ");
+        }
+    }
+
+    private static class ElementAttributeMatcher extends ArgumentMatcher<Element>
+    {
+        private final String name;
+        private final ArgumentMatcher<Attribute> expectedValueMatcher;
+
+        private ElementAttributeMatcher(String name, ArgumentMatcher<Attribute> expectedValueMatcher)
+        {
+            this.name = checkNotNull(name);
+            this.expectedValueMatcher = checkNotNull(expectedValueMatcher);
+        }
+
+        @Override
+        public boolean matches(Object argument)
+        {
+            assertThat(argument, is(instanceOf(Element.class)));
+            return expectedValueMatcher.matches(((Element) argument).attribute(name));
+        }
+
+        @Override
+        public void describeTo(Description description)
+        {
+            description.appendText("Element with attribute ");
+            description.appendValue(name);
+            description.appendText(" { ");
+            expectedValueMatcher.describeTo(description);
+            description.appendText(" } ");
+        }
     }
 }
