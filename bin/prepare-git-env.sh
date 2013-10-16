82258da2f3844d50638fb05dbd6ebb7a6fbd02f9
Seb Ruiz <sruiz@atlassian.com>
2013-10-16 17:24:12 +1100

    ACDEV-448: Alterantive method to set committer name/email

--- bin/prepare-git-env.sh
+++ bin/prepare-git-env.sh
@@ -7,8 +7,11 @@ echo "Setting git committer env"
 echo "Author: $AUTHOR"
 echo "Email:  $EMAIL"
 
-export GIT_AUTHOR_EMAIL=$EMAIL
-export GIT_AUTHOR_NAME=$AUTHOR
-export GIT_COMMITTER_EMAIL=$EMAIL
-export GIT_COMMITTER_NAME=$AUTHOR
+git config --local --replace-all user.name "$AUTHOR"
+git config --local --replace-all user.email "$EMAIL"
+
+#export GIT_AUTHOR_EMAIL=$EMAIL
+#export GIT_AUTHOR_NAME=$AUTHOR
+#export GIT_COMMITTER_EMAIL=$EMAIL
+#export GIT_COMMITTER_NAME=$AUTHOR
 
