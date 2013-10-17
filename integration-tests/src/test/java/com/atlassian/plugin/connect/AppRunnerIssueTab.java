0e239ceae97599a3acaf086b4af45cde42e7b597
Anders Holmgren <aholmgren@atlassian.com>
2013-10-17 11:35:39 +1100

    ACDEV-444 (IssueTabPage Capability) removed extra capabilities from test

--- integration-tests/src/test/java/com/atlassian/plugin/connect/AppRunnerIssueTab.java
+++ integration-tests/src/test/java/com/atlassian/plugin/connect/AppRunnerIssueTab.java
@@ -4,45 +4,18 @@ import com.atlassian.plugin.connect.plugin.capabilities.beans.ConnectIssueTabPan
 import com.atlassian.plugin.connect.plugin.capabilities.beans.nested.I18nProperty;
 import com.atlassian.plugin.connect.test.server.ConnectCapabilitiesRunner;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-import static com.atlassian.plugin.connect.plugin.capabilities.beans.WebItemCapabilityBean.newWebItemBean;
-
 /**
  * @since version
  */
 public class AppRunnerIssueTab
 {
     public static final String JIRA = "http://localhost:2990/jira";
-    public static final String CONFLUENCE = "http://localhost:1990/confluence";
 
     public static void main(String[] args)
     {
         try
         {
             ConnectCapabilitiesRunner remotePlugin = new ConnectCapabilitiesRunner(JIRA,"my-plugin")
-                    .addCapability(newWebItemBean()
-                            .withName(new I18nProperty("AC General Web Item", "ac.gen"))
-                            .withLocation("system.top.navigation.bar")
-                            .withWeight(1)
-                            .withLink("/irwi")
-                            .build())
-                    .addCapability(newWebItemBean()
-                            .withName(new I18nProperty("Quick project link", "ac.qp"))
-                            .withLocation("system.top.navigation.bar")
-                            .withWeight(1)
-                            .withLink(JIRA + "/browse/ACDEV-1234")
-                            .build())
-                    .addCapability(newWebItemBean()
-                            .withName(new I18nProperty("google link","ac.gl"))
-                            .withLocation("system.top.navigation.bar")
-                            .withWeight(1)
-                            .withLink("http://www.google.com")
-                            .build())
                     .addCapability(ConnectIssueTabPanelCapabilityBean.newIssueTabPageBean()
                             .withName(new I18nProperty("My Tab","ac.mytab"))
                             .withWeight(1)
@@ -60,14 +33,4 @@ public class AppRunnerIssueTab
         }
     }
 
-    private static final class MessageServlet extends HttpServlet
-    {
-        @Override
-        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
-        {
-            resp.setContentType("text/plain");
-            resp.getWriter().write(req.getParameter("message"));
-            resp.getWriter().close();
-        }
-    }
 }
