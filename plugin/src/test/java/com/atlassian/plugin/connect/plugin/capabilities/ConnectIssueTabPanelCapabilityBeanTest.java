607ccaf7f81091cfd01fb09b5b82ee2c76147195
Anders Holmgren <aholmgren@atlassian.com>
2013-10-17 16:53:12 +1100

    ACDEV-444 (IssueTabPage Capability) cleaned up test

--- plugin/src/test/java/com/atlassian/plugin/connect/plugin/capabilities/ConnectIssueTabPanelCapabilityBeanTest.java
+++ plugin/src/test/java/com/atlassian/plugin/connect/plugin/capabilities/ConnectIssueTabPanelCapabilityBeanTest.java
@@ -1,7 +1,6 @@
 package com.atlassian.plugin.connect.plugin.capabilities;
 
 import com.atlassian.plugin.connect.plugin.capabilities.beans.ConnectAddonBean;
-import com.atlassian.plugin.connect.plugin.capabilities.beans.ConnectIssueTabPanelCapabilityBean;
 import com.atlassian.plugin.connect.plugin.capabilities.beans.nested.I18nProperty;
 import com.atlassian.plugin.connect.plugin.capabilities.gson.CapabilitiesGsonFactory;
 import com.google.gson.Gson;
@@ -15,30 +14,17 @@ import static com.atlassian.plugin.connect.plugin.capabilities.beans.ConnectIssu
 import static com.atlassian.plugin.connect.plugin.capabilities.beans.RemoteContainerCapabilityBean.newRemoteContainerBean;
 import static com.atlassian.plugin.connect.plugin.capabilities.beans.nested.OAuthBean.newOAuthBean;
 import static com.atlassian.plugin.connect.plugin.capabilities.beans.nested.VendorBean.newVendorBean;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 /**
  * @since version
  */
-public class JunkIssueTabTest
+public class ConnectIssueTabPanelCapabilityBeanTest
 {
     @Test
-    public void testName() throws Exception
-    {
-        ConnectIssueTabPanelCapabilityBean bean = newIssueTabPageBean()
-                .withName(new I18nProperty("My Issue Tab Page", "my.issueTabPage"))
-                .withUrl("/my-general-page")
-                .withWeight(100)
-                .build();
-
-        Gson gson = CapabilitiesGsonFactory.getGson();
-
-        String json = gson.toJson(bean,ConnectIssueTabPanelCapabilityBean.class);
-
-        System.out.println(json);
-    }
-
-    @Test
-    public void testAddon() throws Exception
+    public void producesCorrectJSON() throws Exception
     {
         Map<String,String> links = new HashMap<String,String>();
         links.put("self","http://www.example.com/capabilities");
@@ -64,6 +50,12 @@ public class JunkIssueTabTest
 
         String json = gson.toJson(addon,ConnectAddonBean.class);
 
-        System.out.println(json);
+        assertThat(json, is(equalTo("{\"key\":\"my-plugin\",\"name\":\"My Plugin\",\"version\":\"1.0\",\"description\":" +
+                "\"\",\"vendor\":{\"name\":\"Atlassian\",\"url\":\"http://www.atlassian.com\"},\"links\":{" +
+                "\"self\":\"http://www.example.com/capabilities\",\"homepage\":\"http://www.example.com\"}," +
+                "\"capabilities\":{\"connect-container\":{\"displayUrl\":\"http://www.example.com\",\"oauth\":{" +
+                "\"publicKey\":\"S0m3Publ1cK3y\",\"callback\":\"\",\"requestTokenUrl\":\"\",\"accessTokenUrl\":\"\"," +
+                "\"authorizeUrl\":\"\"}},\"issueTabPanels\":{\"url\":\"/my-general-page\",\"weight\":100,\"name\":" +
+                "{\"value\":\"My Issue Tab Page\",\"i18n\":\"my.issueTabPage\"}}}}")));
     }
 }
