03666a3ca6a6d6954d81dc3394742e40946c6ea1
Anders Holmgren <aholmgren@atlassian.com>
2013-10-18 14:27:20 +1100

    ACDEV-449 (IssueTabPanel Capability): tidied (cherry picked from commit 8829c4b)

--- plugin/src/main/java/com/atlassian/plugin/connect/plugin/capabilities/provider/AbstractConnectTabPanelModuleProvider.java
+++ plugin/src/main/java/com/atlassian/plugin/connect/plugin/capabilities/provider/AbstractConnectTabPanelModuleProvider.java
@@ -4,10 +4,9 @@ import com.atlassian.plugin.ModuleDescriptor;
 import com.atlassian.plugin.Plugin;
 import com.atlassian.plugin.connect.plugin.capabilities.beans.AbstractConnectTabPanelCapabilityBean;
 import com.atlassian.plugin.connect.plugin.capabilities.descriptor.AbstractConnectTabPanelModuleDescriptorFactory;
+import com.google.common.collect.ImmutableList;
 import org.osgi.framework.BundleContext;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 public abstract class AbstractConnectTabPanelModuleProvider<B extends AbstractConnectTabPanelCapabilityBean,
@@ -23,30 +22,16 @@ public abstract class AbstractConnectTabPanelModuleProvider<B extends AbstractCo
     @Override
     public List<ModuleDescriptor> provideModules(Plugin plugin, BundleContext addonBundleContext, List<B> beans)
     {
-        List<ModuleDescriptor> descriptors = new ArrayList<ModuleDescriptor>();
+        ImmutableList.Builder<ModuleDescriptor> builder = ImmutableList.builder();
 
         for (B bean : beans)
         {
-            descriptors.addAll(beanToDescriptors(plugin,addonBundleContext, bean));
+            builder.add(moduleFactory.createModuleDescriptor(plugin, addonBundleContext, createCapabilityBean(bean)));
         }
 
-        return descriptors;
-    }
-
-    private Collection<? extends ModuleDescriptor> beanToDescriptors(Plugin plugin, BundleContext addonBundleContext, B bean)
-    {
-        List<ModuleDescriptor> descriptors = new ArrayList<ModuleDescriptor>();
-
-        B newBean = createCapabilityBean(bean);
-        descriptors.add(createModuleDescriptor(plugin, addonBundleContext, newBean));
-
-        return descriptors;
+        return builder.build();
     }
 
     protected abstract B createCapabilityBean(B bean);
 
-    private ModuleDescriptor createModuleDescriptor(Plugin plugin, BundleContext addonBundleContext, B newBean)
-    {
-        return moduleFactory.createModuleDescriptor(plugin, addonBundleContext, newBean);
-    }
 }
