03d47b643ed5d02156559e795c0db744ab61cfeb
Anders Holmgren <aholmgren@atlassian.com>
2013-10-23 09:42:09 +1100

    ACDEV-471 (ComponentTabPanel Capability): post review changes

--- plugin/src/test/java/com/atlassian/plugin/connect/plugin/capabilities/descriptor/ConnectComponentTabPanelModuleDescriptorFactoryTest.java
+++ plugin/src/test/java/com/atlassian/plugin/connect/plugin/capabilities/descriptor/ConnectComponentTabPanelModuleDescriptorFactoryTest.java
@@ -2,9 +2,10 @@ package com.atlassian.plugin.connect.plugin.capabilities.descriptor;
 
 import com.atlassian.jira.plugin.componentpanel.ComponentTabPanel;
 import com.atlassian.plugin.connect.plugin.capabilities.beans.AbstractConnectTabPanelCapabilityBean;
-import com.atlassian.plugin.connect.plugin.capabilities.beans.ConnectComponentTabPanelCapabilityBean;
 import com.atlassian.plugin.connect.plugin.capabilities.beans.nested.I18nProperty;
 import com.atlassian.plugin.connect.plugin.capabilities.util.ConnectAutowireUtil;
+
+import static com.atlassian.plugin.connect.plugin.capabilities.beans.ConnectComponentTabPanelCapabilityBean.newComponentTabPanelBean;
 
 
 public class ConnectComponentTabPanelModuleDescriptorFactoryTest extends AbstractConnectTabPanelModuleDescriptorFactoryTest
@@ -21,14 +22,12 @@ public class ConnectComponentTabPanelModuleDescriptorFactoryTest extends Abstrac
     }
 
     @Override
-    protected AbstractConnectTabPanelCapabilityBean createCapabilityBean()
+    protected AbstractConnectTabPanelCapabilityBean createCapabilityBean(String name, String i18NameKey, String url, int weight)
     {
-        return ConnectComponentTabPanelCapabilityBean.newComponentTabPanelBean()
-                .withName(new I18nProperty("My Tab Page", "my.tabpage"))
-                .withUrl("http://www.google.com")
-                .withWeight(99)
+        return newComponentTabPanelBean()
+                .withName(new I18nProperty(name, i18NameKey))
+                .withUrl(url)
+                .withWeight(weight)
                 .build();
     }
-
-
 }
