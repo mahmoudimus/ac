a221e3c3e69c39906d603a9bbcb4e7ba1b93e219
Peter Brownlow <pbrownlow@atlassian.com>
2013-10-23 10:50:52 +1100

    ACDEV-480: refactored WebItemModuleDescriptorFactoryTest to have less code and one assertion per test method

--- plugin/src/test/java/com/atlassian/plugin/connect/plugin/capabilities/descriptor/WebItemModuleDescriptorFactoryTest.java
+++ plugin/src/test/java/com/atlassian/plugin/connect/plugin/capabilities/descriptor/WebItemModuleDescriptorFactoryTest.java
@@ -1,25 +1,27 @@
 package com.atlassian.plugin.connect.plugin.capabilities.descriptor;
 
-import java.util.HashMap;
-
-import javax.servlet.http.HttpServletRequest;
-
 import com.atlassian.plugin.Plugin;
-import com.atlassian.plugin.connect.plugin.capabilities.beans.nested.I18nProperty;
 import com.atlassian.plugin.connect.plugin.capabilities.beans.WebItemCapabilityBean;
+import com.atlassian.plugin.connect.plugin.capabilities.beans.nested.I18nProperty;
 import com.atlassian.plugin.connect.plugin.capabilities.testobjects.PluginForTests;
 import com.atlassian.plugin.connect.plugin.capabilities.testobjects.RemotablePluginAccessorFactoryForTests;
 import com.atlassian.plugin.connect.plugin.capabilities.testobjects.descriptor.WebItemModuleDescriptorFactoryForTests;
 import com.atlassian.plugin.connect.spi.module.DynamicMarkerCondition;
 import com.atlassian.plugin.web.WebFragmentHelper;
 import com.atlassian.plugin.web.WebInterfaceManager;
+import com.atlassian.plugin.web.conditions.ConditionLoadingException;
 import com.atlassian.plugin.web.descriptors.WebItemModuleDescriptor;
-
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.invocation.InvocationOnMock;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.mockito.stubbing.Answer;
 import org.osgi.framework.BundleContext;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
 
 import static com.atlassian.plugin.connect.plugin.capabilities.beans.WebItemCapabilityBean.newWebItemBean;
 import static org.junit.Assert.assertEquals;
@@ -28,26 +30,20 @@ import static org.mockito.Matchers.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-/**
- * @since version
- */
+@RunWith(MockitoJUnitRunner.class)
 public class WebItemModuleDescriptorFactoryTest
 {
-    Plugin plugin;
-    WebInterfaceManager webInterfaceManager;
-    WebFragmentHelper webFragmentHelper;
-    WebItemModuleDescriptorFactory webItemFactory;
-    HttpServletRequest servletRequest;
-    
-    @Before
-    public void setup()
-    {
-        plugin = new PluginForTests("my-key", "My Plugin");
+    @Mock private WebInterfaceManager webInterfaceManager;
+    @Mock private WebFragmentHelper webFragmentHelper;
+    @Mock private HttpServletRequest servletRequest;
+    private WebItemModuleDescriptor descriptor;
 
-        webInterfaceManager = mock(WebInterfaceManager.class);
-        webFragmentHelper = mock(WebFragmentHelper.class);
-        webItemFactory = new WebItemModuleDescriptorFactory(new WebItemModuleDescriptorFactoryForTests(webInterfaceManager), new IconModuleFragmentFactory(new RemotablePluginAccessorFactoryForTests()));
-        servletRequest = mock(HttpServletRequest.class);
+    @Before
+    public void setup() throws ConditionLoadingException
+    {
+        Plugin plugin = new PluginForTests("my-key", "My Plugin");
+
+        WebItemModuleDescriptorFactory webItemFactory = new WebItemModuleDescriptorFactory(new WebItemModuleDescriptorFactoryForTests(webInterfaceManager), new IconModuleFragmentFactory(new RemotablePluginAccessorFactoryForTests()));
 
         when(servletRequest.getContextPath()).thenReturn("http://ondemand.com/jira");
         
@@ -63,28 +59,52 @@ public class WebItemModuleDescriptorFactoryTest
                     }
                 }
         );
-    }
-    
-    @Test
-    public void simpleDescriptorCreation() throws Exception
-    {
-        
+
         when(webFragmentHelper.loadCondition(anyString(), any(Plugin.class))).thenReturn(new DynamicMarkerCondition());
-        
+
         WebItemCapabilityBean bean = newWebItemBean()
                 .withName(new I18nProperty("My Web Item", "my.webitem"))
                 .withLink("http://www.google.com")
                 .withLocation("atl.admin/menu")
                 .build();
 
-        WebItemModuleDescriptor descriptor = webItemFactory.createModuleDescriptor(plugin, mock(BundleContext.class), bean);
-        descriptor.enabled();
-        
+        this.descriptor = webItemFactory.createModuleDescriptor(plugin, mock(BundleContext.class), bean);
+        this.descriptor.enabled();
+    }
+    
+    @Test
+    public void completeKeyIsCorrect()
+    {
         assertEquals("my-key:my-web-item", descriptor.getCompleteKey());
+    }
+
+    @Test
+    public void setionIsCorrect()
+    {
         assertEquals("atl.admin/menu", descriptor.getSection());
+    }
+
+    @Test
+    public void urlIsCorrect()
+    {
         assertEquals("http://www.google.com", descriptor.getLink().getDisplayableUrl(mock(HttpServletRequest.class), new HashMap<String, Object>()));
+    }
+
+    @Test
+    public void weightIsCorrect()
+    {
         assertEquals(100, descriptor.getWeight());
+    }
+
+    @Test
+    public void iconIsCorrect()
+    {
         assertNull(descriptor.getIcon());
+    }
+
+    @Test
+    public void styleClassIsCorrect()
+    {
         assertEquals("",descriptor.getStyleClass());
     }
 }
