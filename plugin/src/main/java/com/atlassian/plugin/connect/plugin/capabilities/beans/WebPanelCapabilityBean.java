a95f25da6350558f6cda59820aab61f29a6203ea
Steve Hetland <shetland@atlassian.com>
2013-11-01 19:14:43 +0100

    typo

--- plugin/src/main/java/com/atlassian/plugin/connect/plugin/capabilities/beans/WebPanelCapabilityBean.java
+++ plugin/src/main/java/com/atlassian/plugin/connect/plugin/capabilities/beans/WebPanelCapabilityBean.java
@@ -27,7 +27,7 @@ public class WebPanelCapabilityBean extends NameToKeyBean
     private String url;
     /**
      *  Determines the order in which web panels appear. Web panels are displayed top to bottom or left to right in
-     *  order of ascending weight. The 'lightest' weight is displayed first, while the 'heaviest' weights sink to the
+     *  order of ascending weight. The "lightest" weight is displayed first, while the "heaviest" weights sink to the
      *  bottom. The weight values for most panels start from 100, and the weights for the links
      *  generally start from 10. The weight is incremented by 10 for each in sequence to leave room for custom
      *  panels.
