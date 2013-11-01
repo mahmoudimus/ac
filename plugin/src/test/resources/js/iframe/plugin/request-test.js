4c7ef9e4bf91fe6e95e23ef3130d5f1ba9a78b4e
Chris Whittington <cwhittington@atlassian.com>
2013-11-01 13:03:45 +1100

    ACDEV-487 add tests for failure callbacks, simpler function checker, indentation

--- plugin/src/test/resources/js/iframe/plugin/request-test.js
+++ plugin/src/test/resources/js/iframe/plugin/request-test.js
@@ -26,11 +26,10 @@
         _rpc.init();
         module("Request Plugin", {
             teardown: function () {
-                    xdmMock.request.reset();
+                xdmMock.request.reset();
             },
             isFunction: function(functionToCheck) {
-                var getType = {};
-                return functionToCheck && getType.toString.call(functionToCheck) === '[object Function]';
+                return functionToCheck && (functionToCheck instanceof Function);
             },
             invokeSuccessRequestResponse: function(args){
                 return xdmMock.request.args[0][1]( args );
@@ -64,12 +63,6 @@
             ok(this.isFunction(xdmMock.request.args[0][2]));
         });
 
-        test("host request is passed fail callback", function () {
-            request('/foo/bar');
-
-            ok(this.isFunction(xdmMock.request.args[0][2]));
-        });
-
         test("custom success callback passed", function () {
             var successSpy = sinon.spy();
 
@@ -90,7 +83,7 @@
             ok(failureSpy.calledOnce, 'error callback is executed on failure');
         });
 
-        test("callback getAllResponseHeaders returns a formatted string of headers", function () {
+        test("success callback getAllResponseHeaders returns a formatted string of headers", function () {
             var successSpy = sinon.spy(),
             successHeaders = {
                 abc: 123
@@ -99,11 +92,10 @@
             request('/foo/bar', {success: successSpy});
             //execute success response from host
             this.invokeSuccessRequestResponse( [{}, {}, {headers: successHeaders}] );
-
             equal(successSpy.args[0][2].getAllResponseHeaders(), 'abc: 123', 'all success headers are returned');
         });
 
-        test("callback getResponseHeader returns the response header", function () {
+        test("success callback getResponseHeader returns the response header", function () {
             var successSpy = sinon.spy(),
             successHeaders = {
                 abc: 123
@@ -114,6 +106,33 @@
             this.invokeSuccessRequestResponse( [{}, {}, {headers: successHeaders}] );
 
             equal(successSpy.args[0][2].getResponseHeader('abc'), 123, 'correct value is returned for header');
+
+        });
+
+        test("error callback getAllResponseHeaders returns a formatted string of headers", function () {
+            var errorSpy = sinon.spy(),
+            errorHeaders = {
+                abc: 123
+            };
+
+            request('/foo/bar', {error: errorSpy});
+            //execute error response from host
+            this.invokeErrorRequestResponse( [{headers: errorHeaders}, {}, {}] );
+
+            equal(errorSpy.args[0][0].getAllResponseHeaders(), 'abc: 123', 'all error headers are returned');
+        });
+
+        test("error callback getResponseHeader returns the response header", function () {
+            var errorSpy = sinon.spy(),
+            errorHeaders = {
+                abc: 123
+            };
+
+            request('/foo/bar', {error: errorSpy});
+            //execute error response from host
+            this.invokeErrorRequestResponse( [{headers: errorHeaders}, {}, {}] );
+
+            equal(errorSpy.args[0][0].getResponseHeader('abc'), 123, 'correct value is returned for header');
 
         });
 
