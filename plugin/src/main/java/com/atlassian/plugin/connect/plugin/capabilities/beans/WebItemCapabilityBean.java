383ab04f6cd35be451f27b0212f4c5abfec7315d
Seb Ruiz <me@sebruiz.net>
2013-11-05 10:10:13 +1100

    ACDEV-499: Add <p/> tags

--- plugin/src/main/java/com/atlassian/plugin/connect/plugin/capabilities/beans/WebItemCapabilityBean.java
+++ plugin/src/main/java/com/atlassian/plugin/connect/plugin/capabilities/beans/WebItemCapabilityBean.java
@@ -13,12 +13,18 @@ import java.util.List;
  * that’s inserted into some standard place in the Atlassian application interface, such as the
  * administration menu.
  *
+ * <p/>
+ *
  * The form that the link takes can vary depending on the location. For instance, a web item in the header bar
  * (with a location section of system.top.navigation.bar) adds a link to the navigation bar across the top of the
  * interface. On the other hand, a web item in the opsbar-operation location section in JIRA adds an item to the issue
  * operation buttons.
  *
+ * <p/>
+ *
  * A web item link can open a new page in the application or a dialog, depending on your configuration.
+ *
+ * <p/>
  *
  * Web links are a simple and useful way to extend Atlassian applications. If you want to extend an Atlassian
  * application and don't know where to start, a web item may be all you need.
