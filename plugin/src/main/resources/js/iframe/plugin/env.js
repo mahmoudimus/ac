8de78e045bb780ce4319a627e5687ee79a4ab33b
Chris Whittington <cwhittington@atlassian.com>
2013-11-06 12:41:03 +1100

    ACDEV-489 clarify fix for IE8

--- plugin/src/main/resources/js/iframe/plugin/env.js
+++ plugin/src/main/resources/js/iframe/plugin/env.js
@@ -75,9 +75,11 @@ AP.define("env", ["_dollar", "_rpc"], function ($, rpc) {
   return $.extend(apis, {
 
     meta: function (name) {
+      //IE8 fallback: querySelectorAll will never find nodes by name.
       if(navigator.userAgent.indexOf('MSIE 8') >= 0){
-        var metas = document.getElementsByTagName('meta'),
-        i = 0;
+        var i,
+        metas = document.getElementsByTagName('meta');
+
         for (i=0; i<metas.length; i++) {
           if(metas[i].getAttribute("name") === 'ap-' + name) {
              return metas[i].getAttribute("content");
