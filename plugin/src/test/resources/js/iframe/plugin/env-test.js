7a1d870f3899b04fe1588290cf219c8d581c028c
Chris Whittington <cwhittington@atlassian.com>
2013-11-11 16:49:57 +1100

    ACDEV-492 js unit tests for env and IE8 compatability

--- plugin/src/test/resources/js/iframe/plugin/env-test.js
+++ plugin/src/test/resources/js/iframe/plugin/env-test.js
@@ -1,17 +1,22 @@
+var xdmMockEnv;
 (function(){
 
     var context = require.config({
         context: Math.floor(Math.random() * 1000000),
         baseUrl: 'base/src/main/resources/js/iframe/plugin',
-
         map: {
             '*': {
                 '_xdm': '_xdmMockEnvTest'
             }
+        },
+        paths: {
+            '_xdmMockEnvTest': '/base/src/test/resources/js/iframe/plugin/_xdmMockEnvTest'
         }
     });
 
-    var xdmMock = {
+
+
+    xdmMockEnv = {
         init: function() {},
         getLocation: sinon.spy(),
         getUser: sinon.spy(),
@@ -22,11 +27,6 @@
         sizeToParent: sinon.spy()
     };
 
-    define('_xdmMockEnvTest', function () {
-        return function() {
-            return xdmMock;
-        };
-    });
     context(["_rpc", "env", "_dollar"], function(_rpc, env, $) {
         _rpc.init();
 
@@ -35,35 +35,41 @@
                 this.createFixtureContainer();
 
                 this.clock = sinon.useFakeTimers();
-                xdmMock.getLocation.reset();
-                xdmMock.getUser.reset();
-                xdmMock.getTimeZone.reset();
-                xdmMock.showMessage.reset();
-                xdmMock.clearMessage.reset();
-                xdmMock.resize.reset();
-                xdmMock.sizeToParent.reset();
+                xdmMockEnv.getLocation.reset();
+                xdmMockEnv.getUser.reset();
+                xdmMockEnv.getTimeZone.reset();
+                xdmMockEnv.showMessage.reset();
+                xdmMockEnv.clearMessage.reset();
+                xdmMockEnv.resize.reset();
+                xdmMockEnv.sizeToParent.reset();
+            },
+            createMetaTag: function(name, content){
+                var meta = document.createElement('meta');
+                meta.setAttribute('name', name);
+                meta.content = content;
+                document.getElementsByTagName('head')[0].appendChild(meta);
+                return meta;
             },
             createFixtureContainer: function(){
                 this.container = document.createElement('div');
-                this.container.id = 'qunit-container';
-                this.container.innerHTML =
-                '<meta name="ap-local-base-url" content="http://www.example.com/confluence" />' +
-                '<meta name="ap-fixture" content="foo bar" />';
                 document.body.appendChild(this.container);
+                this.metaBaseUrl =  this.createMetaTag('ap-local-base-url', 'http://www.example.com/confluence');
+                this.metaFixture =  this.createMetaTag('ap-fixture', 'foo bar');
             },
             createAcContainer: function(opts){
                 var acContainer = document.createElement('div');
                 $.extend(acContainer, opts);
                 this.container.appendChild(acContainer);
-
                 return acContainer;
             },
             teardown: function() {
                 this.clock.restore();
                 document.body.removeChild(this.container);
+                var head = document.getElementsByTagName('head')[0];
+                head.removeChild(this.metaBaseUrl);
+                head.removeChild(this.metaFixture);
             },
         });
-
 
         test("meta", function() {
             equal(env.meta('fixture'), 'foo bar');
@@ -108,7 +114,6 @@
             dim = env.size(null, null, acContainer);
 
             equal(dim.h, height);
-
         });
 
         test("auto resize causes 100% width to be returned", function() {
@@ -121,7 +126,6 @@
             dim = env.size(null, null, acContainer);
 
             equal(dim.w, "100%");
-
         });
 
         test("size returns passed width", function() {
@@ -132,7 +136,6 @@
             });
             dim = env.size(width, null, acContainer);
             equal(dim.w, width);
-
         });
 
         test("size returns passed height", function() {
@@ -148,76 +151,76 @@
         //xdm bridge methods.
         test("getLocation calls remote getLocation", function() {
             env.getLocation();
-            ok(xdmMock.getLocation.calledOnce);
+            ok(xdmMockEnv.getLocation.calledOnce);
         });
 
         test("getLocation passes callback to remote method", function() {
             var spy = sinon.spy();
             env.getLocation(spy);
-            equal(xdmMock.getLocation.args[0][0], spy);
+            equal(xdmMockEnv.getLocation.args[0][0], spy);
         });
 
         test("getUser calls remote getUser", function() {
             env.getUser();
-            ok(xdmMock.getUser.calledOnce);
+            ok(xdmMockEnv.getUser.calledOnce);
         });
 
         test("getUser passes callback to remote method", function() {
             var spy = sinon.spy();
             env.getUser(spy);
-            equal(xdmMock.getUser.args[0][0], spy);
+            equal(xdmMockEnv.getUser.args[0][0], spy);
         });
 
         test("getTimeZone calls remote getTimeZone", function() {
             env.getTimeZone();
-            ok(xdmMock.getTimeZone.calledOnce);
+            ok(xdmMockEnv.getTimeZone.calledOnce);
         });
 
         test("getTimeZone passes callback to remote method", function() {
             var spy = sinon.spy();
             env.getTimeZone(spy);
-            equal(xdmMock.getTimeZone.args[0][0], spy);
+            equal(xdmMockEnv.getTimeZone.args[0][0], spy);
         });
 
         test("showMessage calls remote showMessage", function() {
             env.showMessage();
-            ok(xdmMock.showMessage.calledOnce);
+            ok(xdmMockEnv.showMessage.calledOnce);
         });
 
         test("showMessage passes id to remote method", function() {
             var id = "foo";
             env.showMessage(id);
-            equal(xdmMock.showMessage.args[0][0], id);
+            equal(xdmMockEnv.showMessage.args[0][0], id);
         });
 
         test("showMessage passes title to remote method", function() {
             var title = "foo title";
             env.showMessage(null, title);
-            equal(xdmMock.showMessage.args[0][1], title);
+            equal(xdmMockEnv.showMessage.args[0][1], title);
         });
 
         test("showMessage passes body to remote method", function() {
             var body = "foo body";
             env.showMessage(null, null, body);
-            equal(xdmMock.showMessage.args[0][2], body);
+            equal(xdmMockEnv.showMessage.args[0][2], body);
         });
 
         test("clearMessage calls remote clearMessage", function() {
             env.clearMessage();
-            ok(xdmMock.clearMessage.calledOnce);
+            ok(xdmMockEnv.clearMessage.calledOnce);
         });
 
         test("clearMessage passes id to remote method", function() {
             var id = "bar";
             env.clearMessage(id);
-            equal(xdmMock.clearMessage.args[0][0], id);
+            equal(xdmMockEnv.clearMessage.args[0][0], id);
         });
 
         test("resize calls remote resize", function() {
             env.resize();
             //resize runs every 50ms.
             this.clock.tick(50);
-            ok(xdmMock.resize.calledOnce);
+            ok(xdmMockEnv.resize.calledOnce);
         });
 
         test("resize calls remote resize with width", function() {
@@ -225,7 +228,7 @@
             env.resize(width);
             //resize runs every 50ms.
             this.clock.tick(50);
-            equal(xdmMock.resize.args[0][0], width);
+            equal(xdmMockEnv.resize.args[0][0], width);
         });
 
         test("resize calls remote resize with height", function() {
@@ -233,16 +236,15 @@
             env.resize(null, height);
             //resize runs every 50ms.
             this.clock.tick(50);
-            equal(xdmMock.resize.args[0][1], height);
+            equal(xdmMockEnv.resize.args[0][1], height);
         });
 
         test("sizeToParent calls remote sizeToParent", function() {
             env.sizeToParent();
             //sizeToParent runs every 50ms.
             this.clock.tick(50);
-            ok(xdmMock.sizeToParent.calledOnce);
+            ok(xdmMockEnv.sizeToParent.calledOnce);
         });
-
 
     });
 
