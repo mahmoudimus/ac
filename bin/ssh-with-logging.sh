ee62b7cc4f99de52f00d07ec855daf6c5e400a02
Jonathan Doklovic <doklovic@atlassian.com>
2013-11-13 13:16:50 -0800

    bumping jgitflow to fix missing origin on bamboo

--- bin/ssh-with-logging.sh
+++ bin/ssh-with-logging.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-
+echo "SSH With Logging" > ./.git/ssh.txt
 CURDIR=`pwd`
 LISTING=`ls -al`
-echo "$CURDIR\n" > ./.git/ssh.txt
+echo "$CURDIR\n" >> ./.git/ssh.txt
 echo "$LISTING\n" >> ./.git/ssh.txt
 echo "ssh -vvv $@ \n" >> ./.git/ssh.txt
 exec ssh -vvv "$@" >> ./.git/ssh.txt
