a46616c649d540a1f5599792ae6dbfd8cc0f581a
Peter Brownlow <pbrownlow@atlassian.com>
2013-11-15 00:06:51 +1100

    ACDEV-526: refactored scopes filters to share common logic

--- plugin/src/main/java/com/atlassian/plugin/connect/plugin/module/permission/ApiScopingFilter.java
+++ plugin/src/main/java/com/atlassian/plugin/connect/plugin/module/permission/ApiScopingFilter.java
@@ -1,45 +1,17 @@
 package com.atlassian.plugin.connect.plugin.module.permission;
 
-import javax.annotation.Nonnull;
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
+import com.atlassian.plugin.connect.plugin.PermissionManager;
+import com.atlassian.plugin.connect.plugin.product.WebSudoService;
+import com.atlassian.sal.api.user.UserManager;
 
 /**
  * A filter to restrict incoming requests unless they have been authorized via api scopes.
  * Handles requests by looking for the add-on key as a request attribute.
  */
-public class ApiScopingFilter implements Filter, RequestAddOnKeyLabeler
+public class ApiScopingFilter extends ApiScopingFilterBase
 {
-    /**
-     * Set by an authorisation {@link Filter} calling {@link #setAddOnKey(HttpServletRequest, String)} to indicate the Connect add-on that is the origin of the current request.
-     */
-    private static final String ADD_ON_KEY = "Add-On-Key";
-
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException
+    protected ApiScopingFilter(PermissionManager permissionManager, UserManager userManager, WebSudoService webSudoService, String ourConsumerKey)
     {
-    }
-
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException
-    {
-    }
-
-    @Override
-    public void destroy()
-    {
-    }
-
-    @Override
-    public String getAddOnKey(@Nonnull HttpServletRequest request)
-    {
-        return (String) request.getAttribute(ADD_ON_KEY);
-    }
-
-    @Override
-    public void setAddOnKey(@Nonnull HttpServletRequest request, @Nonnull String addOnKey)
-    {
-        request.setAttribute(ADD_ON_KEY, addOnKey);
+        super(permissionManager, userManager, webSudoService, ourConsumerKey);
     }
 }
