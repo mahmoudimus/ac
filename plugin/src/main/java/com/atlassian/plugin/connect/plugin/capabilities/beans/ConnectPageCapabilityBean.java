e1dd15c46e9644a8d94d561c03ca6dc121dd2fd4
Anders Holmgren <aholmgren@atlassian.com>
2013-11-14 15:30:27 +1100

    ACDEV-482: javadocs

--- plugin/src/main/java/com/atlassian/plugin/connect/plugin/capabilities/beans/ConnectPageCapabilityBean.java
+++ plugin/src/main/java/com/atlassian/plugin/connect/plugin/capabilities/beans/ConnectPageCapabilityBean.java
@@ -18,28 +18,6 @@ import static com.google.common.base.Strings.isNullOrEmpty;
  * The page module takes care of integrating the add-on content into the application for you. The add-on content
  * automatically gets the page styles and decorators from the host application.
  *
- *
- *
- * Adds a web item to a specified location in the application interface. A web item is a hyperlink
- * that’s inserted into some standard place in the Atlassian application interface, such as the
- * administration menu.
- *
- * <p/>
- *
- * The form that the link takes can vary depending on the location. For instance, a web item in the header bar
- * (with a location section of system.top.navigation.bar) adds a link to the navigation bar across the top of the
- * interface. On the other hand, a web item in the opsbar-operation location section in JIRA adds an item to the issue
- * operation buttons.
- *
- * <p/>
- *
- * A web item link can open a new page in the application or a dialog, depending on your configuration.
- *
- * <p/>
- *
- * Web links are a simple and useful way to extend Atlassian applications. If you want to extend an Atlassian
- * application and don't know where to start, a web item may be all you need.
- *
  * @since 1.0
  */
 public class ConnectPageCapabilityBean extends BeanWithKeyAndParamsAndConditions
@@ -48,7 +26,6 @@ public class ConnectPageCapabilityBean extends BeanWithKeyAndParamsAndConditions
     private Integer weight;
     private String location;
     private IconBean icon;
-//    private String application; TODO: Figure out what to do with application. Only reference to it I see is in DescriptorPermissionsReader. Not sure how that ties in
 
     public ConnectPageCapabilityBean(ConnectPageCapabilityBeanBuilder builder)
     {
