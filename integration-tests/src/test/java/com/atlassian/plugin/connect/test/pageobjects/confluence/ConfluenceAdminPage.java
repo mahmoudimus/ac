f08d9887deee771a4c3e486b1bb65d04712415af
Tim Pettersen <tim@atlassian.com>
2013-11-22 15:09:59 -0600

    ACDEV-481: functional tests for adminPages capability

---
+++ integration-tests/src/test/java/com/atlassian/plugin/connect/test/pageobjects/confluence/ConfluenceAdminPage.java
@@ -0,0 +1,72 @@
+package com.atlassian.plugin.connect.test.pageobjects.confluence;
+
+import com.atlassian.pageobjects.PageBinder;
+import com.atlassian.pageobjects.binder.Init;
+import com.atlassian.pageobjects.elements.PageElement;
+import com.atlassian.pageobjects.elements.PageElementFinder;
+import com.atlassian.pageobjects.elements.timeout.TimeoutType;
+import com.atlassian.plugin.connect.test.pageobjects.AdminPage;
+import com.atlassian.plugin.connect.test.pageobjects.RemotePluginTestPage;
+import com.atlassian.webdriver.AtlassianWebDriver;
+import org.openqa.selenium.By;
+
+import javax.inject.Inject;
+
+/**
+ *
+ */
+public class ConfluenceAdminPage implements AdminPage
+{
+    @Inject
+    private AtlassianWebDriver driver;
+
+    @Inject
+    private PageElementFinder elementFinder;
+
+    @Inject
+    private PageBinder pageBinder;
+    private final String pageKey;
+
+    private PageElement linkElement;
+
+
+    public ConfluenceAdminPage(String pageKey)
+    {
+        this.pageKey = pageKey;
+    }
+
+    @Init
+    public void init()
+    {
+    }
+
+    @Override
+    public boolean isRemotePluginLinkPresent()
+    {
+        return findLinkElement() != null;
+    }
+
+    @Override
+    public RemotePluginTestPage clickRemotePluginLink()
+    {
+        findLinkElement().click();
+        return pageBinder.bind(RemotePluginTestPage.class, pageKey);
+    }
+
+    @Override
+    public String getRemotePluginLinkHref()
+    {
+        return findLinkElement().getAttribute("href");
+    }
+
+    private PageElement findLinkElement()
+    {
+        if (linkElement == null)
+        {
+            // pageKey == linkId
+            linkElement = elementFinder.find(By.id(pageKey), TimeoutType.DEFAULT);
+        }
+        return linkElement;
+    }
+
+}
