5831636cfd405979ea923e14f37db593ddb4ab7a
Sam Day <sday@atlassian.com>
2013-12-04 11:52:28 +1100

    AC-834: move SpaceAdminActionDescriptor out of SpaceAdminTabActionDescriptorFactory and rename it to SpaceToolsActionDescriptor

--- plugin/src/main/java/com/atlassian/plugin/connect/plugin/capabilities/descriptor/SpaceAdminTabActionDescriptorFactory.java
+++ plugin/src/main/java/com/atlassian/plugin/connect/plugin/capabilities/descriptor/SpaceAdminTabActionDescriptorFactory.java
@@ -1,48 +1,17 @@
 package com.atlassian.plugin.connect.plugin.capabilities.descriptor;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-
-import com.atlassian.confluence.event.events.plugin.XWorkStateChangeEvent;
-import com.atlassian.confluence.plugin.descriptor.PluginAwareActionConfig;
 import com.atlassian.event.api.EventPublisher;
 import com.atlassian.plugin.ModuleDescriptor;
 import com.atlassian.plugin.Plugin;
-import com.atlassian.plugin.connect.plugin.module.confluence.SpaceAdminIFrameAction;
-import com.atlassian.plugin.connect.plugin.module.confluence.SpaceAdminTabContextInterceptor;
+import com.atlassian.plugin.connect.plugin.module.confluence.SpaceToolsActionDescriptor;
 import com.atlassian.plugin.connect.plugin.module.page.SpaceAdminTabContext;
-import com.atlassian.plugin.descriptors.AbstractModuleDescriptor;
-import com.atlassian.plugin.module.ModuleFactory;
 
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-import com.opensymphony.webwork.dispatcher.VelocityResult;
-import com.opensymphony.xwork.ObjectFactory;
-import com.opensymphony.xwork.config.Configuration;
-import com.opensymphony.xwork.config.ConfigurationException;
-import com.opensymphony.xwork.config.ConfigurationManager;
-import com.opensymphony.xwork.config.ConfigurationProvider;
-import com.opensymphony.xwork.config.ConfigurationUtil;
-import com.opensymphony.xwork.config.entities.ActionConfig;
-import com.opensymphony.xwork.config.entities.InterceptorConfig;
-import com.opensymphony.xwork.config.entities.PackageConfig;
-import com.opensymphony.xwork.config.entities.ResultConfig;
-import com.opensymphony.xwork.config.providers.InterceptorBuilder;
-
-import org.dom4j.Element;
-import org.dom4j.dom.DOMElement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SpaceAdminTabActionDescriptorFactory
 {
-    private static final String NAMESPACE_PREFIX = "/plugins/ac/";
-    private static final String DEFAULT_INTERCEPTOR_STACK = "validatingStack";
-    private static final String VELOCITY_TEMPLATE = "/velocity/space-tab-page.vm";
-
     private final EventPublisher eventPublisher;
 
     @Autowired
@@ -53,89 +22,7 @@ public class SpaceAdminTabActionDescriptorFactory
 
     public ModuleDescriptor create(Plugin plugin, SpaceAdminTabContext context)
     {
-        return new SpaceTabActionDescriptor(plugin, context);
+        return new SpaceToolsActionDescriptor(eventPublisher, plugin, context);
     }
 
-    private class SpaceTabActionDescriptor extends AbstractModuleDescriptor implements ConfigurationProvider
-    {
-        private final SpaceAdminTabContext context;
-
-        public SpaceTabActionDescriptor(Plugin plugin, SpaceAdminTabContext context)
-        {
-            super(ModuleFactory.LEGACY_MODULE_FACTORY);
-            this.context = context;
-
-            Element element = new DOMElement("module")
-                .addAttribute("key", "action-" + context.getWebItemKey());
-
-            super.init(plugin, element);
-        }
-
-        @Override
-        public void enabled()
-        {
-            super.enabled();
-
-            ConfigurationManager.getConfigurationProviders();
-            ConfigurationManager.addConfigurationProvider(this);
-
-            eventPublisher.publish(new XWorkStateChangeEvent(this));
-        }
-
-        @Override
-        public void disabled()
-        {
-            List configurationProviders = ConfigurationManager.getConfigurationProviders();
-            synchronized (configurationProviders)
-            {
-                configurationProviders.remove(this);
-            }
-
-            eventPublisher.publish(new XWorkStateChangeEvent(this));
-
-            super.disabled();
-        }
-
-        @Override
-        public Object getModule()
-        {
-            return null;
-        }
-
-        @Override
-        public void init(Configuration configuration) throws ConfigurationException
-        {
-            String key = context.getWebItemKey();
-
-            List parentStack = ConfigurationUtil.buildParentsFromString(configuration, "default");
-            String namespace = NAMESPACE_PREFIX + plugin.getKey();
-            PackageConfig packageConfig = new PackageConfig(key, namespace, false, null, parentStack);
-
-            InterceptorConfig interceptorConfig = new InterceptorConfig("space-tab-context",
-                SpaceAdminTabContextInterceptor.class.getName(), Collections.EMPTY_MAP);
-            ObjectFactory.getObjectFactory().buildInterceptor(interceptorConfig, Collections.EMPTY_MAP);
-            packageConfig.addInterceptorConfig(interceptorConfig);
-
-            Map<String, ResultConfig> results = Maps.newHashMap();
-            Map resultParameters = ImmutableMap.of("location", VELOCITY_TEMPLATE);
-            ResultConfig resultConfig = new ResultConfig("success", VelocityResult.class, resultParameters);
-            results.put(resultConfig.getName(), resultConfig);
-
-            ActionConfig actionConfig = new PluginAwareActionConfig(null, SpaceAdminIFrameAction.class.getName(),
-                ImmutableMap.of("context", context), results, Lists.newArrayList(), plugin);
-            actionConfig.addInterceptors(InterceptorBuilder.constructInterceptorReference(packageConfig,
-                interceptorConfig.getName(), Collections.EMPTY_MAP));
-            actionConfig.addInterceptors(InterceptorBuilder.constructInterceptorReference(packageConfig,
-                DEFAULT_INTERCEPTOR_STACK, Collections.EMPTY_MAP));
-            packageConfig.addActionConfig(key, actionConfig);
-
-            configuration.addPackageConfig(key, packageConfig);
-        }
-
-        @Override
-        public boolean needsReload()
-        {
-            return true;
-        }
-    }
 }
---
+++ plugin/src/main/java/com/atlassian/plugin/connect/plugin/module/confluence/SpaceToolsActionDescriptor.java
@@ -0,0 +1,127 @@
+package com.atlassian.plugin.connect.plugin.module.confluence;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import com.atlassian.confluence.event.events.plugin.XWorkStateChangeEvent;
+import com.atlassian.confluence.plugin.descriptor.PluginAwareActionConfig;
+import com.atlassian.event.api.EventPublisher;
+import com.atlassian.plugin.Plugin;
+import com.atlassian.plugin.connect.plugin.capabilities.descriptor.SpaceAdminTabActionDescriptorFactory;
+import com.atlassian.plugin.connect.plugin.module.page.SpaceAdminTabContext;
+import com.atlassian.plugin.descriptors.AbstractModuleDescriptor;
+import com.atlassian.plugin.module.ModuleFactory;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.opensymphony.webwork.dispatcher.VelocityResult;
+import com.opensymphony.xwork.ObjectFactory;
+import com.opensymphony.xwork.config.Configuration;
+import com.opensymphony.xwork.config.ConfigurationException;
+import com.opensymphony.xwork.config.ConfigurationManager;
+import com.opensymphony.xwork.config.ConfigurationProvider;
+import com.opensymphony.xwork.config.ConfigurationUtil;
+import com.opensymphony.xwork.config.entities.ActionConfig;
+import com.opensymphony.xwork.config.entities.InterceptorConfig;
+import com.opensymphony.xwork.config.entities.PackageConfig;
+import com.opensymphony.xwork.config.entities.ResultConfig;
+import com.opensymphony.xwork.config.providers.InterceptorBuilder;
+
+import org.dom4j.Element;
+import org.dom4j.dom.DOMElement;
+
+/**
+ * This is a special case descriptor that models an XWork action to display a space tools page. There is no clean
+ * page decorators that can be used to render the space tools pages - they have a hardcoded expectation that they're
+ * working from inside an xwork action.
+ */
+public class SpaceToolsActionDescriptor extends AbstractModuleDescriptor implements ConfigurationProvider
+{
+    private static final String NAMESPACE_PREFIX = "/plugins/ac/";
+    private static final String DEFAULT_INTERCEPTOR_STACK = "validatingStack";
+    private static final String VELOCITY_TEMPLATE = "/velocity/space-tab-page.vm";
+
+    private final EventPublisher eventPublisher;
+    private final SpaceAdminTabContext context;
+
+    public SpaceToolsActionDescriptor(EventPublisher eventPublisher, Plugin plugin, SpaceAdminTabContext context)
+    {
+        super(ModuleFactory.LEGACY_MODULE_FACTORY);
+        this.eventPublisher = eventPublisher;
+        this.context = context;
+
+        Element element = new DOMElement("module")
+            .addAttribute("key", "action-" + context.getWebItemKey());
+
+        super.init(plugin, element);
+    }
+
+    @Override
+    public void enabled()
+    {
+        super.enabled();
+
+        ConfigurationManager.getConfigurationProviders();
+        ConfigurationManager.addConfigurationProvider(this);
+
+        eventPublisher.publish(new XWorkStateChangeEvent(this));
+    }
+
+    @Override
+    public void disabled()
+    {
+        List configurationProviders = ConfigurationManager.getConfigurationProviders();
+        synchronized (configurationProviders)
+        {
+            configurationProviders.remove(this);
+        }
+
+        eventPublisher.publish(new XWorkStateChangeEvent(this));
+
+        super.disabled();
+    }
+
+    @Override
+    public Object getModule()
+    {
+        return null;
+    }
+
+    @Override
+    public void init(Configuration configuration) throws ConfigurationException
+    {
+        String key = context.getWebItemKey();
+
+        List parentStack = ConfigurationUtil.buildParentsFromString(configuration, "default");
+        String namespace = NAMESPACE_PREFIX + plugin.getKey();
+        PackageConfig packageConfig = new PackageConfig(key, namespace, false, null, parentStack);
+
+        InterceptorConfig interceptorConfig = new InterceptorConfig("space-tab-context",
+            SpaceAdminTabContextInterceptor.class.getName(), Collections.EMPTY_MAP);
+        ObjectFactory.getObjectFactory().buildInterceptor(interceptorConfig, Collections.EMPTY_MAP);
+        packageConfig.addInterceptorConfig(interceptorConfig);
+
+        Map<String, ResultConfig> results = Maps.newHashMap();
+        Map resultParameters = ImmutableMap.of("location", VELOCITY_TEMPLATE);
+        ResultConfig resultConfig = new ResultConfig("success", VelocityResult.class, resultParameters);
+        results.put(resultConfig.getName(), resultConfig);
+
+        ActionConfig actionConfig = new PluginAwareActionConfig(null, SpaceAdminIFrameAction.class.getName(),
+            ImmutableMap.of("context", context), results, Lists.newArrayList(), plugin);
+        actionConfig.addInterceptors(InterceptorBuilder.constructInterceptorReference(packageConfig,
+            interceptorConfig.getName(), Collections.EMPTY_MAP));
+        actionConfig.addInterceptors(InterceptorBuilder.constructInterceptorReference(packageConfig,
+            DEFAULT_INTERCEPTOR_STACK, Collections.EMPTY_MAP));
+        packageConfig.addActionConfig(key, actionConfig);
+
+        configuration.addPackageConfig(key, packageConfig);
+    }
+
+    @Override
+    public boolean needsReload()
+    {
+        return true;
+    }
+}
