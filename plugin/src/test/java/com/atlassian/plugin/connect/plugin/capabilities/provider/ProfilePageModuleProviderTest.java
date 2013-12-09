d1bc7949b1fdfb037bb37aee7d0bec959807c6dd
Anders Holmgren <aholmgren@atlassian.com>
2013-12-10 07:17:59 +1100

    ACDEV-483: fixed test

--- plugin/src/test/java/com/atlassian/plugin/connect/plugin/capabilities/provider/ProfilePageModuleProviderTest.java
+++ plugin/src/test/java/com/atlassian/plugin/connect/plugin/capabilities/provider/ProfilePageModuleProviderTest.java
@@ -1,17 +1,9 @@
 package com.atlassian.plugin.connect.plugin.capabilities.provider;
 
 import com.atlassian.plugin.connect.plugin.capabilities.beans.nested.IFrameServletBean;
-import com.atlassian.plugin.connect.spi.module.IFrameParams;
-import com.google.common.base.Objects;
-import org.hamcrest.Description;
 import org.junit.Test;
-import org.mockito.ArgumentMatcher;
 
-import static org.hamcrest.Matchers.instanceOf;
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
 import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.argThat;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.verify;
 
@@ -26,13 +18,13 @@ public class ProfilePageModuleProviderTest extends AbstractPageModuleProviderTes
     @Test
     public void fetchesDefaultLocationFromProductAccessorWhenNotSpecified()
     {
-        verify(productAccessor).getPreferredProfileSectionKey();
+        verify(productAccessor).getPreferredGeneralSectionKey(); // strange but true
     }
 
     @Test
     public void fetchesDefaultWeightFromProductAccessorWhenNotSpecified()
     {
-        verify(productAccessor).getPreferredProfileWeight();
+        verify(productAccessor).getPreferredGeneralWeight(); // strange but true
     }
 
     @Test
