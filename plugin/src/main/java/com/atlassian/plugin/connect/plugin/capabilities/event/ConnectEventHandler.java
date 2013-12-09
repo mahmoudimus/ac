6540ce82a2484720fca5022524602df327e8e5be
Peter Brownlow <pbrownlow@atlassian.com>
2013-12-09 15:32:40 +1100

    Merge branch 'develop' of ssh://stash.atlassian.com:7997/ac/atlassian-connect into feature/ACDEV-414-distribute-shared-secret
    Conflicts:
    	plugin/src/main/java/com/atlassian/plugin/connect/plugin/capabilities/event/ConnectEventHandler.java

--- plugin/src/main/java/com/atlassian/plugin/connect/plugin/capabilities/event/ConnectEventHandler.java
+++ plugin/src/main/java/com/atlassian/plugin/connect/plugin/capabilities/event/ConnectEventHandler.java
@@ -1,11 +1,5 @@
 package com.atlassian.plugin.connect.plugin.capabilities.event;
 
-import java.net.URI;
-
-import javax.inject.Inject;
-import javax.inject.Named;
-import javax.ws.rs.core.MediaType;
-
 import com.atlassian.event.api.EventPublisher;
 import com.atlassian.httpclient.api.HttpClient;
 import com.atlassian.httpclient.api.Request;
@@ -39,16 +33,19 @@ import com.atlassian.upm.api.util.Option;
 import com.atlassian.upm.spi.PluginInstallException;
 import com.atlassian.uri.UriBuilder;
 import com.atlassian.webhooks.spi.plugin.RequestSigner;
-
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Strings;
-
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.Constants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.ws.rs.core.MediaType;
+import java.net.URI;
 
 import static com.atlassian.plugin.connect.plugin.capabilities.beans.ConnectAddonEventData.newConnectAddonEventData;
 import static com.google.common.base.Preconditions.checkNotNull;
