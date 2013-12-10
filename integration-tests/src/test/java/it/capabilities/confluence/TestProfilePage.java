d3e7312d09256dd28e548a86b3ffe43f8173f25d
Anders Holmgren <aholmgren@atlassian.com>
2013-12-10 18:36:42 +1100

    ACDEV-484: pulled out base test class

--- integration-tests/src/test/java/it/capabilities/confluence/TestProfilePage.java
+++ integration-tests/src/test/java/it/capabilities/confluence/TestProfilePage.java
@@ -3,68 +3,25 @@ package it.capabilities.confluence;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 
-import com.atlassian.fugue.Option;
-import com.atlassian.plugin.connect.plugin.capabilities.beans.nested.I18nProperty;
-import com.atlassian.plugin.connect.test.pageobjects.LinkedRemoteContent;
-import com.atlassian.plugin.connect.test.pageobjects.RemotePluginEmbeddedTestPage;
 import com.atlassian.plugin.connect.test.pageobjects.confluence.ConfluenceUserProfilePage;
-import com.atlassian.plugin.connect.test.server.ConnectRunner;
-import it.confluence.ConfluenceWebDriverTestBase;
-import it.servlet.ConnectAppServlets;
-import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
-import static com.atlassian.plugin.connect.plugin.capabilities.beans.ConnectPageModuleBean.newPageBean;
-import static com.atlassian.plugin.connect.test.pageobjects.RemoteWebItem.ItemMatchingMode.LINK_TEXT;
-import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertThat;
 
 /**
  * Test of profile page in Confluence
  */
-public class TestProfilePage extends ConfluenceWebDriverTestBase
+public class TestProfilePage extends AbstractPageTst
 {
-    private static final String PLUGIN_KEY = "my-plugin";
-
-    private static ConnectRunner remotePlugin;
 
     @BeforeClass
     public static void startConnectAddOn() throws Exception
     {
-        remotePlugin = new ConnectRunner(product.getProductInstance().getBaseUrl(), PLUGIN_KEY)
-                .addModule(
-                        "profilePages",
-                        newPageBean()
-                                .withName(new I18nProperty("My Awesome Page", null))
-                                .withUrl("/pg")
-                                .withWeight(1234)
-                                .build())
-                .addRoute("/pg", ConnectAppServlets.apRequestServlet())
-                .start();
-    }
-
-    @AfterClass
-    public static void stopConnectAddOn() throws Exception
-    {
-        if (remotePlugin != null)
-        {
-            remotePlugin.stopAndUninstall();
-        }
+        startConnectAddOn("profilePages");
     }
 
     @Test
     public void canClickOnPageLinkAndSeeAddonContents() throws MalformedURLException, URISyntaxException
     {
-        loginAsAdmin();
-
-        ConfluenceUserProfilePage profilePage = product.visit(ConfluenceUserProfilePage.class);
-        LinkedRemoteContent addonPage = profilePage.findConnectPage(LINK_TEXT, "My Awesome Page",
-                Option.<String>none(), "my-awesome-page");
-        RemotePluginEmbeddedTestPage addonContentPage = addonPage.click();
-        assertThat(addonContentPage.isLoaded(), equalTo(true));
-        assertThat(addonContentPage.getMessage(), equalTo("Success"));
+        runCanClickOnPageLinkAndSeeAddonContents(ConfluenceUserProfilePage.class);
     }
-
-
 }
