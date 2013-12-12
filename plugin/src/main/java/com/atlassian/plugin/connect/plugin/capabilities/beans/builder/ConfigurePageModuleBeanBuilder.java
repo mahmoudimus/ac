ef9b57da6607c57c68fc25c39f2063533598d0a5
Anders Holmgren <aholmgren@atlassian.com>
2013-12-12 18:15:41 +1100

    ACDEV-484: pr review updates

--- plugin/src/main/java/com/atlassian/plugin/connect/plugin/capabilities/beans/builder/ConfigurePageModuleBeanBuilder.java
+++ plugin/src/main/java/com/atlassian/plugin/connect/plugin/capabilities/beans/builder/ConfigurePageModuleBeanBuilder.java
@@ -1,14 +1,9 @@
 package com.atlassian.plugin.connect.plugin.capabilities.beans.builder;
 
-import java.util.Map;
-
-import com.atlassian.plugin.connect.plugin.capabilities.beans.ConditionalBean;
 import com.atlassian.plugin.connect.plugin.capabilities.beans.ConfigurePageModuleBean;
-import com.atlassian.plugin.connect.plugin.capabilities.beans.nested.I18nProperty;
-import com.atlassian.plugin.connect.plugin.capabilities.beans.nested.IconBean;
 
-public class ConfigurePageModuleBeanBuilder<T extends ConfigurePageModuleBeanBuilder, B extends ConfigurePageModuleBean>
-        extends ConnectPageModuleBeanBuilder<T, B>
+public class ConfigurePageModuleBeanBuilder
+        extends ConnectPageModuleBeanBuilder<ConfigurePageModuleBeanBuilder, ConfigurePageModuleBean>
 {
     private Boolean isDefault;
 
@@ -22,71 +17,16 @@ public class ConfigurePageModuleBeanBuilder<T extends ConfigurePageModuleBeanBui
         isDefault = defaultBean.isDefault();
     }
 
-    public T setAsDefault()
+    public ConfigurePageModuleBeanBuilder setAsDefault()
     {
         isDefault = true;
-        return (T) this;
-    }
-
-    public T withUrl(String url)
-    {
-        return (T) super.withUrl(url);
-    }
-
-    public T withWeight(int weight)
-    {
-        return (T) super.withWeight(weight);
+        return this;
     }
 
     @Override
-    public T withConditions(ConditionalBean... beans)
+    public ConfigurePageModuleBean build()
     {
-        super.withConditions(beans);
-        return (T) this;
-    }
-
-    @Override
-    public T withParams(Map<String, String> params)
-    {
-        super.withParams(params);
-        return (T) this;
-    }
-
-    @Override
-    public T withParam(String key, String value)
-    {
-        super.withParam(key, value);
-        return (T) this;
-    }
-
-    @Override
-    public T withKey(String key)
-    {
-        super.withKey(key);
-        return (T) this;
-    }
-
-    @Override
-    public T withName(I18nProperty name)
-    {
-        super.withName(name);
-        return (T) this;
-    }
-
-    public T withLocation(String location)
-    {
-        return (T) super.withLocation(location);
-    }
-
-    public T withIcon(IconBean icon)
-    {
-        return (T) super.withIcon(icon);
-    }
-
-    @Override
-    public B build()
-    {
-        return (B) new ConfigurePageModuleBean(this);
+        return new ConfigurePageModuleBean(this);
     }
 
 }
