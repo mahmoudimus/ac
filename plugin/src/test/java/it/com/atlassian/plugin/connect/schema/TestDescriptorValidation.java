b807fac42698f8c53155ae65c6321a7fcb71b336
Tim Pettersen <tim@atlassian.com>
2013-12-12 11:43:40 -0800

    ACDEV-674: inject ConnectDescriptorValidator instead of mocking it out

--- plugin/src/test/java/it/com/atlassian/plugin/connect/schema/TestDescriptorValidation.java
+++ plugin/src/test/java/it/com/atlassian/plugin/connect/schema/TestDescriptorValidation.java
@@ -1,38 +1,24 @@
 package it.com.atlassian.plugin.connect.schema;
 
-import com.atlassian.plugin.connect.plugin.capabilities.schema.ConnectSchemaLocator;
+import com.atlassian.plugin.connect.plugin.capabilities.schema.ConnectDescriptorValidator;
 import com.atlassian.plugin.connect.plugin.capabilities.schema.DescriptorValidationResult;
-import com.atlassian.plugin.connect.plugin.capabilities.schema.JsonDescriptorValidator;
-import com.atlassian.plugin.spring.scanner.ProductFilter;
 
 import com.atlassian.plugins.osgi.test.AtlassianPluginsTestRunner;
-import org.apache.commons.io.IOUtils;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static com.atlassian.plugin.connect.plugin.capabilities.TestFileReader.readAddonTestFile;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 @RunWith (AtlassianPluginsTestRunner.class)
 public class TestDescriptorValidation
 {
-    public static final String CONFLUENCE_SCHEMA = "/schema/confluence-schema.json";
+    private ConnectDescriptorValidator validator;
 
-    private JsonDescriptorValidator validator;
-
-    @Before
-    public void setup() throws Exception
+    public TestDescriptorValidation(final ConnectDescriptorValidator validator)
     {
-        ConnectSchemaLocator schemaLocator = mock(ConnectSchemaLocator.class);
-        String schema = IOUtils.toString(getClass().getResourceAsStream(CONFLUENCE_SCHEMA));
-        when(schemaLocator.getSchema(any(ProductFilter.class))).thenReturn(schema);
-
-        validator = new JsonDescriptorValidator(schemaLocator);
+        this.validator = validator;
     }
 
     @Test
