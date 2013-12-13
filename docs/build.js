81f2603dc2d47eb9a6cabd6fbbb4f97bf5c0e128
Tim Pettersen <tim@atlassian.com>
2013-12-13 14:43:37 -0800

    ACDEV-696: progress towards transforming schema objects into an easily rendered model.

--- docs/build.js
+++ docs/build.js
@@ -5,10 +5,11 @@ var fs = require('fs-extra');
 var util = require('util');
 var jiraSchema = require('../plugin/target/classes/schema/jira-schema.json');
 var confluenceSchema = require('../plugin/target/classes/schema/confluence-schema.json');
+var renderMarkdown = require("markdown-js").markdown;
 
-function storeEntity(obj, bucket) {
-    // store entities with ids
-    var id = obj.id || (obj.items && obj.items.id);
+function storeEntity(obj, key, bucket) {
+    // store entities with ids if they have it, or key if they don't
+    var id = obj.id || (obj.items && key);
     if (id && !bucket[id]) {
         bucket[id] = obj;
     }
@@ -17,9 +18,9 @@ function storeEntity(obj, bucket) {
 
 function findEntities(schemaProperties) {
     var bucket = {};
-    _.forEach(schemaProperties, function(val) {
+    _.forEach(schemaProperties, function(val, key) {
         if (typeof val === "object") {
-            storeEntity(val, bucket);
+            storeEntity(val, key, bucket);
         }
     });
     return bucket;
@@ -27,27 +28,66 @@ function findEntities(schemaProperties) {
 
 function findEntitiesRecursively(json, bucket) {
     bucket = bucket || {};
-    _.forEach(json, function(val) {
+    _.forEach(json, function(val, key) {
         if (typeof val === "object") {
-            storeEntity(val, bucket);
+            storeEntity(val, key, bucket);
             findEntitiesRecursively(val, bucket);
         }
     });
     return bucket;
 }
 
-var entityRoots = {
+function collapseArrayAndObjectProperties(properties, required) {
+    return _.map(properties, function(property, id) {
+        switch (property.type) {
+            case "array":
+                property.id = id;
+                property = _.pick(property, ["id", "type", "title"]);
+                break;
+            case "object":
+                property = _.pick(property, ["id", "type", "title"]);
+                break;
+            default:
+                // primitive
+                break;
+        }
+
+        if (required && required.indexOf(id) > -1) {
+            property.required = true;
+        }
+        property.key = id;
+
+        return property;
+    });
+}
+
+function schemaToModel(schemaEntity, id) {
+    var name = schemaEntity.title || id;
+    var description = renderMarkdown(schemaEntity.description || name);
+    var properties = collapseArrayAndObjectProperties(schemaEntity.properties, schemaEntity.required);
+
+    return {
+        id: id,
+        name: name,
+        description: description,
+        properties: properties
+    };
+}
+
+var entities = {
     topLevel: findEntities(jiraSchema.properties),
     jira: findEntities(jiraSchema.properties.modules.properties),
     confluence: findEntities(confluenceSchema.properties.modules.properties)
 };
 
-var allRoots = _.extend(findEntitiesRecursively(jiraSchema), findEntitiesRecursively(confluenceSchema));
+var allEntities = _.extend(findEntitiesRecursively(jiraSchema), findEntitiesRecursively(confluenceSchema));
+entities.nested = _.omit(allEntities, _.keys(entities.topLevel), _.keys(entities.jira), _.keys(entities.confluence));
 
-entityRoots.nested = _.omit(allRoots, _.keys(entityRoots.topLevel), _.keys(entityRoots.jira), _.keys(entityRoots.confluence));
+entities = _.mapValues(entities, function(val) {
+    return _.map(val, schemaToModel);
+});
 
-console.log(util.inspect(entityRoots, {depth: 3}));
-
+console.log(util.inspect(entities, {depth: 5}));
 
 // OLD STUFF BELOW HERE
 
@@ -76,9 +116,6 @@ harpGlobals.globals.schemas.confluence = confluenceSchema;
 for (var k in confluenceSchema.properties.modules.properties) {
     fs.outputFile('./public/modules/confluence/'+k+'.md', String(confluenceSchema.properties.modules.properties[k].items.description).replace(/\n /g,"\n"));
 }
-
-
-
 
 // Store schema info into Harp globals
 fs.writeFile('./harp.json', JSON.stringify(harpGlobals,null,2), function(err) {
