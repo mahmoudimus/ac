87e62ae1172e6a8c56a6a09044c0bedc4da49b5d
Tim Pettersen <tim@atlassian.com>
2013-12-16 07:59:05 -0800

    ACDEV-696: watch filesystem and rebuild docs

--- docs/build.js
+++ docs/build.js
@@ -3,10 +3,14 @@
 var _ = require('lodash');
 var fs = require('fs-extra');
 var util = require('util');
-var jiraSchema = require('../plugin/target/classes/schema/jira-schema.json');
-var confluenceSchema = require('../plugin/target/classes/schema/confluence-schema.json');
 var renderMarkdown = require("markdown-js").markdown;
 var fork = require("child_process").fork;
+var chokidar = require("chokidar");
+
+var buildDir = "./target";
+var genSrcPrefix = buildDir + "/gensrc/";
+
+var srcFiles = ["public", "package.json"];
 
 function storeEntity(obj, key, bucket) {
     // store entities with ids if they have it, or key if they don't
@@ -82,98 +86,139 @@ function schemaToModel(schemaEntity, id) {
     return model;
 }
 
-var entities = {
-    root: findEntities(jiraSchema.properties),
-    jiraModuleList: findEntities(jiraSchema.properties.modules.properties),
-    confluenceModuleList: findEntities(confluenceSchema.properties.modules.properties)
-};
-
-var allEntities = _.extend(findEntitiesRecursively(jiraSchema), findEntitiesRecursively(confluenceSchema));
-entities.fragment = _.omit(allEntities, _.keys(entities.root), _.keys(entities.jiraModuleList), _.keys(entities.confluenceModuleList));
-
-entities = _.mapValues(entities, function(val) {
-    return _.map(val, schemaToModel);
-});
-
 function uniqueArrayTypes(moduleList) {
     return _.unique(_.pluck(moduleList, "arrayType"));
 }
 
-var jiraTypes = uniqueArrayTypes(entities.jiraModuleList);
-var confluenceTypes = uniqueArrayTypes(entities.confluenceModuleList);
-
-function findNestedModules(ids) {
-    return _.filter(entities.fragment, function (val) {
+function findModulesWithId(entities, ids) {
+    return _.filter(entities, function (val) {
         return ids.indexOf(val.id) > -1;
     });
 }
 
-entities.jira = findNestedModules(jiraTypes);
-entities.confluence = findNestedModules(confluenceTypes);
-entities.fragment = _.difference(entities.fragment, entities.jira, entities.confluence);
+function copySrcFiles(filenames) {
+    if (typeof filenames === "string") filenames = [filenames];
+    _.each(filenames, function (filename) {
+        fs.copySync(filename, genSrcPrefix + filename);
+    });
+}
 
-console.log(util.inspect(entities, {depth: 5}));
+function rebuildHarpSite() {
 
-var buildDir = "./target";
-var genSrcPrefix = buildDir + "/gensrc/";
+    fs.deleteSync(buildDir);
 
-fs.deleteSync(buildDir);
+    var jiraSchema = require('../plugin/target/classes/schema/jira-schema.json');
+    var confluenceSchema = require('../plugin/target/classes/schema/confluence-schema.json');
 
-_.each(["public", "package.json", "node_modules"], function (requiredFile) {
-    fs.copySync(requiredFile, genSrcPrefix + requiredFile);
-});
+    var entities = {
+        root: findEntities(jiraSchema.properties),
+        jiraModuleList: findEntities(jiraSchema.properties.modules.properties),
+        confluenceModuleList: findEntities(confluenceSchema.properties.modules.properties)
+    };
 
-// write out our file structure
+    var allEntities = _.extend(findEntitiesRecursively(jiraSchema), findEntitiesRecursively(confluenceSchema));
+    entities.fragment = _.omit(allEntities, _.keys(entities.root), _.keys(entities.jiraModuleList), _.keys(entities.confluenceModuleList));
 
-var keyToPath = {
-    root: "modules",
-    jira: "modules/jira",
-    confluence: "modules/confluence",
-    fragment: "modules/fragment"
-};
+    entities = _.mapValues(entities, function(val) {
+        return _.map(val, schemaToModel);
+    });
 
-var entityData = {};
-var entityLinks = {};
+    var jiraTypes = uniqueArrayTypes(entities.jiraModuleList);
+    var confluenceTypes = uniqueArrayTypes(entities.confluenceModuleList);
 
-_.each(entities, function(entitySet, parentKey) {
-    var pathMapping = keyToPath[parentKey];
-    if (pathMapping) {
-        entityData[parentKey] = {};
-        _.each(entitySet, function(entity) {
-            entity = _.clone(entity);
-            entityData[parentKey][entity.id] = entity;
-            entity.selfLink = pathMapping + '/' + entity.id;
-            console.log(entity.id + " selfLink: " + entity.selfLink);
+    entities.jira = findModulesWithId(entities.fragment, jiraTypes);
+    entities.confluence = findModulesWithId(entities.fragment, confluenceTypes);
+    entities.fragment = _.difference(entities.fragment, entities.jira, entities.confluence);
 
-            entityLinks[entity.id] = entity.selfLink;
+    copySrcFiles(srcFiles);
+    copySrcFiles("node_modules");
 
-            var placeholder =
-                    "Placeholder - this file indicates static web directory " +
-                    "structure and is not actually used in rendering.\n\n" +
-                    "Here's the model JSON for this file, y'know for debugging " +
-                    "and stuff:\n\n" + JSON.stringify(entity, null, 2);
+    // write out our file structure
 
-            var filePath = genSrcPrefix + 'public/' + entity.selfLink +'.md';
+    var keyToPath = {
+        root: "modules",
+        jira: "modules/jira",
+        confluence: "modules/confluence",
+        fragment: "modules/fragment"
+    };
 
-            console.log("Writing to " + filePath);
+    var entityData = {};
+    var entityLinks = {};
 
-            fs.outputFileSync(filePath, placeholder);
-        });
+    _.each(entities, function(entitySet, parentKey) {
+        var pathMapping = keyToPath[parentKey];
+        if (pathMapping) {
+            entityData[parentKey] = {};
+            _.each(entitySet, function(entity) {
+                entity = _.clone(entity);
+                entityData[parentKey][entity.id] = entity;
+                entity.selfLink = pathMapping + '/' + entity.id;
+
+                entityLinks[entity.id] = entity.selfLink;
+
+                var placeholder =
+                        "Placeholder - this file indicates static web directory " +
+                        "structure and is not actually used in rendering.\n\n" +
+                        "Here's the model JSON for this file, y'know for debugging " +
+                        "and stuff:\n\n" + JSON.stringify(entity, null, 2);
+
+                var filePath = genSrcPrefix + 'public/' + entity.selfLink +'.md';
+
+                fs.outputFileSync(filePath, placeholder);
+            });
+        }
+    });
+
+    var harpGlobals = require('./globals.json');
+
+    harpGlobals.globals.entityLinks = entityLinks;
+    harpGlobals.globals.entities = entityData;
+    harpGlobals.globals.schemas = {};
+
+    // Store schema info into Harp globals
+    fs.outputFileSync(genSrcPrefix + 'harp.json', JSON.stringify(harpGlobals,null,2));
+}
+
+function startHarpServerAndWatchSrcFiles() {
+    var harpServer;
+    var restarting = false;
+
+    function startHarpServer() {
+        return fork('./node_modules/harp/bin/harp', ["server"], {'cwd': genSrcPrefix});
     }
-});
 
-var harpGlobals = require('./globals.json');
+    function restartHarpServer() {
+        if (restarting) return;
 
-harpGlobals.globals.entityLinks = entityLinks;
-harpGlobals.globals.entities = entityData;
-harpGlobals.globals.schemas = {};
+        restarting = true;
+        harpServer.on('exit', function() {
+            rebuildHarpSite();
+            harpServer = startHarpServer();
+            restarting = false;
+        });
+        harpServer.kill();
+    }
 
-// Store schema info into Harp globals
-fs.outputFileSync(genSrcPrefix + 'harp.json', JSON.stringify(harpGlobals,null,2));
+    harpServer = startHarpServer();
+
+    var watcher = chokidar.watch(srcFiles, {persistent:true});
+    _.each(['add', 'addDir', 'change', 'unlink', 'unlinkDir'], function(event) {
+        watcher.on(event, function(path) {
+            console.log(event + " on " + path + "! Rebuilding..");
+            restartHarpServer();
+        });
+    });
+}
+
+function compileHarpSources() {
+    fork('./node_modules/harp/bin/harp', ["-o", "../www", "compile"], {'cwd': genSrcPrefix});
+}
+
+rebuildHarpSite();
 
 if (process.argv.length > 2 && process.argv[2].toLowerCase().indexOf("serve") === 0) {
-    fork('./node_modules/harp/bin/harp', ["server"], {'cwd': genSrcPrefix});
+    startHarpServerAndWatchSrcFiles()
 } else {
-    fork('./node_modules/harp/bin/harp', ["-o", "../www", "compile"], {'cwd': genSrcPrefix});
+    compileHarpSources();
 }
 
