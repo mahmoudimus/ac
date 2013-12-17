dd2d8926cfaae70fab415da9de630e9f9b73e40c
Anders Holmgren <aholmgren@atlassian.com>
2013-12-17 15:19:06 +1100

    ACDEV-746: removed <p> inserted by intellij

--- plugin/src/main/java/com/atlassian/plugin/connect/plugin/capabilities/beans/ConfigurePageModuleBean.java
+++ plugin/src/main/java/com/atlassian/plugin/connect/plugin/capabilities/beans/ConfigurePageModuleBean.java
@@ -8,12 +8,12 @@ import org.apache.commons.lang3.builder.HashCodeBuilder;
 /**
  * A configure page module is a page module used to configure the addon itself.
  * Other than that it is the same as other pages.
- * <p/>
+ *
  * If more than one configure page is specified, then exactly one of them must have the [default] field set.
  * A 'configure' button will link to this page from the manage plugins administration console.
- * <p/>
+ *
  * If there is only one configure page specified then it will automatically be used regardless of the default field.
- * <p/>
+ *
  * If no configure page modules are specified then no configure button will be included on the install page.
  */
 public class ConfigurePageModuleBean extends ConnectPageModuleBean
