2286f0b3bab88656ba245b26ac22a1b3ab4d7c73
Anders Holmgren <aholmgren@atlassian.com>
2013-12-17 16:28:56 +1100

    ACDEV-757: changed javadoc to reflect the name isDefault

--- plugin/src/main/java/com/atlassian/plugin/connect/plugin/capabilities/beans/ConfigurePageModuleBean.java
+++ plugin/src/main/java/com/atlassian/plugin/connect/plugin/capabilities/beans/ConfigurePageModuleBean.java
@@ -10,15 +10,18 @@ import org.apache.commons.lang3.builder.HashCodeBuilder;
  * A configure page module is a page module used to configure the addon itself.
  * Other than that it is the same as other pages.
  *
- * If more than one configure page is specified, then exactly one of them must have the [default] field set.
+ * If more than one configure page is specified, then exactly one of them must have the [isDefault] field set.
  * A 'configure' button will link to this page from the manage plugins administration console.
  *
- * If there is only one configure page specified then it will automatically be used regardless of the default field.
+ * If there is only one configure page specified then it will automatically be used regardless of the isDefault field.
  *
  * If no configure page modules are specified then no configure button will be included on the install page.
  */
 public class ConfigurePageModuleBean extends ConnectPageModuleBean
 {
+    /**
+     * If there is more than one configure page in an addon then one and only one must be marked as "isDefault" : true
+     */
     @CommonSchemaAttributes(defaultValue = "false")
     private Boolean isDefault;
 
@@ -42,9 +45,6 @@ public class ConfigurePageModuleBean extends ConnectPageModuleBean
         }
     }
 
-    /**
-     * If there is more than one configure page in an addon then one and only one must be marked as default=true
-     */
     public Boolean isDefault()
     {
         return isDefault == null ? false : isDefault;
