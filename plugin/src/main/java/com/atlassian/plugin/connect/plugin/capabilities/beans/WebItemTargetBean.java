792e632fb011078cf20625daacda56934e02bcde
Seb Ruiz <sruiz@atlassian.com>
2013-12-19 09:24:25 +1100

    Fix tests

--- plugin/src/main/java/com/atlassian/plugin/connect/plugin/capabilities/beans/WebItemTargetBean.java
+++ plugin/src/main/java/com/atlassian/plugin/connect/plugin/capabilities/beans/WebItemTargetBean.java
@@ -10,8 +10,7 @@ import java.util.Map;
 
 /**
  * Defines the way a web item link is opened in the browser, such as in a modal or inline dialog.
- * @exampleJson {@see com.atlassian.plugin.connect.plugin.capabilities.beans.ConnectJsonExamples#WEBITEM_TARGET_EXAMPLE}
- * @schemaTitle Web Item Target
+ *
  * @since 1.0
  */
 public class WebItemTargetBean extends BeanWithParams
@@ -31,6 +30,10 @@ public class WebItemTargetBean extends BeanWithParams
         if (null == type)
         {
             type = WebItemTargetType.page;
+        }
+        if (null == options)
+        {
+            options = Collections.emptyMap();
         }
     }
 
