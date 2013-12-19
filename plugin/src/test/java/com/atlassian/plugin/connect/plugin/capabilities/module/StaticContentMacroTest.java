aa45682c44029bc86f390373fdd81439bdc8c2ef
Patrick Streule <pstreule@atlassian.com>
2013-12-19 19:28:06 +1100

    ACDEV-677: Add more tests

--- plugin/src/test/java/com/atlassian/plugin/connect/plugin/capabilities/module/StaticContentMacroTest.java
+++ plugin/src/test/java/com/atlassian/plugin/connect/plugin/capabilities/module/StaticContentMacroTest.java
@@ -1,21 +1,59 @@
 package com.atlassian.plugin.connect.plugin.capabilities.module;
 
-import org.junit.Before;
-import org.junit.Test;
+import com.atlassian.confluence.content.render.xhtml.ConversionContext;
+import com.atlassian.plugin.connect.plugin.capabilities.beans.StaticContentMacroModuleBean;
+import com.atlassian.plugin.connect.plugin.capabilities.beans.builder.StaticContentMacroModuleBeanBuilder;
+import com.atlassian.plugin.connect.plugin.module.confluence.MacroContentManager;
+import com.atlassian.plugin.connect.spi.RemotablePluginAccessor;
+import com.atlassian.plugin.connect.spi.http.HttpMethod;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatcher;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.net.URI;
+import java.util.Map;
+
+import static com.atlassian.plugin.connect.plugin.capabilities.beans.StaticContentMacroModuleBean.newStaticContentMacroModuleBean;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.argThat;
+import static org.mockito.Mockito.verify;
+
 @RunWith(MockitoJUnitRunner.class)
-public class StaticContentMacroTest
+public class StaticContentMacroTest extends AbstractContentMacroTest<StaticContentMacroModuleBean, StaticContentMacro, StaticContentMacroModuleBeanBuilder>
 {
-    @Before
-    public void beforeEachTest()
+    @Mock
+    private MacroContentManager macroContentManager;
+
+
+    @Override
+    protected StaticContentMacroModuleBeanBuilder createBeanBuilder()
     {
+        return newStaticContentMacroModuleBean();
     }
 
-    @Test
-    public void pageIdIsReplaced()
+    protected StaticContentMacro createMacro(StaticContentMacroModuleBean bean)
     {
-
+        return new StaticContentMacro("my-plugin", bean, userManager, macroContentManager, remotablePluginAccessorFactory, urlVariableSubstitutor);
     }
+
+    protected void verifyRendererInvokedWithQueryParameter(String name, String value) throws Exception
+    {
+        verify(macroContentManager).getStaticContent(any(HttpMethod.class), any(URI.class), argThat(hasQueryParam(name, value)),
+                any(ConversionContext.class), any(RemotablePluginAccessor.class));
+    }
+
+    private ArgumentMatcher<Map<String, String>> hasQueryParam(final String name, final String value)
+    {
+        return new ArgumentMatcher<Map<String, String>>()
+        {
+            @Override
+            public boolean matches(Object actual)
+            {
+                Map<String, String[]> map = (Map<String, String[]>) actual;
+                return map.containsKey(name) && value.equals(map.get(name));
+            }
+        };
+    }
+
 }
