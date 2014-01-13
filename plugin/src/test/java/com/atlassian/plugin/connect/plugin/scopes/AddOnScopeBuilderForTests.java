1a5f37a2fdb6880acf9220933d2fd3e5735e03ef
Peter Brownlow <pbrownlow@atlassian.com>
2014-01-13 11:03:52 +1100

    ACDEV-834: corrected static unit test scopes data now that JSON descriptors use regex paths

--- plugin/src/test/java/com/atlassian/plugin/connect/plugin/scopes/AddOnScopeBuilderForTests.java
+++ plugin/src/test/java/com/atlassian/plugin/connect/plugin/scopes/AddOnScopeBuilderForTests.java
@@ -12,15 +12,15 @@ public class AddOnScopeBuilderForTests
     public static Matcher<Collection<AddOnScope>> buildReadScope()
     {
         AddOnScopeApiPath path = new AddOnScopeApiPath.RestApiPath(asList(
-                new RestApiScopeHelper.RestScope("mywork", asList("1", "latest"), "/notification", asList("get")),
-                new RestApiScopeHelper.RestScope("mywork", asList("1", "latest"), "/status", asList("get")),
-                new RestApiScopeHelper.RestScope("mywork", asList("1", "latest"), "/task", asList("get")),
-                new RestApiScopeHelper.RestScope("prototype", asList("1", "latest"), "/search", asList("get")),
-                new RestApiScopeHelper.RestScope("prototype", asList("1", "latest"), "/content", asList("get")),
-                new RestApiScopeHelper.RestScope("prototype", asList("1", "latest"), "/attachment", asList("get")),
-                new RestApiScopeHelper.RestScope("prototype", asList("1", "latest"), "/breadcrumb", asList("get")),
-                new RestApiScopeHelper.RestScope("prototype", asList("1", "latest"), "/space", asList("get")),
-                new RestApiScopeHelper.RestScope("ui", asList("1", "1.0", "latest"), "/content", asList("get"))
+                new RestApiScopeHelper.RestScope("mywork", asList("1", "latest"), "/notification.*", asList("get"), true),
+                new RestApiScopeHelper.RestScope("mywork", asList("1", "latest"), "/status.*", asList("get"), true),
+                new RestApiScopeHelper.RestScope("mywork", asList("1", "latest"), "/task.*", asList("get"), true),
+                new RestApiScopeHelper.RestScope("prototype", asList("1", "latest"), "/search.*", asList("get"), true),
+                new RestApiScopeHelper.RestScope("prototype", asList("1", "latest"), "/content.*", asList("get"), true),
+                new RestApiScopeHelper.RestScope("prototype", asList("1", "latest"), "/attachment.*", asList("get"), true),
+                new RestApiScopeHelper.RestScope("prototype", asList("1", "latest"), "/breadcrumb.*", asList("get"), true),
+                new RestApiScopeHelper.RestScope("prototype", asList("1", "latest"), "/space.*", asList("get"), true),
+                new RestApiScopeHelper.RestScope("ui", asList("1", "1.0", "latest"), "/content.*", asList("get"), true)
         ));
         Matcher<AddOnScope> readScope = new AddOnScopeMatcher("READ", asList(path));
         return new AddOnScopesMatcher(asList(readScope));
