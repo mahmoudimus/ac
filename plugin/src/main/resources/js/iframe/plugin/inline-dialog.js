4155010fedc39cd49fe403eb3dba2890712e3bbc
Chris Whittington <cwhittington@atlassian.com>
2014-01-22 10:45:21 +1100

    inline dialog pr feedback

--- plugin/src/main/resources/js/iframe/plugin/inline-dialog.js
+++ plugin/src/main/resources/js/iframe/plugin/inline-dialog.js
@@ -1,12 +1,12 @@
 AP.define("inline-dialog", ["_dollar", "_rpc"],
 
-  /**
-   * The Inline Dialog module provides a mechanism for launching inline dialogs containing the connect add-on's iframe.
-   * A modal inline dialog displays information without requiring the user to leave the current page.
-   *
-   * For more information, read about the Atlassian User Interface [inline dialog component](https://docs.atlassian.com/aui/latest/docs/inlineDialog.html).
-   * @exports inline-dialog
-   */
+/**
+* The inline dialog is a wrapper for secondary content/controls to be displayed on user request. Consider this component as displayed in context to the triggering control with the dialog overlaying the page content.
+* A inline dialog should be preferred over a modal dialog when a connection between the action has a clear benefit versus having a lower user focus.
+*
+* For more information, read about the Atlassian User Interface [inline dialog component](https://docs.atlassian.com/aui/latest/docs/inlineDialog.html).
+* @exports inline-dialog
+*/
 
 function ($, rpc) {
     "use strict";
