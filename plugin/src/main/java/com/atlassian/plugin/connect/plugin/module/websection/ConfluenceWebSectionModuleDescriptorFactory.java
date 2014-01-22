722e8f9cadf2ff300d47b26548e498878e962c3b
Seb Ruiz <sruiz@atlassian.com>
2014-01-22 18:24:23 +1100

    AC-369: You can't autowire when there are no arguments

--- plugin/src/main/java/com/atlassian/plugin/connect/plugin/module/websection/ConfluenceWebSectionModuleDescriptorFactory.java
+++ plugin/src/main/java/com/atlassian/plugin/connect/plugin/module/websection/ConfluenceWebSectionModuleDescriptorFactory.java
@@ -1,7 +1,6 @@
 package com.atlassian.plugin.connect.plugin.module.websection;
 
 import com.atlassian.confluence.plugin.descriptor.web.descriptors.ConfluenceWebSectionModuleDescriptor;
-import com.atlassian.plugin.connect.spi.RemotablePluginAccessor;
 import com.atlassian.plugin.spring.scanner.annotation.component.ConfluenceComponent;
 import com.atlassian.plugin.web.descriptors.WebSectionModuleDescriptor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 @ConfluenceComponent
 public class ConfluenceWebSectionModuleDescriptorFactory implements ProductSpecificWebSectionModuleDescriptorFactory
 {
-    @Autowired
     public ConfluenceWebSectionModuleDescriptorFactory()
     {
     }
