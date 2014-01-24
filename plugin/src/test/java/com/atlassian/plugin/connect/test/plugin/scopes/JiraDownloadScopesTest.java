807be1f825bb964c12f4cc730c3ecc155a388606
Peter Brownlow <pbrownlow@atlassian.com>
2014-01-24 14:28:27 +1100

    ACDEV-891: moved JiraDownloadScopesTest to the same package as its sibling test classes

--- plugin/src/test/java/com/atlassian/plugin/connect/test/plugin/scopes/JiraDownloadScopesTest.java
+++ plugin/src/test/java/com/atlassian/plugin/connect/test/plugin/scopes/JiraDownloadScopesTest.java
@@ -1,8 +1,7 @@
-package com.atlassian.plugin.connect.plugin.scopes;
+package com.atlassian.plugin.connect.test.plugin.scopes;
 
 import com.atlassian.plugin.connect.modules.beans.nested.ScopeName;
 import com.atlassian.plugin.connect.spi.http.HttpMethod;
-import com.atlassian.plugin.connect.test.plugin.scopes.AbstractScopesTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
