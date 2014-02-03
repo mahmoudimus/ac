3d4ef87a9d7df30054f9ec4efc0b34313c37e112
Seb Ruiz <sruiz@atlassian.com>
2014-02-03 16:44:50 +1100

    Manifest deploy: only ask for password once

--- bin/manifest-deploy.sh
+++ bin/manifest-deploy.sh
@@ -10,8 +10,12 @@ USER=$2
 
 PLUGIN=atlassian-connect-plugin
 
+echo -n "Password: "
+read -s PASSWORD
+echo ""
+
 echo "Promoting atlassian-connect:$VERSION to jira dev"
-curl -u$USER -XPOST https://manifesto.uc-inf.net/api/env/jirastudio-dev/product/jira/plugin/$PLUGIN -H"Content-Type: application/json" -d "{ \"version\": \"$VERSION\" }"
+curl -u$USER:$PASSWORD -XPOST https://manifesto.uc-inf.net/api/env/jirastudio-dev/product/jira/plugin/$PLUGIN -H"Content-Type: application/json" -d "{ \"version\": \"$VERSION\" }"
 echo ""
 echo "Promoting atlassian-connect:$VERSION to conf dev"
-curl -u$USER -XPOST https://manifesto.uc-inf.net/api/env/jirastudio-dev/product/confluence/plugin/$PLUGIN -H"Content-Type: application/json" -d "{ \"version\": \"$VERSION\" }"
+curl -u$USER:$PASSWORD -XPOST https://manifesto.uc-inf.net/api/env/jirastudio-dev/product/confluence/plugin/$PLUGIN -H"Content-Type: application/json" -d "{ \"version\": \"$VERSION\" }"
