74ba54e8d12308141a072d43a5ce9fe9d166f70f
Jonathan Doklovic <doklovic@atlassian.com>
2014-02-04 16:19:19 -0800

    ACDEV-939: javadoc for ConnectAddonRegistry

--- plugin/src/main/java/com/atlassian/plugin/connect/plugin/installer/ConnectAddonRegistry.java
+++ plugin/src/main/java/com/atlassian/plugin/connect/plugin/installer/ConnectAddonRegistry.java
@@ -1,5 +1,11 @@
 package com.atlassian.plugin.connect.plugin.installer;
 
+/**
+ * The ConnectAddonRegistry is used to store information about connect addons so they can be retrieved at runtime without having to inspect the plugin jar or constantly marshall the json descriptor
+ * This is simply a place to centralize access to common bits of info used in various components. Things like the json descriptor, baseurl, etc should be stored here.
+ * We should NOT go overboard here as many values can either be calculated or are not used often enough to store here.
+ * We should only store values that we are going to need often across multiple components in here.
+ */
 public interface ConnectAddonRegistry
 {
 
