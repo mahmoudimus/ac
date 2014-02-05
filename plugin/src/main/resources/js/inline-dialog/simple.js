1a49b865f05aeacefedeba977228ceb6b79b2a14
Sam Day <sday@atlassian.com>
2014-02-05 17:18:20 +1100

    ACDEV-967: fix AP.request calls from remote iframes in inline-dialogs

--- plugin/src/main/resources/js/inline-dialog/simple.js
+++ plugin/src/main/resources/js/inline-dialog/simple.js
@@ -20,6 +20,9 @@ _AP.define("inline-dialog/simple", ["_dollar", "host/_status_helper"], function(
             if (!options.ns) {
                 options.ns = itemId;
             }
+            if (!options.cp) {
+                options.cp = AJS.Meta.get("context-path");
+            }
             options.container = options.ns;
             options.src = options.url = options.url || contentUrl;
             content.data('inlineDialog', $inlineDialog);
