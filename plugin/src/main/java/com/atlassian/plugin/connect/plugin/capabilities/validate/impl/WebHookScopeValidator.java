3f5b00960825215669d3c60c9eec1794d8eb43d9
Tim Pettersen <tim@atlassian.com>
2014-02-10 12:33:32 -0800

    ACDEV-884: wired tests for add-on bean validation

--- plugin/src/main/java/com/atlassian/plugin/connect/plugin/capabilities/validate/impl/WebHookScopeValidator.java
+++ plugin/src/main/java/com/atlassian/plugin/connect/plugin/capabilities/validate/impl/WebHookScopeValidator.java
@@ -7,6 +7,8 @@ import com.atlassian.plugin.connect.modules.beans.nested.ScopeName;
 import com.atlassian.plugin.connect.plugin.capabilities.WebHookScopeService;
 import com.atlassian.plugin.connect.plugin.capabilities.validate.AddOnBeanValidator;
 import com.atlassian.plugin.connect.plugin.descriptor.InvalidDescriptorException;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.inject.Named;
@@ -17,7 +19,7 @@ import javax.inject.Named;
  *
  * @since 1.0
  */
-@Named("web-hook-scopes-validator")
+@Named ("web-hook-scopes-validator")
 public class WebHookScopeValidator implements AddOnBeanValidator
 {
     private final WebHookScopeService webHookScopeService;
@@ -33,15 +35,31 @@ public class WebHookScopeValidator implements AddOnBeanValidator
     {
         for (WebHookModuleBean webHookModuleBean : addon.getModules().getWebhooks())
         {
-            ScopeName requiredScope = webHookScopeService.getRequiredScope(webHookModuleBean.getEvent());
+            final ScopeName requiredScope = webHookScopeService.getRequiredScope(webHookModuleBean.getEvent());
 
-            if (!addon.getScopes().contains(requiredScope))
+            if (!Iterables.any(addon.getScopes(), new ImpliedScopePredicate(requiredScope)))
             {
                 String exceptionMessage = String.format("Add-on '%s' requests web hook '%s' but not the '%s' scope "
                         + "required to receive it. Please request this scope in your descriptor.", plugin.getKey(),
                         webHookModuleBean.getEvent(), requiredScope);
                 throw new InvalidDescriptorException(exceptionMessage, "connect.install.error.missing.scope." + requiredScope.name());
             }
+        }
+    }
+
+    private static class ImpliedScopePredicate implements Predicate<ScopeName>
+    {
+        private final ScopeName requiredScope;
+
+        private ImpliedScopePredicate(final ScopeName requiredScope)
+        {
+            this.requiredScope = requiredScope;
+        }
+
+        @Override
+        public boolean apply(final ScopeName requestedScope)
+        {
+            return requestedScope == requiredScope || requestedScope.getImplied().contains(requiredScope);
         }
     }
 }
