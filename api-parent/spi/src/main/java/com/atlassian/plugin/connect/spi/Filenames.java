ba034ed90bb84df7671ce4ceb34fbd572d20323d
Jonathan Doklovic <doklovic@atlassian.com>
2014-02-11 14:06:27 -0800

    ACDEV-987: removing unused constants in Filenames

--- api-parent/spi/src/main/java/com/atlassian/plugin/connect/spi/Filenames.java
+++ api-parent/spi/src/main/java/com/atlassian/plugin/connect/spi/Filenames.java
@@ -4,6 +4,4 @@ public class Filenames
 {
     public static final String ATLASSIAN_ADD_ON_JSON = "atlassian-connect.json";
     public static final String ATLASSIAN_PLUGIN_XML = "atlassian-plugin.xml";
-    public static final String ADDON_IMPORTS_XML = "META-INF/spring/atlassian-plugins-component-imports.xml";
-    public static final String ADDON_IMPORTS_RESOURCE = "/addon-spring/atlassian-plugins-component-imports.xml";
 }
