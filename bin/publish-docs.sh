f7f8580f231bfa2e590754d8beb5a673c8cc8be9
Seb Ruiz <sruiz@atlassian.com>
2014-02-20 18:24:50 +1100

    Fix doc publishing script

--- bin/publish-docs.sh
+++ bin/publish-docs.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-SSH='ssh'
+SSH="ssh"
 
-if [ -z "$1" ]; then
+if [ -n "$1" ]; then
     SSH=$1
 fi
 
 cd docs
 npm i
 npm run-script build
-rsync -avz --delete -e $SSH target/www/* uploads@developer-app.internal.atlassian.com:/opt/j2ee/domains/atlassian.com/developer-prod/static-content/static/connect/docs
+
+rsync -avz --delete -e 'ssh' target/www/* uploads@developer-app.internal.atlassian.com:/opt/j2ee/domains/atlassian.com/developer-prod/static-content/static/connect/docs
