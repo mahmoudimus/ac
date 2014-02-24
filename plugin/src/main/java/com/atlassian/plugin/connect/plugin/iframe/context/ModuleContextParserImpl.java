7559d9f9596c81fe4a5866c18375c3e4b5835b02
Anders Holmgren <aholmgren@atlassian.com>
2014-02-24 16:13:41 +1100

    ACDEV-845 extracted json context part for easier testing

--- plugin/src/main/java/com/atlassian/plugin/connect/plugin/iframe/context/ModuleContextParserImpl.java
+++ plugin/src/main/java/com/atlassian/plugin/connect/plugin/iframe/context/ModuleContextParserImpl.java
@@ -31,16 +31,11 @@ import static com.google.common.base.Preconditions.checkNotNull;
 @Component
 public class ModuleContextParserImpl implements ModuleContextParser
 {
-    public static final String CONTEXT_PARAMETER_KEY = "product-context";
-    private static final Joiner PATH_COMPONENT_JOINER = Joiner.on('.');
     private static final Logger log = LoggerFactory.getLogger(ModuleContextParserImpl.class);
-    private static final TypeReference<HashMap<String, Object>> MAP_TYPE_REFERENCE =
-            new TypeReference<HashMap<String, Object>>()
-            {
-            };
 
 
     private final ModuleContextFilter moduleContextFilter;
+    private final ModuleContextJsonExtractor moduleContextJsonExtractor = new ModuleContextJsonExtractor();
 
     @Autowired
     public ModuleContextParserImpl(ModuleContextFilter moduleContextFilter)
@@ -52,7 +47,7 @@ public class ModuleContextParserImpl implements ModuleContextParser
     public ModuleContextParameters parseContextParameters(final HttpServletRequest req)
     {
         ModuleContextParameters unfiltered = new HashMapModuleContextParameters();
-        final Map<String, String[]> parameterMap = tryExtractContextFromJson(req.getParameterMap());
+        final Map<String, String[]> parameterMap = moduleContextJsonExtractor.tryExtractContextFromJson(req.getParameterMap());
         for (Map.Entry<String, String[]> entry : parameterMap.entrySet())
         {
             String key = entry.getKey();
@@ -67,107 +62,5 @@ public class ModuleContextParserImpl implements ModuleContextParser
         return moduleContextFilter.filter(unfiltered);
     }
 
-
-    // the product context may be passed from the client as a json object. If so we pull them out and pretend they were request params. Not pretty but hey
-    private Map<String, String[]> tryExtractContextFromJson(Map<String, String[]> requestParams) throws InvalidContextParameterException
-    {
-        if (!requestParams.containsKey(CONTEXT_PARAMETER_KEY))
-        {
-            return requestParams;
-        }
-
-        final String[] contextParam = requestParams.get(CONTEXT_PARAMETER_KEY);
-        if (contextParam == null)
-            return ImmutableMap.copyOf(requestParams);
-
-        final String contextJsonStr = contextParam[0];
-        ObjectMapper objectMapper = new ObjectMapper();
-        try
-        {
-            Map<String, Object> contextMap = objectMapper.readValue(contextJsonStr, MAP_TYPE_REFERENCE);
-
-            final Map<String, String[]> mutableParams = Maps.newHashMap(requestParams);
-            mutableParams.remove(CONTEXT_PARAMETER_KEY);
-            final Map<String, String[]> contextParams = transformToPathForm(contextMap);
-            checkSameParams(mutableParams, contextParams);
-            return ImmutableMap.<String, String[]>builder()
-                    .putAll(mutableParams)
-                    // context params take precedence and will overwrite any params with same key from the url query
-                    .putAll(contextParams)
-                    .build();
-        }
-        catch (IOException e)
-        {
-            throw new InvalidContextParameterException("Failed to parse context Json", e);
-        }
-
-    }
-
-    private void checkSameParams(Map<String, String[]> mutableParams, Map<String, String[]> contextParams)
-    {
-        for (String key : contextParams.keySet())
-        {
-            if (mutableParams.containsKey(key))
-            {
-                log.warn("same parameter key ({}) found in both url query and context json. Value from URL query will be overridden");
-            }
-        }
-    }
-
-    private Map<String, String[]> transformToPathForm(Map<String, Object> nestedMapsParams)
-    {
-        final ImmutableMap.Builder<String, String[]> builder = ImmutableMap.builder();
-        final Iterable<Pair<List<String>, String[]>> pairs = transformToPathFormPairs(nestedMapsParams);
-        for (Pair<List<String>, String[]> pair : pairs)
-        {
-            builder.put(createPath(pair.getLeft()), pair.getRight());
-        }
-
-        return builder.build();
-    }
-
-    private String createPath(List<String> pathComponents)
-    {
-        return PATH_COMPONENT_JOINER.join(pathComponents);
-    }
-
-    private Iterable<Pair<List<String>, String[]>> transformToPathFormPairs(Map<?, ?> nestedMapsParams)
-    {
-        final ImmutableList.Builder<Pair<List<String>, String[]>> resultBuilder = ImmutableList.builder();
-
-        for (final Map.Entry<?, ?> entry : nestedMapsParams.entrySet())
-        {
-            Iterable<Pair<List<String>, String[]>> pairs = transformNestedValue(entry.getValue());
-            // prepend our key to list of path components
-            final Iterable<Pair<List<String>, String[]>> result =
-                    Iterables.transform(pairs, new Function<Pair<List<String>, String[]>, Pair<List<String>, String[]>>()
-                    {
-                        @Override
-                        public Pair<List<String>, String[]> apply(@Nullable Pair<List<String>, String[]> pair)
-                        {
-                            checkNotNull(pair);
-                            final ImmutableList.Builder<String> builder = ImmutableList.builder();
-                            builder.add(ObjectUtils.toString(entry.getKey()));
-                            builder.addAll(pair.getLeft());
-                            return Pair.of((List<String>) builder.build(), pair.getValue());
-                        }
-                    });
-
-            resultBuilder.addAll(result);
-        }
-
-
-        return resultBuilder.build();
-    }
-
-    private Iterable<Pair<List<String>, String[]>> transformNestedValue(Object value)
-    {
-        if (value instanceof Map)
-            return transformToPathFormPairs((Map<?, ?>) value);
-
-        String[] arrValue = value instanceof String[] ? (String[]) value : new String[]{value.toString()};
-
-        return ImmutableList.of(Pair.of((List<String>) ImmutableList.<String>of(), arrValue));
-    }
 
 }
---
+++ plugin/src/test/java/com/atlassian/plugin/connect/plugin/iframe/context/ModuleContextJsonExtractorTest.java
@@ -0,0 +1,68 @@
+package com.atlassian.plugin.connect.plugin.iframe.context;
+
+import java.util.Map;
+
+import com.google.common.collect.ImmutableMap;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static com.atlassian.plugin.connect.plugin.iframe.context.ModuleContextJsonExtractor.CONTEXT_PARAMETER_KEY;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasEntry;
+import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class ModuleContextJsonExtractorTest
+{
+    ModuleContextJsonExtractor extractor = new ModuleContextJsonExtractor();
+
+    @Test
+    public void shouldConvertToNestedForm() //throws MalformedRequestException, UnauthorisedException, ResourceNotFoundException
+    {
+        final Map<String, String[]> requestParams = ImmutableMap.<String, String[]>of(
+                "project.id", new String[]{"10"},
+                "project.key", new String[]{"myKey"},
+                "other.blah", new String[]{"stuff"});
+
+        final Map<String, String[]> paramsInNestedForm = extractor.tryExtractContextFromJson(requestParams);
+        assertThat(paramsInNestedForm.entrySet(), hasSize(2));
+
+        final Object projectMapObj = paramsInNestedForm.get("project");
+        assertThat(projectMapObj, is(notNullValue()));
+        assertThat(projectMapObj, is(instanceOf(Map.class)));
+        Map<?, ?> projectMap = (Map<?, ?>) projectMapObj;
+        assertThat(projectMap.get("id"), is(equalTo((Object) "10")));
+        assertThat(projectMap.get("key"), is(equalTo((Object) "myKey")));
+
+
+        final Object otherMapObj = paramsInNestedForm.get("other");
+        assertThat(otherMapObj, is(notNullValue()));
+        assertThat(otherMapObj, is(instanceOf(Map.class)));
+        Map<?, ?> otherMap = (Map<?, ?>) otherMapObj;
+        assertThat(otherMap.get("blah"), is(equalTo((Object) "stuff")));
+    }
+
+    @Test
+    public void shouldExtractContextAndAddToParamMap()
+    {
+        String[] contextStr = new String[]{"{\"project\":{\"id\":10100}}"};
+
+        Map<String, String[]> requestParams = ImmutableMap.of(CONTEXT_PARAMETER_KEY, contextStr, "someparam", new String[]{"somevalue"});
+
+        final Map<String, String[]> contextMap = extractor.tryExtractContextFromJson(requestParams);
+        assertThat(contextMap, hasEntry("project.id", new String[]{"10100"}));
+        assertThat(contextMap, hasEntry("someparam", new String[]{"somevalue"}));
+        assertThat(contextMap, not(hasKey((Object) CONTEXT_PARAMETER_KEY)));
+        assertThat(contextMap.size(), is(2));
+    }
+
+    // TODO: more tests. negative tests. corner cases
+}
