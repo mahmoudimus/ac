60433b574f7a8b497144964f6bee9c0bdba08766
Tim Pettersen <tim@atlassian.com>
2014-02-27 14:07:13 -0800

    AC-992: assert standard and context parameters are passed to general pages

--- integration-tests/src/test/java/it/capabilities/confluence/TestGeneralPage.java
+++ integration-tests/src/test/java/it/capabilities/confluence/TestGeneralPage.java
@@ -19,11 +19,14 @@ import redstone.xmlrpc.XmlRpcFault;
 
 import java.net.MalformedURLException;
 import java.net.URI;
+import java.util.Map;
 
 import static com.atlassian.fugue.Option.some;
 import static com.atlassian.plugin.connect.modules.beans.ConnectPageModuleBean.newPageBean;
+import static it.capabilities.ConnectAsserts.verifyContainsStandardAddOnQueryParamters;
 import static it.servlet.condition.ToggleableConditionServlet.toggleableConditionBean;
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.hasEntry;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -74,7 +77,7 @@ public class TestGeneralPage extends ConfluenceWebDriverTestBase
     {
         loginAsAdmin();
 
-        createAndVisitViewPage();
+        ConfluenceViewPage createdPage = createAndVisitViewPage();
         ConfluenceGeneralPage generalPage = product.getPageBinder().bind(ConfluenceGeneralPage.class, PAGE_KEY,
                 "My Awesome Page", true);
 
@@ -86,6 +89,11 @@ public class TestGeneralPage extends ConfluenceWebDriverTestBase
         RemotePluginTestPage addonContentsPage = generalPage.clickRemotePluginLink();
 
         assertThat(addonContentsPage.isFullSize(), is(true));
+
+        // check iframe url params
+        Map<String,String> iframeQueryParams = addonContentsPage.getIframeQueryParams();
+        verifyContainsStandardAddOnQueryParamters(iframeQueryParams, product.getProductInstance().getContextPath());
+        assertThat(iframeQueryParams, hasEntry("page_id", createdPage.getPageId()));
     }
 
     @Test
