9cf7c3301f23854da4e528bc14c0377c791b28c0
Seb Ruiz <sruiz@atlassian.com>
2014-02-27 14:27:34 +1100

    Print used conditions

--- bin/marketplace/download-connect-descriptors.js
+++ bin/marketplace/download-connect-descriptors.js
@@ -3,30 +3,14 @@
 var request = require('request'),
     _ = require('lodash'),
     fs = require('fs'),
+    extend = require('node.extend'),
+    nomnom = require('nomnom'),
+    xml2js = require('xml2js'),
     util = require('util');
 
-var downloadDestination = "descriptors/",
-    baseUrl = "https://marketplace.atlassian.com";
-    uri = baseUrl + "/rest/1.0/plugins?hosting=ondemand&addOnType=three&includePrivate=true&limit=50";
-
-var opts = require("nomnom")
-   .option('debug', {
-      abbr: 'd',
-      flag: true,
-      help: 'Print debugging info'
-   })
-   .option('user', {
-      abbr: 'u',
-      help: 'Marketplace username'
-   })
-   .option('pass', {
-      help: 'Marketplace password'
-   })
-   .parse();
-
-var getAddonPage = function (uri) {
+var getAddonPage = function (opts) {
     return request({
-        uri: uri,
+        uri: opts.baseUrl + opts.uri,
         method: "GET",
         username: opts.user,
         password: opts.pass,
@@ -60,17 +44,19 @@ var getAddonPage = function (uri) {
 
                 var descriptorUrl = _.find(version.links, { 'rel': 'descriptor' }).href;
 
-                downloadDescriptor(key, descriptorUrl);
+                downloadDescriptor(opts, key, descriptorUrl);
             });
 
             if (!!nextRequestUri) {
-                getAddonPage(baseUrl + nextRequestUri);
+                var nextOpts = extend({}, opts);
+                nextOpts.uri = nextRequestUri;
+                getAddonPage(nextOpts);
             }
         }
     });
 };
 
-var downloadDescriptor = function (addonKey, descriptorUrl) {
+var downloadDescriptor = function (opts, addonKey, descriptorUrl) {
     request({
         uri: descriptorUrl,
         method: "GET"
@@ -78,24 +64,83 @@ var downloadDescriptor = function (addonKey, descriptorUrl) {
         if (error) {
             console.log("Unable to download descriptor for add-on", addonKey);
         } else { 
-
-            var fileName = "descriptors/" + addonKey + '-descriptor';
-            fs.writeFile(fileName, body, function (err) {
+            var type = 'xml';
+            try {
+                JSON.parse(body);
+                type = 'json';
+            } catch (e) {
+            }
+            var filename = "descriptors/" + addonKey + '-descriptor' + "." + type;
+            fs.writeFile(filename, body, function (err) {
                 if (err) {
                     console.log("Unable to write descriptor for add-on " + addonKey + " to disk", err);
-                } else {
-                    console.log("Downloaded descriptor", fileName);
+                    return;
+                }
+
+                if (opts.debug) {
+                    console.log(type, '\t', filename);
+                }
+
+                if (opts.descriptorDownloadedCallback) {
+                    opts.descriptorDownloadedCallback(addonKey, filename, type, body, opts);
                 }
             });
         }
     });
 }
 
-function go() {
-    if (!fs.existsSync(downloadDestination)) {
-        fs.mkdirSync(downloadDestination);
+var getCliOpts = function () {
+    return nomnom
+       .option('debug', {
+          abbr: 'd',
+          flag: true,
+          help: 'Print debugging info'
+       })
+       .option('user', {
+          abbr: 'u',
+          help: 'Marketplace username'
+       })
+       .option('pass', {
+          help: 'Marketplace password'
+       })
+       .parse();
+}
+
+function go(opts) {
+    opts = extend(opts, getCliOpts());
+    if (!fs.existsSync(opts.downloadDestination)) {
+        fs.mkdirSync(opts.downloadDestination);
     }
-    getAddonPage(uri);
+    getAddonPage(opts);
 };
 
-go();
+go({
+    baseUrl: "https://marketplace.atlassian.com",
+    uri: "/rest/1.0/plugins?hosting=ondemand&addOnType=three&includePrivate=true&limit=50",
+    downloadDestination: "descriptors/",
+    descriptorDownloadedCallback: function (addonKey, filename, type, body, opts) {
+        var conditions = [];
+        if (type === 'json') {
+            var descriptor = JSON.parse(body);
+            _.forEach(descriptor.modules, function (module) {
+                if (module.conditions) {
+                    conditions.push(module.conditions);
+                }
+            });
+        } else {
+            xml2js.parseString(body, function (err, descriptor) {
+                _.forEach(descriptor['atlassian-plugin'], function (module) {
+                    _.forEach(module, function (m) {
+                        if (m.conditions) {
+                            conditions.push(m.conditions);
+                        }
+                    })
+                });
+            });
+        }
+
+        if (conditions.length) {
+            console.log(addonKey + " : " + util.inspect(conditions, { depth: 5, colors: true }));
+        }
+    }
+});
