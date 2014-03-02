8e5aafac743d709761bf018926d499a69e290196
Anders Holmgren <aholmgren@atlassian.com>
2014-03-03 10:06:21 +1100

    ACDEV-1095 add javadocs

--- plugin/src/main/java/com/atlassian/plugin/connect/plugin/iframe/render/uri/IFrameUriBuilder.java
+++ plugin/src/main/java/com/atlassian/plugin/connect/plugin/iframe/render/uri/IFrameUriBuilder.java
@@ -66,6 +66,12 @@ public interface IFrameUriBuilder
          */
         InitializedBuilder includeStandardParams(boolean includeStandardParams);
 
+        /**
+         * uiParameters is an opaque (to the server) object where the client code can pass in the request and have the server
+         * return it as part of the signed iFrame url. As it has it's own query parameter "ui-params" the contents should not
+         * be confused with the resource keys (e.g. issue.key) so we don't need to validate them even though they end up
+         * in the signed url
+         */
         InitializedBuilder uiParams(Option<String> uiParameters);
 
         /**
