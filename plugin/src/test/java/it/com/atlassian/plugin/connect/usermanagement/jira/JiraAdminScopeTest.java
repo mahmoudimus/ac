c616155b1961726fde361ce254adac85b4dc3cec
Peter Brownlow <pbrownlow@atlassian.com>
2014-03-04 17:12:22 +1100

    ACDEV-1112: added annotations to new jira test classes

--- plugin/src/test/java/it/com/atlassian/plugin/connect/usermanagement/jira/JiraAdminScopeTest.java
+++ plugin/src/test/java/it/com/atlassian/plugin/connect/usermanagement/jira/JiraAdminScopeTest.java
@@ -4,7 +4,12 @@ import com.atlassian.jira.security.GlobalPermissionManager;
 import com.atlassian.jwt.applinks.JwtApplinkFinder;
 import com.atlassian.plugin.connect.modules.beans.nested.ScopeName;
 import com.atlassian.plugin.connect.testsupport.TestPluginInstaller;
+import com.atlassian.plugins.osgi.test.Application;
+import com.atlassian.plugins.osgi.test.AtlassianPluginsTestRunner;
+import org.junit.runner.RunWith;
 
+@Application("jira")
+@RunWith(AtlassianPluginsTestRunner.class)
 public class JiraAdminScopeTest extends JiraAdminScopeTestBase
 {
     public JiraAdminScopeTest(TestPluginInstaller testPluginInstaller, JwtApplinkFinder jwtApplinkFinder, GlobalPermissionManager jiraPermissionManager)
