8a86d53afb8b417ee2078ec2e817e784af97d477
Seb Ruiz <sruiz@atlassian.com>
2014-03-06 14:31:59 +1100

    ACDEV-1128: Create healthcheck which validates all users in the atlassian-addons group have the correct email

---
+++ plugin/src/main/java/com/atlassian/plugin/connect/healthcheck/AtlassianAddonsGroupHealthCheck.java
@@ -0,0 +1,80 @@
+package com.atlassian.plugin.connect.healthcheck;
+
+import com.atlassian.crowd.exception.ApplicationNotFoundException;
+import com.atlassian.crowd.manager.application.ApplicationManager;
+import com.atlassian.crowd.manager.application.ApplicationService;
+import com.atlassian.crowd.model.application.Application;
+import com.atlassian.crowd.model.group.Group;
+import com.atlassian.crowd.model.user.User;
+import com.atlassian.crowd.search.EntityDescriptor;
+import com.atlassian.crowd.search.builder.QueryBuilder;
+import com.atlassian.crowd.search.builder.Restriction;
+import com.atlassian.crowd.search.query.QueryUtils;
+import com.atlassian.crowd.search.query.entity.EntityQuery;
+import com.atlassian.crowd.search.query.entity.GroupQuery;
+import com.atlassian.crowd.search.query.entity.restriction.constants.UserTermKeys;
+import com.atlassian.crowd.search.query.membership.MembershipQuery;
+import com.atlassian.healthcheck.core.DefaultHealthStatus;
+import com.atlassian.healthcheck.core.HealthCheck;
+import com.atlassian.healthcheck.core.HealthStatus;
+import com.atlassian.healthcheck.core.HealthStatusExtended;
+import com.atlassian.plugin.connect.plugin.usermanagement.ConnectAddOnUserGroupProvisioningService;
+import com.google.common.collect.Sets;
+
+import java.util.List;
+import java.util.Set;
+
+public class AtlassianAddonsGroupHealthCheck implements HealthCheck
+{
+    private static final String DESCRIPTION = "Ensures only connect add-ons are in the atlassian-addons group";
+
+
+    private final ApplicationManager applicationManager;
+    private final ApplicationService applicationService;
+    private final ConnectAddOnUserGroupProvisioningService groupProvisioningService;
+
+    public AtlassianAddonsGroupHealthCheck(ApplicationManager applicationManager, ApplicationService applicationService,
+            ConnectAddOnUserGroupProvisioningService groupProvisioningService)
+    {
+        this.applicationManager = applicationManager;
+        this.applicationService = applicationService;
+        this.groupProvisioningService = groupProvisioningService;
+    }
+
+    @Override
+    public HealthStatus check()
+    {
+        long time = System.currentTimeMillis();
+        try
+        {
+            Application application = applicationManager.findByName(groupProvisioningService.getCrowdApplicationName());
+
+            MembershipQuery<User> query = QueryBuilder
+                .queryFor(User.class, EntityDescriptor.user())
+                .childrenOf(EntityDescriptor.group())
+                .withName("atlassian-addons")
+                .startingAt(0)
+                .returningAtMost(EntityQuery.ALL_RESULTS);
+
+            List<User> users = applicationService.searchDirectGroupRelationships(application, query);
+
+            Set<User> usersWithIncorrectEmails = Sets.newHashSet();
+            for (User user : users)
+            {
+                if (!"noreply@mailer.atlassian.com".equals(user.getEmailAddress()))
+                {
+                    usersWithIncorrectEmails.add(user);
+                }
+            }
+
+            boolean isHealthy = usersWithIncorrectEmails.isEmpty();
+            String reason = isHealthy ? "" : "Add-on group has users with incorrect email addresses. This may indicate a customer license workaround.";
+            return new DefaultHealthStatus(isHealthy, reason, time, com.atlassian.healthcheck.core.Application.Plugin, HealthStatusExtended.Severity.CRITICAL, "");
+        }
+        catch (ApplicationNotFoundException e)
+        {
+            return new DefaultHealthStatus(false, "Could not find application " + e.getApplicationName(),
+                    time, com.atlassian.healthcheck.core.Application.Plugin, HealthStatusExtended.Severity.CRITICAL, null);
+        }
+    }
+}
