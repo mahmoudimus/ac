c84a746c0a84a83cb5788b58f58c4e4a373ecb6b
Anders Holmgren <aholmgren@atlassian.com>
2014-03-13 10:35:21 +1100

    ACDEV-1149 review updates

--- modules/src/test/java/com/atlassian/plugin/connect/modules/beans/nested/ScopeUtilTest.java
+++ modules/src/test/java/com/atlassian/plugin/connect/modules/beans/nested/ScopeUtilTest.java
@@ -1,168 +1,85 @@
 package com.atlassian.plugin.connect.modules.beans.nested;
 
+import javax.annotation.Nullable;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 
+import com.google.common.base.Function;
+import com.google.common.base.Predicate;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Sets;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-import static com.atlassian.plugin.connect.modules.beans.nested.ScopeName.ADMIN;
-import static com.atlassian.plugin.connect.modules.beans.nested.ScopeName.DELETE;
-import static com.atlassian.plugin.connect.modules.beans.nested.ScopeName.READ;
-import static com.atlassian.plugin.connect.modules.beans.nested.ScopeName.SPACE_ADMIN;
-import static com.atlassian.plugin.connect.modules.beans.nested.ScopeName.WRITE;
 import static com.atlassian.plugin.connect.modules.beans.nested.ScopeUtil.isTransitionDownToReadOrLess;
-import static com.atlassian.plugin.connect.modules.beans.nested.ScopeUtil.isTransitionUpFromReadOrLess;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+@RunWith(Parameterized.class)
 public class ScopeUtilTest
 {
-
     private static Set<ScopeName> EMPTY = ImmutableSet.of();
-
-    @Test
-    public void notTransitionDownWhenBothSetsEmpty()
+    private static final Set<ScopeName> ALL_SCOPES = ImmutableSet.copyOf(Arrays.asList(ScopeName.values()));
+    private static final Set<ScopeName> READ_OR_LESS = Sets.newHashSet(null, ScopeName.READ);
+    private static final Set<ScopeName> GREATER_THAN_READ = ImmutableSet.copyOf(Iterables.filter(ALL_SCOPES, new Predicate<ScopeName>()
     {
-        assertThat(isTransitionDownToReadOrLess(EMPTY, EMPTY), is(false));
+        @Override
+        public boolean apply(@Nullable ScopeName scope)
+        {
+            return !READ_OR_LESS.contains(scope);
+        }
+    }));
+
+    // Not transition down when previous is read or less
+    private static Collection<Object[]> PARAMS_NOT_TRANSITION_DOWN_WHEN_PREVIOUS_READ_OR_LESS =
+            generateTestParams(READ_OR_LESS, ALL_SCOPES, false);
+
+    private final boolean expectedResult;
+    private final Set<ScopeName> previousScopes;
+    private final Set<ScopeName> newScopes;
+
+    public ScopeUtilTest(boolean expectedResult, ScopeName previousTopScope, ScopeName newTopScope)
+    {
+
+        this.expectedResult = expectedResult;
+        this.previousScopes = set(previousTopScope);
+        this.newScopes = set(newTopScope);
+    }
+
+    private static Set<ScopeName> set(ScopeName scope)
+    {
+        return scope == null ? EMPTY : ImmutableSet.of(scope);
+    }
+
+    private static Collection<Object[]> generateTestParams(Set<ScopeName> prev, Set<ScopeName> nuevo, final boolean expected)
+    {
+        final Set<List<ScopeName>> variants = Sets.cartesianProduct(prev, nuevo);
+        final Iterable<Object[]> params = Iterables.transform(variants, new Function<List<ScopeName>, Object[]>()
+        {
+            @Override
+            public Object[] apply(@Nullable List<ScopeName> variant)
+            {
+                return new Object[]{variant.get(0), variant.get(1), expected};
+            }
+        });
+        return ImmutableList.copyOf(params);
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> parameters()
+    {
+        return PARAMS_NOT_TRANSITION_DOWN_WHEN_PREVIOUS_READ_OR_LESS;
     }
 
     @Test
-    public void notTransitionDownWhenPreviousEmpty()
+    public void notTransitionDownWhenPreviousReadOrLess()
     {
-        assertThat(isTransitionDownToReadOrLess(EMPTY, ImmutableSet.of(READ)), is(false));
+        assertThat(isTransitionDownToReadOrLess(previousScopes, newScopes), is(expectedResult));
     }
 
-    @Test
-    public void notTransitionDownWhenPreviousReadAndNewEmpty()
-    {
-        assertThat(isTransitionDownToReadOrLess(ImmutableSet.of(READ), EMPTY), is(false));
-    }
-
-    @Test
-    public void notTransitionDownWhenPreviousReadAndNewRead()
-    {
-        assertThat(isTransitionDownToReadOrLess(ImmutableSet.of(READ), ImmutableSet.of(READ)), is(false));
-    }
-
-    @Test
-    public void isTransitionDownWhenPreviousWriteAndNewEmpty()
-    {
-        assertThat(isTransitionDownToReadOrLess(ImmutableSet.of(WRITE), EMPTY), is(true));
-    }
-
-    @Test
-    public void isTransitionDownWhenPreviousDeleteAndNewEmpty()
-    {
-        assertThat(isTransitionDownToReadOrLess(ImmutableSet.of(DELETE), EMPTY), is(true));
-    }
-
-    @Test
-    public void isTransitionDownWhenPreviousSpaceAdminAndNewEmpty()
-    {
-        assertThat(isTransitionDownToReadOrLess(ImmutableSet.of(SPACE_ADMIN), EMPTY), is(true));
-    }
-
-    @Test
-    public void isTransitionDownWhenPreviousAdminAndNewEmpty()
-    {
-        assertThat(isTransitionDownToReadOrLess(ImmutableSet.of(ADMIN), EMPTY), is(true));
-    }
-
-    @Test
-    public void isTransitionDownWhenPreviousWriteAndNewRead()
-    {
-        assertThat(isTransitionDownToReadOrLess(ImmutableSet.of(WRITE), ImmutableSet.of(READ)), is(true));
-    }
-
-    @Test
-    public void isTransitionDownWhenPreviousDeleteAndNewRead()
-    {
-        assertThat(isTransitionDownToReadOrLess(ImmutableSet.of(DELETE), ImmutableSet.of(READ)), is(true));
-    }
-
-    @Test
-    public void isTransitionDownWhenPreviousSpaceAdminAndNewRead()
-    {
-        assertThat(isTransitionDownToReadOrLess(ImmutableSet.of(SPACE_ADMIN), ImmutableSet.of(READ)), is(true));
-    }
-
-    @Test
-    public void isTransitionDownWhenPreviousAdminAndNewRead()
-    {
-        assertThat(isTransitionDownToReadOrLess(ImmutableSet.of(ADMIN), ImmutableSet.of(READ)), is(true));
-    }
-
-    // transition up tests
-
-    @Test
-    public void notTransitionUpWhenBothSetsEmpty()
-    {
-        assertThat(isTransitionUpFromReadOrLess(EMPTY, EMPTY), is(false));
-    }
-
-    @Test
-    public void notTransitionUpWhenNewEmpty()
-    {
-        assertThat(isTransitionUpFromReadOrLess(ImmutableSet.of(READ), EMPTY), is(false));
-    }
-
-    @Test
-    public void notTransitionUpWhenPreviousWriteAndNewWrite()
-    {
-        assertThat(isTransitionUpFromReadOrLess(ImmutableSet.of(WRITE), ImmutableSet.of(WRITE)), is(false));
-    }
-
-    @Test
-    public void notTransitionUpWhenPreviousWriteAndNewAdmin()
-    {
-        assertThat(isTransitionUpFromReadOrLess(ImmutableSet.of(WRITE), ImmutableSet.of(ADMIN)), is(false));
-    }
-
-    @Test
-    public void isTransitionUpWhenPreviousEmptyAndNewWrite()
-    {
-        assertThat(isTransitionUpFromReadOrLess(EMPTY, ImmutableSet.of(WRITE)), is(true));
-    }
-
-    @Test
-    public void isTransitionUpWhenPreviousEmptyAndNewDelete()
-    {
-        assertThat(isTransitionUpFromReadOrLess(EMPTY, ImmutableSet.of(DELETE)), is(true));
-    }
-
-    @Test
-    public void isTransitionUpWhenPreviousEmptyAndNewSpaceAdmin()
-    {
-        assertThat(isTransitionUpFromReadOrLess(EMPTY, ImmutableSet.of(SPACE_ADMIN)), is(true));
-    }
-
-    @Test
-    public void isTransitionUpWhenPreviousEmptyAndNewAdmin()
-    {
-        assertThat(isTransitionUpFromReadOrLess(EMPTY, ImmutableSet.of(ADMIN)), is(true));
-    }
-
-    @Test
-    public void isTransitionUpWhenPreviousReadAndNewWrite()
-    {
-        assertThat(isTransitionUpFromReadOrLess(ImmutableSet.of(READ), ImmutableSet.of(WRITE)), is(true));
-    }
-
-    @Test
-    public void isTransitionUpWhenPreviousReadAndNewDelete()
-    {
-        assertThat(isTransitionUpFromReadOrLess(ImmutableSet.of(READ), ImmutableSet.of(DELETE)), is(true));
-    }
-
-    @Test
-    public void isTransitionUpWhenPreviousReadAndNewSpaceAdmin()
-    {
-        assertThat(isTransitionUpFromReadOrLess(ImmutableSet.of(READ), ImmutableSet.of(SPACE_ADMIN)), is(true));
-    }
-
-    @Test
-    public void isTransitionUpWhenPreviousReadAndNewAdmin()
-    {
-        assertThat(isTransitionUpFromReadOrLess(ImmutableSet.of(READ), ImmutableSet.of(ADMIN)), is(true));
-    }
 }
