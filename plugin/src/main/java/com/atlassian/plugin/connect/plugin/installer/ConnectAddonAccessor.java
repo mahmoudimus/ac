3bc81f47ebf123a12eec3e062e5dc0ac09b6e6f2
Peter Brownlow <pbrownlow@atlassian.com>
2014-03-14 11:55:39 +1100

    ACDEV-1111: PR feedback: handling nulls in ConnectAddonAccessor

--- plugin/src/main/java/com/atlassian/plugin/connect/plugin/installer/ConnectAddonAccessor.java
+++ plugin/src/main/java/com/atlassian/plugin/connect/plugin/installer/ConnectAddonAccessor.java
@@ -1,9 +1,5 @@
 package com.atlassian.plugin.connect.plugin.installer;
 
-import javax.annotation.Nullable;
-import javax.inject.Inject;
-import javax.inject.Named;
-
 import com.atlassian.plugin.Plugin;
 import com.atlassian.plugin.PluginAccessor;
 import com.atlassian.plugin.connect.modules.beans.ConnectAddonBean;
@@ -13,6 +9,10 @@ import com.atlassian.plugin.predicate.PluginPredicate;
 import com.google.common.base.Function;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.annotation.Nullable;
+import javax.inject.Inject;
+import javax.inject.Named;
 
 import static com.google.common.collect.Iterables.transform;
 
@@ -45,7 +45,7 @@ public class ConnectAddonAccessor
             @Override
             public ConnectAddonBean apply(@Nullable String descriptorJsonStr)
             {
-                return ConnectModulesGsonFactory.getGson().fromJson(descriptorJsonStr, ConnectAddonBean.class);
+                return null == descriptorJsonStr ? null : ConnectModulesGsonFactory.getGson().fromJson(descriptorJsonStr, ConnectAddonBean.class);
             }
         });
     }
@@ -57,14 +57,14 @@ public class ConnectAddonAccessor
             @Override
             public boolean matches(Plugin plugin)
             {
-                return connectIdentifier.isConnectAddOn(plugin);
+                return null != plugin && connectIdentifier.isConnectAddOn(plugin);
             }
         }), new Function<Plugin, String>()
         {
             @Override
             public String apply(@Nullable Plugin plugin)
             {
-                return descriptorRegistry.getDescriptor(plugin.getKey());
+                return null == plugin ? null : descriptorRegistry.getDescriptor(plugin.getKey());
             }
         });
     }
