b59f31f4d09b96fb43c1dd94dcc6605ceea3d358
Peter Brownlow <pbrownlow@atlassian.com>
2014-03-20 16:17:05 +1100

    UNIFIED-82: created SQL script for removing "atlassian-addons" group membership from users with usernames that do NOT start with "addon_"

---
+++ bin/remove-non-addons-from-addons-group.sql
@@ -0,0 +1,4 @@
+-- This script reduces the manual workload on OnDemand support in response to https://jira.atlassian.com/browse/UNIFIED-82.
+-- To be run on the 'crowd' db in OnDemand instances where we have detected unexpected group membership.
+
+delete from cwd_membership where lower_parent_name = 'atlassian-addons' and lower_child_name not like 'addon_%';
