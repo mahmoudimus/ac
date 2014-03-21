4353ba6cff5487731ebb3786347bf65b17e0d507
Peter Brownlow <pbrownlow@atlassian.com>
2014-03-21 11:57:31 +1100

    UNIFIED-82: displaying the rows to be deleted before deleting them, just in case

--- bin/remove-non-addons-from-addons-group.sql
+++ bin/remove-non-addons-from-addons-group.sql
@@ -1,4 +1,7 @@
 -- This script reduces the manual workload on OnDemand support in response to https://jira.atlassian.com/browse/UNIFIED-82.
 -- To be run on the 'crowd' db in OnDemand instances where we have detected unexpected group membership.
 
+begin transaction;
+select * from cwd_membership where lower_parent_name = 'atlassian-addons' and lower_child_name not like 'addon_%';
 delete from cwd_membership where lower_parent_name = 'atlassian-addons' and lower_child_name not like 'addon_%';
+commit;
