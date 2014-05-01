bf997e4f3352f7e0e03846a9715a3cdb21361e6d
Travis Smith <tsmith@atlassian.com>
2014-05-01 13:08:04 -0700

    ACDEV-1208: Cleanup + only dereference schemas
    * vim settings where `tabstop=2`. Changed it to 4 because it matches
    * added condition for derferencing that includes checking for the file
      to actually be a schema. Don't need to dereference anything else

--- docs/de-ref.js
+++ docs/de-ref.js
@@ -6,68 +6,69 @@ var fs = require('fs-extra');
 var program = require('commander');
 
 program
-  .option('-d, --debug', 'Output debug information')
-  .parse(process.argv);
+    .option('-d, --debug', 'Output debug information')
+    .parse(process.argv);
 
-var debug = program.debug ? console.log : function() {/* no-op */};
+var debug = program.debug ? console.log : function () {/* no-op */
+};
 
 var sourceSchemaDirectory = "../plugin/target/classes/";
 var targetSchemaDirectory = "schema/";
 
 var files = {
-  jiraSchema:       'schema/jira-schema.json',
-  confluenceSchema: 'schema/confluence-schema.json',
-  jiraScopes:       'com/atlassian/connect/scopes.jira.json',
-  confluenceScopes: 'com/atlassian/connect/scopes.confluence.json',
-  commonScopes:     'com/atlassian/connect/scopes.common.json'
+    jiraSchema:       'schema/jira-schema.json',
+    confluenceSchema: 'schema/confluence-schema.json',
+    jiraScopes:       'com/atlassian/connect/scopes.jira.json',
+    confluenceScopes: 'com/atlassian/connect/scopes.confluence.json',
+    commonScopes:     'com/atlassian/connect/scopes.common.json'
 };
 
 function dereference(object, objectRoot, path) {
-  if (object == null) return;
+    if (object == null) return;
 
-  if (typeof object === 'object')
-  {
-    for(var prop in object) {
-      dereference(object[prop], objectRoot, path + "." + prop)
+    if (typeof object === 'object') {
+        for (var prop in object) {
+            dereference(object[prop], objectRoot, path + "." + prop)
+        }
+
+        if (object['$ref']) {
+            var reference = object['$ref'];
+            // HACK: this is needed for other stuff
+            // keep the { $ref: "#" } around.
+            if (reference != "#") {
+                delete object['$ref'];
+
+                var jPath = reference.replace("#", "").split('/').join('.');
+                var foundObj = jsonPath(objectRoot, "$" + jPath);
+                // jsonPath returns an array, we want the first item
+                if (foundObj && foundObj[0])
+                    foundObj = foundObj[0];
+
+                // copy junk over
+                for (var property in foundObj)
+                    // only copy if it's not already there
+                    if (object[property] === undefined)
+                        object[property] = foundObj[property];
+            }
+        }
     }
-
-    if (object['$ref']) {
-      var reference = object['$ref'];
-      // HACK: this is needed for other stuff
-      // keep the { $ref: "#" } around. 
-      if (reference != "#") {
-        delete object['$ref'];
-
-        var jPath = reference.replace("#", "").split('/').join('.');
-        var foundObj = jsonPath(objectRoot, "$" + jPath);
-        // jsonPath returns an array, we want the first item
-        if (foundObj && foundObj[0])
-          foundObj = foundObj[0];
-
-        // copy junk over
-        for(var property in foundObj)
-          // only copy if it's not already there
-          if (object[property] === undefined)
-            object[property] = foundObj[property];
-      }
+    else if (typeof object === 'array') {
+        for (var i = 0; i < object.length; i++)
+            dereference(object[i], objectRoot, path);
     }
-  } 
-  else if (typeof object === 'array')
-  {
-    for(var i = 0; i < object.length; i++)
-      dereference(object[i], objectRoot, path);
-  }
 }
 
-for(var file in files) {
-  var source = sourceSchemaDirectory + files[file];
-  var target = targetSchemaDirectory + files[file];
+for (var file in files) {
+    var source = sourceSchemaDirectory + files[file];
+    var target = targetSchemaDirectory + files[file];
 
-  debug("copying", source, "to", target);
+    debug("copying", source, "to", target);
 
-  var sourceJson = fs.readJsonSync(source);
+    var sourceJson = fs.readJsonSync(source);
 
-  dereference(sourceJson, sourceJson, "$");
+    // only need to dereference the schemas, save a few ms on the step
+    if (file.match(/Schema$/))
+        dereference(sourceJson, sourceJson, "$");
 
-  fs.outputJsonSync(target, sourceJson);
+    fs.outputJsonSync(target, sourceJson);
 }
