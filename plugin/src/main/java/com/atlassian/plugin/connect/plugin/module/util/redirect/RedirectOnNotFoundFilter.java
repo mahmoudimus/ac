850b2a090b09e949985569bcb4fcf72d6d2b199e
Anders Holmgren <aholmgren@atlassian.com>
2014-05-07 18:06:28 +1000

    AC-988 tidy

--- plugin/src/main/java/com/atlassian/plugin/connect/plugin/module/util/redirect/RedirectOnNotFoundFilter.java
+++ plugin/src/main/java/com/atlassian/plugin/connect/plugin/module/util/redirect/RedirectOnNotFoundFilter.java
@@ -1,11 +1,12 @@
 package com.atlassian.plugin.connect.plugin.module.util.redirect;
 
-import org.apache.commons.httpclient.HttpStatus;
-import org.apache.http.HttpHeaders;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.servlet.*;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServletResponseWrapper;
@@ -13,13 +14,19 @@ import java.io.CharArrayWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.http.HttpHeaders;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
  * A Servlet filter that performs permanent redirects from the given "from" pattern to the given "to" text
- * when it get
+ * when it the response would otherwise have been a 404
  */
-public class RedirectOnNotFoundFilter implements Filter {
+public class RedirectOnNotFoundFilter implements Filter
+{
     private static final Logger log = LoggerFactory.getLogger(RedirectOnNotFoundFilter.class);
     private static final String FROM_PATTERN = "from.pattern";
     private static final String TO_TEXT = "to.text";
@@ -28,7 +35,7 @@ public class RedirectOnNotFoundFilter implements Filter {
     private String toPattern;
 
     /**
-     * Requires two servlet config parameters to be set. e.g.
+     * Requires two filter config parameters to be set. e.g.
      * <p/>
      * <init-param>
      * <param-name>from.pattern</param-name>
@@ -41,24 +48,22 @@ public class RedirectOnNotFoundFilter implements Filter {
      *
      * @param filterConfig
      */
-    public void init(FilterConfig filterConfig) {
+    public void init(FilterConfig filterConfig)
+    {
         fromPattern = checkNotNull(filterConfig.getInitParameter(FROM_PATTERN));
         toPattern = checkNotNull(filterConfig.getInitParameter(TO_TEXT));
     }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
-                         FilterChain filterChain) throws IOException, ServletException {
-        log.info("****************doFilter");
+                         FilterChain filterChain) throws IOException, ServletException
+    {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
         RedirectingHttpServletResponseWrapper wrapper = new RedirectingHttpServletResponseWrapper(response);
         filterChain.doFilter(servletRequest, wrapper);
-        log.info("****************after filterChain.doFilter");
 
         if (wrapper.is404())
         {
-            log.info("****************is 404");
-
             HttpServletRequest request = (HttpServletRequest) servletRequest;
 
             final StringBuffer requestURL = request.getRequestURL();
@@ -66,7 +71,6 @@ public class RedirectOnNotFoundFilter implements Filter {
 
             final String newUrl = requestURL.replace(index, index + fromPattern.length(), toPattern).toString();
             log.debug("Redirecting from {} to {}", new Object[]{request.getRequestURI(), newUrl});
-            log.info("****************Redirecting from {} to {}", new Object[]{request.getRequestURI(), newUrl});
             response.setStatus(HttpStatus.SC_MOVED_PERMANENTLY);
             response.addHeader(HttpHeaders.LOCATION, newUrl);
             response.getWriter().close();
@@ -75,23 +79,27 @@ public class RedirectOnNotFoundFilter implements Filter {
 
 
     @Override
-    public void destroy() {
+    public void destroy()
+    {
 
     }
 }
 
-class RedirectingHttpServletResponseWrapper extends HttpServletResponseWrapper {
+class RedirectingHttpServletResponseWrapper extends HttpServletResponseWrapper
+{
     private static final Logger log = LoggerFactory.getLogger(RedirectOnNotFoundFilter.class);
 
     private PrintWriter devNullWriter;
     private ServletOutputStream devNullOutputStream;
 
-    public RedirectingHttpServletResponseWrapper(HttpServletResponse response) {
+    public RedirectingHttpServletResponseWrapper(HttpServletResponse response)
+    {
         super(response);
     }
 
     @Override
-    public void setStatus(int sc) {
+    public void setStatus(int sc)
+    {
         checkStatus(sc);
         if (!is404())
         {
@@ -100,7 +108,8 @@ class RedirectingHttpServletResponseWrapper extends HttpServletResponseWrapper {
     }
 
     @Override
-    public void setStatus(int sc, String sm) {
+    public void setStatus(int sc, String sm)
+    {
         checkStatus(sc);
         if (!is404())
         {
@@ -108,47 +117,9 @@ class RedirectingHttpServletResponseWrapper extends HttpServletResponseWrapper {
         }
     }
 
-    private void checkStatus(int sc) {
-        log.info("****************checkStatus ", sc);
-        if (sc == HttpStatus.SC_NOT_FOUND)
-        {
-            devNullWriter = new PrintWriter(new CharArrayWriter());
-            devNullOutputStream  = new ServletOutputStream() {
-
-                @Override
-                public void write(int b) throws IOException
-                {
-                    // dev null
-                }
-            };
-        }
-        log.info("****************devNullWriter ", devNullWriter);
-    }
-
-    @Override
-    public PrintWriter getWriter() throws IOException {
-        log.info("****************getWriter ", devNullWriter);
-        return devNullWriter != null ? devNullWriter : super.getWriter();
-    }
-
-
-    @Override
-    public ServletOutputStream getOutputStream() throws IOException
-    {
-        log.info("****************getOutputStream ", devNullOutputStream);
-        return devNullOutputStream != null ? devNullOutputStream :
-                super.getOutputStream();
-    }
-
-    boolean is404() {
-        return devNullWriter != null;
-    }
-
-
     @Override
     public void sendError(int sc) throws IOException
     {
-        log.info("****************sendError ");
         checkStatus(sc);
         if (!is404())
         {
@@ -159,7 +130,6 @@ class RedirectingHttpServletResponseWrapper extends HttpServletResponseWrapper {
     @Override
     public void sendError(int sc, String msg) throws IOException
     {
-        log.info("****************sendError ");
         checkStatus(sc);
         if (!is404())
         {
@@ -168,9 +138,39 @@ class RedirectingHttpServletResponseWrapper extends HttpServletResponseWrapper {
     }
 
     @Override
-    public void sendRedirect(String location) throws IOException
+    public PrintWriter getWriter() throws IOException
     {
-        log.info("****************sendRedirect");
-//        super.sendRedirect(location);
+        return devNullWriter != null ? devNullWriter : super.getWriter();
     }
+
+
+    @Override
+    public ServletOutputStream getOutputStream() throws IOException
+    {
+        return devNullOutputStream != null ? devNullOutputStream :
+                super.getOutputStream();
+    }
+
+    boolean is404()
+    {
+        return devNullWriter != null;
+    }
+
+    private void checkStatus(int sc)
+    {
+        if (sc == HttpStatus.SC_NOT_FOUND)
+        {
+            devNullWriter = new PrintWriter(new CharArrayWriter());
+            devNullOutputStream = new ServletOutputStream()
+            {
+
+                @Override
+                public void write(int b) throws IOException
+                {
+                    // dev null
+                }
+            };
+        }
+    }
+
 }
