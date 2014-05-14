be6dbeb433696377186d8b9617e79cd8278cff50
Jonathan Doklovic <doklovic@atlassian.com>
2014-05-14 12:48:26 -0700

    ACDEV-346: velocity killer now checking for nulls

--- plugin/src/main/java/com/atlassian/plugin/connect/plugin/capabilities/util/VelocityKiller.java
+++ plugin/src/main/java/com/atlassian/plugin/connect/plugin/capabilities/util/VelocityKiller.java
@@ -3,6 +3,8 @@ package com.atlassian.plugin.connect.plugin.capabilities.util;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import com.google.common.base.Strings;
+
 /**
  * Class used to sanistize strings by removing ${} and just leaving the variable names.
  * This is used by anything that needs to pass descriptor string values to a method that renders velocity. e.g. WebLabel
@@ -13,6 +15,11 @@ public class VelocityKiller
     
     public static String attack(String rawValue)
     {
+        if(Strings.isNullOrEmpty(rawValue) || rawValue.length() < 2)
+        {
+            return rawValue;
+        }
+        
         Matcher matcher = VELOCITY_VAR_PATTERN.matcher(rawValue);
         
         return matcher.replaceAll("$1");
