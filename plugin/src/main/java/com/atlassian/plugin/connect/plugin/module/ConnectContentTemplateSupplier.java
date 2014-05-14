0df74115f5e79f30bc6ebfba87e1b300a0432fd7
Matthew Jensen <mjensen@atlassian.com>
2014-05-14 10:08:22 -0700

    AC-1083 upgrade confluence and manually set create content plugin until release

--- plugin/src/main/java/com/atlassian/plugin/connect/plugin/module/ConnectContentTemplateSupplier.java
+++ plugin/src/main/java/com/atlassian/plugin/connect/plugin/module/ConnectContentTemplateSupplier.java
@@ -1,8 +1,6 @@
 package com.atlassian.plugin.connect.plugin.module;
 
 import com.atlassian.confluence.plugins.createcontent.extensions.ContentTemplateSupplier;
-import com.atlassian.plugin.web.ContextProvider;
-import com.atlassian.plugin.web.NoOpContextProvider;
 
 /**
  * A content template supplier that allows the template to be loaded from a URL or inline.
@@ -11,20 +9,5 @@ public class ConnectContentTemplateSupplier implements ContentTemplateSupplier {
     @Override
     public String getTemplateContent() {
         return "<h1>My Template</h1>";
-    }
-
-    @Override
-    public ContextProvider getContextProvider() {
-        return new NoOpContextProvider();
-    }
-
-    @Override
-    public void enabled() {
-        // nothing to do here
-    }
-
-    @Override
-    public void disabled() {
-        // nothing to do here
     }
 }
