7239919603452fa4bb3ac878dd9b99cdd22627e2
Anders Holmgren <aholmgren@atlassian.com>
2014-06-13 12:24:17 +1000

    AC-1274 eazy bi is jira only test

--- plugin/src/test/java/it/com/atlassian/plugin/connect/plugin/threeleggedauth/ThreeLeggedAuthFilterEazyBITest.java
+++ plugin/src/test/java/it/com/atlassian/plugin/connect/plugin/threeleggedauth/ThreeLeggedAuthFilterEazyBITest.java
@@ -16,6 +16,7 @@ import com.atlassian.plugin.connect.plugin.threeleggedauth.NoUserAgencyException
 import com.atlassian.plugin.connect.plugin.threeleggedauth.ThreeLeggedAuthService;
 import com.atlassian.plugin.connect.testsupport.TestPluginInstaller;
 import com.atlassian.plugin.connect.testsupport.filter.AddonTestFilterResults;
+import com.atlassian.plugins.osgi.test.Application;
 import com.atlassian.plugins.osgi.test.AtlassianPluginsTestRunner;
 import com.atlassian.sal.api.ApplicationProperties;
 import it.com.atlassian.plugin.connect.TestAuthenticator;
@@ -24,6 +25,7 @@ import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertEquals;
 
+@Application("jira")
 @RunWith(AtlassianPluginsTestRunner.class)
 public class ThreeLeggedAuthFilterEazyBITest extends ThreeLeggedAuthFilterTestBase
 {
