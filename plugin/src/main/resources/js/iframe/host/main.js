2ee273243e335a794473ac2455ae4b1b349ba9d4
Chris Whittington <cwhittington@atlassian.com>
2014-06-17 17:13:37 +1000

    AC-765 add comment to explain why we need this code

--- plugin/src/main/resources/js/iframe/host/main.js
+++ plugin/src/main/resources/js/iframe/host/main.js
@@ -87,7 +87,7 @@ _AP.define("host/main", ["_dollar", "_xdm", "host/_addons", "_rpc", "_ui-params"
   }
 
   return function (options) {
-
+    // AC-765 if we are about to replace an old instance of the connect iframe. Destroy it.
     $content = contentDiv(options.ns);
     $contentIframe = $content.find("iframe");
     if($contentIframe.length){
