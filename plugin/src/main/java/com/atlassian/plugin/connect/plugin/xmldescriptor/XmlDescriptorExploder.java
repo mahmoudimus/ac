63a616f717f3ccb7b7e4ab089cd76bf69c523cb1
Peter Brownlow <pbrownlow@atlassian.com>
2014-06-24 13:21:45 +0100

    ACDEV-1302: made XmlDescriptorExploder retain the newest instance rather than the oldest, in the hope that it has the service references least likely to result in a ServiceProxyDestroyedException, and added a catch around service proxy invocations

--- plugin/src/main/java/com/atlassian/plugin/connect/plugin/xmldescriptor/XmlDescriptorExploder.java
+++ plugin/src/main/java/com/atlassian/plugin/connect/plugin/xmldescriptor/XmlDescriptorExploder.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Component;
 import javax.inject.Inject;
 import java.util.concurrent.atomic.AtomicReference;
 
+import static java.util.Arrays.asList;
+
 @Component
 public class XmlDescriptorExploder
 {
@@ -28,7 +30,7 @@ public class XmlDescriptorExploder
         this.darkFeatureManager = darkFeatureManager;
         this.eventPublisher = eventPublisher;
 
-        if (!instanceRef.compareAndSet(null, this))
+        if (null != instanceRef.getAndSet(this))
         {
             log.warn(getClass().getSimpleName() + " would ideally not be instantiated more than once. This is at least the second instantiation.");
         }
@@ -48,12 +50,26 @@ public class XmlDescriptorExploder
 
     private void notifyAndExplodeInternal(String addOnKey, StackTraceElement[] stackTrace)
     {
-        if (darkFeatureManager.isFeatureEnabledForCurrentUser(DARK_FEATURE_NOTIFY))
+        boolean shouldThrow = false;
+
+        // this code runs in prod and does not provide a feature to the users so we should take extra care to cause exceptions
+        try
         {
-            eventPublisher.publish(new XmlDescriptorCodeInvokedEvent(addOnKey, stackTrace));
+            if (darkFeatureManager.isFeatureEnabledForCurrentUser(DARK_FEATURE_NOTIFY))
+            {
+                eventPublisher.publish(new XmlDescriptorCodeInvokedEvent(addOnKey, stackTrace));
+            }
+
+            shouldThrow = darkFeatureManager.isFeatureEnabledForCurrentUser(DARK_FEATURE_EXPLODE);
+        }
+        catch (Exception e)
+        {
+            final String message = String.format("Failed to run the %s dark feature and event publishing code for add-on '%s' and stack trace '%s' due to exception: ",
+                    XmlDescriptorCodeInvokedEvent.class.getSimpleName(), addOnKey, asList(stackTrace));
+            log.error(message, e);
         }
 
-        if (darkFeatureManager.isFeatureEnabledForCurrentUser(DARK_FEATURE_EXPLODE))
+        if (shouldThrow)
         {
             throw new RuntimeException("The Connect XML descriptor is unsupported. Deprecation was announced in February 2014 for May 2014. Please see <https://developer.atlassian.com/static/connect/docs/resources/deprecations.html> for more information.");
         }
