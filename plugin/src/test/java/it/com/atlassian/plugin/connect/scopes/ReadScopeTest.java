48b6f7b0a4790f1b426c88202aff80de96af7093
Peter Brownlow <pbrownlow@atlassian.com>
2014-07-07 13:48:43 +1000

    fixed comment

--- plugin/src/test/java/it/com/atlassian/plugin/connect/scopes/ReadScopeTest.java
+++ plugin/src/test/java/it/com/atlassian/plugin/connect/scopes/ReadScopeTest.java
@@ -27,7 +27,7 @@ public class ReadScopeTest extends ScopeTestBase
     @ParameterizedWiredTest.Parameters(name="httpRequest", length=2)
     protected Object[][] data = new Object[][]
     {
-            // TODO: ACDEV- new Object[]{ HttpMethod.GET,  "/rest/atlassian-connect/1/license", true },
+            // TODO: ACDEV-1333 new Object[]{ HttpMethod.GET,  "/rest/atlassian-connect/1/license", true },
             new Object[]{ HttpMethod.GET,  "/rest/applinks/2.0/entities", true },
             new Object[]{ HttpMethod.POST, "/rest/applinks/2.0/entities", false }
     };
