b8bce4b207767c6b59c35f30482469d8152b9784
Peter Brownlow <pbrownlow@atlassian.com>
2014-07-08 14:33:35 +1000

    ACDEV-1339: ported ConnectVersionTabPanelModuleBeanTest from xml to json, added an OAuth annotation

--- integration-tests/src/test/java/it/jira/TestVersionTabPage.java
+++ integration-tests/src/test/java/it/jira/TestVersionTabPage.java
@@ -4,31 +4,30 @@ import com.atlassian.jira.functest.framework.FunctTestConstants;
 import com.atlassian.jira.testkit.client.restclient.Version;
 import com.atlassian.jira.testkit.client.restclient.VersionClient;
 import com.atlassian.jira.tests.TestBase;
-import com.atlassian.plugin.connect.api.xmldescriptor.XmlDescriptor;
-import com.atlassian.plugin.connect.plugin.module.jira.versiontab.VersionTabPageModuleDescriptor;
+import com.atlassian.plugin.connect.modules.beans.nested.I18nProperty;
+import com.atlassian.plugin.connect.plugin.ConnectPluginInfo;
+import com.atlassian.plugin.connect.plugin.capabilities.provider.ConnectTabPanelModuleProvider;
+import com.atlassian.plugin.connect.test.AddonTestUtils;
 import com.atlassian.plugin.connect.test.pageobjects.jira.JiraVersionTabPage;
-import com.atlassian.plugin.connect.test.server.AtlassianConnectAddOnRunner;
-import com.atlassian.plugin.connect.test.server.module.VersionTabPageModule;
+import com.atlassian.plugin.connect.test.server.ConnectRunner;
 import it.servlet.ConnectAppServlets;
 import org.junit.*;
 
 import java.rmi.RemoteException;
 
+import static com.atlassian.plugin.connect.modules.beans.ConnectTabPanelModuleBean.newTabPanelBean;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 /**
  * Test of remote version tab panel in JIRA
  */
-@XmlDescriptor
 public class TestVersionTabPage extends TestBase
 {
-    private static AtlassianConnectAddOnRunner remotePlugin;
+    private static ConnectRunner remotePlugin;
+    private static String moduleKey;
 
     private static final String PROJECT_KEY = FunctTestConstants.PROJECT_HOMOSAP_KEY;
-    private static final String MODULE_KEY = "jira-version-tab-panel";
-    private static final String ACTUAL_MODULE_KEY = VersionTabPageModuleDescriptor.VERSION_TAB_PAGE_MODULE_PREFIX + MODULE_KEY;
-
     private static final String VERSION_NAME = "2.7.1";
     private String versionId;
 
@@ -36,12 +35,19 @@ public class TestVersionTabPage extends TestBase
     @BeforeClass
     public static void startConnectAddOn() throws Exception
     {
-        remotePlugin = new AtlassianConnectAddOnRunner(jira().environmentData().getBaseUrl().toString())
-                .addOAuth()
-                .add(VersionTabPageModule.key(MODULE_KEY)
-                        .name("Version Tab Panel")
-                        .path("/ipp?version_id=${version.id}&project_id=${project.id}&project_key=${project.key}")
-                        .resource(ConnectAppServlets.apRequestServlet()))
+        final String route = "/ipp?version_id=${version.id}&project_id=${project.id}&project_key=${project.key}";
+        moduleKey = AddonTestUtils.randomModuleKey();
+        remotePlugin = new ConnectRunner(jira().environmentData().getBaseUrl().toString(), AddonTestUtils.randomAddOnKey())
+                .addJWT()
+                .addInstallLifecycle()
+                .addRoute(ConnectRunner.INSTALLED_PATH, ConnectAppServlets.installHandlerServlet())
+                .addModule(ConnectTabPanelModuleProvider.VERSION_TAB_PANELS, newTabPanelBean()
+                        .withName(new I18nProperty("Version Tab Panel", "my.versionTabPanel"))
+                        .withKey(moduleKey)
+                        .withUrl(route)
+                        .withWeight(100)
+                        .build())
+                .addRoute(route, ConnectAppServlets.apRequestServlet())
                 .start();
 
     }
@@ -73,7 +79,7 @@ public class TestVersionTabPage extends TestBase
     public void testVersionTabPanel() throws RemoteException
     {
         jira().gotoLoginPage().loginAsSysadminAndGoToHome();
-        JiraVersionTabPage versionTabPage = jira().goTo(JiraVersionTabPage.class, PROJECT_KEY, versionId, remotePlugin.getPluginKey(), ACTUAL_MODULE_KEY);
+        JiraVersionTabPage versionTabPage = jira().goTo(JiraVersionTabPage.class, PROJECT_KEY, versionId, ConnectPluginInfo.getPluginKey(), remotePlugin.getAddon().getKey() + "__" + moduleKey);
 
         versionTabPage.clickTab();
 
