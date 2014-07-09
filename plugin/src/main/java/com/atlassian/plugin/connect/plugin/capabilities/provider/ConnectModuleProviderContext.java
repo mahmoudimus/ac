0c8ac965bde910229a4637c2307f7ef8d7bc6fea
Anders Holmgren <aholmgren@atlassian.com>
2014-07-09 16:18:10 +1000

    AC-1201 added java docs

--- plugin/src/main/java/com/atlassian/plugin/connect/plugin/capabilities/provider/ConnectModuleProviderContext.java
+++ plugin/src/main/java/com/atlassian/plugin/connect/plugin/capabilities/provider/ConnectModuleProviderContext.java
@@ -2,10 +2,16 @@ package com.atlassian.plugin.connect.plugin.capabilities.provider;
 
 import com.atlassian.plugin.connect.modules.beans.ConnectAddonBean;
 
+/**
+ * A context for ConnectModuleProvider's
+ */
 public interface ConnectModuleProviderContext
 {
     ConnectAddonBean getConnectAddonBean();
 
+    /**
+     * A helper for qualifying location names so they match the qualified names of module keys
+     */
     ModuleLocationQualifier getLocationQualifier();
 }
 
