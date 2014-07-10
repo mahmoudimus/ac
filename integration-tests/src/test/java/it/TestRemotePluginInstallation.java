01d87a5dde6260302b88240f0131aa52e5a386e9
Peter Brownlow <pbrownlow@atlassian.com>
2014-07-10 16:12:53 +1000

    ACDEV-1339: ported it.TestRemotePluginInstallation  from xml descriptor to json descriptor

--- integration-tests/src/test/java/it/TestRemotePluginInstallation.java
+++ integration-tests/src/test/java/it/TestRemotePluginInstallation.java
@@ -1,59 +1,53 @@
 package it;
 
 import com.atlassian.pageobjects.page.HomePage;
-import com.atlassian.plugin.connect.api.xmldescriptor.XmlDescriptor;
+import com.atlassian.plugin.connect.modules.beans.nested.I18nProperty;
 import com.atlassian.plugin.connect.test.pageobjects.GeneralPage;
-import com.atlassian.plugin.connect.test.server.AtlassianConnectAddOnRunner;
-import com.atlassian.plugin.connect.test.server.module.GeneralPageModule;
+import com.atlassian.plugin.connect.test.server.ConnectRunner;
 import it.servlet.ConnectAppServlets;
-import org.apache.http.client.HttpResponseException;
-import org.junit.Ignore;
 import org.junit.Test;
 
+import static com.atlassian.plugin.connect.modules.beans.ConnectPageModuleBean.newPageBean;
 import static org.junit.Assert.assertTrue;
 
-@XmlDescriptor
 public class TestRemotePluginInstallation extends ConnectWebDriverTestBase
 {
-    public static final String EXTRA_PREFIX = "servlet-";
-    
     @Test
     public void testChangedKey() throws Exception
     {
         loginAsAdmin();
-        AtlassianConnectAddOnRunner pluginFirst = new AtlassianConnectAddOnRunner(product.getProductInstance().getBaseUrl(), "pluginFirst")
-                .add(GeneralPageModule.key("changedPage")
-                                      .name("Changed Page")
-                                      .path("/page")
-                                      .resource(ConnectAppServlets.helloWorldServlet()))
-                .start();
-        product.visit(HomePage.class);
-        assertTrue(product.getPageBinder().bind(GeneralPage.class, "changedPage", "Changed Page",EXTRA_PREFIX)
-                          .clickRemotePluginLink()
-                          .isLoaded());
-        pluginFirst.stopAndUninstall();
+        ConnectRunner pluginFirst = createAddOn("pluginFirst");
+        assertThatWeCanVisitThePage(pluginFirst);
 
-        AtlassianConnectAddOnRunner pluginSecond = new AtlassianConnectAddOnRunner(product.getProductInstance().getBaseUrl(), "pluginSecond")
-                .add(GeneralPageModule.key("changedPage")
-                                      .name("Changed Page")
-                                      .path("/page")
-                                      .resource(ConnectAppServlets.helloWorldServlet()))
-                .start();
-        product.visit(HomePage.class);
-        assertTrue(product.getPageBinder().bind(GeneralPage.class, "changedPage", "Changed Page", EXTRA_PREFIX)
-                          .clickRemotePluginLink()
-                          .isLoaded());
-        pluginSecond.stopAndUninstall();
+        ConnectRunner pluginSecond = createAddOn("pluginSecond");
+        assertThatWeCanVisitThePage(pluginSecond);
     }
 
-    //TODO: JD fix this, need better trapping of errors during install
-    @Ignore
-    @Test(expected = HttpResponseException.class)
-    public void testUnknownModuleAndFail() throws Exception
+    private void assertThatWeCanVisitThePage(ConnectRunner addOn) throws Exception
     {
-        new AtlassianConnectAddOnRunner(product.getProductInstance().getBaseUrl(), "appFirst")
-                .description("foo")
-                .addUnknownModule("some-key")
+        try
+        {
+            product.visit(HomePage.class);
+            assertTrue(product.getPageBinder().bind(GeneralPage.class, "changedPage", "Changed Page", addOn.getAddon().getKey())
+                    .clickAddOnLink()
+                    .isLoaded());
+        }
+        finally
+        {
+            addOn.stopAndUninstall();
+        }
+    }
+
+    private ConnectRunner createAddOn(String addOnKey) throws Exception
+    {
+        return new ConnectRunner(product.getProductInstance().getBaseUrl(), addOnKey)
+                .addModule("generalPages", newPageBean()
+                        .withKey("changedPage")
+                        .withName(new I18nProperty("Changed Page", null))
+                        .withUrl("/page")
+                        .build())
+                .addRoute("/page", ConnectAppServlets.helloWorldServlet())
+                .setAuthenticationToNone()
                 .start();
     }
 }
