ad6b0fa97003c0b0ef483f1b44f2c47b741ea260
Peter Brownlow <pbrownlow@atlassian.com>
2014-07-18 14:32:50 +1000

    AC-1229: refactored test code

--- plugin/src/test/java/it/com/atlassian/plugin/connect/installer/XmlOAuthToJsonJwtUpdateTest.java
+++ plugin/src/test/java/it/com/atlassian/plugin/connect/installer/XmlOAuthToJsonJwtUpdateTest.java
@@ -65,6 +65,33 @@ public class XmlOAuthToJsonJwtUpdateTest
             "                FOtr7x6M5Z7XZsPhNIjDjE+oBHu+Hzc1/bGyWpZrzc04nqCNTsmOvID1H50Jt778\n" +
             "                mQIDAQAB\n" +
             "                -----END PUBLIC KEY-----";
+    private static final String OLD_PRIVATE_KEY = "-----BEGIN RSA PRIVATE KEY-----\n" +
+            "MIIEpQIBAAKCAQEAtWghUS3KRC01kJkeU83PS2CSH6Vma2twv7p7JVHUIWl69tNP\n" +
+            "vn46mYt3gbFQgmBv74pAJJkb1FvBNxhy0B199fXgZUVS+6R7597t0hVG610Zbl4A\n" +
+            "r7xvs/h3ACwUhSib3ad496nghLvOnarrJIgwsNQeCaBz+FczCYQAt7Mk8vGjE1XS\n" +
+            "ha6FEBNKIYijgoZhqLGcTxxyBIDmMwyNU2OzlknyFZ1ZhYIPKuw069njJjbuL0Nv\n" +
+            "9PXKO+pNZMQvwo7WrepJ0B0VG5KYjDzetFzxFOtr7x6M5Z7XZsPhNIjDjE+oBHu+\n" +
+            "Hzc1/bGyWpZrzc04nqCNTsmOvID1H50Jt778mQIDAQABAoIBAQCLTi6fn1E/L5R9\n" +
+            "uQfQBTEVylAMG0DeZsBLi5G7o+4Jxm2WE8meGGM5vB8GqjqQFCyBP6JoOGdlmRx0\n" +
+            "CcNJTAyJj8pFGopSEgrQkaIBfTNb1L+NwIQ4b7U7+CayLCeJ5hhji5LaZUqzw2E0\n" +
+            "NKekAy2Y7Rsv+1ZzM8tOmF7QsrJCGL8/WOq7QLVmuMKawOeGzp7H6RQ9BPjOBB/Q\n" +
+            "swe2G5uzLY/SkTlVjEriVGuHP1NzX2t7QzV1HpLtVhLrCBKZvo1LJ9sfSXxOZmM3\n" +
+            "/uCNE3+JMaTsMk0DSdZnY4pwfOPOfbUAf0TBHAKIu2styybPS46RPGRPDbxhkCTP\n" +
+            "Wc1P7kCxAoGBAOl3I9n8NXwKfG30Z9KENgTXHYPvVdJKhcpRiqklq7KKv6oqmp4Z\n" +
+            "0/A44whbaWl4P0ugmYsRifDJpVNE0C0Mw0nzWpuq84y6/GeU/VP+M+B17/yDMElj\n" +
+            "3EXITNM2XrZlD6iYI6WleaNasKW5M1pVOhGiHdVTQxTRWm+eZYfiYmLtAoGBAMbq\n" +
+            "ownQ1mDogKZUol5zq09z/6NeXFZpVhDNiaQzuX2J8wBh9Bkb9njtNPyg5+XoCw/c\n" +
+            "0/UsplTSCEKxDWFHTOKCAEiqA7HQfHbqpykwX76DNnilCIKa1AXhVeUmu/+YYaOO\n" +
+            "SAuyHOZlbKJaK8mjk4EEe1Mr39z537elsh1JDS7dAoGBAOQFqi14yKA6+abG5DRX\n" +
+            "Tw9RLxGyS4cVpDCjjaOBGH5MR8Ci1dr+7OIeHZgG+CC8Ak4SMIUEf05/FAsNFao6\n" +
+            "Ye6zUVbjE/bqliVw/i/wAqkDZ36gfyPe9b/uTyKnYsAQWsfWuFJMGU6z//4MsZxT\n" +
+            "y2B3j13QcZ8+jm6gLRgXwvJNAoGATEF/JzAsPxJi32DqrhLhxZ/OjK6L74SKPf7N\n" +
+            "mWlK3tmXkrn6ffW+UzV8bqywue5u7zHU/9SSH0o1aHu/iV9wFhWITlL+/5fRXzUt\n" +
+            "yBiHW92pcC60SH1acraj2ykyQRYFuFG/RNyPP7P6JXMz/iT7UyaIsKXNOEWCgkC/\n" +
+            "O4LZzvECgYEA4HaAE2ne4Wjevnsz8tQm97Of5XImPNI1EcIPH8shpsxB88ubw0hr\n" +
+            "KUXwyj0dMvq7mB+8nJL9Hg/QWcqJ+buLRPcoqqaveRYXrVSnT+9MnYQ5LzqRdH3b\n" +
+            "QBUnpzF6+17WlvPbE4t/OD0wzP6mUPh8UOgsU2bb+cVv4cMbroujLq8=\n" +
+            "-----END RSA PRIVATE KEY-----";
 
     private static final String JWT_VERSION = "jwt-version";
     private static final String JWT_VERSION_SLASHED = "/" + JWT_VERSION;
@@ -126,6 +153,16 @@ public class XmlOAuthToJsonJwtUpdateTest
     {
         final HttpMethod httpMethod = HttpMethod.GET;
         URI uri = URI.create(requestUtil.getApplicationRestUrl("/applinks/1.0/manifest"));
+        uri = signOAuthUri(httpMethod, uri);
+
+        return requestUtil.requestBuilder()
+                .setMethod(httpMethod)
+                .setUri(uri)
+                .build();
+    }
+
+    private URI signOAuthUri(HttpMethod httpMethod, URI uri) throws IOException, OAuthException, URISyntaxException
+    {
         final Map<String, String> oAuthParams = new HashMap<String, String>();
         {
             oAuthParams.put(OAuth.OAUTH_SIGNATURE_METHOD, OAuth.RSA_SHA1);
@@ -135,37 +172,15 @@ public class XmlOAuthToJsonJwtUpdateTest
             oAuthParams.put(OAuth.OAUTH_TIMESTAMP, String.valueOf(System.currentTimeMillis() / 1000));
         }
         final OAuthMessage oAuthMessage = new OAuthMessage(httpMethod.toString(), uri.toString(), oAuthParams.entrySet());
-        final String privateKey = "-----BEGIN RSA PRIVATE KEY-----\n" +
-                "MIIEpQIBAAKCAQEAtWghUS3KRC01kJkeU83PS2CSH6Vma2twv7p7JVHUIWl69tNP\n" +
-                "vn46mYt3gbFQgmBv74pAJJkb1FvBNxhy0B199fXgZUVS+6R7597t0hVG610Zbl4A\n" +
-                "r7xvs/h3ACwUhSib3ad496nghLvOnarrJIgwsNQeCaBz+FczCYQAt7Mk8vGjE1XS\n" +
-                "ha6FEBNKIYijgoZhqLGcTxxyBIDmMwyNU2OzlknyFZ1ZhYIPKuw069njJjbuL0Nv\n" +
-                "9PXKO+pNZMQvwo7WrepJ0B0VG5KYjDzetFzxFOtr7x6M5Z7XZsPhNIjDjE+oBHu+\n" +
-                "Hzc1/bGyWpZrzc04nqCNTsmOvID1H50Jt778mQIDAQABAoIBAQCLTi6fn1E/L5R9\n" +
-                "uQfQBTEVylAMG0DeZsBLi5G7o+4Jxm2WE8meGGM5vB8GqjqQFCyBP6JoOGdlmRx0\n" +
-                "CcNJTAyJj8pFGopSEgrQkaIBfTNb1L+NwIQ4b7U7+CayLCeJ5hhji5LaZUqzw2E0\n" +
-                "NKekAy2Y7Rsv+1ZzM8tOmF7QsrJCGL8/WOq7QLVmuMKawOeGzp7H6RQ9BPjOBB/Q\n" +
-                "swe2G5uzLY/SkTlVjEriVGuHP1NzX2t7QzV1HpLtVhLrCBKZvo1LJ9sfSXxOZmM3\n" +
-                "/uCNE3+JMaTsMk0DSdZnY4pwfOPOfbUAf0TBHAKIu2styybPS46RPGRPDbxhkCTP\n" +
-                "Wc1P7kCxAoGBAOl3I9n8NXwKfG30Z9KENgTXHYPvVdJKhcpRiqklq7KKv6oqmp4Z\n" +
-                "0/A44whbaWl4P0ugmYsRifDJpVNE0C0Mw0nzWpuq84y6/GeU/VP+M+B17/yDMElj\n" +
-                "3EXITNM2XrZlD6iYI6WleaNasKW5M1pVOhGiHdVTQxTRWm+eZYfiYmLtAoGBAMbq\n" +
-                "ownQ1mDogKZUol5zq09z/6NeXFZpVhDNiaQzuX2J8wBh9Bkb9njtNPyg5+XoCw/c\n" +
-                "0/UsplTSCEKxDWFHTOKCAEiqA7HQfHbqpykwX76DNnilCIKa1AXhVeUmu/+YYaOO\n" +
-                "SAuyHOZlbKJaK8mjk4EEe1Mr39z537elsh1JDS7dAoGBAOQFqi14yKA6+abG5DRX\n" +
-                "Tw9RLxGyS4cVpDCjjaOBGH5MR8Ci1dr+7OIeHZgG+CC8Ak4SMIUEf05/FAsNFao6\n" +
-                "Ye6zUVbjE/bqliVw/i/wAqkDZ36gfyPe9b/uTyKnYsAQWsfWuFJMGU6z//4MsZxT\n" +
-                "y2B3j13QcZ8+jm6gLRgXwvJNAoGATEF/JzAsPxJi32DqrhLhxZ/OjK6L74SKPf7N\n" +
-                "mWlK3tmXkrn6ffW+UzV8bqywue5u7zHU/9SSH0o1aHu/iV9wFhWITlL+/5fRXzUt\n" +
-                "yBiHW92pcC60SH1acraj2ykyQRYFuFG/RNyPP7P6JXMz/iT7UyaIsKXNOEWCgkC/\n" +
-                "O4LZzvECgYEA4HaAE2ne4Wjevnsz8tQm97Of5XImPNI1EcIPH8shpsxB88ubw0hr\n" +
-                "KUXwyj0dMvq7mB+8nJL9Hg/QWcqJ+buLRPcoqqaveRYXrVSnT+9MnYQ5LzqRdH3b\n" +
-                "QBUnpzF6+17WlvPbE4t/OD0wzP6mUPh8UOgsU2bb+cVv4cMbroujLq8=\n" +
-                "-----END RSA PRIVATE KEY-----";
-        final OAuthConsumer oAuthConsumer = new OAuthConsumer(null, oAuthPlugin.getKey(), privateKey, new OAuthServiceProvider(null, null, null));
-        oAuthConsumer.setProperty(RSA_SHA1.PRIVATE_KEY, privateKey);
+        final OAuthConsumer oAuthConsumer = new OAuthConsumer(null, oAuthPlugin.getKey(), OLD_PRIVATE_KEY, new OAuthServiceProvider(null, null, null));
+        oAuthConsumer.setProperty(RSA_SHA1.PRIVATE_KEY, OLD_PRIVATE_KEY);
         final OAuthSignatureMethod oAuthSignatureMethod = OAuthSignatureMethod.newSigner(oAuthMessage, new OAuthAccessor(oAuthConsumer));
         oAuthSignatureMethod.sign(oAuthMessage);
+        return addOAuthParamsToRequest(uri, oAuthMessage);
+    }
+
+    private static URI addOAuthParamsToRequest(URI uri, OAuthMessage oAuthMessage) throws IOException
+    {
         StringBuilder sb = new StringBuilder("?");
         {
             boolean isFirst = true;
@@ -183,16 +198,7 @@ public class XmlOAuthToJsonJwtUpdateTest
 
             uri = URI.create(uri + sb.toString());
         }
-
-        System.out.println("################## " + getClass().getSimpleName());
-        System.out.println(uri);
-        System.out.println("##################");
-        //String authHeader = signedRequest.
-        return requestUtil.requestBuilder()
-                .setMethod(httpMethod)
-                .setUri(uri)
-                //.setHeader("Authorization", authHeader)
-                .build();
+        return uri;
     }
 
     @AfterClass
