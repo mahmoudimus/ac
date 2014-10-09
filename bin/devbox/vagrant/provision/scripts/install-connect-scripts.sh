42e82aa691964cbbe0fd0df69f06231ddbea7651
Tanguy Crusson <tcrusson@atlassian.com>
2014-10-09 17:26:13 +1100

    typo

--- bin/devbox/vagrant/provision/scripts/install-connect-scripts.sh
+++ bin/devbox/vagrant/provision/scripts/install-connect-scripts.sh
@@ -11,7 +11,7 @@ echo 'Linking atlas-run-connect'
 sudo ln -s /home/vagrant/scripts/atlas-run-connect.sh /usr/bin/atlas-run-connect
 echo 'Changing .profile'
 sudo echo '/home/vagrant/scripts/oracle-license.sh' >> /home/vagrant/.profile
-echo 'Changing hostmane to localhost'
+echo 'Changing hostname to localhost'
 sudo sed -i "s/precise32/localhost/g" /etc/hosts
 sudo sed -i "s/precise32/localhost/g" /etc/hostname
 echo 'Running npm install'
