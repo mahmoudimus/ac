037d5676dc128eefa1efbfac52a4ccbbe8364a53
Tanguy Crusson <tcrusson@atlassian.com>
2014-10-27 10:23:25 +1100

    build

--- bin/devbox/startup-commands/publish-startup-commands.sh
+++ bin/devbox/startup-commands/publish-startup-commands.sh
@@ -6,7 +6,7 @@ if [ -n "$1" ]; then
     SSH=$1
 fi
 
-if [ -d "target" ]; then
-	echo "New command was created, deploying to DAC"
+#if [ -d "target" ]; then
+	#echo "New command was created, deploying to DAC"
 	scp target/* uploads@developer-app.internal.atlassian.com:~/static/
-fi
+	#fi
