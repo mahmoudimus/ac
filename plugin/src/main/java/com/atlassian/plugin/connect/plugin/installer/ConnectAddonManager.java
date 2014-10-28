918c38cf199f0439ec56ff94203934190ce2550f
James Hazelwood <jhazelwood@atlassian.com>
2014-10-28 13:17:59 +1100

    ACDEV-1607 No need to localise the message

--- plugin/src/main/java/com/atlassian/plugin/connect/plugin/installer/ConnectAddonManager.java
+++ plugin/src/main/java/com/atlassian/plugin/connect/plugin/installer/ConnectAddonManager.java
@@ -577,7 +577,7 @@ public class ConnectAddonManager
             if (e.getCause() instanceof UnknownHostException)
             {
                 String i18nMessage = i18nResolver.getText("connect.install.error.remote.host.bad.domain",
-                        e.getCause().getLocalizedMessage().replace(": Name or service not known", "").replace(": nodename nor servname provided, or not known", ""));
+                        e.getCause().getMessage().replace(": Name or service not known", "").replace(": nodename nor servname provided, or not known", ""));
                 throw new LifecycleCallbackException(message, Option.some(i18nMessage));
             }
             else if (e.getCause() instanceof SocketTimeoutException)
