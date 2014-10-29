5d5b2ff84c65290309d32341a56fe7a70093316b
James Hazelwood <jhazelwood@atlassian.com>
2014-10-29 13:36:55 +1100

    ACDEV-1615 Revert changes to test filter and move waiting logic into the post function test (seeing as that's how it's done elsewhere, e.g. AbstractAddonLifecycleTest

--- test-support-plugin/src/main/java/com/atlassian/plugin/connect/testsupport/filter/DefaultAddonTestFilterResults.java
+++ test-support-plugin/src/main/java/com/atlassian/plugin/connect/testsupport/filter/DefaultAddonTestFilterResults.java
@@ -1,65 +1,34 @@
 package com.atlassian.plugin.connect.testsupport.filter;
 
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.TimeUnit;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DefaultAddonTestFilterResults implements AddonTestFilterResults
 {
-    private volatile ConcurrentHashMap<String, BlockingQueue<ServletRequestSnapshot>> requestMap;
+    private Map<String, ServletRequestSnapshot> requestMap;
 
     public DefaultAddonTestFilterResults()
     {
-        this.requestMap = new ConcurrentHashMap<String, BlockingQueue<ServletRequestSnapshot>>();
+        this.requestMap = new HashMap<String, ServletRequestSnapshot>();
     }
 
     @Override
     public void put(String key, ServletRequestSnapshot req)
     {
-        BlockingQueue<ServletRequestSnapshot> existingQueue, newQueue;
-        newQueue = new LinkedBlockingQueue<ServletRequestSnapshot>();
-        existingQueue = requestMap.putIfAbsent(key, newQueue);
-        try
-        {
-            if (existingQueue == null)
-            {
-                newQueue.put(req);
-            }
-            else
-            {
-                existingQueue.put(req);
-            }
-        }
-        catch (InterruptedException e)
-        {
-            throw new RuntimeException(e);
-        }
+        requestMap.put(key, req);
     }
 
     @Override
     public ServletRequestSnapshot getRequest(String addonKey, String resource)
     {
         String res = (resource.startsWith("/")) ? resource : "/" + resource;
-        requestMap.putIfAbsent(addonKey + res, new LinkedBlockingQueue<ServletRequestSnapshot>());
-        try
-        {
-            return requestMap.get(addonKey + res).poll(10, TimeUnit.SECONDS);
-        }
-        catch (InterruptedException e)
-        {
-            throw new RuntimeException(e);
-        }
+        return requestMap.get(addonKey + res);
     }
 
     @Override
     public void clearRequest(String addonKey, String resource)
     {
         String res = (resource.startsWith("/")) ? resource : "/" + resource;
-        BlockingQueue<ServletRequestSnapshot> queue = requestMap.get(addonKey + res);
-        if (queue != null)
-        {
-            queue.clear();
-        }
+        requestMap.remove(addonKey + res);
     }
 }
