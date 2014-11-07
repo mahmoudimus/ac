9e852730d67a45c334a868e917e60c9caede5678
Chris Whittington <cwhittington@atlassian.com>
2014-11-07 14:09:04 +0000

    ACDEV-1363 refactor confluence macro code to fit new style

--- plugin/src/main/resources/js/confluence/macro/editor-rpc.js
+++ plugin/src/main/resources/js/confluence/macro/editor-rpc.js
@@ -1,21 +1,25 @@
-require(["_dollar", "confluence/macro/editor", "_rpc"], function($, editor, rpc) {
-    rpc.extend(function () {
-        return {
-            internals: {
-                saveMacro: function (updatedParams, updatedMacroBody) {
-                    editor.saveMacro(updatedParams, updatedMacroBody);
-                },
-                closeMacroEditor: function () {
-                    editor.close();
-                },
-                getMacroData: function (callback) {
-                    editor.getMacroData(callback);
-                },
-                getMacroBody: function (callback) {
-                    editor.getMacroBody(callback);
+(function($, extend, require){
+    "use strict";
+
+    require(["ac/confluence/macro/editor"], function(editor) {
+        extend(function () {
+            return {
+                internals: {
+                    saveMacro: function (updatedParams, updatedMacroBody) {
+                        editor.saveMacro(updatedParams, updatedMacroBody);
+                    },
+                    closeMacroEditor: function () {
+                        editor.close();
+                    },
+                    getMacroData: function (callback) {
+                        editor.getMacroData(callback);
+                    },
+                    getMacroBody: function (callback) {
+                        editor.getMacroBody(callback);
+                    }
                 }
-            }
-        };
+            };
+        });
     });
 
-});
+}(AJS.$, _AP.extend, require));
