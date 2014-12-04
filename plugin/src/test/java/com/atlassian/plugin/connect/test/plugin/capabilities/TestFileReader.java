995eea8b96643ed31e902ce77e982581e7514db3
Matthew Jensen <mjensen@atlassian.com>
2014-12-04 16:14:10 +1100

    ACDEV-1674 set encoding when loading test files

--- plugin/src/test/java/com/atlassian/plugin/connect/test/plugin/capabilities/TestFileReader.java
+++ plugin/src/test/java/com/atlassian/plugin/connect/test/plugin/capabilities/TestFileReader.java
@@ -9,6 +9,5 @@ public class TestFileReader
     public static String readAddonTestFile(String fileName) throws IOException
     {
         return IOUtils.toString(TestFileReader.class.getClassLoader().getResourceAsStream("testfiles/capabilities/" + fileName), Charset.forName("UTF-8"));
-//        return FileUtils.readFile(new DefaultResourceLoader().getResource("classpath:/testfiles/capabilities/" + fileName).getFile());
     }
 }
