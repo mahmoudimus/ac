86c6d3930fc4167287920a876291f2975e0def7c
Matthew Jensen <mjensen@atlassian.com>
2014-12-10 12:53:03 +1100

    ACDEV-1674 old version of io utils provided by products requires us to use a different form of the toString method

--- plugin/src/test/java/com/atlassian/plugin/connect/test/plugin/capabilities/TestFileReader.java
+++ plugin/src/test/java/com/atlassian/plugin/connect/test/plugin/capabilities/TestFileReader.java
@@ -2,12 +2,16 @@ package com.atlassian.plugin.connect.test.plugin.capabilities;
 
 import org.apache.commons.io.IOUtils;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 
 public class TestFileReader
 {
     public static String readAddonTestFile(String fileName) throws IOException
     {
-        return IOUtils.toString(TestFileReader.class.getClassLoader().getResourceAsStream("testfiles/capabilities/" + fileName), Charset.forName("UTF-8"));
+        // old version of io utils in the products require us to use this form of toString
+        return IOUtils.toString(
+                new InputStreamReader(TestFileReader.class.getClassLoader().getResourceAsStream("testfiles/capabilities/" + fileName),
+                        Charset.forName("UTF-8")));
     }
 }
