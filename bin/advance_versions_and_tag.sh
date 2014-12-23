bc844838467f071cef2276f3aa4de54f2997993e
Peter Brownlow <pbrownlow@atlassian.com>
2014-12-23 11:17:15 +1100

    bin/advance_versions_and_tag.sh : committing -SNAPSHOT versions to master branch as the freezer plan will remove the -SNAPSHOT suffix, and because it currently expectes changed pom files (fails when there aren't any)

--- bin/advance_versions_and_tag.sh
+++ bin/advance_versions_and_tag.sh
@@ -26,27 +26,25 @@ PREFIX="--> "
 SNAPSHOT="-SNAPSHOT"
 STARTING_VERSION=$(mvn -npu org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -v '\[' | grep -iv 'download' | grep -ve '[0-9]*/[0-9]*K')
 NEW_VERSION=`echo ${STARTING_VERSION} | sed "s/${SNAPSHOT}//"`
-echo "${PREFIX} Setting new version in poms: ${NEW_VERSION}"
-mvn versions:set -DnewVersion=${NEW_VERSION}
+echo "${PREFIX} next release version: '${NEW_VERSION}'"
+echo "${PREFIX} switching to master branch"
+git checkout master
+echo "${PREFIX} merging develop into master"
+git merge develop
+echo "${PREFIX} git-tagging ${NEW_VERSION}"
+git tag ${NEW_VERSION}
+echo "${PREFIX} git-pushing master branch to origin; this will cause the Freezer plan to start the release"
+git push --tags origin master
+echo "${PREFIX} switching back to develop"
+git checkout develop
+echo "${PREFIX} incrementing -SNAPSHOT version in poms"
+mvn --batch-mode release:update-versions -DautoVersionSubmodules=true
 POM_FILENAMES=`git status --porcelain | grep " M .*pom.xml" | sed "s/ M //"`
 echo "${PREFIX} pom files: ${POM_FILENAMES}"
 echo "${PREFIX} git-adding pom files"
 git add ${POM_FILENAMES}
 echo "${PREFIX} git-committing pom files"
-git commit -m "removed -SNAPSHOT suffix; release version is ${NEW_VERSION}" $POM_FILENAMES
-echo "${PREFIX} switching to master branch"
-git checkout master
-echo "${PREFIX} merging develop into master; this will cause the Freezer plan to start the release"
-git merge develop
-echo "${PREFIX} git-tagging $NEW_VERSION"
-git tag $NEW_VERSION
-echo "${PREFIX} git-pushing master branch to origin"
-git push --tags origin master
-echo "${PREFIX} switching back to develop"
-git checkout develop
-NEW_SNAPSHOT_VERSION="${NEW_VERSION}${SNAPSHOT}"
-echo "${PREFIX} Setting new version in poms: $NEW_SNAPSHOT_VERSION"
-mvn versions:set -DnewVersion=${NEW_SNAPSHOT_VERSION}
+git commit -m "removed -SNAPSHOT suffix; release version is ${NEW_VERSION}" ${POM_FILENAMES}
 echo "${PREFIX} git-pushing develop branch to origin"
 git push origin develop
 echo "${PREFIX} done!"
