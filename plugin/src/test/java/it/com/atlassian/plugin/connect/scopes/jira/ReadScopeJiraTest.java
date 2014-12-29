b843c500deafacf28e99f98e8669190c3476d781
Einar Pehrson <epehrson@atlassian.com>
2014-12-29 11:07:19 +1100

    AC-1472 Added /secure/useravatar to READ scope whitelist

--- plugin/src/test/java/it/com/atlassian/plugin/connect/scopes/jira/ReadScopeJiraTest.java
+++ plugin/src/test/java/it/com/atlassian/plugin/connect/scopes/jira/ReadScopeJiraTest.java
@@ -1,5 +1,10 @@
+
 package it.com.atlassian.plugin.connect.scopes.jira;
 
+import com.atlassian.jira.bc.project.ProjectService;
+import com.atlassian.jira.project.Project;
+import com.atlassian.jira.user.ApplicationUser;
+import com.atlassian.jira.user.util.UserManager;
 import com.atlassian.jwt.writer.JwtWriterFactory;
 import com.atlassian.plugin.connect.modules.beans.nested.ScopeName;
 import com.atlassian.plugin.connect.plugin.registry.ConnectAddonRegistry;
@@ -10,22 +15,33 @@ import com.atlassian.plugins.osgi.test.AtlassianPluginsTestRunner;
 import com.atlassian.sal.api.ApplicationProperties;
 import it.com.atlassian.plugin.connect.TestAuthenticator;
 import it.com.atlassian.plugin.connect.scopes.ScopeTestBase;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.io.IOException;
 
 @Application("jira")
 @RunWith(AtlassianPluginsTestRunner.class)
 public class ReadScopeJiraTest extends ScopeTestBase
 {
+    private static final String ADMIN_USERNAME = "admin";
+
+    private final UserManager userManager;
+    private final ProjectService projectService;
 
     public ReadScopeJiraTest(TestPluginInstaller testPluginInstaller,
                              TestAuthenticator testAuthenticator,
                              JwtWriterFactory jwtWriterFactory,
                              ConnectAddonRegistry connectAddonRegistry,
-                             ApplicationProperties applicationProperties)
+                             ApplicationProperties applicationProperties,
+                             UserManager userManager,
+                             ProjectService projectService)
     {
         super(ScopeName.READ, testPluginInstaller, testAuthenticator, jwtWriterFactory, connectAddonRegistry,
                 applicationProperties);
+        this.userManager = userManager;
+        this.projectService = projectService;
     }
 
     @Test
@@ -38,5 +54,29 @@ public class ReadScopeJiraTest extends ScopeTestBase
     public void shouldForbidPutGreenhopperRankBefore() throws Exception
     {
         assertForbiddenRequest(HttpMethod.PUT, "/rest/greenhopper/1.0/api/rank/before");
+    }
+
+    @Test
+    public void shouldAllowGetSecureProjectAvatar() throws Exception
+    {
+        Project project = createProject();
+
+        assertValidRequest(HttpMethod.GET, "/secure/projectavatar?pid=" + project.getId());
+    }
+
+    @Test
+    public void shouldAllowGetSecureUserAvatar() throws Exception
+    {
+        assertValidRequest(HttpMethod.GET, "/secure/useravatar?ownerId=" + ADMIN_USERNAME);
+    }
+
+    private Project createProject() throws IOException
+    {
+        int keyLength = 6;
+        String key = RandomStringUtils.randomAlphabetic(keyLength).toUpperCase();
+        ApplicationUser user = userManager.getUserByKey(ADMIN_USERNAME);
+        ProjectService.CreateProjectValidationResult result = projectService.validateCreateProject(
+                user.getDirectoryUser(), key, key, null, ADMIN_USERNAME, null, null);
+        return projectService.createProject(result);
     }
 }
