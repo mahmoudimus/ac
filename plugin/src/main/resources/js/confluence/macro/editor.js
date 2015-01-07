9792e775f3844c019e3c4f180d7c31c9c4ea359f
Edith Tom <etom@atlassian.com>
2015-01-07 13:56:54 +1100

    CE-74 Restore editor focus before attempting to get selection when opening a custom macro editor to avoid HierarchyRequestError in IE

--- plugin/src/main/resources/js/confluence/macro/editor.js
+++ plugin/src/main/resources/js/confluence/macro/editor.js
@@ -66,6 +66,9 @@
              * @param {String|Number} [opts.height="50%"] height of the dialog, expressed as either absolute pixels (eg 600) or percent (eg 50%)
              */
             openCustomEditor: function(macroData, opts) {
+                // CE-74: if any macros cause the editor to lose focus before this is executed, this will cause
+                // a HierarchyRequestError in Internet Explorer, so we'll restore focus just in case.
+                AJS.Rte.getEditor().focus();
                 var editorSelection = AJS.Rte.getEditor().selection;
                 var bm = editorSelection.getBookmark();
 
