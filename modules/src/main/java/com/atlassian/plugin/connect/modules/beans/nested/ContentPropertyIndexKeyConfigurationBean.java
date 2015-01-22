adde062b1e5d78ef9d68445326ac87ffba6bdc1f
Maciej Zasada <mzasada@atlassian.com>
2015-01-23 10:04:03 +1100

    CRA-378 Polish docs

--- modules/src/main/java/com/atlassian/plugin/connect/modules/beans/nested/ContentPropertyIndexKeyConfigurationBean.java
+++ modules/src/main/java/com/atlassian/plugin/connect/modules/beans/nested/ContentPropertyIndexKeyConfigurationBean.java
@@ -2,12 +2,29 @@ package com.atlassian.plugin.connect.modules.beans.nested;
 
 import java.util.List;
 
+import com.atlassian.json.schema.annotation.Required;
+
 /**
- * Representation of a group of extractions from a single JSON documents, associated with a given content property key.
+ * Representation of a list of extraction recipes for a given content property key. It defines which JSON values
+ * should be extracted into the Confluence search index, so that they can be used later on for content search using
+ * CQL.
+ *
+ *#### Example
+ *
+ * @exampleJson {@see com.atlassian.plugin.connect.plugin.capabilities.beans.ConnectJsonExamples#CONTENT_PROPERTY_EXAMPLE}
+ * @schemaTitle Index Key Configuration
  */
 public class ContentPropertyIndexKeyConfigurationBean
 {
+    /**
+     * The key of the property from which the data is indexed.
+     */
+    @Required
     private final String propertyKey;
+    /**
+     * The list with references to values of JSON object which will be indexed and the types of referenced values.
+     */
+    @Required
     private final List<ContentPropertyIndexExtractionConfigurationBean> extractions;
 
     public ContentPropertyIndexKeyConfigurationBean(String propertyKey,
