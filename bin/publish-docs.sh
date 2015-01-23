8de5ff35b24244f4b167243c620a68f0a6464f4e
Seb Ruiz <sruiz@atlassian.com>
2015-01-23 12:49:49 +1100

    ACDEV-1738: Beginning of version path additions

--- bin/publish-docs.sh
+++ bin/publish-docs.sh
@@ -10,4 +10,11 @@ cd docs
 npm i
 npm run-script build
 
-rsync -avz --delete -e 'ssh' target/www/* uploads@developer-app.internal.atlassian.com:/opt/j2ee/domains/atlassian.com/developer-prod/static-content/static/connect/docs
+VERSION=
+
+if [ -z "${VERSION}" ]; then
+    echo "VERSION not specified!"
+    exit 1
+fi
+
+rsync -avz --delete -e 'ssh' target/www/* uploads@developer-app.internal.atlassian.com:/opt/j2ee/domains/atlassian.com/developer-prod/static-content/static/connect/$VERSION/docs/
