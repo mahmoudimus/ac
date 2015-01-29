23d542300b84bae2944faf9a93ccdabd01104ecc
James Hazelwood <jhazelwood@atlassian.com>
2015-01-30 09:21:13 +1100

    ACDEV-1306 Use annotation instead of component-import for upgrade task class

--- plugin/src/main/java/com/atlassian/plugin/connect/plugin/upgrade/ConnectAddOnUserAttributeUpgradeTask.java
+++ plugin/src/main/java/com/atlassian/plugin/connect/plugin/upgrade/ConnectAddOnUserAttributeUpgradeTask.java
@@ -16,6 +16,8 @@ import com.atlassian.plugin.connect.plugin.usermanagement.ConnectAddOnUserGroupP
 import com.atlassian.sal.api.message.Message;
 import com.atlassian.sal.api.upgrade.PluginUpgradeTask;
 
+import org.springframework.stereotype.Component;
+
 import static com.atlassian.crowd.search.builder.QueryBuilder.queryFor;
 import static com.atlassian.crowd.search.query.entity.EntityQuery.ALL_RESULTS;
 import static com.atlassian.plugin.connect.plugin.usermanagement.ConnectAddOnUserUtil.Constants.ADDON_USER_GROUP_KEY;
@@ -27,6 +29,7 @@ import static com.atlassian.plugin.connect.plugin.usermanagement.ConnectAddOnUse
  * A {@link com.atlassian.sal.api.upgrade.PluginUpgradeTask} that will iterate over all Connect AddOn Users and add a new attribute to them
  * <em>synch.APPLICATION_NAME.atlassian-connect-user</em>
  */
+@Component
 public class ConnectAddOnUserAttributeUpgradeTask implements PluginUpgradeTask
 {
     private final ApplicationService applicationService;
