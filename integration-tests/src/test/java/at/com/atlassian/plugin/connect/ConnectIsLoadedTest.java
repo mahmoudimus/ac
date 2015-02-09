75d11f736f872931541475cce20007ddebe5e171
James Hazelwood <jhazelwood@atlassian.com>
2015-02-09 11:44:47 +1100

    ACDEV-1655 Retrieve url in ConnectIsLoadedTest so that we can run it locally without hassle

--- integration-tests/src/test/java/at/com/atlassian/plugin/connect/ConnectIsLoadedTest.java
+++ integration-tests/src/test/java/at/com/atlassian/plugin/connect/ConnectIsLoadedTest.java
@@ -1,20 +1,27 @@
 package at.com.atlassian.plugin.connect;
 
-import com.atlassian.test.categories.OnDemandAcceptanceTest;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import org.apache.commons.codec.binary.Base64;
-import org.apache.commons.io.IOUtils;
-import org.junit.Test;
-import org.junit.experimental.categories.Category;
-
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collection;
+
+import com.atlassian.jira.webtests.util.LocalTestEnvironmentData;
+import com.atlassian.pageobjects.TestedProduct;
+import com.atlassian.pageobjects.TestedProductFactory;
+import com.atlassian.plugin.connect.test.pageobjects.confluence.FixedConfluenceTestedProduct;
+import com.atlassian.test.categories.OnDemandAcceptanceTest;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.IOUtils;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
@@ -29,19 +36,27 @@ public class ConnectIsLoadedTest
     private final static String PASSWORD = "admin";
     private final static String CONNECT_PLUGIN_KEY = "com.atlassian.plugins.atlassian-connect-plugin";
     private final static String KEY_PROPERTY = "key";
+    private static String jiraUrl;
+    private static String confluenceUrl;
+
+    @BeforeClass
+    public static void setUp()
+    {
+        TestedProduct<?> confluenceProduct = TestedProductFactory.create(FixedConfluenceTestedProduct.class.getName());
+        confluenceUrl = confluenceProduct.getProductInstance().getBaseUrl();
+        jiraUrl = new LocalTestEnvironmentData().getBaseUrl().toString();
+    }
 
     @Test
     public void connectShouldBeLoadedInJira() throws IOException
     {
-        System.out.println("Running ConnectIsLoadedTest.connectShouldBeLoadedInJira()!");
-        connectShouldBeLoaded(System.getProperty("baseurl.jira"));
+        connectShouldBeLoaded(jiraUrl);
     }
 
     @Test
     public void connectShouldBeLoadedInConfluence() throws IOException
     {
-        System.out.println("Running ConnectIsLoadedTest.connectShouldBeLoadedInConfluence()!");
-        connectShouldBeLoaded(System.getProperty("baseurl.confluence"));
+        connectShouldBeLoaded(confluenceUrl);
     }
 
     private void connectShouldBeLoaded(String productBaseUrl) throws IOException
