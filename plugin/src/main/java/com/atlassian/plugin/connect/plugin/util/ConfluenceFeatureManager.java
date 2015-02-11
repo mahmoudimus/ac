504bb500d910a80fce224f634846b5dcfd8a3ed1
James Hazelwood <jhazelwood@atlassian.com>
2015-02-11 18:08:32 +1100

    ACDEV-1306 Missing autowired annotation

--- plugin/src/main/java/com/atlassian/plugin/connect/plugin/util/ConfluenceFeatureManager.java
+++ plugin/src/main/java/com/atlassian/plugin/connect/plugin/util/ConfluenceFeatureManager.java
@@ -3,11 +3,14 @@ package com.atlassian.plugin.connect.plugin.util;
 import com.atlassian.confluence.setup.settings.CoreFeaturesManager;
 import com.atlassian.plugin.spring.scanner.annotation.component.ConfluenceComponent;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 @ConfluenceComponent
 public class ConfluenceFeatureManager implements FeatureManager
 {
     private final CoreFeaturesManager coreFeaturesManager;
 
+    @Autowired
     public ConfluenceFeatureManager(final CoreFeaturesManager coreFeaturesManager)
     {
         this.coreFeaturesManager = coreFeaturesManager;
