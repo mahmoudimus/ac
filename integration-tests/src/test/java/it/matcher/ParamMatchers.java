bba0c946399ae7c645a542de3aa45c69624f892e
Einar Pehrson <epehrson@atlassian.com>
2015-02-12 15:23:37 +1100

    ACDEV-1784 Added missing import

--- integration-tests/src/test/java/it/matcher/ParamMatchers.java
+++ integration-tests/src/test/java/it/matcher/ParamMatchers.java
@@ -5,6 +5,7 @@ import org.hamcrest.Matcher;
 import java.util.TimeZone;
 
 import static it.matcher.MatchesPattern.matchesPattern;
+import static org.hamcrest.Matchers.isIn;
 
 /**
  * Matchers for parameters passed to Connect add-ons.
