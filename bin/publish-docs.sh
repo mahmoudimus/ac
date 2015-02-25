b1f8005e1bc0022d990e6fc169381e997f72499b
Jake Furler <jfurler@atlassian.com>
2015-02-26 10:04:42 +1100

    ACDEV-1738: pull request comments

--- bin/publish-docs.sh
+++ bin/publish-docs.sh
@@ -6,9 +6,11 @@ NEW_VERSION=`echo ${VERSION} | sed "s/-SNAPSHOT//"`
 
 cd docs
 
+echo "${NEW_VERSION}"
+
 if [ -z "${NEW_VERSION}" ]; then
     # Control will enter here if $VERSION not specified.
-    echo "VERSION not specified!"
+    echo "Could not determine version from pom.xml"
     exit 1
 fi
 
@@ -18,13 +20,17 @@ npm run-script build
 #SET THE DESTINATION PATH ON THE NEXT FILE SYSTEM
 DESTINATIONHOST="uploads@developer-app.internal.atlassian.com"
 DESTINATIONPATH="/opt/j2ee/domains/atlassian.com/developer-prod/static-content/static/connect/docs/"
+#DESTINATIONHOST="jfurler@localhost"
+#DESTINATIONPATH="$HOME/atlassian-connect/test/ac-docs/"
 
+echo "$DESTINATIONHOST:$DESTINATIONPATH/$NEW_VERSION"
 
 rsync -avz --delete -e 'ssh' target/gensrc/www/* "$DESTINATIONHOST:$DESTINATIONPATH/$NEW_VERSION"
 
 
 if [ "$1" == "updateSymlink" ]; then
 	ssh "$DESTINATIONHOST" "cd $DESTINATIONPATH; ln -sfn ./$NEW_VERSION latest"
+	echo "'latest' symlink updated."
 fi
 
-echo "Done!"
+echo "Docs published to https://developer.atlassian.com/static/connect/docs"
