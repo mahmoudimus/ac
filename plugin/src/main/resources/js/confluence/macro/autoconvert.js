6a84923ba62aecb7f7f1055fb5a6b082ed8e3971
Matthew Jensen <mjensen@atlassian.com>
2015-03-02 13:36:16 +1100

    CE-33: Fixed javascript closure, removed more fastDev stuff

--- plugin/src/main/resources/js/confluence/macro/autoconvert.js
+++ plugin/src/main/resources/js/confluence/macro/autoconvert.js
@@ -3,36 +3,41 @@
         var data = WRM.data.claim("com.atlassian.plugins.atlassian-connect-plugin:confluence-atlassian-connect-autoconvert-resources.connect-autoconvert-data");
         var arrayLength = data.length;
 
-        console.log("found [ "+arrayLength+" ] autoconvert definitions");
+        console.log("found [ " + arrayLength + " ] autoconvert definitions");
 
-        if (arrayLength>0) {
+        if (arrayLength > 0) {
             for (var i = 0; i < arrayLength; i++) {
-                var macroName = data[i].macroName;
-                // TODO convert pattern to regex
-                var pattern = data[i].autoconvert.pattern;
+                var handler = function (i) {
+                    return function (uri, node, done) {
+                        var macroName = data[i].macroName;
+                        // TODO convert pattern to regex
+                        var pattern = data[i].autoconvert.pattern;
 
-                console.log("registering autoconvert handler for [ " + macroName + " ] and pattern [ " + pattern + " ]");
+                        console.log("registering autoconvert handler for [ " + macroName + " ] and pattern [ " + pattern + " ]");
 
-                tinymce.plugins.Autoconvert.autoConvert.addHandler(function (uri, node, done) {
-                    var matches = uri.source.match(pattern);
-                    if (matches) {
-                        console.log("matched autoconvert pattern [ " + pattern + " ] with uri [ " + uri + " ]");
-                        var params = {};
-                        // TODO generate macro parameters from matched groups
+                        var matches = uri.source.match(pattern);
+                        if (matches) {
+                            console.log("matched autoconvert pattern [ " + pattern + " ] with uri [ " + uri + " ]");
+                            var params = {};
+
+                            // TODO generate macro parameters from matched groups
 //                    for (var j = 0; j < autoConvertMappings[i].parameters.length; j++) {
 //                        params[autoConvertMappings[i].parameters[j]] = matches[j + 1];
 //                    }
-                        var macro = {
-                            name: macroName,
-                            params: params
-                        };
-                        tinymce.plugins.Autoconvert.convertMacroToDom(macro, done, done);
-                    } else {
-                        console.log("did not match autoconvert pattern [ " + pattern + " ] with uri [ " + uri + " ]");
-                        done();
-                    }
+                            var macro = {
+                                name: macroName,
+                                params: params
+                            };
+                            tinymce.plugins.Autoconvert.convertMacroToDom(macro, done, done);
+                        } else {
+                            console.log("did not match autoconvert pattern [ " + pattern + " ] with uri [ " + uri + " ]");
+                            done();
+                        }
 
-                });
+                    }
+                }(i);
+
+                tinymce.plugins.Autoconvert.autoConvert.addHandler(handler);
             }
 
         }
