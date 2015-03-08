5a00d1c41b717097d0688733075cc26e482063d5
Jake Furler <jfurler@atlassian.com>
2015-03-08 15:25:28 +1100

    ACDEV-1738: added gensrc directory to path

--- bin/publish-docs.sh
+++ bin/publish-docs.sh
@@ -10,4 +10,4 @@ cd docs
 npm i
 npm run-script build
 
-rsync -avz --delete -e 'ssh' target/www/* uploads@developer-app.internal.atlassian.com:/opt/j2ee/domains/atlassian.com/developer-prod/static-content/static/connect/docs
+rsync -avz --delete -e 'ssh' target/gensrc/www/* uploads@developer-app.internal.atlassian.com:/opt/j2ee/domains/atlassian.com/developer-prod/static-content/static/connect/docs
