4056a78f3a1c842550dabf565743aa16677c9205
Jake Furler <jfurler@atlassian.com>
2015-03-17 11:46:55 +1100

    ACDEV-1738: testing

--- bin/publish-docs.sh
+++ bin/publish-docs.sh
@@ -4,5 +4,9 @@ DESTINATIONHOST="uploads@developer-app.internal.atlassian.com"
 DESTINATIONPATH="/opt/j2ee/domains/atlassian.com/developer-prod/static-content/static/connect/docs/"
 $NEW_VERSION="1.1.26"
 
-ssh "$DESTINATIONHOST" "cd $DESTINATIONPATH; ln -sfn ./$NEW_VERSION latest; ls -l"
+#ssh "$DESTINATIONHOST" "cd $DESTINATIONPATH; ln -sfn ./$NEW_VERSION latest; ls -l"
+
+ln -sfn ./$NEW_VERSION latest
+rsync -avz -e 'ssh' latest "$DESTINATIONHOST:$DESTINATIONPATH"
+
 echo "'latest' symlink updated."
