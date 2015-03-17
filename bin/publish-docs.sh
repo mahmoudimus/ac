821bda64328035a9c4b2e0b3e2c3143292f881a1
Jake Furler <jfurler@atlassian.com>
2015-03-17 13:59:43 +1100

    ACDEV-1738: moving docs publishing over to versioned directories

--- bin/publish-docs.sh
+++ bin/publish-docs.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
 
-SSH="ssh"
-
-if [ -n "$1" ]; then
-    SSH=$1
-fi
+#GET THE MVN BUILD VERSION NUMBER
+VERSION=$(mvn -npu org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -v '\[' | grep -iv 'download' | grep -ve '[0-9]*/[0-9]*K')
+NEW_VERSION=`echo ${VERSION} | sed "s/-SNAPSHOT//"`
 
 cd docs
+
+if [ -z "${NEW_VERSION}" ]; then
+    # Control will enter here if $VERSION not specified.
+    echo "VERSION not specified!"
+    exit 1
+fi
+
 npm i
 npm run-script build
 
-rsync -avz --delete -e 'ssh' target/gensrc/www/* uploads@developer-app.internal.atlassian.com:/opt/j2ee/domains/atlassian.com/developer-prod/static-content/static/connect/docs
+#SET THE DESTINATION PATH ON THE NEXT FILE SYSTEM
+DESTINATION="uploads@developer-app.internal.atlassian.com:/opt/j2ee/domains/atlassian.com/developer-prod/static-content/static/connect/docs/"
+#DESTINATION="$HOME/atlassian-connect/test/ac-docs/"
+
+
+rsync -avz --delete -e 'ssh' target/gensrc/www/* "$DESTINATION/$NEW_VERSION"
+
+
+if [ "$1" == "updateSymlink" ]; then
+    ln -sfn ./$NEW_VERSION latest
+    rsync -avz -e 'ssh' latest "$DESTINATION"
+fi
+
+echo "Done!"
