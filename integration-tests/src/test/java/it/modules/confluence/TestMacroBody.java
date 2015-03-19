d28e549d01274b1f5b56462de44e665a211c4e8d
Matthew Jensen <mjensen@atlassian.com>
2015-03-20 09:03:15 +1100

    CE-79 * Integration test for macro body collection by hash or id, for static and dynamic macros

---
+++ integration-tests/src/test/java/it/modules/confluence/TestMacroBody.java
@@ -0,0 +1,183 @@
+package it.modules.confluence;
+
+import com.atlassian.confluence.api.model.content.ContentRepresentation;
+import com.atlassian.confluence.it.Page;
+import com.atlassian.confluence.pageobjects.page.content.ViewPage;
+import com.atlassian.plugin.connect.modules.beans.DynamicContentMacroModuleBean;
+import com.atlassian.plugin.connect.modules.beans.StaticContentMacroModuleBean;
+import com.atlassian.plugin.connect.modules.beans.nested.I18nProperty;
+import com.atlassian.plugin.connect.modules.beans.nested.MacroBodyType;
+import com.atlassian.plugin.connect.modules.beans.nested.MacroOutputType;
+import com.atlassian.plugin.connect.modules.beans.nested.ScopeName;
+import com.atlassian.plugin.connect.test.AddonTestUtils;
+import com.atlassian.plugin.connect.test.HttpUtils;
+import com.atlassian.plugin.connect.test.pageobjects.confluence.RenderedMacro;
+import com.atlassian.plugin.connect.test.server.ConnectRunner;
+import com.google.common.collect.Maps;
+import it.servlet.HttpContextServlet;
+import it.servlet.InstallHandlerServlet;
+import it.servlet.macro.BodyHandler;
+import it.servlet.macro.MacroBodyServlet;
+import it.util.TestUser;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+import static com.atlassian.plugin.connect.modules.beans.DynamicContentMacroModuleBean.newDynamicContentMacroModuleBean;
+import static com.atlassian.plugin.connect.modules.beans.StaticContentMacroModuleBean.newStaticContentMacroModuleBean;
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * This test case will collect the macro body from confluence in all the different ways possible.  It will check
+ * both static and dynamic macros being collected by hash or id.
+ */
+public class TestMacroBody extends AbstractConfluenceWebDriverTest
+{
+    private static ConnectRunner remotePlugin;
+
+    @BeforeClass
+    public static void startConnectAddOn() throws Exception
+    {
+        DynamicContentMacroModuleBean dynamicContentMacroById = newDynamicContentMacroModuleBean()
+                .withUrl("/render-dynamic-by-id?pageId={page.id}&pageVersion={page.version}&macroId={macro.id}")
+                .withDescription(new I18nProperty("Dynamic Content Macro By Id", ""))
+                .withKey("dynamic-macro-by-id")
+                .withName(new I18nProperty("Dynamic Macro By Id", ""))
+                .withOutputType(MacroOutputType.BLOCK)
+                .withBodyType(MacroBodyType.RICH_TEXT)
+                .build();
+
+        StaticContentMacroModuleBean staticContentMacroById = newStaticContentMacroModuleBean()
+                .withUrl("/render-static-by-id?pageId={page.id}&pageVersion={page.version}&macroId={macro.id}")
+                .withDescription(new I18nProperty("Static Content Macro By Id", ""))
+                .withKey("static-macro-by-id")
+                .withName(new I18nProperty("Static Macro By Id", ""))
+                .withOutputType(MacroOutputType.BLOCK)
+                .withBodyType(MacroBodyType.RICH_TEXT)
+                .build();
+
+        DynamicContentMacroModuleBean dynamicContentMacroByHash = newDynamicContentMacroModuleBean()
+                .withUrl("/render-dynamic-by-hash?pageId={page.id}&pageVersion={page.version}&macroHash={macro.hash}")
+                .withDescription(new I18nProperty("Dynamic Content Macro By Hash", ""))
+                .withKey("dynamic-macro-by-hash")
+                .withName(new I18nProperty("Dynamic Macro By Hash", ""))
+                .withOutputType(MacroOutputType.BLOCK)
+                .withBodyType(MacroBodyType.RICH_TEXT)
+                .build();
+
+        StaticContentMacroModuleBean staticContentMacroByHash = newStaticContentMacroModuleBean()
+                .withUrl("/render-static-by-hash?pageId={page.id}&pageVersion={page.version}&macroHash={macro.hash}")
+                .withDescription(new I18nProperty("Static Content Macro By Hash", ""))
+                .withKey("static-macro-by-hash")
+                .withName(new I18nProperty("Static Macro By Hash", ""))
+                .withOutputType(MacroOutputType.BLOCK)
+                .withBodyType(MacroBodyType.RICH_TEXT)
+                .build();
+
+        final InstallHandlerServlet installHandlerServlet = new InstallHandlerServlet();
+        String addonKey = AddonTestUtils.randomAddOnKey();
+        String baseUrl = product.getProductInstance().getBaseUrl();
+        BodyHandler dynamicMacroBodyHandler = new BodyHandler()
+        {
+            public void processBody(HttpServletRequest req, HttpServletResponse resp, Map<String, Object> context, String body) throws IOException
+            {
+                Map<String, Object> data = Maps.newHashMap(context);
+                data.put("body", body);
+                HttpUtils.renderHtml(resp, "confluence/macro/dynamic-macro-body.mu", data);
+            }
+        };
+        BodyHandler staticMacroBodyHandler = new BodyHandler()
+        {
+            public void processBody(HttpServletRequest req, HttpServletResponse resp, Map<String, Object> context, String body) throws IOException
+            {
+                resp.setContentType("text/html");
+                byte[] bytes = body.getBytes();
+                resp.setContentLength(bytes.length);
+                resp.getOutputStream().write(bytes);
+                resp.getOutputStream().close();
+            }
+        };
+        remotePlugin = new ConnectRunner(product.getProductInstance().getBaseUrl(), addonKey)
+                .addJWT(installHandlerServlet)
+                .addScope(ScopeName.ADMIN) // for using ap.request
+                .addModules("dynamicContentMacros", dynamicContentMacroById, dynamicContentMacroByHash)
+                .addModules("staticContentMacros", staticContentMacroById, staticContentMacroByHash)
+                .addRoute("/installed", installHandlerServlet)
+                .addRoute("/render-dynamic-by-id", new HttpContextServlet(new MacroBodyServlet(MacroBodyServlet.CollectionType.BY_ID, baseUrl, addonKey, installHandlerServlet, dynamicMacroBodyHandler)))
+                .addRoute("/render-static-by-id", new HttpContextServlet(new MacroBodyServlet(MacroBodyServlet.CollectionType.BY_ID, baseUrl, addonKey, installHandlerServlet, staticMacroBodyHandler)))
+                .addRoute("/render-dynamic-by-hash", new HttpContextServlet(new MacroBodyServlet(MacroBodyServlet.CollectionType.BY_HASH, baseUrl, addonKey, installHandlerServlet, dynamicMacroBodyHandler)))
+                .addRoute("/render-static-by-hash", new HttpContextServlet(new MacroBodyServlet(MacroBodyServlet.CollectionType.BY_HASH, baseUrl, addonKey, installHandlerServlet, staticMacroBodyHandler)))
+                .start();
+    }
+
+    @AfterClass
+    public static void stopConnectAddOn() throws Exception
+    {
+        if (remotePlugin != null)
+        {
+            remotePlugin.stopAndUninstall();
+        }
+    }
+
+    @Test
+    public void testDynamicMacroById() throws Exception
+    {
+        testDynamicMacro("dynamic-macro-by-id");
+    }
+
+    @Test
+    public void testDynamicMacroByHash() throws Exception
+    {
+        testDynamicMacro("dynamic-macro-by-hash");
+    }
+
+    private void testDynamicMacro(String macroKey)
+    {
+        String body = "<h1>Test Dynamic Macro: " + macroKey + "</h1>";
+        Page page = createPage(macroKey, body);
+        String pageId = rpc.content.createPage(page, ContentRepresentation.STORAGE).getIdAsString();
+        getProduct().login(TestUser.ADMIN.confUser(), ViewPage.class, pageId);
+        RenderedMacro renderedMacro = connectPageOperations.findMacroWithIdPrefix(macroKey, 0);
+        String bodyFromPage = renderedMacro.getIFrameElement("body");
+        assertEquals(body, bodyFromPage);
+    }
+
+    @Test
+    public void testStaticMacroById() throws Exception
+    {
+        testStaticMacro("static-macro-by-id");
+    }
+
+    @Test
+    public void testStaticMacroByHash() throws Exception
+    {
+        testStaticMacro("static-macro-by-hash");
+    }
+
+    private void testStaticMacro(String macroKey)
+    {
+        String headingText = "Test Static Macro " + macroKey + "";
+        Page page = createPage(macroKey, "<h1>" + headingText + "</h1>");
+        String pageId = rpc.content.createPage(page, ContentRepresentation.STORAGE).getIdAsString();
+        ViewPage viewPage = getProduct().login(TestUser.ADMIN.confUser(), ViewPage.class, pageId);
+
+        // we can't assert on the html here because confluence will have transformed it
+        // so we assert on the contents of the h1 and look for the string we put in to the body
+        String headingTextFromPage = viewPage.getMainContent().find(By.tagName("h1")).getText();
+        assertEquals(headingText, headingTextFromPage);
+    }
+
+    private Page createPage(String macroKey, String body)
+    {
+        long tag = System.currentTimeMillis();
+        String bodyWrapper = "<ac:rich-text-body>" + body + "</ac:rich-text-body>";
+        String storageFormat = "<ac:structured-macro ac:name=\"" + macroKey + "\">" + bodyWrapper + "</ac:structured-macro>";
+        return new Page(TestSpace.DEMO, "test page - " + tag, storageFormat);
+    }
+}
---
+++ integration-tests/src/test/java/it/servlet/macro/MacroBodyServlet.java
@@ -0,0 +1,102 @@
+package it.servlet.macro;
+
+import com.atlassian.plugin.connect.spi.http.HttpMethod;
+import com.atlassian.plugin.connect.test.AddonTestUtils;
+import it.servlet.ContextServlet;
+import it.servlet.InstallHandlerServlet;
+import org.apache.commons.io.IOUtils;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.util.Map;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * A Test servlet that can be used to test logic that requires the collection of the macro
+ * body.  This servlet can collect the macro body from confluence using the macro id or the
+ * macro hash.
+ *
+ * You need to define your macro url to include the following parameters:
+ * pageId - {page.id}
+ * pageVersion - {page.version}
+ * macroId - {macro.id} (for collection by id)
+ * macroHash - {macro.hash} (for collection by hash)
+ *
+ * The body handler will be called with the collected body.
+ */
+public class MacroBodyServlet extends ContextServlet
+{
+    public static enum CollectionType {
+        BY_HASH,
+        BY_ID
+    }
+
+    private final CollectionType collectionType;
+    private final String baseUrl;
+    private final InstallHandlerServlet installHandlerServlet;
+    private final BodyHandler bodyHandler;
+    private final String addonKey;
+
+    public MacroBodyServlet(CollectionType collectionType, String baseUrl, String addonKey,
+                            InstallHandlerServlet installHandlerServlet, BodyHandler bodyHandler)
+    {
+        this.collectionType = collectionType;
+        this.baseUrl = baseUrl;
+        this.addonKey = addonKey;
+        this.installHandlerServlet = installHandlerServlet;
+        this.bodyHandler = bodyHandler;
+    }
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp, Map<String, Object> context)
+    {
+        try
+        {
+            String pageId = req.getParameter("pageId");
+            String pageVersion = req.getParameter("pageVersion");
+
+            URL url = null;
+            switch (collectionType) {
+                case BY_ID:
+                    String macroId = req.getParameter("macroId");
+                    url = URI.create(baseUrl + "/rest/api/content/" + pageId + "/history/" + pageVersion + "/macro/id/" + macroId).toURL();
+                    break;
+                case BY_HASH:
+                    String macroHash = req.getParameter("macroHash");
+                    url = URI.create(baseUrl + "/rest/api/content/" + pageId + "/history/" + pageVersion + "/macro/hash/" + macroHash).toURL();
+                    break;
+            }
+
+            String sharedSecret = checkNotNull(installHandlerServlet.getInstallPayload().getSharedSecret());
+            String jwt = AddonTestUtils.generateJwtSignature(HttpMethod.GET,
+                    url.toURI(),
+                    addonKey,
+                    sharedSecret,
+                    baseUrl,
+                    null);
+
+            URL authenticatedUrl = new URL(url + "?jwt=" + jwt);
+            HttpURLConnection connection = (HttpURLConnection) authenticatedUrl.openConnection();
+            connection.setRequestMethod("GET");
+            String json = IOUtils.toString(connection.getInputStream());
+
+            JSONObject o = (JSONObject) JSONValue.parse(json);
+            String body = o.get("body").toString();
+
+            bodyHandler.processBody(req, resp, context, body);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace(System.err);
+            // this should cause the test to fail
+            throw new RuntimeException(e);
+        }
+    }
+
+}
