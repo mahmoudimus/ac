77e55b982cf0f9dd4380685399324ef30b4e6283
Matthew Jensen <mjensen@atlassian.com>
2015-03-24 11:31:25 +1100

    CE-33 * Refactor autoconvert to not conflict with keyboard shortcuts

--- plugin/src/main/resources/js/confluence/macro/autoconvert.js
+++ plugin/src/main/resources/js/confluence/macro/autoconvert.js
@@ -1,54 +1,54 @@
 (function () {
+
+    function escapePattern(str) { return str.replace(/[\-\[\]\/\(\)\*\+\?\.\\\^\$\|]/g, "\\$&"); };
+    function replaceAll(find, replace, str) { return str.replace(new RegExp(find, 'g'), replace); };
+
+    var factory = function (autoconvertDef) {
+        var macroName = autoconvertDef.macroName;
+        var urlParameter = autoconvertDef.autoconvert.urlParameter;
+        var pattern = autoconvertDef.matcherBean.pattern;
+
+        // build a regex from the defined autoconvert pattern
+        pattern = escapePattern(pattern);
+        pattern = replaceAll('{}', '.*?', pattern);
+        pattern = "^"+pattern+"$";
+
+        console.log("registering autoconvert handler for [ " + macroName + " ] and pattern [ " + pattern + " ]");
+
+        return function (uri, node, done) {
+            var matches = uri.source.match(pattern);
+
+            if (matches) {
+                console.log("matched autoconvert pattern [ " + pattern + " ] with uri [ " + uri + " ]");
+                var params = {};
+
+                if (urlParameter != null) {
+                    params[urlParameter] = uri.source;
+                }
+
+                var macro = {
+                    name: macroName,
+                    params: params
+                };
+                tinymce.plugins.Autoconvert.convertMacroToDom(macro, done, done);
+            } else {
+                console.log("did not match autoconvert pattern [ " + pattern + " ] with uri [ " + uri + " ]");
+                done();
+            }
+        }
+    };
+
     AJS.bind("init.rte", function () {
         var autoconvertDefs = WRM.data.claim("com.atlassian.plugins.atlassian-connect-plugin:confluence-atlassian-connect-autoconvert-resources.connect-autoconvert-data");
-        var numAutoconvertDefs = autoconvertDefs.length;
-        console.log("found [ " + numAutoconvertDefs + " ] autoconvert definitions");
-
-        if (numAutoconvertDefs > 0) {
-            for (var i = 0; i < numAutoconvertDefs; i++) {
-                var handler = function (i) {
-                    var macroName = autoconvertDefs[i].macroName;
-                    var urlParameter = autoconvertDefs[i].autoconvert.urlParameter;
-                    var pattern = autoconvertDefs[i].matcherBean.pattern;
-
-                    // build a regex from the defined autoconvert pattern
-                    pattern = escapePattern(pattern);
-                    pattern = replaceAll('{}', '.*?', pattern);
-                    pattern = "^"+pattern+"$";
-
-                    console.log("registering autoconvert handler for [ " + macroName + " ] and pattern [ " + pattern + " ]");
-
-                    return function (uri, node, done) {
-                        var matches = uri.source.match(pattern);
-
-                        if (matches) {
-                            console.log("matched autoconvert pattern [ " + pattern + " ] with uri [ " + uri + " ]");
-                            var params = {};
-
-                            if (urlParameter != null) {
-                                params[urlParameter] = uri.source;
-                            }
-
-                            var macro = {
-                                name: macroName,
-                                params: params
-                            };
-                            tinymce.plugins.Autoconvert.convertMacroToDom(macro, done, done);
-                        } else {
-                            console.log("did not match autoconvert pattern [ " + pattern + " ] with uri [ " + uri + " ]");
-                            done();
-                        }
-
-                    }
-                }(i);
-                tinymce.plugins.Autoconvert.autoConvert.addHandler(handler);
+        if(autoconvertDefs) {
+            var numAutoconvertDefs = autoconvertDefs.length;
+            console.log("found [ " + numAutoconvertDefs + " ] autoconvert definitions");
+            if (numAutoconvertDefs > 0) {
+                for (var i = 0; i < numAutoconvertDefs; i++) {
+                    tinymce.plugins.Autoconvert.autoConvert.addHandler(factory(autoconvertDefs[i]));
+                }
             }
         }
     });
-    // escape everything that could interfere with the regex, except curly braces
-    function escapePattern(str) { return str.replace(/[\-\[\]\/\(\)\*\+\?\.\\\^\$\|]/g, "\\$&"); }
 
-    function replaceAll(find, replace, str) {
-        return str.replace(new RegExp(find, 'g'), replace);
-    }
 })();
