3e97ca2bd33b8b3c9ecf26e07ad3827e1c6d05bc
Kate West-Walker <kwestwalker@atlassian.com>
2015-03-31 19:15:39 +1100

    CE-33 Adding autoconvert qunit tests (unstable progress commit)

--- plugin/src/test/resources/js/confluence/macro/autoconvert-test.js
+++ plugin/src/test/resources/js/confluence/macro/autoconvert-test.js
@@ -35,4 +35,64 @@ define(['ac/confluence/macro/autoconvert'], function(Autoconvert) {
         WRM.data.claim.restore();
         Autoconvert.factory.restore();
     });
+
+    test("Wildcard placement", function() {
+        WRM.data.claim = function() {
+            autoconvertdef = {
+                "macroName": "",
+                "autoconvert": {
+                    "urlParameter": "url"
+                },
+                "matcherBean": {
+                    "pattern": "http:/{}/example.com/",
+                    "pattern": "http://example.com/{}{}{}/",
+                    "pattern": "http://example.com/{}{}",
+                    "pattern": "http://example.com/{}/{}/{}/{}/{}/{}"
+                }
+            }
+            return autoconvertdef;
+        };
+
+        sinon.stub(WRM.data, "claim").returns([{}, {}]);
+        sinon.stub(Autoconvert, "factory").returns(function() {});
+        var spy = sinon.spy(tinymce.plugins.Autoconvert.autoConvert, "addHandler");
+        Autoconvert.registerAutoconvertHandlers();
+        ok(spy.calledTwice);
+        WRM.data.claim.restore();
+        Autoconvert.factory.restore();
+    })
+
+
 });
+
+// Super greedy ones
+WRM.data.claim = function() {
+    autoconvertdef = {
+        "macroName": "",
+        "autoconvert": {
+            "urlParameter": "url"
+        },
+        "matcherBean": {
+            "pattern": "http://{}",
+            "pattern": "{}"
+        }
+    }
+    return autoconvertdef;
+};
+
+// Dangerous security ones
+WRM.data.claim = function() {
+    autoconvertdef = {
+        "macroName": "",
+        "autoconvert": {
+            "urlParameter": "url"
+        },
+        "matcherBean": {
+            "pattern": "alert('hi there')",
+            "pattern": "'",
+            "pattern": "(.*)",
+            "pattern": "^(\w+\d+)+a$"
+        }
+    }
+    return autoconvertdef;
+};
