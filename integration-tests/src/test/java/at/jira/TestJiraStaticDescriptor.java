e18c5bbb9897f2f6c94837bbef63192edfb8216a
James Hazelwood <jhazelwood@atlassian.com>
2015-03-31 15:42:49 +1100

    ACDEV-1843 No need to create a project in the JIRA static descriptor test - we get one for free from our parent class

--- integration-tests/src/test/java/at/jira/TestJiraStaticDescriptor.java
+++ integration-tests/src/test/java/at/jira/TestJiraStaticDescriptor.java
@@ -1,13 +1,9 @@
 package at.jira;
 
-import java.util.List;
-
 import com.atlassian.jira.rest.api.issue.IssueCreateResponse;
-import com.atlassian.jira.testkit.client.restclient.Project;
 import com.atlassian.test.categories.OnDemandAcceptanceTest;
 
 import com.google.common.base.Optional;
-import com.google.common.base.Predicate;
 
 import org.junit.After;
 import org.junit.Before;
@@ -20,14 +16,11 @@ import at.util.ExternalAddonInstaller;
 import it.jira.JiraWebDriverTestBase;
 import it.util.TestUser;
 
-import static com.google.common.collect.Iterables.any;
 import static it.util.TestUser.ADMIN;
 
 @Category (OnDemandAcceptanceTest.class)
 public class TestJiraStaticDescriptor extends JiraWebDriverTestBase
 {
-    private static final String PROJECT_KEY = "ACTEST";
-    private static final String PROJECT_NAME = "Atlassian Connect Test";
     private static final String WEB_ITEM_ID = "com.atlassian.connect.acceptance.test__opsbar-test-web-item";
 
     protected static final ExternalAddonInstaller externalAddonInstaller = new ExternalAddonInstaller(
@@ -45,9 +38,8 @@ public class TestJiraStaticDescriptor extends JiraWebDriverTestBase
     @Test
     public void testAcActionWebItemIsPresent()
     {
-        createConnectProject();
         IssueCreateResponse response = product.backdoor().issues().loginAs("admin")
-                .createIssue(PROJECT_KEY, "Atlassian Connect Web Panel Test Issue");
+                .createIssue(project.getKey(), "Atlassian Connect Web Panel Test Issue");
 
         product.quickLogin(ADMIN.getUsername(), ADMIN.getPassword());
         product.goToViewIssue(response.key());
@@ -60,23 +52,5 @@ public class TestJiraStaticDescriptor extends JiraWebDriverTestBase
     {
         log.info("Cleaning up after " + getClass().getName());
         externalAddonInstaller.uninstall();
-    }
-
-    private void createConnectProject()
-    {
-        List<Project> projects = product.backdoor().project().getProjects();
-        boolean projectExists = any(projects, new Predicate<Project>()
-        {
-            @Override
-            public boolean apply(Project project)
-            {
-                return PROJECT_KEY.equals(project.key);
-            }
-        });
-
-        if (!projectExists)
-        {
-            product.backdoor().project().addProject(PROJECT_NAME, PROJECT_KEY, ADMIN.getUsername());
-        }
     }
 }
