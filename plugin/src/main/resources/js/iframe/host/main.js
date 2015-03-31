54e6ba619c3ae213d47ae7fab597ee6da404f2ee
Chris Whittington <cwhittington@atlassian.com>
2015-03-31 18:19:35 +0100

    HC-9903 fix requires

--- plugin/src/main/resources/js/iframe/host/main.js
+++ plugin/src/main/resources/js/iframe/host/main.js
@@ -5,3 +5,11 @@
 define('connect-host', function(){
     return _AP;
 });
+
+require('ac/history');
+require('ac/cookie');
+require('ac/env');
+require('ac/inline-dialog');
+require('ac/dialog');
+require('ac/messages');
+require('ac/request');
