12103f3f2a2495798a87ac81a532d3d0f211bd69
Matthew Jensen <mjensen@atlassian.com>
2015-04-02 11:31:13 +1100

    CE-33 predelcare function variables to they are in scope

--- plugin/src/main/resources/js/confluence/macro/autoconvert-init.js
+++ plugin/src/main/resources/js/confluence/macro/autoconvert-init.js
@@ -1,6 +1,8 @@
 (function($, require){
-
     require(["ac/confluence/macro/autoconvert"], function(Autoconvert) {
-        AJS.bind("init.rte", Autoconvert.registerAutoconvertHandlers);
+        AJS.bind("init.rte", function() {
+            var autoconvertDefs = WRM.data.claim("com.atlassian.plugins.atlassian-connect-plugin:confluence-atlassian-connect-autoconvert-resources.connect-autoconvert-data");
+            Autoconvert.registerAutoconvertHandlers(autoconvertDefs,tinymce)
+        });
     });
 })(AJS.$, require);
--- plugin/src/main/resources/js/confluence/macro/autoconvert.js
+++ plugin/src/main/resources/js/confluence/macro/autoconvert.js
@@ -14,30 +14,28 @@
          the macro is inserted into the editor.
          */
 
-        return {
-            escapePattern: function(str) {
-                return str.replace(/[\-\[\]\/\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
-            },
-            replaceAll: function(find, replace, str) {
-                return str.replace(new RegExp(find, 'g'), replace);
-            },
-            factory: function (autoconvertDef) {
+        var escapePattern = function(str) {
+            return str.replace(/[\-\[\]\/\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+        };
+
+        var replaceAll = function(find, replace, str) {
+            return str.replace(new RegExp(find, 'g'), replace);
+        };
+
+        var factory = function (autoconvertDef) {
                 var macroName = autoconvertDef.macroName;
                 var urlParameter = autoconvertDef.autoconvert.urlParameter;
                 var pattern = autoconvertDef.matcherBean.pattern;
 
                 // build a regex from the defined autoconvert pattern
-                pattern = this.escapePattern(pattern);
-                pattern = this.replaceAll('{}', '.*?', pattern);
+                pattern = escapePattern(pattern);
+                pattern = replaceAll('{}', '.*?', pattern);
                 pattern = "^" + pattern + "$";
-
-                console.log("registering autoconvert handler for [ " + macroName + " ] and pattern [ " + pattern + " ]");
 
                 return function (uri, node, done) {
                     var matches = uri.source.match(pattern);
 
                     if (matches) {
-                        console.log("matched autoconvert pattern [ " + pattern + " ] with uri [ " + uri + " ]");
                         var params = {};
 
                         if (urlParameter != null) {
@@ -50,19 +48,22 @@
                         };
                         tinymce.plugins.Autoconvert.convertMacroToDom(macro, done, done);
                     } else {
-                        console.log("did not match autoconvert pattern [ " + pattern + " ] with uri [ " + uri + " ]");
                         done();
                     }
                 }
-            },
-            registerAutoconvertHandlers: function () {
-                var autoconvertDefs = WRM.data.claim("com.atlassian.plugins.atlassian-connect-plugin:confluence-atlassian-connect-autoconvert-resources.connect-autoconvert-data");
+            };
+
+        return {
+            escapePattern: escapePattern,
+            replaceAll: replaceAll,
+            factory: factory,
+            registerAutoconvertHandlers: function (autoconvertDefs,tinymce) {
                 if (autoconvertDefs) {
                     var numAutoconvertDefs = autoconvertDefs.length;
                     console.log("found [ " + numAutoconvertDefs + " ] autoconvert definitions");
                     if (numAutoconvertDefs > 0) {
                         for (var i = 0; i < numAutoconvertDefs; i++) {
-                            tinymce.plugins.Autoconvert.autoConvert.addHandler(this.factory(autoconvertDefs[i]));
+                            tinymce.plugins.Autoconvert.autoConvert.addHandler(factory(autoconvertDefs[i]));
                         }
                     }
                 }
