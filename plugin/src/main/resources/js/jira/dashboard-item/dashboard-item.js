801887bcd59f8816904b9ec1d3b290db4bc091f3
Chris Whittington <cwhittington@atlassian.com>
2015-04-13 18:51:16 +0100

    ACJIRA-248 host side spike

--- plugin/src/main/resources/js/jira/dashboard-item/dashboard-item.js
+++ plugin/src/main/resources/js/jira/dashboard-item/dashboard-item.js
@@ -1,16 +1,36 @@
 (function($, define){
     "use strict";
 
-    define("atlassian-connect/connect-dashboard-item", [], function() {
+    require(['connect-host'], function(_AP) {
+
+        _AP.extend(function () {
+            return {
+                init: function (state, xdm) {
+                    // register handle for the edit button in jira (if needed)
+                    $("my-button").click(function(){
+                        xdm.setGadgetEdit();
+                    });
+
+                },
+                stubs: ["setGadgetEdit"]
+            };
+        });
+    });
+
+
+
+
+//    define("ac/jira/dashboard-item", [], function() {
         var Gadget = function(API) {
             this.API = API;
         };
 
         Gadget.prototype.renderEdit = function(element, preferences) {
             // send the event to the dashboard-item
+
         };
         return Gadget;
-    });
+//    });
 
     $(document).ready(function() {
 
