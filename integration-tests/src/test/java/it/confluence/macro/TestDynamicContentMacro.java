b91d2f60a7e68ff346d8ce11ccc64e48b5997570
Matthew Jensen <mjensen@atlassian.com>
2015-04-28 14:49:29 +1000

    CE-79 remove redundant tests

--- integration-tests/src/test/java/it/confluence/macro/TestDynamicContentMacro.java
+++ integration-tests/src/test/java/it/confluence/macro/TestDynamicContentMacro.java
@@ -1,14 +1,11 @@
 package it.confluence.macro;
 
 import com.atlassian.confluence.api.model.content.Content;
-import com.atlassian.confluence.api.model.content.ContentRepresentation;
-import com.atlassian.confluence.api.model.content.ContentType;
 import com.atlassian.confluence.it.Page;
 import com.atlassian.confluence.pageobjects.page.content.CreatePage;
 import com.atlassian.confluence.pageobjects.page.content.EditContentPage;
 import com.atlassian.confluence.pageobjects.page.content.ViewPage;
 import com.atlassian.plugin.connect.modules.beans.DynamicContentMacroModuleBean;
-import com.atlassian.plugin.connect.modules.beans.StaticContentMacroModuleBean;
 import com.atlassian.plugin.connect.modules.beans.nested.EmbeddedStaticContentMacroBean;
 import com.atlassian.plugin.connect.modules.beans.nested.I18nProperty;
 import com.atlassian.plugin.connect.modules.beans.nested.MacroBodyType;
@@ -17,23 +14,17 @@ import com.atlassian.plugin.connect.modules.beans.nested.MacroOutputType;
 import com.atlassian.plugin.connect.modules.beans.nested.MacroRenderModesBean;
 import com.atlassian.plugin.connect.modules.beans.nested.ScopeName;
 import com.atlassian.plugin.connect.test.AddonTestUtils;
-import com.atlassian.plugin.connect.test.HttpUtils;
 import com.atlassian.plugin.connect.test.pageobjects.RemotePluginDialog;
 import com.atlassian.plugin.connect.test.pageobjects.confluence.ConfluencePageWithRemoteMacro;
 import com.atlassian.plugin.connect.test.pageobjects.confluence.RenderedMacro;
 import com.atlassian.plugin.connect.test.server.ConnectRunner;
-import com.google.common.collect.Maps;
 import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.parser.PdfReaderContentParser;
 import com.itextpdf.text.pdf.parser.SimpleTextExtractionStrategy;
 import com.itextpdf.text.pdf.parser.TextExtractionStrategy;
 import it.confluence.MacroStorageFormatBuilder;
 import it.servlet.ConnectAppServlets;
-import it.servlet.HttpContextServlet;
 import it.servlet.InstallHandlerServlet;
-import it.servlet.macro.BodyHandler;
-import it.servlet.macro.MacroBodyServlet;
-import it.util.TestUser;
 import org.apache.commons.io.IOUtils;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -47,15 +38,12 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Map;
 import java.util.concurrent.Callable;
 
 import static com.atlassian.plugin.connect.modules.beans.DynamicContentMacroModuleBean.newDynamicContentMacroModuleBean;
-import static com.atlassian.plugin.connect.modules.beans.StaticContentMacroModuleBean.newStaticContentMacroModuleBean;
 import static com.atlassian.plugin.connect.modules.util.ModuleKeyUtils.randomName;
 import static com.atlassian.plugin.connect.test.Utils.loadResourceAsString;
 import static it.matcher.ParamMatchers.isVersionNumber;
-import static junit.framework.TestCase.assertEquals;
 import static org.hamcrest.CoreMatchers.both;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
@@ -92,68 +80,8 @@ public class TestDynamicContentMacro extends AbstractContentMacroTest
     @BeforeClass
     public static void startConnectAddOn() throws Exception
     {
-
-        DynamicContentMacroModuleBean dynamicContentMacroById = newDynamicContentMacroModuleBean()
-                .withUrl("/render-dynamic-by-id?pageId={page.id}&pageVersion={page.version}&macroId={macro.id}")
-                .withDescription(new I18nProperty("Dynamic Content Macro By Id", ""))
-                .withKey("dynamic-macro-by-id")
-                .withName(new I18nProperty("Dynamic Macro By Id", ""))
-                .withOutputType(MacroOutputType.BLOCK)
-                .withBodyType(MacroBodyType.RICH_TEXT)
-                .build();
-
-        StaticContentMacroModuleBean staticContentMacroById = newStaticContentMacroModuleBean()
-                .withUrl("/render-static-by-id?pageId={page.id}&pageVersion={page.version}&macroId={macro.id}")
-                .withDescription(new I18nProperty("Static Content Macro By Id", ""))
-                .withKey("static-macro-by-id")
-                .withName(new I18nProperty("Static Macro By Id", ""))
-                .withOutputType(MacroOutputType.BLOCK)
-                .withBodyType(MacroBodyType.RICH_TEXT)
-                .build();
-
-        DynamicContentMacroModuleBean dynamicContentMacroByHash = newDynamicContentMacroModuleBean()
-                .withUrl("/render-dynamic-by-hash?pageId={page.id}&pageVersion={page.version}&macroHash={macro.hash}")
-                .withDescription(new I18nProperty("Dynamic Content Macro By Hash", ""))
-                .withKey("dynamic-macro-by-hash")
-                .withName(new I18nProperty("Dynamic Macro By Hash", ""))
-                .withOutputType(MacroOutputType.BLOCK)
-                .withBodyType(MacroBodyType.RICH_TEXT)
-                .build();
-
-        StaticContentMacroModuleBean staticContentMacroByHash = newStaticContentMacroModuleBean()
-                .withUrl("/render-static-by-hash?pageId={page.id}&pageVersion={page.version}&macroHash={macro.hash}")
-                .withDescription(new I18nProperty("Static Content Macro By Hash", ""))
-                .withKey("static-macro-by-hash")
-                .withName(new I18nProperty("Static Macro By Hash", ""))
-                .withOutputType(MacroOutputType.BLOCK)
-                .withBodyType(MacroBodyType.RICH_TEXT)
-                .build();
-
         final InstallHandlerServlet installHandlerServlet = new InstallHandlerServlet();
         String addonKey = AddonTestUtils.randomAddOnKey();
-        String baseUrl = product.getProductInstance().getBaseUrl();
-        BodyHandler dynamicMacroBodyHandler = new BodyHandler()
-        {
-            public void processBody(HttpServletRequest req, HttpServletResponse resp, Map<String, Object> context, String body) throws IOException
-            {
-                Map<String, Object> data = Maps.newHashMap(context);
-                data.put("body", body);
-                HttpUtils.renderHtml(resp, "confluence/macro/dynamic-macro-body.mu", data);
-            }
-        };
-        BodyHandler staticMacroBodyHandler = new BodyHandler()
-        {
-            public void processBody(HttpServletRequest req, HttpServletResponse resp, Map<String, Object> context, String body) throws IOException
-            {
-                resp.setContentType("text/html");
-                byte[] bytes = body.getBytes();
-                resp.setContentLength(bytes.length);
-                resp.getOutputStream().write(bytes);
-                resp.getOutputStream().close();
-            }
-        };
-
-
 
         DynamicContentMacroModuleBean simpleMacro = createSimpleMacro(newDynamicContentMacroModuleBean());
         DynamicContentMacroModuleBean allParameterTypesMacro = createAllParametersMacro(newDynamicContentMacroModuleBean());
@@ -230,21 +158,9 @@ public class TestDynamicContentMacro extends AbstractContentMacroTest
                 )
                 .build();
 
-//        remotePlugin = new ConnectRunner(product.getProductInstance().getBaseUrl(), addonKey)
-//                .addJWT(installHandlerServlet)
-//                .addScope(ScopeName.ADMIN) // for using ap.request
-//                .addModules("dynamicContentMacros", dynamicContentMacroById, dynamicContentMacroByHash)
-//                .addModules("staticContentMacros", staticContentMacroById, staticContentMacroByHash)
-//                .addRoute("/installed", installHandlerServlet)
-//                .addRoute("/render-dynamic-by-id", new HttpContextServlet(new MacroBodyServlet(MacroBodyServlet.CollectionType.BY_ID, baseUrl, addonKey, installHandlerServlet, dynamicMacroBodyHandler)))
-//                .addRoute("/render-static-by-id", new HttpContextServlet(new MacroBodyServlet(MacroBodyServlet.CollectionType.BY_ID, baseUrl, addonKey, installHandlerServlet, staticMacroBodyHandler)))
-//                .addRoute("/render-dynamic-by-hash", new HttpContextServlet(new MacroBodyServlet(MacroBodyServlet.CollectionType.BY_HASH, baseUrl, addonKey, installHandlerServlet, dynamicMacroBodyHandler)))
-//                .addRoute("/render-static-by-hash", new HttpContextServlet(new MacroBodyServlet(MacroBodyServlet.CollectionType.BY_HASH, baseUrl, addonKey, installHandlerServlet, staticMacroBodyHandler)))
-//                .start();
-
         remotePlugin = new ConnectRunner(product.getProductInstance().getBaseUrl(), addonKey)
-
                 .addJWT(installHandlerServlet)
+                .setAuthenticationToNone()
                 .addScope(ScopeName.ADMIN) // for using ap.request
                 .addModules("dynamicContentMacros",
                         simpleMacro,
@@ -262,10 +178,8 @@ public class TestDynamicContentMacro extends AbstractContentMacroTest
                         clientSideBodyEditingMacroScriptInjection,
                         macroInTableMacro,
                         slowMacro,
-                        dynamicMacroWithFallback,
-                        dynamicContentMacroById, dynamicContentMacroByHash
+                        dynamicMacroWithFallback
                 )
-                .addModules("staticContentMacros", staticContentMacroById, staticContentMacroByHash)
                 .addRoute(DEFAULT_MACRO_URL, ConnectAppServlets.helloWorldServlet())
                 .addRoute("/render-editor", ConnectAppServlets.macroEditor())
                 .addRoute("/macro-body-editor", ConnectAppServlets.macroBodyEditor(EDITED_MACRO_BODY))
@@ -278,12 +192,6 @@ public class TestDynamicContentMacro extends AbstractContentMacroTest
                 .addRoute("/slow-macro", new SlowMacroServlet(22))
                 .addRoute("/dynamic-macro", ConnectAppServlets.helloWorldServlet())
                 .addRoute("/dynamic-macro-static", ConnectAppServlets.dynamicMacroStaticServlet())
-
-                .addRoute("/installed", installHandlerServlet)
-                .addRoute("/render-dynamic-by-id", new HttpContextServlet(new MacroBodyServlet(MacroBodyServlet.CollectionType.BY_ID, baseUrl, addonKey, installHandlerServlet, dynamicMacroBodyHandler)))
-                .addRoute("/render-static-by-id", new HttpContextServlet(new MacroBodyServlet(MacroBodyServlet.CollectionType.BY_ID, baseUrl, addonKey, installHandlerServlet, staticMacroBodyHandler)))
-                .addRoute("/render-dynamic-by-hash", new HttpContextServlet(new MacroBodyServlet(MacroBodyServlet.CollectionType.BY_HASH, baseUrl, addonKey, installHandlerServlet, dynamicMacroBodyHandler)))
-                .addRoute("/render-static-by-hash", new HttpContextServlet(new MacroBodyServlet(MacroBodyServlet.CollectionType.BY_HASH, baseUrl, addonKey, installHandlerServlet, staticMacroBodyHandler)))
 
                 .start();
     }
@@ -663,173 +571,4 @@ public class TestDynamicContentMacro extends AbstractContentMacroTest
         }
     }
 
-
-
-
-
-
-
-
-
-//
-//
-//    protected ConfluenceRestClient restClient = new ConfluenceRestClient(getProduct());
-//    private static ConnectRunner remotePlugin;
-//
-//    @BeforeClass
-//    public static void startConnectAddOn() throws Exception
-//    {
-//        DynamicContentMacroModuleBean dynamicContentMacroById = newDynamicContentMacroModuleBean()
-//                .withUrl("/render-dynamic-by-id?pageId={page.id}&pageVersion={page.version}&macroId={macro.id}")
-//                .withDescription(new I18nProperty("Dynamic Content Macro By Id", ""))
-//                .withKey("dynamic-macro-by-id")
-//                .withName(new I18nProperty("Dynamic Macro By Id", ""))
-//                .withOutputType(MacroOutputType.BLOCK)
-//                .withBodyType(MacroBodyType.RICH_TEXT)
-//                .build();
-//
-//        StaticContentMacroModuleBean staticContentMacroById = newStaticContentMacroModuleBean()
-//                .withUrl("/render-static-by-id?pageId={page.id}&pageVersion={page.version}&macroId={macro.id}")
-//                .withDescription(new I18nProperty("Static Content Macro By Id", ""))
-//                .withKey("static-macro-by-id")
-//                .withName(new I18nProperty("Static Macro By Id", ""))
-//                .withOutputType(MacroOutputType.BLOCK)
-//                .withBodyType(MacroBodyType.RICH_TEXT)
-//                .build();
-//
-//        DynamicContentMacroModuleBean dynamicContentMacroByHash = newDynamicContentMacroModuleBean()
-//                .withUrl("/render-dynamic-by-hash?pageId={page.id}&pageVersion={page.version}&macroHash={macro.hash}")
-//                .withDescription(new I18nProperty("Dynamic Content Macro By Hash", ""))
-//                .withKey("dynamic-macro-by-hash")
-//                .withName(new I18nProperty("Dynamic Macro By Hash", ""))
-//                .withOutputType(MacroOutputType.BLOCK)
-//                .withBodyType(MacroBodyType.RICH_TEXT)
-//                .build();
-//
-//        StaticContentMacroModuleBean staticContentMacroByHash = newStaticContentMacroModuleBean()
-//                .withUrl("/render-static-by-hash?pageId={page.id}&pageVersion={page.version}&macroHash={macro.hash}")
-//                .withDescription(new I18nProperty("Static Content Macro By Hash", ""))
-//                .withKey("static-macro-by-hash")
-//                .withName(new I18nProperty("Static Macro By Hash", ""))
-//                .withOutputType(MacroOutputType.BLOCK)
-//                .withBodyType(MacroBodyType.RICH_TEXT)
-//                .build();
-//
-//        final InstallHandlerServlet installHandlerServlet = new InstallHandlerServlet();
-//        String addonKey = AddonTestUtils.randomAddOnKey();
-//        String baseUrl = product.getProductInstance().getBaseUrl();
-//        BodyHandler dynamicMacroBodyHandler = new BodyHandler()
-//        {
-//            public void processBody(HttpServletRequest req, HttpServletResponse resp, Map<String, Object> context, String body) throws IOException
-//            {
-//                Map<String, Object> data = Maps.newHashMap(context);
-//                data.put("body", body);
-//                HttpUtils.renderHtml(resp, "confluence/macro/dynamic-macro-body.mu", data);
-//            }
-//        };
-//        BodyHandler staticMacroBodyHandler = new BodyHandler()
-//        {
-//            public void processBody(HttpServletRequest req, HttpServletResponse resp, Map<String, Object> context, String body) throws IOException
-//            {
-//                resp.setContentType("text/html");
-//                byte[] bytes = body.getBytes();
-//                resp.setContentLength(bytes.length);
-//                resp.getOutputStream().write(bytes);
-//                resp.getOutputStream().close();
-//            }
-//        };
-//        remotePlugin = new ConnectRunner(product.getProductInstance().getBaseUrl(), addonKey)
-//                .addJWT(installHandlerServlet)
-//                .addScope(ScopeName.ADMIN) // for using ap.request
-//                .addModules("dynamicContentMacros", dynamicContentMacroById, dynamicContentMacroByHash)
-//                .addModules("staticContentMacros", staticContentMacroById, staticContentMacroByHash)
-//                .addRoute("/installed", installHandlerServlet)
-//                .addRoute("/render-dynamic-by-id", new HttpContextServlet(new MacroBodyServlet(MacroBodyServlet.CollectionType.BY_ID, baseUrl, addonKey, installHandlerServlet, dynamicMacroBodyHandler)))
-//                .addRoute("/render-static-by-id", new HttpContextServlet(new MacroBodyServlet(MacroBodyServlet.CollectionType.BY_ID, baseUrl, addonKey, installHandlerServlet, staticMacroBodyHandler)))
-//                .addRoute("/render-dynamic-by-hash", new HttpContextServlet(new MacroBodyServlet(MacroBodyServlet.CollectionType.BY_HASH, baseUrl, addonKey, installHandlerServlet, dynamicMacroBodyHandler)))
-//                .addRoute("/render-static-by-hash", new HttpContextServlet(new MacroBodyServlet(MacroBodyServlet.CollectionType.BY_HASH, baseUrl, addonKey, installHandlerServlet, staticMacroBodyHandler)))
-//                .start();
-//    }
-
-//    @BeforeClass
-//    public static void logoutBeforeClass()
-//    {
-//        getProduct().logOutFast();
-//    }
-
-//    @After
-//    public void logoutAfter()
-//    {
-//        logoutBeforeClass();
-//    }
-//
-//    @AfterClass
-//    public static void stopConnectAddOn() throws Exception
-//    {
-//        if (remotePlugin != null)
-//        {
-//            remotePlugin.stopAndUninstall();
-//        }
-//    }
-
-    @Test
-    public void testDynamicMacroById() throws Exception
-    {
-        testDynamicMacro("dynamic-macro-by-id");
-    }
-
-    @Test
-    public void testDynamicMacroByHash() throws Exception
-    {
-        testDynamicMacro("dynamic-macro-by-hash");
-    }
-
-    private void testDynamicMacro(String macroKey)
-    {
-        Content page = createPage2(macroKey, "<h1>Hello world</h1>");
-        getProduct().viewPage(String.valueOf(page.getId().asLong()));
-        connectPageOperations.waitUntilNConnectIFramesPresent(1);
-
-        RenderedMacro renderedMacro1 = connectPageOperations.findMacroWithIdPrefix(macroKey, 0);
-        String content1 = renderedMacro1.getIFrameElement("body");
-        assertThat(content1, is("<h1>Hello world</h1>"));
-    }
-
-    @Test
-    public void testStaticMacroById() throws Exception
-    {
-        testStaticMacro("static-macro-by-id");
-    }
-
-    @Test
-    public void testStaticMacroByHash() throws Exception
-    {
-        testStaticMacro("static-macro-by-hash");
-    }
-
-    private void testStaticMacro(String macroKey)
-    {
-        String headingText = "Test Static Macro " + macroKey + "";
-        Content page = createPage2(macroKey, "<h1>" + headingText + "</h1>");
-        ViewPage viewPage = getProduct().viewPage(String.valueOf(page.getId().asLong()));
-        String headingTextFromPage = viewPage.getMainContent().find(By.tagName("h1")).getText();
-        assertEquals(headingText, headingTextFromPage);
-    }
-
-    protected Content createContent(String title, String body)
-    {
-        Content content = Content.builder(ContentType.PAGE)
-                .space(TestSpace.DEMO.getKey())
-                .title(title)
-                .body(body, ContentRepresentation.STORAGE)
-                .build();
-        return restClient.content().create(content).claim();
-    }
-
-    private Content createPage2(String macroKey, String macroBody)
-    {
-        long tag = System.currentTimeMillis();
-        String contentBody = new MacroStorageFormatBuilder(macroKey).richTextBody(macroBody).build();
-        return createContent("test page - " + tag, contentBody);
-    }
 }
