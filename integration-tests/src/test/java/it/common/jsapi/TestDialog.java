f2d2e54c78b6697a9b8e1189ccdd2738ce14b4e5
Einar Pehrson <epehrson@atlassian.com>
2015-05-22 17:36:22 +1000

    ACDEV-2038 Moved last web item tests out of jsapi group

--- integration-tests/src/test/java/it/common/jsapi/TestDialog.java
+++ integration-tests/src/test/java/it/common/jsapi/TestDialog.java
@@ -1,17 +1,12 @@
 package it.common.jsapi;
 
-import com.atlassian.pageobjects.page.HomePage;
 import com.atlassian.plugin.connect.modules.beans.AddOnUrlContext;
 import com.atlassian.plugin.connect.modules.beans.WebItemTargetType;
 import com.atlassian.plugin.connect.modules.beans.nested.I18nProperty;
 import com.atlassian.plugin.connect.modules.util.ModuleKeyUtils;
 import com.atlassian.plugin.connect.test.AddonTestUtils;
-import com.atlassian.plugin.connect.test.pageobjects.ConnectAddOnEmbeddedTestPage;
-import com.atlassian.plugin.connect.test.pageobjects.GeneralPage;
 import com.atlassian.plugin.connect.test.pageobjects.RemoteCloseDialogPage;
 import com.atlassian.plugin.connect.test.pageobjects.RemoteDialogOpeningPage;
-import com.atlassian.plugin.connect.test.pageobjects.RemotePluginAwarePage;
-import com.atlassian.plugin.connect.test.pageobjects.RemotePluginDialog;
 import com.atlassian.plugin.connect.test.server.ConnectRunner;
 import it.common.MultiProductWebDriverTestBase;
 import it.servlet.ConnectAppServlets;
@@ -25,19 +20,16 @@ import static com.atlassian.plugin.connect.modules.beans.WebItemTargetBean.newWe
 import static it.modules.ConnectAsserts.verifyIframeURLHasVersionNumber;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.endsWith;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class TestDialog extends MultiProductWebDriverTestBase
 {
+
     private static final String ADDON_GENERALPAGE = "ac-general-page";
     private static final String ADDON_DIALOG = "my-dialog";
     private static final String ADDON_GENERALPAGE_WEBITEM_DIALOG = "general-page-opening-webitem-dialog";
     private static final String ADDON_WEBITEM_DIALOG = "my-webitem-dialog";
-    private static final String REMOTE_PLUGIN_DIALOG_KEY = "remotePluginDialog";
-    private static final String SIZE_TO_PARENT_DIALOG_KEY = "sizeToParentDialog";
 
     private static ConnectRunner runner;
 
@@ -78,33 +70,12 @@ public class TestDialog extends MultiProductWebDriverTestBase
                                 .withTarget(newWebItemTargetBean()
                                         .withType(WebItemTargetType.dialog)
                                         .build())
-                                .build(),
-                        newWebItemBean()
-                                .withKey(REMOTE_PLUGIN_DIALOG_KEY)
-                                .withName(new I18nProperty("EE", null))
-                                .withUrl("/rpd")
-                                .withTarget(newWebItemTargetBean()
-                                        .withType(WebItemTargetType.dialog)
-                                        .build())
-                                .withLocation(getGloballyVisibleLocation())
-                                .build(),
-                        newWebItemBean()
-                                .withKey(SIZE_TO_PARENT_DIALOG_KEY)
-                                .withName(new I18nProperty("SzP", null))
-                                .withUrl("/fsd")
-                                .withTarget(newWebItemTargetBean()
-                                        .withType(WebItemTargetType.dialog)
-                                        .build())
-                                .withLocation(getGloballyVisibleLocation())
                                 .build()
                 )
-
                 .addRoute("/pg", ConnectAppServlets.openDialogServlet())
                 .addRoute("/my-dialog-url", ConnectAppServlets.closeDialogServlet())
                 .addRoute("/general-page", ConnectAppServlets.openDialogServlet(ADDON_WEBITEM_DIALOG))
                 .addRoute("/my-webitem-dialog", ConnectAppServlets.closeDialogServlet())
-                .addRoute("/rpd", ConnectAppServlets.dialogServlet())
-                .addRoute("/fsd", ConnectAppServlets.sizeToParentServlet())
                 .start();
     }
 
@@ -174,32 +145,5 @@ public class TestDialog extends MultiProductWebDriverTestBase
         assertThat(closeDialogPage.getIFrameSize().getHeight(), is(356));
         assertTrue(closeDialogPage.getFromQueryString("ui-params").length() > 0);
         verifyIframeURLHasVersionNumber(closeDialogPage);
-    }
-
-    @Test
-    public void testLoadGeneralDialog()
-    {
-        login(testUserFactory.basicUser());
-        HomePage homePage = product.visit(HomePage.class);
-
-        RemotePluginAwarePage page = product.getPageBinder().bind(GeneralPage.class, REMOTE_PLUGIN_DIALOG_KEY, runner.getAddon().getKey());
-        ConnectAddOnEmbeddedTestPage remotePluginTest = page.clickAddOnLink();
-        assertThat(remotePluginTest.getLocation(), endsWith(homePage.getUrl()));
-
-        // Exercise the dialog's submit button.
-        RemotePluginDialog dialog = product.getPageBinder().bind(RemotePluginDialog.class, remotePluginTest);
-        assertFalse(dialog.wasSubmitted());
-        dialog.submitAndWaitUntilSubmitted();
-        dialog.submitAndWaitUntilHidden();
-    }
-
-    @Test
-    public void testSizeToParentDoesNotWorkInDialog()
-    {
-        login(testUserFactory.basicUser());
-        product.visit(HomePage.class);
-        RemotePluginAwarePage page = product.getPageBinder().bind(GeneralPage.class, SIZE_TO_PARENT_DIALOG_KEY, runner.getAddon().getKey());
-        ConnectAddOnEmbeddedTestPage remotePluginTest = page.clickAddOnLink();
-        assertTrue(remotePluginTest.isNotFullSize());
     }
 }
