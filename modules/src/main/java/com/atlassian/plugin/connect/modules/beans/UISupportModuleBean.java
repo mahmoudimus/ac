2bb8845c2a50e1c509cd87571fb1df2fb06b5b8d
Matthew Jensen <mjensen@atlassian.com>
2015-06-02 15:39:50 +1000

    CE-155 explicit mappings for data-uri

--- modules/src/main/java/com/atlassian/plugin/connect/modules/beans/UISupportModuleBean.java
+++ modules/src/main/java/com/atlassian/plugin/connect/modules/beans/UISupportModuleBean.java
@@ -2,19 +2,16 @@ package com.atlassian.plugin.connect.modules.beans;
 
 import com.atlassian.json.schema.annotation.Required;
 import com.atlassian.json.schema.annotation.SchemaDefinition;
-import com.atlassian.json.schema.annotation.StringSchemaAttributes;
 import com.atlassian.plugin.connect.modules.beans.builder.UISupportModuleBeanBuilder;
 import com.atlassian.plugin.connect.modules.beans.nested.I18nProperty;
 
 @SchemaDefinition("uiSupport")
 public class UISupportModuleBean extends BaseModuleBean
 {
-    @StringSchemaAttributes(format = "defaultOperator")
     private String defaultOperator;
     @Required
     private I18nProperty name;
     private I18nProperty tooltip;
-    @StringSchemaAttributes(format = "dataUri")
     private String dataUri;
 
     public UISupportModuleBean(UISupportModuleBeanBuilder builder)
