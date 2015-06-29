e3b9678da60845f6d1c1e673e06c962c050641ec
Einar Pehrson <epehrson@atlassian.com>
2015-06-29 16:12:49 +1000

    ACDEV-2079 Removed duplicated component import for Active Objects

---
+++ wired-tests/src/test/java/it/com/atlassian/plugin/connect/plugin/PluginDescriptorTest.java
@@ -0,0 +1,53 @@
+package it.com.atlassian.plugin.connect.plugin;
+
+import com.atlassian.plugin.ModuleDescriptor;
+import com.atlassian.plugin.PluginAccessor;
+import com.atlassian.plugin.connect.api.util.ConnectPluginInfo;
+import com.atlassian.plugin.predicate.ModuleDescriptorPredicate;
+import com.atlassian.plugins.osgi.test.AtlassianPluginsTestRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.Collection;
+
+@RunWith(AtlassianPluginsTestRunner.class)
+public class PluginDescriptorTest
+{
+
+    private final PluginAccessor pluginAccessor;
+
+    public PluginDescriptorTest(PluginAccessor pluginAccessor)
+    {
+        this.pluginAccessor = pluginAccessor;
+    }
+
+    @Test
+    public void getModulesForDescriptors()
+    {
+        final Collection<ModuleDescriptor<Object>> moduleDescriptors = pluginAccessor.getModuleDescriptors(new ModuleDescriptorPredicate<Object>()
+        {
+            @Override
+            public boolean matches(ModuleDescriptor<?> moduleDescriptor)
+            {
+                return ConnectPluginInfo.getPluginKey().equals(moduleDescriptor.getPluginKey());
+            }
+        });
+        for (ModuleDescriptor<Object> moduleDescriptor : moduleDescriptors)
+        {
+            if (moduleDescriptor.getKey().equals("analyticsWhitelist"))
+            {
+                // getModule() appears to be broken in AnalyticsWhitelistModuleDescriptor from analytics-client
+                continue;
+            }
+
+            try
+            {
+                moduleDescriptor.getModule();
+            }
+            catch (UnsupportedOperationException e)
+            {
+                // Module types, web resources etc. do not expose classes via getMethod(
+            }
+        }
+    }
+}
