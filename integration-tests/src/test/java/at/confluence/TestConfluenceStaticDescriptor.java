ef9f28d1e0a4c7dedab937159927b40ca3d0cd68
James Hazelwood <jhazelwood@atlassian.com>
2015-07-01 12:12:42 +1000

    ACDEV-2053 Don't inherit from ConfluenceWebDriverTestBase in TestConfluenceStaticDescriptor

--- integration-tests/src/test/java/at/confluence/TestConfluenceStaticDescriptor.java
+++ integration-tests/src/test/java/at/confluence/TestConfluenceStaticDescriptor.java
@@ -1,6 +1,9 @@
 package at.confluence;
 
+import com.atlassian.confluence.pageobjects.ConfluenceTestedProduct;
 import com.atlassian.confluence.pageobjects.page.DashboardPage;
+import com.atlassian.plugin.connect.test.pageobjects.ConnectPageOperations;
+import com.atlassian.plugin.connect.test.pageobjects.TestedProductProvider;
 import com.atlassian.test.categories.OnDemandAcceptanceTest;
 
 import com.google.common.base.Optional;
@@ -13,15 +16,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import at.util.ExternalAddonInstaller;
-import it.confluence.ConfluenceWebDriverTestBase;
+import it.util.TestUser;
 
 @Category (OnDemandAcceptanceTest.class)
-public class TestConfluenceStaticDescriptor extends ConfluenceWebDriverTestBase
+public class TestConfluenceStaticDescriptor
 {
-    public static final String WEB_ITEM_ID = "com.atlassian.connect.acceptance.test__browse-test-web-item";
+    private static final String WEB_ITEM_ID = "com.atlassian.connect.acceptance.test__browse-test-web-item";
     private static final Logger log = LoggerFactory.getLogger(TestConfluenceStaticDescriptor.class);
-    private static final ExternalAddonInstaller externalAddonInstaller =
-            new ExternalAddonInstaller(product.getProductInstance().getBaseUrl(), testUserFactory.admin());
+    private static final TestUser ADMIN = new TestUser("admin");
+
+    private final ConfluenceTestedProduct product = TestedProductProvider.getConfluenceTestedProduct();
+    private final ExternalAddonInstaller externalAddonInstaller =
+            new ExternalAddonInstaller(product.getProductInstance().getBaseUrl(), ADMIN);
+    private final ConnectPageOperations connectPageOperations = new ConnectPageOperations(
+            product.getPageBinder(), product.getTester().getDriver());
 
     @Before
     public void installAddon() throws Exception
@@ -33,8 +41,14 @@ public class TestConfluenceStaticDescriptor extends ConfluenceWebDriverTestBase
     @Test
     public void testAcDashboardWebItemIsPresent()
     {
-        loginAndVisit(testUserFactory.basicUser(), DashboardPage.class);
+        logOut();
+        product.login(ADMIN.confUser(), DashboardPage.class);
         connectPageOperations.findWebItem(WEB_ITEM_ID, Optional.<String>absent());
+    }
+
+    private void logOut()
+    {
+        product.getTester().getDriver().manage().deleteAllCookies();
     }
 
     @After
