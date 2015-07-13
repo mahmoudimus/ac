11c98cd4a75bb6954b1fa621a3de5c743d44ae2e
Jake Furler <jfurler@atlassian.com>
2015-07-13 15:49:12 +1000

    ACDEV-2045 test serialization

--- modules/src/main/java/com/atlassian/plugin/connect/modules/gson/DynamicModuleSerializer.java
+++ modules/src/main/java/com/atlassian/plugin/connect/modules/gson/DynamicModuleSerializer.java
@@ -4,7 +4,7 @@ import com.google.gson.*;
 
 import java.lang.reflect.Type;
 
-public class DynamicModuleSerializer implements JsonDeserializer<JsonObject>
+public class DynamicModuleSerializer implements JsonDeserializer<JsonObject>, JsonSerializer<JsonObject>
 {
     @Override
     public JsonObject deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
@@ -12,5 +12,11 @@ public class DynamicModuleSerializer implements JsonDeserializer<JsonObject>
     {
         return json.getAsJsonObject();
         
+    }
+    
+    @Override
+    public JsonElement serialize(JsonObject object, Type typeOfT, JsonSerializationContext context)
+    {
+        return object;
     }
 }
