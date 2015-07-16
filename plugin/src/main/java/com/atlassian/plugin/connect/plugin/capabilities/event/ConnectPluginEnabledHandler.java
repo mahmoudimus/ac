d7a0c1ddc22b00548c64f4dfc67653d4e9e42b11
Einar Pehrson <epehrson@atlassian.com>
2015-07-16 15:38:07 +1000

    ACDEV-2112 Move UserPropertyService hack to before add-on enablement

--- plugin/src/main/java/com/atlassian/plugin/connect/plugin/capabilities/event/ConnectPluginEnabledHandler.java
+++ plugin/src/main/java/com/atlassian/plugin/connect/plugin/capabilities/event/ConnectPluginEnabledHandler.java
@@ -1,15 +1,12 @@
 package com.atlassian.plugin.connect.plugin.capabilities.event;
 
 import com.atlassian.event.api.EventPublisher;
-import com.atlassian.plugin.connect.api.util.ConnectPluginInfo;
-import com.atlassian.plugin.connect.plugin.installer.ConnectAddonManager;
 import com.atlassian.plugin.connect.api.registry.ConnectAddonRegistry;
+import com.atlassian.plugin.connect.plugin.Jira7ComponentBridge;
+import com.atlassian.plugin.connect.plugin.installer.ConnectAddonManager;
 import com.atlassian.plugin.connect.plugin.util.AbstractInitializingComponent;
 import com.atlassian.plugin.spring.scanner.annotation.export.ExportAsService;
 import com.atlassian.sal.api.lifecycle.LifecycleAware;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -20,23 +17,26 @@ public class ConnectPluginEnabledHandler extends AbstractInitializingComponent
 {
     private final ConnectAddonRegistry addonRegistry;
     private final ConnectAddonManager addonManager;
+    private final Jira7ComponentBridge jira7ComponentBridge;
 
     @Inject
     public ConnectPluginEnabledHandler(ConnectAddonRegistry addonRegistry,
-            ConnectAddonManager addonManager, EventPublisher eventPublisher)
+            ConnectAddonManager addonManager, EventPublisher eventPublisher, Jira7ComponentBridge jira7ComponentBridge)
     {
         super(eventPublisher);
         this.addonRegistry = addonRegistry;
         this.addonManager = addonManager;
+        this.jira7ComponentBridge = jira7ComponentBridge;
     }
 
     @Override
     protected void finalInit()
     {
-        pluginEnabled();
+        jira7ComponentBridge.makeUserPropertyServiceAvailableInPluginContainer();
+        enableAddons();
     }
 
-    private void pluginEnabled()
+    private void enableAddons()
     {
         //enable all the addons if needed
         for (String addonKey : addonRegistry.getAddonKeysToEnableOnRestart())
@@ -44,5 +44,4 @@ public class ConnectPluginEnabledHandler extends AbstractInitializingComponent
             addonManager.enableConnectAddon(addonKey);
         }
     }
-
 }
