8777d6c0574f4ee9dd7d6341612b880aab3aecce
James Hazelwood <jhazelwood@atlassian.com>
2015-07-17 16:17:00 +1000

    ACDEV-2117 Use a subclass for the factory to appease Confluence (attempt 1)

---
+++ plugin/src/main/java/com/atlassian/plugin/connect/jira/bridge/BrowseContextHelper.java
@@ -0,0 +1,8 @@
+package com.atlassian.plugin.connect.jira.bridge;
+
+import com.atlassian.jira.compatibility.factory.project.browse.BrowseContextHelperBridgeFactory;
+import com.atlassian.plugin.spring.scanner.annotation.component.JiraComponent;
+
+@JiraComponent
+public class BrowseContextHelper extends BrowseContextHelperBridgeFactory
+{ }
