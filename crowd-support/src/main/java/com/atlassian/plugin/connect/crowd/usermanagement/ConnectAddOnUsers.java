4b538b525130773b79afed7143ea69cc55f74a42
Dave Haden <dhaden@atlassian.com>
2015-08-26 11:24:41 +1000

    ACDEV-1968 Removed Crowd Upgrade task, moved ConnectAddonUsers to usermanagement

---
+++ crowd-support/src/main/java/com/atlassian/plugin/connect/crowd/usermanagement/ConnectAddOnUsers.java
@@ -0,0 +1,14 @@
+package com.atlassian.plugin.connect.crowd.usermanagement;
+
+import com.atlassian.crowd.exception.ApplicationNotFoundException;
+import com.atlassian.crowd.model.user.User;
+
+/**
+ * Finds connect add-on users
+ *
+ * <strong>Not suitable for general-purpose add-on user management</strong>
+ */
+public interface ConnectAddOnUsers
+{
+    Iterable<User> getAddonUsers();
+}
