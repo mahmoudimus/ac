cec1e2c40de1fd2af17493e437fc8e6d21f8e452
Dave Haden <dhaden@atlassian.com>
2015-09-16 19:42:01 +1000

    ACDEV-2207 Moved ConnectCrowdPermissions to include an interface

--- crowd-support/src/main/java/com/atlassian/plugin/connect/crowd/usermanagement/permissions/ConnectCrowdPermissions.java
+++ crowd-support/src/main/java/com/atlassian/plugin/connect/crowd/usermanagement/permissions/ConnectCrowdPermissions.java
@@ -1,66 +1,11 @@
 package com.atlassian.plugin.connect.crowd.usermanagement.permissions;
 
-import com.atlassian.plugin.connect.spi.host.HostProperties;
-import com.atlassian.plugin.spring.scanner.annotation.component.JiraComponent;
-import com.atlassian.usermanagement.client.api.ManagedPermissionsService;
-import com.atlassian.usermanagement.client.api.UserManagementLockService;
-import com.atlassian.usermanagement.client.api.exception.ManagedPermissionsException;
-import com.atlassian.usermanagement.client.api.exception.UserManagementException;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-
-@JiraComponent
-public class ConnectCrowdPermissions
+public interface ConnectCrowdPermissions
 {
-    private static final Logger log = LoggerFactory.getLogger(ConnectCrowdPermissions.class);
-
-    private final HostProperties hostProperties;
-    private final UserManagementLockService userManagementLockService;
-    private final ManagedPermissionsService managedPermissionsService;
-
-    @Autowired
-    public ConnectCrowdPermissions(HostProperties hostProperties,
-            UserManagementLockService userManagementLockService,
-            ManagedPermissionsService managedPermissionsService)
-    {
-        this.hostProperties = hostProperties;
-        this.userManagementLockService = userManagementLockService;
-        this.managedPermissionsService = managedPermissionsService;
-    }
-
-    public boolean setPermissionsForGroup(String groupName)
-    {
-
-        try
-        {
-            userManagementLockService.unlockUserManagement();
-        }
-        catch (UserManagementException e)
-        {
-            log.warn("Could not unlock User Management. Some other Crowd process in control?", e); // Presuming locked. Does it error if locked already?
-            return false;
-        }
-
-        grantAdminPermission(hostProperties.getKey(), groupName);
-
-        try
-        {
-            managedPermissionsService.publishConfigToUserManagement();
-        }
-        catch (ManagedPermissionsException e)
-        {
-            log.warn("Could not publish admin changes to Crowd Remote");
-            return false;
-        }
-        return true;
-//https://dhaden-alt-test.atlassian.net/admin/rest/um/1/accessconfig/group?productId=product%3Ajira%3Ajira
-    }
-
-
-    private void grantAdminPermission(String productId, String groupName)
-    {
-
-    }
+    /**
+     *
+     * @param groupName
+     * @return
+     */
+    public boolean setPermissionsForGroup(String groupName);
 }
---
+++ crowd-support/src/main/java/com/atlassian/plugin/connect/crowd/usermanagement/permissions/ConnectCrowdPermissionsImpl.java
@@ -0,0 +1,77 @@
+package com.atlassian.plugin.connect.crowd.usermanagement.permissions;
+
+import com.atlassian.plugin.connect.spi.host.HostProperties;
+import com.atlassian.plugin.spring.scanner.annotation.component.JiraComponent;
+import com.atlassian.usermanagement.client.api.ManagedPermissionsService;
+import com.atlassian.usermanagement.client.api.UserManagementLockService;
+import com.atlassian.usermanagement.client.api.exception.ManagedPermissionsException;
+import com.atlassian.usermanagement.client.api.exception.UserManagementException;
+
+import com.google.common.annotations.VisibleForTesting;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@JiraComponent
+public class ConnectCrowdPermissionsImpl implements ConnectCrowdPermissions
+{
+    private static final Logger log = LoggerFactory.getLogger(ConnectCrowdPermissionsImpl.class);
+
+    private final HostProperties hostProperties;
+    private final UserManagementLockService userManagementLockService;
+    private final ManagedPermissionsService managedPermissionsService;
+
+    @VisibleForTesting
+    private ConnectCrowdPermissionsImpl()
+    {
+        this.hostProperties = null;
+        this.userManagementLockService = null;
+        this.managedPermissionsService = null;
+    }
+
+    @Autowired
+    public ConnectCrowdPermissionsImpl(HostProperties hostProperties,
+            UserManagementLockService userManagementLockService,
+            ManagedPermissionsService managedPermissionsService)
+    {
+        this.hostProperties = hostProperties;
+        this.userManagementLockService = userManagementLockService;
+        this.managedPermissionsService = managedPermissionsService;
+    }
+
+    @Override
+    public boolean setPermissionsForGroup(String groupName)
+    {
+
+        try
+        {
+            userManagementLockService.unlockUserManagement();
+        }
+        catch (UserManagementException e)
+        {
+            log.warn("Could not unlock User Management. Some other Crowd process in control?", e); // Presuming locked. Does it error if locked already?
+            return false;
+        }
+
+        grantAdminPermission(hostProperties.getKey(), groupName);
+
+        try
+        {
+            managedPermissionsService.publishConfigToUserManagement();
+        }
+        catch (ManagedPermissionsException e)
+        {
+            log.warn("Could not publish admin changes to Crowd Remote");
+            return false;
+        }
+        return true;
+//https://dhaden-alt-test.atlassian.net/admin/rest/um/1/accessconfig/group?productId=product%3Ajira%3Ajira
+    }
+
+
+    private void grantAdminPermission(String productId, String groupName)
+    {
+
+    }
+}
