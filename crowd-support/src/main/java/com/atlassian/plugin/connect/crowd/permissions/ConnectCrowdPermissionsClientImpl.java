e2f76839a499ad8e4eb071178245eb135650de90
James Hazelwood <jhazelwood@atlassian.com>
2015-09-18 14:30:55 +1000

    ACDEV-2207 Get rid of references to UserManagementException

--- crowd-support/src/main/java/com/atlassian/plugin/connect/crowd/permissions/ConnectCrowdPermissionsClientImpl.java
+++ crowd-support/src/main/java/com/atlassian/plugin/connect/crowd/permissions/ConnectCrowdPermissionsClientImpl.java
@@ -18,7 +18,6 @@ import com.atlassian.plugin.connect.crowd.usermanagement.CrowdClientProvider;
 import com.atlassian.plugin.spring.scanner.annotation.component.JiraComponent;
 import com.atlassian.sal.api.net.Request;
 import com.atlassian.sal.api.net.ResponseException;
-import com.atlassian.usermanagement.client.api.exception.UserManagementException;
 
 import org.apache.commons.httpclient.Cookie;
 import org.apache.http.entity.StringEntity;
@@ -56,7 +55,13 @@ public class ConnectCrowdPermissionsClientImpl
             executeAsSysadmin(POST, "/admin/rest/um/1/accessconfig/group?productId=product:jira:jira", "[\"atlassian-addons-admin\"]");
             executeAsSysadmin(PUT, "/admin/rest/um/1/accessconfig/group?hostId=jira&productId=product:jira:jira", "{name: \"atlassian-addons-admin\", use: \"NONE\", admin: \"DIRECT\", defaultUse: false}");
         }
-        catch (CredentialsRequiredException | UserManagementException | ResponseException e)
+        catch (InactiveAccountException
+                | ApplicationPermissionException
+                | ApplicationAccessDeniedException
+                | InvalidAuthenticationException
+                | OperationFailedException
+                | CredentialsRequiredException
+                | ResponseException e)
         {
             log.warn("Could not grant remote admin permission to the group " + groupName + "due to the following exception", e);
             return false;
@@ -65,12 +70,12 @@ public class ConnectCrowdPermissionsClientImpl
     }
 
     private String executeAsSysadmin(Request.MethodType methodType, String url, String JSONString)
-            throws CredentialsRequiredException, UserManagementException, ResponseException
+            throws CredentialsRequiredException, ResponseException, ApplicationPermissionException, InactiveAccountException, ApplicationAccessDeniedException, OperationFailedException, InvalidAuthenticationException
     {
         Option<ApplicationLink> possibleCrowd = first(applicationLinkService.getApplicationLinks(CrowdApplicationType.class));
         if (possibleCrowd.isEmpty())
         {
-            throw new UserManagementException("There was no Crowd application link. This is a problem");
+            throw new OperationFailedException("There was no Crowd application link. This is a problem");
         }
         ReadOnlyApplicationLink crowd = possibleCrowd.get();
         ApplicationLinkRequest request = crowd.createAuthenticatedRequestFactory().createRequest(methodType, url);
@@ -81,20 +86,14 @@ public class ConnectCrowdPermissionsClientImpl
     }
 
     private Cookie generateSysadminCookie(String host)
-            throws UserManagementException
+            throws InactiveAccountException, OperationFailedException, ApplicationAccessDeniedException, ApplicationPermissionException, InvalidAuthenticationException
     {
         final UserAuthenticationContext sysadminAuthenticationContext = new UserAuthenticationContext("sysadmin",
                 null,
                 new ValidationFactor[] {}, "jira");
-
-        try
         {
             String ssoToken = crowdClientProvider.getCrowdClient().authenticateSSOUserWithoutValidatingPassword(sysadminAuthenticationContext);
             return new Cookie(host, "studio.crowd.tokenkey", ssoToken);
-        }
-        catch (ApplicationPermissionException | InactiveAccountException | InvalidAuthenticationException | OperationFailedException | ApplicationAccessDeniedException e)
-        {
-            throw new UserManagementException("Failed to generate token to communicate with Crowd. This is a problem", e);
         }
     }
 }
