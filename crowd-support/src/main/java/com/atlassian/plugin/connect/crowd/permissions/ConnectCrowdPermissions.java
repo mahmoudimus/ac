4373e33e9d978c03f8053d1ca6fe2b6b7c7ba80b
Dave Haden <dhaden@atlassian.com>
2015-09-21 17:50:48 +1000

    ACDEV-2232 Added missing javadocs

--- crowd-support/src/main/java/com/atlassian/plugin/connect/crowd/permissions/ConnectCrowdPermissions.java
+++ crowd-support/src/main/java/com/atlassian/plugin/connect/crowd/permissions/ConnectCrowdPermissions.java
@@ -9,6 +9,8 @@ public interface ConnectCrowdPermissions
      * Grant administer permissions to the group named, via Horde's unified user management
      *
      * @param groupName The name of the group to grant admin permission to
+     * @param productId The name of the core product to grant permissions to
+     * @param applicationId The name of Application inside the product to grant the permissions to
      * @return {@link com.atlassian.plugin.connect.crowd.permissions.ConnectCrowdPermissions.GrantResult#NO_REMOTE_GRANT_NEEDED} if permissions are not managed remotely,
      * {@link com.atlassian.plugin.connect.crowd.permissions.ConnectCrowdPermissions.GrantResult#REMOTE_GRANT_SUCCEEDED} if administer permissions were granted successfully, or
      * {@link com.atlassian.plugin.connect.crowd.permissions.ConnectCrowdPermissions.GrantResult#REMOTE_GRANT_FAILED} if granting of administer permissions failed
