fb8c72d429bf75241bffff7e1793d0c78ec757b1
James Hazelwood <jhazelwood@atlassian.com>
2015-09-21 16:42:37 +1000

    ACDEV-2232 Replace string concatenation with programmatic URI / JSON encoding

--- crowd-support/src/main/java/com/atlassian/plugin/connect/crowd/permissions/ConnectCrowdPermissionsClientImpl.java
+++ crowd-support/src/main/java/com/atlassian/plugin/connect/crowd/permissions/ConnectCrowdPermissionsClientImpl.java
@@ -8,7 +8,12 @@ import com.atlassian.crowd.exception.InvalidAuthenticationException;
 import com.atlassian.crowd.exception.OperationFailedException;
 import com.atlassian.plugin.spring.scanner.annotation.component.JiraComponent;
 import com.atlassian.sal.api.net.ResponseException;
+import com.atlassian.uri.UriBuilder;
 
+import com.google.common.collect.ImmutableMap;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +25,8 @@ import static com.atlassian.sal.api.net.Request.MethodType.PUT;
 public class ConnectCrowdPermissionsClientImpl
         implements ConnectCrowdPermissionsClient
 {
+    public static final String CONFIG_PATH = "/rest/um/1/accessconfig/group";
+
     private static final Logger log = LoggerFactory.getLogger(ConnectCrowdPermissionsClientImpl.class);
 
     private final ConnectCrowdSysadminHttpClient connectCrowdSysadminHttpClient;
@@ -30,16 +37,26 @@ public class ConnectCrowdPermissionsClientImpl
         this.connectCrowdSysadminHttpClient = connectCrowdSysadminHttpClient;
     }
 
+    private String addProductUri(String productId, String applicationId)
+    {
+        return new UriBuilder().setPath(CONFIG_PATH)
+                .addQueryParameter("productId", "product:" + productId + ":" + applicationId).toString();
+    }
+
+    private String configureProductUri(String productId, String applicationId)
+    {
+        return new UriBuilder().setPath(CONFIG_PATH)
+                .addQueryParameter("hostId", productId)
+                .addQueryParameter("productId", "product:" + productId + ":" + applicationId).toString();
+    }
+
     @Override
     public boolean grantAdminPermission(String groupName, String productId, String applicationId)
     {
         try
         {
-            connectCrowdSysadminHttpClient.executeAsSysadmin(POST,
-                    "/rest/um/1/accessconfig/group?productId=product%3A" + productId + "%3A" + applicationId, String.format("[\"%s\"]", groupName));
-            connectCrowdSysadminHttpClient.executeAsSysadmin(PUT,
-                    "/rest/um/1/accessconfig/group?hostId=" + productId + "&productId=product%3A" + productId + "%3A" + applicationId,
-                    String.format("{\"name\": \"%s\", \"use\": \"NONE\", \"admin\": \"DIRECT\", \"defaultUse\": false}", groupName));
+            connectCrowdSysadminHttpClient.executeAsSysadmin(POST, addProductUri(productId, applicationId), groupsList(groupName));
+            connectCrowdSysadminHttpClient.executeAsSysadmin(PUT, configureProductUri(productId, applicationId), groupData(groupName));
         }
         catch (InactiveAccountException
                 | ApplicationPermissionException
@@ -53,5 +70,23 @@ public class ConnectCrowdPermissionsClientImpl
             return false;
         }
         return true;
+    }
+
+    @SuppressWarnings ("unchecked")
+    private String groupsList(String groupName)
+    {
+        final JSONArray jsonArray = new JSONArray();
+        jsonArray.add(groupName);
+        return jsonArray.toJSONString();
+    }
+
+    private String groupData(String groupName)
+    {
+        return new JSONObject(ImmutableMap.of(
+                "name", groupName,
+                "use", "NONE",
+                "admin", "DIRECT",
+                "defaultUse", false
+        )).toJSONString();
     }
 }
