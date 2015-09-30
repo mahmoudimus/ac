d1a1e61587dc6c36d774e860c6be990673b7c42d
Klaus Ihlberg <kihlberg@atlassian.com>
2015-09-30 11:41:30 +1000

    CE-226 Fixed UPM tests.

--- tests/integration-tests/src/test/java/com/atlassian/plugin/connect/test/pageobjects/PluginManagerPage.java
+++ tests/integration-tests/src/test/java/com/atlassian/plugin/connect/test/pageobjects/PluginManagerPage.java
@@ -36,7 +36,7 @@ public class PluginManagerPage implements Page
         {
             driver.navigate().to(productInstance.getBaseUrl() + "/plugins/servlet/upm/manage/user-installed#manage");
             WebDriverPoller poller = new WebDriverPoller(driver);
-            poller.waitUntil(ElementConditions.isVisible(By.id("upm-manage-user-installed-plugins")), 60);
+            poller.waitUntil(ElementConditions.isVisible(By.id("upm-manage-plugins-user-installed")), 60);
             // the list of add-ons loads asynchronously and takes some seconds; wait until at least one is present
             poller.waitUntil(ElementConditions.isVisible(By.className("upm-plugin-name")), 60);
         }
