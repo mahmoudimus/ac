e756770516d6a86e990b65cc76bd6d77f72be3de
Grzegorz Tanczyk <gtanczyk@atlassian.com>
2015-11-03 21:29:34 +0100

    [ACJIRA-628] Check for atl.general and quick edit module

--- jsapi/src/main/resources/js/jira/issue/issue.js
+++ jsapi/src/main/resources/js/jira/issue/issue.js
@@ -1,8 +1,10 @@
 (function($, define){
     "use strict";
-    define("ac/jira/issue", function(){
+    define("ac/jira/issue", ['jquery'], function($){
+        var contextReady;
+
         function createIssueDialog(callback, fields){
-            WRM.require(['wr!com.atlassian.jira.jira-quick-edit-plugin:quick-create-issue']).then(function() {
+            requireResources().then(function() {
                 if(!JIRA || !JIRA.Forms || !JIRA.Forms.createCreateIssueForm){
                     if(console && console.warn){
                         console.warn("Connect: Create issue form is not available");
@@ -32,6 +34,33 @@
                     }
                 });
             }.bind(this));
+        }
+
+        function requireResources() {
+            var result = new $.Deferred();
+
+            getContextReady().then(function() {
+                WRM.require(['wr!com.atlassian.jira.jira-quick-edit-plugin:quick-create-issue']).then(result.resolve.bind(result));
+            });
+
+            return result;
+        }
+
+        function getContextReady() {
+            if (!contextReady) {
+                contextReady = new $.Deferred();
+
+                WRM.curl(['wrm/builder', "wrm/resource-base-url-pattern"], function (Builder, resourceBaseUrlPattern) {
+                    var contexts = new Builder(resourceBaseUrlPattern).initialize(document).contexts;
+                    if (contexts.indexOf('atl.general') > 0) {
+                        contextReady.resolve();
+                    } else {
+                        contextReady.reject();
+                    }
+                });
+            }
+
+            return contextReady;
         }
 
         return {
