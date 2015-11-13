c8079f7d31cea5e08c606a1c93ffb934cd545373
Robert Massaioli <rmassaioli@atlassian.com>
2015-11-13 15:55:15 +1100

    AC-1693: Ensuring that add-on entity properties return JSON and not strings in the value section.

--- components/core/src/main/java/com/atlassian/plugin/connect/plugin/rest/data/RestAddOnProperty.java
+++ components/core/src/main/java/com/atlassian/plugin/connect/plugin/rest/data/RestAddOnProperty.java
@@ -1,9 +1,18 @@
 package com.atlassian.plugin.connect.plugin.rest.data;
 
+import java.io.IOException;
+
 import com.atlassian.plugin.connect.plugin.property.AddOnProperty;
 import org.codehaus.jackson.annotate.JsonProperty;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import javax.annotation.concurrent.Immutable;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Optional;
+import com.google.common.base.Preconditions;
 
 /**
  * This class represents an add-on property
@@ -14,11 +23,11 @@ public class RestAddOnProperty
     @JsonProperty
     private final String key;
     @JsonProperty
-    private final String value;
+    private final JsonNode value;
     @JsonProperty
     private final String self;
 
-    public RestAddOnProperty(@JsonProperty ("key") final String key, @JsonProperty ("value") final String value, @JsonProperty ("self") final String self)
+    public RestAddOnProperty(@JsonProperty ("key") final String key, @JsonProperty ("value") final JsonNode value, @JsonProperty ("self") final String self)
     {
         this.key = key;
         this.value = value;
@@ -27,7 +36,24 @@ public class RestAddOnProperty
 
     public static RestAddOnProperty valueOf(final AddOnProperty addOnProperty, final String baseURL)
     {
-        return new RestAddOnProperty(addOnProperty.getKey(), addOnProperty.getValue(), propertySelf(baseURL, addOnProperty.getKey()));
+        final Optional<JsonNode> potentialValue = parseJson(addOnProperty.getValue());
+        Preconditions.checkState(potentialValue.isPresent(), "The value for the property " + addOnProperty.getKey() + " was not valid JSON.");
+        return new RestAddOnProperty(addOnProperty.getKey(), potentialValue.get(), propertySelf(baseURL, addOnProperty.getKey()));
+    }
+
+    private static Optional<JsonNode> parseJson(String value) {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode actualObj;
+        try
+        {
+            actualObj = mapper.readTree(value);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            return Optional.absent();
+        }
+        return Optional.of(actualObj);
     }
 
     public static String propertySelf(String baseURL, String propertyKey)
