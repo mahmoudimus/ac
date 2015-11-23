f2e551323ce267835f106cb96e13bd3885ccf5a3
Marek Pazik <mpazik@atlassian.com>
2015-11-23 18:01:04 +0100

    ACJIRA-588: Fixed builder for ProjectPermissionModule.

--- components/modules/src/main/java/com/atlassian/plugin/connect/modules/beans/builder/ProjectPermissionModuleBeanBuilder.java
+++ components/modules/src/main/java/com/atlassian/plugin/connect/modules/beans/builder/ProjectPermissionModuleBeanBuilder.java
@@ -1,19 +1,28 @@
 package com.atlassian.plugin.connect.modules.beans.builder;
 
+import com.atlassian.plugin.connect.modules.beans.ConditionalBean;
 import com.atlassian.plugin.connect.modules.beans.ProjectPermissionCategory;
 import com.atlassian.plugin.connect.modules.beans.ProjectPermissionModuleBean;
 import com.atlassian.plugin.connect.modules.beans.nested.I18nProperty;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import static com.google.common.collect.Lists.newArrayList;
+
 public class ProjectPermissionModuleBeanBuilder
-        extends BeanWithKeyParamsAndConditionsBuilder<ProjectPermissionModuleBeanBuilder, ProjectPermissionModuleBean>
+        extends RequiredKeyBeanBuilder<ProjectPermissionModuleBeanBuilder, ProjectPermissionModuleBean>
 {
     private I18nProperty description;
     private ProjectPermissionCategory category;
+    private List<ConditionalBean> conditions;
 
     public ProjectPermissionModuleBeanBuilder()
     {
         this.description = I18nProperty.empty();
         this.category = ProjectPermissionCategory.OTHER;
+        this.conditions = newArrayList();
     }
 
     public ProjectPermissionModuleBeanBuilder(ProjectPermissionModuleBean projectPermissionModuleBean)
@@ -21,6 +30,7 @@ public class ProjectPermissionModuleBeanBuilder
         super(projectPermissionModuleBean);
         this.description = projectPermissionModuleBean.getDescription();
         this.category = projectPermissionModuleBean.getCategory();
+        this.conditions = newArrayList(projectPermissionModuleBean.getConditions());
     }
 
     public ProjectPermissionModuleBeanBuilder withDescription(I18nProperty description)
@@ -33,6 +43,27 @@ public class ProjectPermissionModuleBeanBuilder
     {
         this.category = projectPermissionCategory;
         return this;
+    }
+
+    public ProjectPermissionModuleBeanBuilder withConditions(ConditionalBean... beans)
+    {
+        return withConditions(Arrays.asList(beans));
+    }
+
+    public ProjectPermissionModuleBeanBuilder withConditions(Collection<? extends ConditionalBean> beans)
+    {
+        if (beans != null) // not sure why this comes in as null sometimes
+        {
+            if (null == conditions)
+            {
+                conditions = newArrayList();
+            }
+
+            conditions.addAll(beans);
+        }
+
+        return this;
+
     }
 
     @Override
