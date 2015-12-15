c0c547bcc92d04cdc92ea2b7a4019dc6eb95f502
Kate West-Walker <kwestwalker@atlassian.com>
2015-12-15 18:08:49 +1100

    CE-249 more navigator integration tests

--- tests/core-integration-tests/src/test/java/it/common/jsapi/TestNavigator.java
+++ tests/core-integration-tests/src/test/java/it/common/jsapi/TestNavigator.java
@@ -7,12 +7,13 @@ import com.atlassian.plugin.connect.test.common.util.AddonTestUtils;
 import com.atlassian.plugin.connect.test.pageobjects.RemoteNavigatorGeneralPage;
 import it.common.MultiProductWebDriverTestBase;
 
-import static org.junit.Assert.assertEquals;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static com.atlassian.plugin.connect.modules.beans.ConnectPageModuleBean.newPageBean;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class TestNavigator extends MultiProductWebDriverTestBase
 {
@@ -45,17 +46,77 @@ public class TestNavigator extends MultiProductWebDriverTestBase
         }
     }
 
+    /*
+     If you've been messing around locally on the instance it may redirect to the dashboard
+     being at #recently-viewed or #all-updates depending on what you've clicked. In this case
+     the test isn't broken, you just need a fresh startup.
+     */
     @Test
     public void testNavigateToDashboard() throws Exception
     {
         loginAndClickToNavigate("navigate-to-dashboard");
-        assertEquals(product.getProductInstance().getContextPath(), "/confluence");
+
+        String relativeUrl = getRelativeUrlFromWebDriver();
+        assertEquals("/confluence", relativeUrl);
+
+        makeSureNo404s();
     }
 
-    public void loginAndClickToNavigate(String id) {
+    @Test
+    public void testNavigateToPage() throws Exception
+    {
+        loginAndClickToNavigate("navigate-to-page");
+
+        String relativeUrl = getRelativeUrlFromWebDriver();
+        assertEquals("/pages/viewpage.action?pageId=98311", relativeUrl);
+
+        makeSureNo404s();
+    }
+
+    @Test
+    public void testNavigateToEditPage() throws Exception
+    {
+        loginAndClickToNavigate("navigate-to-edit-page");
+
+        String relativeUrl = getRelativeUrlFromWebDriver();
+        assertEquals("/editpage.action?pageId=98311", relativeUrl);
+
+        makeSureNo404s();
+    }
+
+    @Test
+    public void testNavigateToUserProfile() throws Exception
+    {
+        loginAndClickToNavigate("navigate-to-user-profile");
+
+        String relativeUrl = getRelativeUrlFromWebDriver();
+        assertEquals("/display/~admin", relativeUrl);
+
+        makeSureNo404s();
+    }
+
+    public void loginAndClickToNavigate(String id)
+    {
         RemoteNavigatorGeneralPage page = loginAndVisit(testUserFactory.basicUser(),
                 RemoteNavigatorGeneralPage.class, remotePlugin.getAddon().getKey(), PAGE_KEY);
 
         page.open(id);
+    }
+
+    public void makeSureNo404s()
+    {
+        String pageSource = product.getTester().getDriver().getPageSource();
+        assertFalse(pageSource.contains("Page Not Found"));
+    }
+
+    /*
+     Here we ask webdriver for the absolute url, and we ask the product for its base url.
+     Using both of them, we can be confident of what the relative url actually is, to test with.
+      */
+    public String getRelativeUrlFromWebDriver()
+    {
+        String absoluteUrl = product.getTester().getDriver().getCurrentUrl();
+        String baseUrl = product.getProductInstance().getBaseUrl();
+        return absoluteUrl.replace(baseUrl, "");
     }
 }
--- tests/integration-tests-support/src/main/resources/iframe-navigator.mu
+++ tests/integration-tests-support/src/main/resources/iframe-navigator.mu
@@ -8,11 +8,35 @@
     <body>
         <div class="ac-content">
             <button id="navigate-to-dashboard" onclick="navigateToDashboard()">Navigate to dashboard</button>
+            <button id="navigate-to-page" onclick="navigateToPage()">Navigate to page</button>
+            <button id="navigate-to-edit-page" onclick="navigateToEditPage()">Navigate to edit page</button>
+            <button id="navigate-to-user-profile" onclick="navigateToUserProfile()">Navigate to user profile</button>
         </div>
         <script type="text/javascript">
             function navigateToDashboard() {
                 AP.require('navigator', function(navigator) {
                     navigator.go("dashboard");
+                });
+            }
+        </script>
+        <script type="text/javascript">
+            function navigateToPage() {
+                AP.require('navigator', function(navigator) {
+                    navigator.go("contentview", {contentId: 98311);
+                });
+            }
+        </script>
+        <script type="text/javascript">
+            function navigateToEditPage() {
+                AP.require('navigator', function(navigator) {
+                    navigator.go("contentedit", {contentType: "page", contentId: 98311});
+                });
+            }
+        </script>
+        <script type="text/javascript">
+            function navigateToUserProfile() {
+                AP.require('navigator', function(navigator) {
+                    navigator.go("userprofile", {username: "admin"});
                 });
             }
         </script>
