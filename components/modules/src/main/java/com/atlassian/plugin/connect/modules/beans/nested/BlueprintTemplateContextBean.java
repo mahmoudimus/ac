6e5d350039210fffd17705b4b2d0eb54207b2a7c
Joe Xie <jxie@atlassian.com>
2015-12-15 19:46:43 +1100

    CE-252 : CRA-1187 : a bit better doc

--- components/modules/src/main/java/com/atlassian/plugin/connect/modules/beans/nested/BlueprintTemplateContextBean.java
+++ components/modules/src/main/java/com/atlassian/plugin/connect/modules/beans/nested/BlueprintTemplateContextBean.java
@@ -17,9 +17,14 @@ public class BlueprintTemplateContextBean
 {
     /**
      * A URL which confluence will make a POST request to, during the rendering of the blueprint for which this context bean is
-     * associated. The expected return value is a JSON object with string keys, and values of :
+     * associated. The expected return value is a JSON array of context values, e.g.:
      *
      * @exampleJson {@link ConnectJsonExamples#BLUEPRINT_CONTEXT_RESPONSE_EXAMPLE}
+     *
+     * The `identifier` must be unique
+     * The `representation` must be one of : "plain", "wiki", or "storage". If unset, it defaults to 'plain'
+     * The `value` must be a string that conforms to the `representation`. "plain" is plain text, "wiki" is wiki markup, and "storage" is
+     * valid confluence xhtml storage format as documented on https://confluence.atlassian.com/display/DOC/Confluence+Storage+Format
      *
      * #### Example JSON
      * @exampleJson {@link ConnectJsonExamples#BLUEPRINT_POST_BODY_EXAMPLE}
