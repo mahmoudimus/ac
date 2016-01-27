60298dd80a32f88290dfd480f398a6597b834396
Krzysztof Kercz <kkercz@atlassian.com>
2016-01-27 13:08:08 +0100

    ACJIRA-786: API-fy add-on properties service, lost class

---
+++ components/core/src/main/java/com/atlassian/plugin/connect/plugin/property/AddOnPropertyFactory.java
@@ -0,0 +1,25 @@
+package com.atlassian.plugin.connect.plugin.property;
+
+import java.util.Optional;
+
+import com.atlassian.plugin.connect.api.plugin.property.AddonProperty;
+import com.atlassian.plugin.connect.api.plugin.property.AddonPropertyIterable;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
+import org.codehaus.jackson.JsonNode;
+
+public class AddonPropertyFactory
+{
+
+    public static AddonPropertyIterable fromAddonPropertyAOList(Iterable<AddonPropertyAO> propertyList)
+    {
+        return new AddonPropertyIterable(Lists.transform(Lists.newArrayList(propertyList), AddonPropertyFactory::fromAO));
+    }
+
+    public static AddonProperty fromAO(AddonPropertyAO ao)
+    {
+        final Optional<JsonNode> potentialJsonValue = JsonCommon.parseStringToJson(ao.getValue());
+        Preconditions.checkState(potentialJsonValue.isPresent(), String.format("The addon property %s did not contain valid json: %s", ao.getPropertyKey(), ao.getValue()));
+        return new AddonProperty(ao.getPropertyKey(), potentialJsonValue.get(), ao.getID());
+    }
+}
