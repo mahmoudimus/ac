d47871f321dd27b62fca83d30f09ec4a4f8e6c4e
David Taylor <dtaylor@atlassian.com>
2016-01-29 11:11:20 +1100

    ACDEV-2410 Remove unused DialogModuleDescriptor per Pr comment

--- components/core-extensions/src/main/java/com/atlassian/plugin/connect/plugin/web/dialog/DialogModuleProvider.java
+++ components/core-extensions/src/main/java/com/atlassian/plugin/connect/plugin/web/dialog/DialogModuleProvider.java
@@ -8,14 +8,11 @@ import com.atlassian.plugin.connect.modules.beans.DialogModuleBean;
 import com.atlassian.plugin.connect.modules.beans.DialogModuleMeta;
 import com.atlassian.plugin.connect.plugin.AbstractConnectCoreModuleProvider;
 import com.atlassian.plugin.osgi.bridge.external.PluginRetrievalService;
-import org.dom4j.Element;
-import org.dom4j.dom.DOMElement;
+import com.google.common.collect.ImmutableList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-
-import static java.util.stream.Collectors.toList;
 
 @Component
 public class DialogModuleProvider extends AbstractConnectCoreModuleProvider<DialogModuleBean>
@@ -38,17 +35,6 @@ public class DialogModuleProvider extends AbstractConnectCoreModuleProvider<Dial
     @Override
     public List<ModuleDescriptor> createPluginModuleDescriptors(List<DialogModuleBean> modules, ConnectAddonBean addon)
     {
-        return modules.stream().map(module ->
-        {
-            ModuleDescriptor descriptor = new DialogModuleDescriptor();
-            descriptor.init(pluginRetrievalService.getPlugin(), buildModuleElement(module, addon));
-            return descriptor;
-        }).collect(toList());
-    }
-
-    private Element buildModuleElement(DialogModuleBean module, ConnectAddonBean addon) {
-        // TODO - how much of this is required if the clients of this module use the bean directly? dT
-        return new DOMElement("connectDialog")
-                .addAttribute("key", module.getKey(addon));
+        return ImmutableList.of();
     }
 }
--- components/core-extensions/src/test/java/com/atlassian/plugin/connect/plugin/web/dialog/DialogModuleProviderTest.java
+++ components/core-extensions/src/test/java/com/atlassian/plugin/connect/plugin/web/dialog/DialogModuleProviderTest.java
@@ -1,25 +1,16 @@
 package com.atlassian.plugin.connect.plugin.web.dialog;
 
-import com.atlassian.plugin.ModuleDescriptor;
-import com.atlassian.plugin.Plugin;
 import com.atlassian.plugin.connect.modules.beans.ConnectAddonBean;
 import com.atlassian.plugin.connect.modules.beans.DialogModuleBean;
-import com.atlassian.plugin.osgi.bridge.external.PluginRetrievalService;
-import com.google.common.collect.ImmutableList;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import java.util.List;
-
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
+import static com.google.common.collect.ImmutableList.of;
+import static org.hamcrest.Matchers.empty;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class DialogModuleProviderTest
@@ -28,39 +19,14 @@ public class DialogModuleProviderTest
     private DialogModuleProvider provider;
 
     @Mock
-    private PluginRetrievalService pluginRetrievalService;
-
-    @Mock
     private ConnectAddonBean addon;
 
     @Mock
     private DialogModuleBean module;
 
-    @Mock
-    private Plugin plugin;
-
-    @Before
-    public void setUp()
-    {
-    }
-
     @Test
-    public void moduleDescriptorShouldIncludeDialogOptions()
+    public void providerReturnsEmptyDescriptorList()
     {
-        String moduleKey = "the-key";
-        when(module.getKey(addon)).thenReturn(moduleKey);
-
-        when(pluginRetrievalService.getPlugin()).thenReturn(plugin);
-
-        List<ModuleDescriptor> descriptors = provider.createPluginModuleDescriptors(ImmutableList.of(module), addon);
-
-        // Mock the DialogModuleDescriptor so that we don't have to think about that init() method.
-        ModuleDescriptor expectedDescriptor = mock(DialogModuleDescriptor.class);
-        when(expectedDescriptor.getKey()).thenReturn(moduleKey);
-
-        assertThat(descriptors, hasSize(1));
-
-        ModuleDescriptor actualDescriptor = descriptors.get(0);
-        assertThat(actualDescriptor.getKey(), is(expectedDescriptor.getKey()));
+        assertThat(provider.createPluginModuleDescriptors(of(module), addon), empty());
     }
 }
