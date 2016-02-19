0a68f58c8a46de3988e358444d9f0352739175b4
Einar Pehrson <epehrson@atlassian.com>
2016-02-19 16:42:48 +1100

    ACDEV-2475 Added a wired test to reproduce the problem of UPM breaking on descriptors failing shallow descriptor validation

---
+++ tests/wired-tests/src/test/java/it/com/atlassian/plugin/connect/plugin/lifecycle/upm/ConnectUPMControlHandlerTest.java
@@ -0,0 +1,57 @@
+package it.com.atlassian.plugin.connect.plugin.lifecycle.upm;
+
+import com.atlassian.plugin.Plugin;
+import com.atlassian.plugin.PluginState;
+import com.atlassian.plugin.connect.modules.beans.AuthenticationType;
+import com.atlassian.plugin.connect.plugin.AddonSettings;
+import com.atlassian.plugin.connect.plugin.ConnectAddonRegistry;
+import com.atlassian.plugins.osgi.test.AtlassianPluginsTestRunner;
+import com.atlassian.upm.spi.PluginControlHandler;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.ImmutableMap;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@RunWith(AtlassianPluginsTestRunner.class)
+public class ConnectUPMControlHandlerTest
+{
+
+    private static final String ADDON_KEY = "some-addon-key";
+
+    private final PluginControlHandler pluginControlHandler;
+    private final ConnectAddonRegistry connectAddonRegistry;
+
+    public ConnectUPMControlHandlerTest(PluginControlHandler pluginControlHandler, ConnectAddonRegistry connectAddonRegistry)
+    {
+        this.pluginControlHandler = pluginControlHandler;
+        this.connectAddonRegistry = connectAddonRegistry;
+    }
+
+    @Test
+    public void shouldReturnAddonWithInvalidDescriptor() throws JsonProcessingException
+    {
+        storeInstallationWithInvalidDescriptor();
+        Plugin plugin = pluginControlHandler.getPlugin(ADDON_KEY);
+        assertThat(plugin.getKey(), equals(ADDON_KEY));
+    }
+
+    private void storeInstallationWithInvalidDescriptor() throws JsonProcessingException
+    {
+        AuthenticationType authenticationType = AuthenticationType.NONE;
+        ImmutableMap<String, Object> descriptorMap = ImmutableMap.<String, Object>builder()
+                .put("key", ADDON_KEY)
+                .put("authentication", ImmutableMap.<String, Object>builder().put("type", authenticationType.name()).build())
+                .build();
+        String descriptor = new ObjectMapper().writeValueAsString(descriptorMap);
+
+        AddonSettings settings = new AddonSettings()
+                .setAuth(authenticationType.name())
+                .setBaseUrl("https://example.com")
+                .setDescriptor(descriptor)
+                .setRestartState(PluginState.ENABLED);
+        connectAddonRegistry.storeAddonSettings(ADDON_KEY, settings);
+    }
+}
