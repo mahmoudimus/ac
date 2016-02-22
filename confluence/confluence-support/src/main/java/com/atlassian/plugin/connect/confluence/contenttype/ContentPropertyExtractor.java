e2d8f3dd70139e0d1a2e1bd3d8248ef8dde6e849
Ran Ding <rading@atlassian.com>
2016-02-22 11:59:43 +1100

    CRA-1292 Use sysadmin to retrieve content property

--- confluence/confluence-support/src/main/java/com/atlassian/plugin/connect/confluence/contenttype/ContentPropertyExtractor.java
+++ confluence/confluence-support/src/main/java/com/atlassian/plugin/connect/confluence/contenttype/ContentPropertyExtractor.java
@@ -6,6 +6,8 @@ import com.atlassian.confluence.api.model.content.JsonContentProperty;
 import com.atlassian.confluence.api.service.content.ContentPropertyService;
 import com.atlassian.confluence.content.CustomContentEntityObject;
 import com.atlassian.confluence.rest.api.model.ExpansionsParser;
+import com.atlassian.confluence.user.AuthenticatedUserImpersonator;
+import com.atlassian.confluence.user.persistence.dao.compatibility.FindUserHelper;
 import com.atlassian.fugue.Option;
 
 import com.google.common.base.Strings;
@@ -17,6 +19,8 @@ import org.apache.lucene.document.Document;
  */
 public class ContentPropertyExtractor implements Extractor
 {
+    public static final String SYSADMIN = "sysadmin";
+
     private final ContentPropertyService contentPropertyService;
     private final String contentPropertyKey;
 
@@ -37,23 +41,33 @@ public class ContentPropertyExtractor implements Extractor
         if (searchable instanceof CustomContentEntityObject)
         {
             CustomContentEntityObject ceo = (CustomContentEntityObject) searchable;
-            Option<JsonContentProperty> jsonProperty = contentPropertyService
-                    .find(ExpansionsParser.parse(""))
-                    .withContentId(ceo.getContentId())
-                    .withPropertyKey(contentPropertyKey)
-                    .fetchOne();
 
-            JsonContentProperty jsonContentProperty = jsonProperty.get();
+            /**
+             * ContentPropertyService checks current user permission via AuthenticatedUserThreadLocal.get()
+             * However such information is not exists in the indexing thread.
+             * So we use system admin here to ensure this content gets indexed.
+             */
+            AuthenticatedUserImpersonator.REQUEST_AGNOSTIC.asUser(() -> {
+                Option<JsonContentProperty> jsonProperty = contentPropertyService
+                        .find(ExpansionsParser.parse(""))
+                        .withContentId(ceo.getContentId())
+                        .withPropertyKey(contentPropertyKey)
+                        .fetchOne();
 
-            if (jsonContentProperty != null)
-            {
-                String value = jsonContentProperty.getValue().getValue();
+                JsonContentProperty jsonContentProperty = jsonProperty.getOrNull();
 
-                if (!Strings.isNullOrEmpty(value))
+                if (jsonContentProperty != null)
                 {
-                    defaultSearchableText.append(" ").append(jsonContentProperty.getValue().getValue());
+                    String value = jsonContentProperty.getValue().getValue();
+
+                    if (!Strings.isNullOrEmpty(value))
+                    {
+                        defaultSearchableText.append(" ").append(jsonContentProperty.getValue().getValue());
+                    }
                 }
-            }
+
+                return null;
+            }, FindUserHelper.getUserByUsername(SYSADMIN));
         }
     }
 }
