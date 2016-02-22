4bb7d9b1913770b0bf2faf5e24cb8c65ef14da32
Einar Pehrson <epehrson@atlassian.com>
2016-02-23 09:25:01 +1100

    Revert "Revert "Merge pull request #1224 in AC/atlassian-connect from feature/ACJIRA-729-calendar-component to develop""
    This reverts commit b0546dad0b0f884172ba1acf00a6223c841cd5c3.

---
+++ jira/jira-integration-tests/src/test/java/com/atlassian/connect/test/jira/pageobjects/RemoteDatePickerGeneralPage.java
@@ -0,0 +1,66 @@
+package com.atlassian.connect.test.jira.pageobjects;
+
+import com.atlassian.jira.pageobjects.JiraTestedProduct;
+import com.atlassian.jira.pageobjects.components.CalendarPopup;
+import com.atlassian.pageobjects.Page;
+import com.atlassian.plugin.connect.test.common.pageobjects.ConnectAddonPage;
+import com.atlassian.plugin.connect.test.common.util.IframeUtils;
+import com.atlassian.plugin.connect.test.jira.product.JiraTestedProductAccessor;
+import com.atlassian.webdriver.AtlassianWebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import javax.inject.Inject;
+import java.util.concurrent.Callable;
+
+/**
+ * Page with a date picker control
+ */
+public class RemoteDatePickerGeneralPage extends ConnectAddonPage implements Page
+{
+
+    public static String TEMPLATE_PATH = "jira/iframe-date-picker.mu";
+
+    public static String DATE_TRIGGER = "date-trigger";
+    public static String DATE_FIELD = "date-field";
+    public static String DATE_TIME_TRIGGER = "date-time-trigger";
+    public static String DATE_TIME_FIELD = "date-time-field";
+    public static String TODAY_TRIGGER = "today-trigger";
+    public static String TODAY_FIELD = "today-field";
+
+    protected static JiraTestedProduct product = new JiraTestedProductAccessor().getJiraProduct();
+
+    @Inject
+    protected AtlassianWebDriver driver;
+
+    public RemoteDatePickerGeneralPage(String addonKey, String moduleKey)
+    {
+        super(addonKey, moduleKey, true);
+    }
+
+    @Override
+    public String getUrl()
+    {
+        return IframeUtils.iframeServletPath(addonKey, pageElementKey);
+    }
+
+    public CalendarPopup openDatePicker(String triggerId)
+    {
+        runInFrame(() -> {
+            driver.findElement(By.id(triggerId)).click();
+            return null;
+        });
+        return product.getPageBinder().bind(CalendarPopup.class);
+    }
+
+    public String getSelectedDate(String fieldId)
+    {
+        return runInFrame(() -> driver.findElement(By.id(fieldId)).getAttribute("value"));
+    }
+
+    public String getSelectedIsoDate(String fieldId)
+    {
+        return runInFrame(() -> driver.findElement(By.id(fieldId)).getAttribute("data-iso"));
+    }
+
+}
