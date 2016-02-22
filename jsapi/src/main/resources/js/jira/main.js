76e9c6b8d031ff08815a3b869dcafa38012e9154
Chris Whittington <cwhittington@atlassian.com>
2016-02-22 11:00:58 +0000

    ACJS-164 retry to insert

--- jsapi/src/main/resources/js/jira/main.js
+++ jsapi/src/main/resources/js/jira/main.js
@@ -1,10 +1,10 @@
-require(['ac/jira/events','ac/jira/workflow-post-function'], function(jiraEvents, workflowPostFunctions){
-  connectHost.defineModule("jira", {
+define("ac/jira", ['ac/jira/events','ac/jira/workflow-post-function'], function(jiraEvents, workflowPostFunctions){
+  return {
     refreshIssuePage: jiraEvents.refreshIssuePage,
     getWorkflowConfiguration: workflowPostFunctions.getWorkflowConfiguration,
     _submitWorkflowConfigurationResponse: workflowPostFunctions._submitWorkflowConfigurationResponse
     // isDashboardItemEditable: workflowPostFunctions.isDashboardItemEditable,
     // openCreateIssueDialog: workflowPostFunctions.openCreateIssueDialog,
     // setDashboardItemTitle: workflowPostFunctions.setDashboardItemTitle
-  });
+  };
 });
