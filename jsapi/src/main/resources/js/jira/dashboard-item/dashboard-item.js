df988f0055ac25af93d25f2ed2517eb42ca73935
Chris Whittington <cwhittington@atlassian.com>
2016-02-23 13:27:24 +0000

    ACJS-164 dashboard edit api

--- jsapi/src/main/resources/js/jira/dashboard-item/dashboard-item.js
+++ jsapi/src/main/resources/js/jira/dashboard-item/dashboard-item.js
@@ -51,10 +51,11 @@ define('atlassian-connect/connect-dashboard-item', function() {
             render: function(){
             },
             renderEdit: function($element){
-              var extensionContainer = $element.find(".ap-container");
+              var iframe = $element.find('iframe');
+              var extensionContainer = iframe.closest('.ap-container');
               if(extensionContainer.length === 1){
                 connectHost.broadcastEvent('jira_dashboard_item_edit', {
-                  addon_key: extensionContainer.data('addon_key'),
+                  addon_key: extensionContainer.data('addonKey'),
                   key: extensionContainer.data('key')
                 });
               }
@@ -62,60 +63,3 @@ define('atlassian-connect/connect-dashboard-item', function() {
         };
     };
 });
-
-//                 init: function (state, xdm) {
-//                     // register handle for the edit button in jira (if needed)
-//                     $(xdm.iframe).on('dashboardItemEdit', function(){
-//                         xdm.triggerDashboardItemEdit();
-//                     });
-//                 },
-//                 internals: {
-//                     setDashboardItemTitle: function(title) {
-//                         // TODO this should be replaced with a valid gadget API
-//                         var dashboardItemTitle = $($(this.iframe).parents('.gadget-container').find('h3.dashboard-item-title')[0]);
-//                         dashboardItemTitle.text(title);
-//                     },
-//                     isDashboardItemEditable: function(callback) {
-//                         var configureOption = $(this.iframe).parents('.gadget-container').find('li.configure');
-//                         callback(configureOption.length != 0);
-//                     }
-//                 },
-//                 stubs: ["triggerDashboardItemEdit"]
-//             };
-//         });
-//     });
-
-//     define('atlassian-connect/connect-dashboard-item', function() {
-//         return function(){
-//             return {
-//                 render: function(){
-//                 },
-//                 renderEdit: function($element){
-//                     $element.find('iframe').trigger('dashboardItemEdit');
-//                 }
-//             };
-//         };
-//     });
-
-
-//     $(document).ready(function() {
-
-//         $("body").on("resized",".ap-container", function(e, dimensions) {
-//             var resizedParents = $(e.target).parents(".gadget-inline");
-//             if (resizedParents.length > 0) {
-//                 var inlineGadgetContainer = resizedParents.first();
-//                 var gadgetId = inlineGadgetContainer.attr("id");
-//                 var gadget = new AG.InlineGadgetAPI(inlineGadgetContainer);
-//                 var layout = AG.DashboardManager.getLayout();
-//                 var result = layout.getGadgets().filter(function (g) {
-//                     return "gadget-".concat(g.getId()) == gadgetId;
-//                 });
-//                 result[0].resize();
-//                 layout.refresh();
-//             }
-//         });
-
-//     });
-
-
-// })(AJS.$, define);
